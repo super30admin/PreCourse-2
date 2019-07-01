eb7c929dc1576fe4dd2f01c93c5f5638765be723
Lavan Kumar <11154769+lavanganji@users.noreply.github.com>
2019-06-30 19:46:30 -0500

    completed coding for Exercise_2.java
    Please review the changes.

--- Exercise_2.java
+++ Exercise_2.java
@@ -8,11 +8,29 @@ class QuickSort
        of pivot */
     void swap(int arr[],int i,int j){
         //Your code here   
+        if(i<arr.length && j<arr.length) { // Checking the i and j around the max value that can fit in arr.
+		int ival = arr[i];
+		int jval = arr[j];
+		arr[i] = jval;
+		arr[j] = ival;
+    	}
     }
     
     int partition(int arr[], int low, int high) 
     { 
    	//Write code here for Partition and Swap 
+        int pivot = high; //intially we take the pivot the nth value in array.
+    	int small = low;
+
+    	for (int i=small; i<high; i++) {
+    		 if(arr[i]<arr[high]) {
+    			 swap(arr,i,small);   //swapping i value with small value in the array.
+    			 small++;		     //increment small pointer value.
+    		 }
+    	 }
+    	 swap(arr,small,pivot);    //after traversing n element, swap the small and pivot , to get the pivots exact location in the array.
+    	 return small; 		       //return the small value. ( here we are returning the small value, because it is the 
+                                   //actual pivot value in the array i.e. where pivot needs to be in array.
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
@@ -22,6 +40,14 @@ class QuickSort
     {  
             // Recursively sort elements before 
             // partition and after partition 
+           
+        //Base Case
+    	if(low<high) { // this is the case where sort methods needs to be stop.
+	    	int p  = partition(arr,low,high);  //get the ordered position  of the element at high.
+	    	sort(arr,low,p-1); //sorting the left hand side of p
+	    	sort(arr,p+1,high);  //sorting the right hand side of p
+    	}
+            
     } 
   
     /* A utility function to print array of size n */
