e80b359d51c13d5a097f77e68a213e802106af71
bharatreddypothireddy <35764299+bharatreddypothireddy@users.noreply.github.com>
2019-06-30 19:44:08 -0500

    Completed Iterative QuickSort - Using Stacks
    Completed swap without the use of the third variable, quicksort function using Stack

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,7 +1,12 @@
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+	//Try swapping without extra variable
+        if(i!=j){
+        arr[i] = arr[i]+arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+        }
     } 
   
     /* This function is same in both iterative and 
@@ -9,12 +14,40 @@ class IterativeQuickSort {
     int partition(int arr[], int l, int h) 
     { 
         //Compare elements and swap.
+	int i = l,j=l;
+        while(i<h ){
+            if(arr[i]<=arr[h]){
+                swap(arr,i,j);
+                j++;
+            }
+            i++;
+        }
+        swap(arr,j,h);
+        return j;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
-        //Try using Stack Data Structure to remove recursion.
+	//Try using Stack Data Structure to remove recursion.
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(l);
+        stack.push(h);
+        while(!stack.isEmpty()){
+            h = stack.pop();
+            l = stack.pop();
+            int pivot = partition(arr,l,h);
+            if(pivot-l>1){
+                stack.push(l);
+                stack.push(pivot-1);
+            }
+            if(h-pivot>1){
+                stack.push(pivot+1);
+                stack.push(h);
+            }
+
+        }
+
     } 
   
     // A utility function to print contents of arr 
