5681098bf84c402abf27b42e5892d678040daab5
Manogna Mujje <manogna.mujje@gmail.com>
2019-07-28 17:23:33 -0700

    Completed Exercise_3

--- Exercise_3.java
+++ Exercise_3.java
@@ -20,6 +20,13 @@ class LinkedList
     { 
         //Write your code here
 	//Implement using Fast and slow pointers
+    		Node slow = head;
+    		Node fast = head;
+    		while(fast != null && fast.next != null) {
+    			fast = fast.next.next;
+    			slow = slow.next;
+    		}
+    		System.out.println("Middle element: " + slow.data);
     } 
   
     public void push(int new_data) 
@@ -43,7 +50,7 @@ class LinkedList
     public static void main(String [] args) 
     { 
         LinkedList llist = new LinkedList(); 
-        for (int i=15; i>0; --i) 
+        for (int i=14; i>0; --i) 
         { 
             llist.push(i); 
             llist.printList(); 
