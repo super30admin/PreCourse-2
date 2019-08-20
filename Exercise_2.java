8943c54b2246cdacff73c220272317f1f2a6e2ca
Prakash <prakasa@rose02yd0b8jgh8.es.ad.adp.com>
2019-08-20 16:03:46 -0700

    Completed PreCourse-2

--- Exercise_2.java
+++ Exercise_2.java
@@ -8,12 +8,33 @@ class QuickSort
        of pivot */
     void swap(int arr[],int i,int j){
         //Your code here   
+
+       arr[i] = arr[i] * arr[j];
+       arr[j] = arr[i] / arr[j];
+       arr[i] = arr[i] / arr[j];
+
     }
     
     int partition(int arr[], int low, int high) 
     { 
    	//Write code here for Partition and Swap 
-    } 
+
+        int i = low - 1;
+        int pivot = arr[high];
+
+        for(int j = low; j < high - 1; j++){
+
+            if(arr[j] <= pivot){
+
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+        return i + 1; // The pivot element
+        
+     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
       low  --> Starting index, 
@@ -22,6 +43,15 @@ class QuickSort
     {  
             // Recursively sort elements before 
             // partition and after partition 
+
+        if(low < high){
+
+          int pivot = partition(arr, low, high);
+
+          sort(arr, low, pivot - 1);
+          sort(arr, pivot + 1, high);
+        }
+
     } 
   
     /* A utility function to print array of size n */
--- Exercise_4.java
+++ Exercise_4.java
@@ -6,6 +6,58 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+
+        int i, j, k; 
+        int n1 = m - l + 1; 
+        int n2 =  r - m; 
+      
+        int L[n1], R[n2]; 
+      
+        /* Copy data to temp arrays L[] and R[] */
+
+        for (i = 0; i < n1; i++) 
+            L[i] = arr[l + i]; 
+        for (j = 0; j < n2; j++) 
+            R[j] = arr[m + 1+ j]; 
+      
+        /* Merge the temp arrays back into arr[l..r]*/
+        i = 0; // Initial index of first subarray 
+        j = 0; // Initial index of second subarray 
+        k = l; // Initial index of merged subarray 
+        
+        while (i < n1 && j < n2) 
+        { 
+            if (L[i] <= R[j]) 
+            { 
+                arr[k] = L[i]; 
+                i++; 
+            } 
+            else
+            { 
+                arr[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        } 
+      
+        /* Copy the remaining elements of L[], if there 
+           are any */
+        while (i < n1) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+      
+        /* Copy the remaining elements of R[], if there 
+           are any */
+        while (j < n2) 
+        { 
+            arr[k] = R[j]; 
+            j++; 
+            k++; 
+        } 
+
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +66,16 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+
+        if(l < r){
+
+            int mid = l + (r - l) / 2;
+
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            merge(arr, l, m, r);
+        }
     } 
   
     /* A utility function to print array of size n */
@@ -40,3 +102,10 @@ class MergeSort
         printArray(arr); 
     } 
 } 
+
+
+// For Merge Sort with O(1) extra space merge and O(n lg n) time
+
+// https://www.geeksforgeeks.org/merge-sort-with-o1-extra-space-merge-and-on-lg-n-time/
+
+// We will use the concecpt of % and / to store two values in same place.
