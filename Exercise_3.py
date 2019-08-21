720a3dbc10e8e6ea1cb2b379d8869f456bd255c7
Rakshitha94 <rakshithalr3007@gmail.com>
2019-08-21 00:28:48 -0700

    Completed

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,55 @@
 # Node class  
 class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
+   # Function to initialise the node object
+    def __init__(self, data):
+        self.data=data
+        self.next=None
         
 class LinkedList: 
   
-    def __init__(self): 
+    def __init__(self):
+        self.head=None
         
   
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+    # def push(self, new_data):
+    #     newnode=Node(new_data)
+    #     newnode.next=self.head
+    #     self.head=newnode
 
+    def push(self, new_data):
+
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while (last.next is not None):
+            last = last.next
+
+
+        last.next = new_node
+
+
+
+        # Function to get the middle of
+    # the linked list 
+    def printMiddle(self):
+        slowpointer=self.head
+        fastpointer=self.head
+        if self.head is not None:
+            while(fastpointer is not None and fastpointer.next is not None):
+                fastpointer=fastpointer.next.next
+                slowpointer=slowpointer.next
+            print( slowpointer.data)
 # Driver code 
 list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
+list1.push(1)
+list1.push(2)
+list1.push(3)
+list1.push(4)
+list1.push(5)
+list1.push(6)
+list1.push(7)
+list1.push(8)
+list1.push(9)
 list1.printMiddle() 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,57 @@
-# Python program for implementation of MergeSort 
+# Python program for implementation of MergeSort
+
+#space complexity is o(nlogn)
 def mergeSort(arr):
-  
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
+    if len(arr) > 1:
+        mid=len(arr)//2
+        L=arr[:mid]
+        R=arr[mid:]
+        print(L)
+        print(R)
+        mergeSort(L)
+        mergeSort(R)
+
+        i=j=k=0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k]=L[i]
+                i+=1
+            else:
+                arr[k]=R[j]
+                j+=1
+            k+=1
+
+        while i < len(L):
+                arr[k]=L[i]
+                i+=1
+                k+=1
+
+        while j < len(R):
+            arr[k]=R[j]
+            j+=1
+            k+=1
+        print("L:", L)
+        print("R:", R)
+        print("arr:", arr)
+
+
+# Code to print the list
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+
     #write your code here
-  
+
 # driver code to test the above code 
 if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
+    arr = [12, 11, 13, 5, 6, 7]
+
+    print("Given array is", end="\n")
     printList(arr) 
-    mergeSort(arr) 
+    mergeSort(arr)
+    print("*******")
     print("Sorted array is: ", end="\n") 
     printList(arr) 
