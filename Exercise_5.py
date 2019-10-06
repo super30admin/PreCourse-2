d8805c81805c961a43f80502e12792c6da7ac75a
sindu819 <sindu819@gmail.com>
2019-10-06 13:15:51 -0700

    Completed Precourse_2 module

--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,57 @@
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+def partition(arr, low, high):
+    #write your code here
+    i = (low-1)
+    pivot = arr[high]
 
+    for j in range(low,high):
+        if arr[j] < pivot:
+            i=i+1
+            arr[i],arr[j] =arr[j],arr[i]
+    arr[i+1],arr[high] = arr[high],arr[i+1]
 
-def quickSortIterative(arr, l, h):
-  #write your code here
+    return (i+1)
+
+def quickSortIterative(arr, low, high):
+    #write your code here
+    size = len(arr)
+    stack = [0] * size
+    top = -1
+
+    top = top+1
+    stack[top] = low
+    top = top+1
+    stack[top] = high
+
+    while top >= 0:
+
+        high = stack[top]
+        top = top-1
+        low = stack[top]
+        top = top-1
+
+        piv = partition(arr,low,high)
+
+        if piv-1 > low:
+            top = top + 1
+            stack[top] = low
+            top = top + 1
+            stack[top] = piv - 1
+
+        if piv+1 < high:
+            top = top+1
+            stack[top] = piv+1
+            top =top+1
+            stack[top] = high
+            
+            
+# Driver code to test above
+arr = [4, 3, 5, 2, 1, 3, 2, 3]
+n = len(arr)
+quickSortIterative(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+    print (arr[i], end= " ")
 
