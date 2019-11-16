b48b851ffb571e5fe3274e1de00235bb27c8a0b9
devashishnyati <devashishnyati@gmail.com>
2019-11-16 02:15:18 -0800

    Precourewrok 2 Ex 5 fix

--- Exercise_5.py
+++ Exercise_5.py
@@ -3,6 +3,7 @@
 # Partition the array till the stack is empty by selecting a pivot. Bring all the values smaller than pivot to the left and greater than pivot to the right.
 # Pop the first element in the stack everytime
 # If p-1 is greater than low, push low and p-1 to the stack, if p+1 is less than high, push p+1 and high in the stack.
+# Basically if we have left side to partition index, we put it in the stack and if we have right side to the partition index we put it in the stack.
 # Do this till the stack is empty
 
 # Time Complexity: O(nlogn)
@@ -16,27 +17,28 @@ def partition(arr, l, h):
           arr[i], arr[pIndex] = arr[pIndex], arr[i]
           pIndex += 1
   arr[pIndex], arr[h] = arr[h], arr[pIndex]
-  return pIndex
+  return pIndex 
 
 def quickSortIterative(arr, l, h):
   #write your code here
   stack = []
-  stack.append(h)
   stack.append(l)
-  while stack and len(stack) >1:
-    l = stack[-1]
-    h = stack[-2]
-    stack.pop()
-    p = partition(arr, l, h)
-    if p-1 > l:
-      stack.append(l)
+  stack.append(h)
+
+  while stack:
+    new_h = stack.pop()
+    new_l = stack.pop()
+    p = partition(arr, new_l, new_h)
+    if p - 1 > new_l:
+      stack.append(new_l)
       stack.append(p-1)
-    if p+1 < h:
+    if p+1 < new_h:
       stack.append(p+1)
-      stack.append(h)
+      stack.append(new_h)
+  print(arr)
 
 
-arr = [10, 7, 8, 9, 1, 5] 
+arr = [10, 7, 8, 9, 1, 5, 4] 
 n = len(arr) 
 quickSortIterative(arr,0,n-1) 
 print ("Sorted array is:") 
