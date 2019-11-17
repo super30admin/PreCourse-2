1a6d8d2095a60a0b9a235361519fe09a074609fc
Anoop Martin <33829132+SuperStriker@users.noreply.github.com>
2019-11-17 00:54:44 -0800

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -2,7 +2,17 @@ class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+       if(r>= l){
+       int middle = l+(r-l)/2;
+       if(arr[middle]== x){
+       return middle;
+       }
+       else if(arr[middle] > x) return binarySearch(arr,l,middle-1,x);
+       
+       else  return binarySearch(arr, middle+1,r,x);
+       }
+	return -1;
+       
     } 
   
     // Driver method to test above 
@@ -11,7 +21,7 @@ class BinarySearch {
         BinarySearch ob = new BinarySearch(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
-        int x = 10; 
+        int x = 11; 
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
