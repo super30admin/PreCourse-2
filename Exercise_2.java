86ee373731737c0bf990b5f3dee5dfd94269accd
AvinavTyagi <avinav.tyagi@sjsu.edu>
2019-11-18 00:29:39 -0800

    PreCourse_2 completed

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,28 +1,45 @@
-class QuickSort 
-{ 
-    /* This function takes last element as pivot, 
-       places the pivot element at its correct 
-       position in sorted array, and places all 
-       smaller (smaller than pivot) to left of 
-       pivot and all greater elements to right 
-       of pivot */
-    void swap(int arr[],int i,int j){
-        //Your code here   
+class QuickSort {
+    /*
+     * This function takes last element as pivot, places the pivot element at its
+     * correct position in sorted array, and places all smaller (smaller than pivot)
+     * to left of pivot and all greater elements to right of pivot
+     */
+    void swap(int arr[], int i, int j){
+        // Your code here
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    
-    int partition(int arr[], int low, int high) 
-    { 
-   	//Write code here for Partition and Swap 
-    } 
-    /* The main function that implements QuickSort() 
-      arr[] --> Array to be sorted, 
-      low  --> Starting index, 
-      high  --> Ending index */
-    void sort(int arr[], int low, int high) 
-    {  
-            // Recursively sort elements before 
-            // partition and after partition 
-    } 
+	
+    int partition(int arr[], int low, int high){
+        // Write code here for Partition and Swap
+        int pivot = arr[high];
+        int i = low - 1, j = low;//swapping indexes i+1 and j when element at j < pivot
+        while (j < high) {
+            if (arr[j] <= pivot) {
+                swap(arr, i + 1, j);
+                i++;
+            }
+            j++;
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    /**
+     * The main function that implements QuickSort() 
+     * arr[] --> Array to be sorted,
+     * low --> Starting index, high --> Ending index
+     */
+    void sort(int arr[], int low, int high){
+        // Recursively sort elements before
+        // partition and after partition
+        if (low < high) {
+            int p = partition(arr, low, high);
+            sort(arr, low, p - 1);
+            sort(arr, p + 1, high);
+        }
+    }
   
     /* A utility function to print array of size n */
     static void printArray(int arr[]) 
