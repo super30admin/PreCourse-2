ef419138cb34fb371aad610e951460b14839d5b7
Kamalesh <kamalesh.anantha@gmail.com>
2019-12-17 17:25:55 -0600

    Prercourse2: Exercise_2 solution

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,13 +2,30 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
+    if low < high:
+        pivot = high
+        lowPointer = low
+        for i in range(low,len(arr)):
+            if arr[i] < arr[pivot]:
+                temp = arr[i]
+                arr[i] = arr[lowPointer]
+                arr[lowPointer] = temp
+                lowPointer += 1
+        temp = arr[pivot] 
+        arr[pivot] = arr[lowPointer]
+        arr[lowPointer] = temp
+        return lowPointer       
+
   
     #write your code here
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
+    if low < high:
+        partitionIndex = partition(arr, low, high)
+        quickSort(arr, low, partitionIndex - 1)
+        quickSort(arr,partitionIndex + 1, high)
     
     #write your code here
   
@@ -18,6 +35,6 @@ n = len(arr)
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
+    print ("%d" %arr[i], end = " "), 
   
  
