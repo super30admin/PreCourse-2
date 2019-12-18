1c1c686481ac015b62bf366d3df255879172d803
Sanjay Vishwakarma <svishwak@andrew.cmu.edu>
2019-12-18 11:35:32 -0800

    Exercise 4 complete!

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,42 +1,76 @@
-class MergeSort 
-{ 
-    // Merges two subarrays of arr[]. 
-    // First subarray is arr[l..m] 
-    // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
-    } 
-  
-    // Main function that sorts arr[l..r] using 
-    // merge() 
-    void sort(int arr[], int l, int r) 
-    { 
-	//Write your code here
-        //Call mergeSort from here 
-    } 
-  
-    /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
-  
-    // Driver method 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
-  
-        System.out.println("Given Array"); 
-        printArray(arr); 
-  
-        MergeSort ob = new MergeSort(); 
-        ob.sort(arr, 0, arr.length-1); 
-  
-        System.out.println("\nSorted array"); 
-        printArray(arr); 
-    } 
-} 
+public class MergeSort {
+	void merge(int arr[], int l, int m, int r) {
+		// Setting the sizes of two arrays....
+		int n1 = (m - l) + 1; // ---> 0 + 2 = 2
+		int n2 = r - m;
+		// creating arrays for sorting and merging......
+		int[] L = new int[n1];
+		int[] R = new int[n2];
+		for (int i = 0; i < n1; i++) {
+			L[i] = arr[i];
+		}
+		for (int j = 0; j < n2; j++) {
+			R[j] = arr[m + 1 + j];
+		}
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		while (i < n1 && j < n2) {
+			if (L[i] <= R[j]) {
+				arr[k] = L[i];
+				i++;
+			} else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+		while (i < n1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+		while (j < n2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
+	void sort(int arr[], int l, int r) {
+		if (l < r) {
+			int m = (l + r) / 2;
+			sort(arr, l, m);
+			sort(arr, m + 1, r);
+			merge(arr, l, m, r);
+		}
+	}
+
+	/* A utility function to print array of size n */
+	static void printArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	// Driver method
+	public static void main(String args[]) {
+		int arr[] = { 12, 11, 13, 5, 6, 7 };
+		MergeSort mergeSort = new MergeSort();
+		int l = 0;
+		int r = arr.length - 1;
+		int m = (l + r) / 2;
+		mergeSort.merge(arr, 0, m, r);
+
+		System.out.println("Given Array");
+		printArray(arr);
+
+		MergeSort ob = new MergeSort();
+		ob.sort(arr, 0, arr.length - 1);
+
+		System.out.println("\nSorted array");
+		printArray(arr);
+
+	}
+}
