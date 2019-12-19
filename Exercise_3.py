0a14421c0cc1f92b7fb720bae540dc2ca03b925c
Nikhil Agrawal <nikrasiya@gmail.com>
2019-12-19 06:32:33 -0500

    Add files via upload

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,68 @@
 # Node class  
-class Node:  
-  
+class Node:
+
     # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self) -> None:
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self) -> None:
+        # cur = self.head
+        # count = 0
+        # while cur:
+        #     count += 1
+        #     cur = cur.next
+        # mid = count // 2 + 1
+        # cur = self.head
+        # while count > mid:
+        #     cur = cur.next
+        #     count -= 1
+        # print(cur.data)
+        """
+            Logic: The fast pointer moves two steps at time
+            while the slow pointer takes one step.
+            For instance if there were 50 nodes, when the fast pointer
+            reaches the last node, the slow pointer would be at the 25th position.
+            Time Complexity - O(n) Linear
+            Since we need to go through all the elements once
+            Space Complexity - O(1) Constant
+            We always require only two nodes (slow and fast)
+        """
+        slow = fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        print(slow.data)
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+    def printList(self) -> None:
+        cur = self.head
+        result = []
+        while cur:
+            result.append(f'{cur.data}')
+            cur = cur.next
+        print(' -> '.join(result))
+
+
+if __name__ == '__main__':
+    # Driver code
+    list1 = LinkedList()
+    list1.push(5)
+    list1.push(4)
+    list1.push(2)
+    list1.push(3)
+    list1.push(1)
+    list1.printList()
+    list1.printMiddle()
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,20 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+    # write your code here
+    pass
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    # write your code here
+    pass
 
+
+if __name__ == "__main__":
+    # Driver code to test above
+    arr = [10, 7, 8, 9, 1, 5]
+    n = len(arr)
+    quickSort(arr, 0, n - 1)
+    print("Sorted array is:")
+    for i in range(n):
+        print("%d" % arr[i]),
