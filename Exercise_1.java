6a0225c424d6e7edd7b0589f5e3d1185e7e9d79d
Lohith <a.lohith@yahoo.in>
2019-12-19 19:54:21 -0600

    Completed Binary Search Precourse 2

--- Exercise_1.java
+++ Exercise_1.java
@@ -2,7 +2,24 @@ class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+        if(arr.length == 0){
+            System.out.println("Empty array");
+            return -1;
+        }else{
+            while(l <= r) {
+                int mid = (l + r)/2;
+                if(arr[mid] == x){
+                    return mid;
+                }
+                else if(x < arr[mid]){
+                    r = mid - 1;
+                }
+                else{
+                    l = mid + 1;
+                }
+            }
+            return -1;
+        }
     } 
   
     // Driver method to test above 
@@ -11,7 +28,7 @@ class BinarySearch {
         BinarySearch ob = new BinarySearch(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
-        int x = 10; 
+        int x = 40;
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
