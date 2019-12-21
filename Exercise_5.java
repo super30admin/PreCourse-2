841d6b87c43b6daf310697ab490e6f6487a190bc
Lohith <a.lohith@yahoo.in>
2019-12-21 14:23:13 -0600

    Completed iterative Quick Sort Precourse 2

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,21 +1,53 @@
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
+    {
+        int temp;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        /*arr[i]=arr[i]+arr[j];
+        arr[j]=arr[i]-arr[j];
+        arr[i]=arr[i]-arr[j];*/
     } 
   
     /* This function is same in both iterative and 
        recursive*/
     int partition(int arr[], int l, int h) 
-    { 
-        //Compare elements and swap.
+    {
+        int pivot = arr[h];
+        int i = l - 1;
+
+        for(int j = l; j <= h; j++){
+            if(arr[j] < pivot){
+                i++;
+                swap(arr, i , j);
+            }
+        }
+        swap(arr, i+1, h);
+        return i+1;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
-        //Try using Stack Data Structure to remove recursion.
-    } 
+    void QuickSort(int arr[], int l, int h) {
+        int stack[] = new int[h - l + 1];
+        int top = -1;
+        stack[++top] = l;
+        stack[++top] = h;
+        while (top >= 0) {
+            h = stack[top--];
+            l = stack[top--];
+            int p = partition(arr, l, h);
+            if (p - 1 > l) {
+                stack[++top] = l;
+                stack[++top] = p - 1;
+            }
+            if (p + 1 < h) {
+                stack[++top] = p + 1;
+                stack[++top] = h;
+            }
+        }
+    }
   
     // A utility function to print contents of arr 
     void printArr(int arr[], int n) 
@@ -29,7 +61,7 @@ class IterativeQuickSort {
     public static void main(String args[]) 
     { 
         IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3, 1, 2, 3, 6 };
         ob.QuickSort(arr, 0, arr.length - 1); 
         ob.printArr(arr, arr.length); 
     } 
