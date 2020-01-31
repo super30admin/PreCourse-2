599349f062a22e775181592f8e2c0f26070d4b4e
AbhiniveshP <pabhinivesh@gmail.com>
2020-01-31 13:19:40 -0700

    Completed 4 Questions

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,55 @@
 # Python program for implementation of Quicksort Sort 
-  
+
+def swap (arr, i, j):
+    # swap function to swap elements at given indices
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    return
+
 # give you explanation for the approach
 def partition(arr,low,high):
   
-  
+    # O(n) time complexity | O(1) space complexity
     #write your code here
-  
+    count = 0                           # count all elements lower than element at low index
+    for i in range(low + 1, high + 1):
+        if arr[i] < arr[low]:
+            count += 1
+    swap(arr, low, low + count)
+
+    pivot = low + count
+
+    while (low < pivot and high > pivot):   # push all lower elements to left and all higher elements to right
+        if (arr[low] >= arr[pivot]):
+            if (arr[high] < arr[pivot]):
+                swap(arr, low, high)
+                low += 1
+                high -= 1
+            else:
+                high -= 1
+        else:
+            low += 1
+
+    return pivot                        # return the pivot index
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
+
+    # O(n.logn) Time Complexity | O(1) Space Complexity
     #write your code here
+    if (low >= high):       # base case for recursion in quick sort
+        return
+    pivot = partition(arr, low, high)   # extract the pivot index
+    quickSort(arr, low, pivot)          # recursively run from low till before pivot index
+    quickSort(arr, pivot+1, high)       # recursively run from after pivot index till high
   
 # Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
+arr = [10, 7, 8, 9, 1, 5]
 n = len(arr) 
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
-  
+    print ("%d" %arr[i]),
+
  
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,64 @@
 # Python program for implementation of MergeSort 
+def merge(arr, left_index, right_index):
+
+    # O(n) Time Complexity | O(n) Space Complexity
+
+    new_arr = []
+    curr_index = 0
+
+    left_ptr = left_index
+    right_ptr = int(right_index / 2) + 1
+
+    left_max_index = right_ptr - 1
+    right_max_index = right_index
+
+    while (True):           # 4 conditions to be checked and if none of the 4 satisfy, break the loop
+        if (left_ptr <= left_max_index and right_ptr <= right_max_index) and (arr[left_ptr] < arr[right_ptr]):
+            new_arr.append(arr[left_ptr])
+            left_ptr += 1
+            curr_index += 1
+            continue
+        if (left_ptr <= left_max_index and right_ptr <= right_max_index) and (arr[left_ptr] >= arr[right_ptr]):
+            new_arr.append(arr[right_ptr])
+            right_ptr += 1
+            curr_index += 1
+            continue
+        if (left_ptr > left_max_index and right_ptr <= right_max_index):
+            new_arr.append(arr[right_ptr])
+            right_ptr += 1
+            curr_index += 1
+            continue
+        if (left_ptr <= left_max_index and right_ptr > right_max_index):
+            new_arr.append(arr[left_ptr])
+            left_ptr += 1
+            curr_index += 1
+            continue
+        break
+
+    for i in range(len(new_arr)):
+        arr[left_index + i] = new_arr[i]
+
+
+def mergeSortHelper(arr, left_index, right_index):
+
+    # O (n.logn) Time Complexity | O(n) Space Complexity
+    if (left_index >= right_index):
+        return
+    mid_index = int((right_index + left_index)/ 2)
+    mergeSortHelper(arr, left_index, mid_index)     # recursive merge sort on first half
+    mergeSortHelper(arr, mid_index + 1, right_index)    # recursive merge sort on second half
+    merge(arr, left_index, right_index)             # merge the two halves
+
 def mergeSort(arr):
   
   #write your code here
+    mergeSortHelper(arr, 0, len(arr) - 1)
   
 # Code to print the list 
 def printList(arr): 
     
     #write your code here
+    print(arr)
   
 # driver code to test the above code 
 if __name__ == '__main__': 
