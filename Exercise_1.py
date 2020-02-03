117a48515d41447baad34b7906fae0b8753790b1
Sayan Guha <sayan@Sayans-MacBook-Pro.local>
2020-02-03 01:08:25 -0600

    added recursive binary search implementation

--- Exercise_1.py
+++ Exercise_1.py
@@ -33,13 +33,25 @@ def binarySearch(arr, l, r, x):
 
     return -1
 
+def recBinarySearch(arr, l, r, x):
 
+    if l <= r:
+        mid = int ((l + r) / 2)
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            l = mid + 1
+            return recBinarySearch(arr,l,r,x)
+        else:
+            r = mid - 1
+            return recBinarySearch(arr, l, r, x)
+    return -1
 # Test array
 arr = [2, 3, 4, 10, 40]
 x = 10
 
 # Function call
-result = binarySearch(arr, 0, len(arr) - 1, x)
+result = recBinarySearch(arr, 0, len(arr) - 1, x)
 
 if result != -1:
     print("Element is present at index % d" % result)
