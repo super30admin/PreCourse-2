dd4fc7b3088add9893e65f5c84045d4cce783f41
AroopN <aroopnanda97@gmail.com>
2020-03-03 22:12:04 -0600

    Completed Precourse2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,41 @@
 # Node class  
+
+## Time Complexity = O(1) for both insertion as well as for getting the middle element
+## Space Complexity  =  O(N) to store the linked list . No auxillary space is used
+
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
-        
+        self.val = data
+        self.next = None
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
+        self.length = 0
+        self.mid = None
   
     def push(self, new_data): 
+        new_node = Node(new_data)
+        self.length += 1
+        if not self.head:
+            self.head = new_node
+            self.mid = new_node
+            return
+        self.head.next = new_node
+        self.head = new_node
+        if self.length%2==1:
+            self.mid=self.mid.next
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        if self.mid:
+            print(self.mid.val)
+        else:
+            print(None)
 
 # Driver code 
 list1 = LinkedList() 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,48 @@
 # Python program for implementation of MergeSort 
+
+## Time Complexity = O(NlogN) as we recursively divide the array in half and merge them after sorting them
+## Space Complexity  =  O(N) as we use auxillary arrays to store left and right sub arrays
+
 def mergeSort(arr):
-  
+  if len(arr)==1:
+    return
+  mid = len(arr)//2
+  left = arr[:mid]
+  right = arr[mid:]
+  mergeSort(left)
+  mergeSort(right)
+  i,j,k=0,0,0
+  while(i<len(left) and j<len(right)):
+    if left[i]<right[j]:
+      arr[k] = left[i]
+      i+=1
+      k+=1
+    elif right[i] < left[j]:
+      arr[k] = right[j]
+      j+= 1
+      k+=1
+    else:
+      arr[k] = right[j]
+      arr[k] = left[i]
+      i+=1
+      k+=2
+      j+=1
+  while i < len(left):
+    arr[k] = left[i]
+    i += 1
+    k += 1
+  while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+
   #write your code here
   
 # Code to print the list 
 def printList(arr): 
-    
+  for i in arr:
+    print(i, end=" ")
     #write your code here
   
 # driver code to test the above code 
