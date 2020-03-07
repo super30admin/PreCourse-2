8171b66c945f4fad1bd77377498d56c40fe3504f
AroopN <aroopnanda97@gmail.com>
2020-03-07 00:36:27 -0600

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,6 +1,6 @@
 # Node class  
 
-## Time Complexity = O(1) for both insertion as well as for getting the middle element
+## Time Complexity = O(1) for insertion, O(n) for  getting the middle element
 ## Space Complexity  =  O(N) to store the linked list . No auxillary space is used
 
 class Node:  
@@ -13,29 +13,30 @@ class LinkedList:
   
     def __init__(self): 
         self.head = None
-        self.length = 0
-        self.mid = None
+        self.start = None
   
     def push(self, new_data): 
         new_node = Node(new_data)
-        self.length += 1
         if not self.head:
             self.head = new_node
-            self.mid = new_node
+            self.start = new_node
             return
         self.head.next = new_node
         self.head = new_node
-        if self.length%2==1:
-            self.mid=self.mid.next
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
-        if self.mid:
-            print(self.mid.val)
-        else:
+        slow = self.start
+        if not slow:
             print(None)
+            return
+        fast = slow.next
+        while(fast and fast.next):
+            slow=slow.next
+            fast=fast.next.next
+        print(slow.val)
 
 # Driver code 
 list1 = LinkedList() 
