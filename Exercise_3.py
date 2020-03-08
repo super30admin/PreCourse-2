91c23f359ae95017e7baceba1d28eca63040cbe2
Akshay Shah <akshay.vjti11@gmail.com>
2020-03-07 20:40:48 -0700

    adding the completed files for precourse2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,55 @@
-# Node class  
+# Node class
+"""
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None
+// Your code here along with comments explaining your approach
+Algorithm explanation
+1) We use 2 pointer approach,where slow pointer moves 1 node ahead,whereas
+fast pointer moves 2 nodes ahead
+2) We get the middle node as the fast reaches the end of the list
+"""
+
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
-        
+        self.data = data
+        self.next = None
+
 class LinkedList: 
   
-    def __init__(self): 
-        
+    def __init__(self):
+        self.head = None
   
-    def push(self, new_data): 
+    def push(self, new_data):
+        newnode = Node(new_data)
+        if not self.head:
+            self.head = newnode
+            self.head.next = None
+        else:
+            curr = self.head
+            while curr and curr.next:
+                curr = curr.next
+            curr.next = newnode
         
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        curr = self.head
+        slow = fast = curr
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        print(slow.data)
 
 # Driver code 
 list1 = LinkedList() 
-list1.push(5) 
+list1.push(5)
 list1.push(4) 
 list1.push(2) 
 list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+list1.push(1)
+list1.printMiddle()
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,75 @@
 # Python program for implementation of MergeSort 
+"""
+// Time Complexity : O(nlogn)
+// Space Complexity : O(nlogn)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None
+// Your code here along with comments explaining your approach
+Algorithm explanation
+Merge
+
+Mergesort
+- We divide the arr into two halves untill we reach single element
+- Left half gets the array elements till the mid
+- Right half gets the array elements from mid till the end
+
+Merge
+- Using the left and right half of the array, we place the elements in order
+- Initialize i,j,k to track the left, right and inplace arr
+- while i < len(left) and j < len(right)
+    - if left[i] <= right[j] then arr[k] = left[i] , i+=1
+    else
+        arr[k] = right[j], j+=1
+- While there are elements remaining in the left, add to arr using k
+- While there are elements remaining in the right, add to arr using k
+"""
+
+def merge(arr,left,right):
+    n1 = len(left)
+    n2 = len(right)
+    i,j,k = 0,0,0
+
+    while i < n1 and j < n2:
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i+=1
+        else:
+            arr[k] = right[j]
+            j+=1
+        k+=1
+
+    while i < n1:
+        arr[k] = left[i]
+        k+=1
+        i+=1
+
+    while j < n2:
+        arr[k] = right[j]
+        k+=1
+        j+=1
+
+
 def mergeSort(arr):
-  
-  #write your code here
+    #write your code here
+    N = len(arr)
+    if N > 1:
+        mid = N // 2
+        mergeSort(arr[:mid])
+        mergeSort(arr[mid:])
+        merge(arr,arr[:mid],arr[mid:])
   
 # Code to print the list 
-def printList(arr): 
+def printList(arr):
     
     #write your code here
+    for i in range(len(arr)):
+        print(arr[i],end= " ")
   
 # driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end="\n")  
     printList(arr) 
-    mergeSort(arr) 
+    mergeSort(arr)
     print("Sorted array is: ", end="\n") 
-    printList(arr) 
+    printList(arr)
