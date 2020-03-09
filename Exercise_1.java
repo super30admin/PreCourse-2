7d3ef90e48cdc37781c915d9e89642880ba916ef
Debanjana Dasgupta <debanjana_ddg@yahoo.co.in>
2020-03-09 01:09:41 -0500

    Add files via upload
    Exercise_1 : Binary search done

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,6 +3,20 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        if(r >= l){
+            int mid = l + (r-1)/2;
+
+            if(arr[mid] == x){
+                return mid;
+            }
+
+            if(arr[mid] > x){
+                return binarySearch(arr, l, mid-1,x);
+            }
+
+            return binarySearch(arr, mid+1, r, x);
+        }
+        return -1;
     } 
   
     // Driver method to test above 
@@ -11,7 +25,7 @@ class BinarySearch {
         BinarySearch ob = new BinarySearch(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
-        int x = 10; 
+        int x = 3; 
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
