a809f0b42e3f0aeb28c0471648bf92a2fb6e245e
Jacob Stephen <you@example.com>
2020-04-27 10:49:07 -0400

    Completed Design for 1,2,3

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,18 +1,31 @@
 # Python program for implementation of Quicksort Sort 
-  
+ #Time Complexity = Here it is O(N^2) Since the array is sorted
+ #and the pivot element is taken is 5 which is lesser than all values except 1 so in right hand 
+ #side of  pivot we have to interate through every element
+#Space Complexity = O(Log(N) 
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
-
+    pivot = arr[high]
+    i =low
+    while i < len(arr):
+        if arr[i] < pivot:
+            (arr[i],arr[low]) = (arr[low],arr[i])
+            low = low +1
+        i+=1
+    (arr[low],arr[high]) = (arr[high],arr[low])
+    return low
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
+def quickSort(arr,low,high):
+  if low < high:
+     pi = partition(arr,low,high)
+   
+     quickSort(arr,low,pi-1)
+     quickSort(arr,pi+1,high)
     
-    #write your code here
   
 # Driver code to test above 
+
+    
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
 quickSort(arr,0,n-1) 
