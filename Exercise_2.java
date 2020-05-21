9c57a43bbe137dc2c712161433a27f0606cbf947
chirag <cr.tagadiya@gmail.com>
2020-05-21 11:54:42 -0400

    QUICK SORT LAST INDEX AS PIVOT

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,4 @@
-class QuickSort 
+public class Exercise_2 
 { 
     /* This function takes last element as pivot, 
        places the pivot element at its correct 
@@ -8,11 +8,25 @@ class QuickSort
        of pivot */
     void swap(int arr[],int i,int j){
         //Your code here   
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
     int partition(int arr[], int low, int high) 
     { 
-   	//Write code here for Partition and Swap 
+       //Write code here for Partition and Swap 
+       int pivot = high;
+       int small = low;
+       for (int i = low; i <= high;i++){
+           if (arr[i] <  arr[pivot]){
+               swap(arr, small, i);
+               small++;
+           } 
+           
+       }
+       swap(arr, small, pivot);
+       return small;
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
@@ -22,6 +36,12 @@ class QuickSort
     {  
             // Recursively sort elements before 
             // partition and after partition 
+            if (low < high){
+                int pa = partition(arr, low, high);
+                sort(arr, low, pa-1);
+                sort(arr, pa+1, high);
+            }
+            
     } 
   
     /* A utility function to print array of size n */
@@ -39,7 +59,9 @@ class QuickSort
         int arr[] = {10, 7, 8, 9, 1, 5}; 
         int n = arr.length; 
   
-        QuickSort ob = new QuickSort(); 
+        Exercise_2 ob = new Exercise_2(); 
+        System.out.println("Before sorting array");
+        printArray(arr);
         ob.sort(arr, 0, n-1); 
   
         System.out.println("sorted array"); 
