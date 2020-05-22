ebbcd2d708460044b42db27f9100e54102185014
soumitra9 <mehrotrasm105@gmail.com>
2020-05-22 00:35:32 -0400

    Pre_course2 Python

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,21 +1,69 @@
+# Time Complexity : push() - O(1), printMiddle() - O(n^2)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this : I was not able to think of a solution where I can find the middle
+  # with obtaining the length of the LinkedList, and thats why my solution is O(n^2)
+
+
+# Your code here along with comments explaining your approach
+'''
+1. Pushing the node is simple, attaching the new node to head and , reinitializing head to be pointing to the newly added node
+2. For finding the middle element, first I have cretaed a separate function calculates the length of the LL in O(n)
+3. Then, it traverses the Linked list to the middle length(n)//2, and fetched the middle element
+'''
+
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+
+        self.data = data
+        self.next = None
+
         
 class LinkedList: 
   
-    def __init__(self): 
+    def __init__(self):
+
+        self.head = None
         
   
-    def push(self, new_data): 
+    def push(self, new_data):
+
+        new_data_node = Node(new_data)
+        if self.head is None:
+            self.head = new_data_node
+
+        else:
+            new_data_node.next = self.head
+            self.head = new_data_node
         
-  
+
+    def get_length(self):
+        node_iterator = self.head
+
+        count = 0
+        while node_iterator:
+            count += 1
+            node_iterator = node_iterator.next
+
+        return count
+
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
 
+        length = self.get_length()
+
+        mid_element = int((length-1)//2)
+        node_iterator = self.head
+        c = 0
+        while node_iterator and c < mid_element:
+            node_iterator = node_iterator.next
+            c = c + 1
+
+        print (node_iterator.data)
 # Driver code 
 list1 = LinkedList() 
 list1.push(5) 
@@ -23,4 +71,5 @@ list1.push(4)
 list1.push(2) 
 list1.push(3) 
 list1.push(1) 
+list1.push(6)
 list1.printMiddle() 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,63 @@
+# Time Complexity : O(n log n) where n is the number of nodes
+# Space Complexity : O(h) = O(log n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : Yes a little bit, I got confused about how to imitate pre order 
+  # traversal iteratively, and how to account for low,high. But eventually after a little bit of effort I was able to figure out
+
+
+
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
+
+'''
+I have use the Pre-order tree traversal using Stack to achieve the iterative quicksort.
+Then accounting for right part and pusging it to the satck and then left part.
+The left part is pushed after right so that left one popped first
+
+'''
 def partition(arr, l, h):
-  #write your code here
+    pivot_element = arr[h]
+    pIndex = l
+    for i in range(l, h):
+        if arr[i] < pivot_element:
+            temp = arr[i]
+            arr[i] = arr[pIndex]
+            arr[pIndex] = temp
+            pIndex += 1
+    
+    temp = arr[pIndex]
+    arr[pIndex] = arr[h]
+    arr[h] = temp
+
+    return pIndex
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    
+    stack = []
+    current = (l,h)
+    stack.append(current)
+
+    while(len(stack) > 0):
+        
+        current = stack.pop()
+        low, high = current[0], current[1]
+
+        p = partition(arr, low, high)
+
+        if p+1 < high: 
+            stack.append((p+1,high))
+        if p-1 > low:
+            stack.append((low, p-1))
+    
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i])
+
+
+
 
