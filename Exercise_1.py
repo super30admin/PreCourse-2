ccddb2b9480a7d7130f138b46b4b22d757ec8ab5
Samarth Juneja <samarthj@buffalo.edu>
2020-05-22 15:12:37 +0530

    Competed Binary Search

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,46 @@
-# Python code to implement iterative Binary  
-# Search. 
-  
-# It returns location of x in given array arr  
-# if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
+# Time Complexity :
+# Space Complexity :
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this :
+
+
+# Your code here along with comments explaining your approach
+def iterativeBinarySearch(arr, l, r, x):
+    left = l
+    right = r
+
+    while left <= right:
+        mid = left + (right - left) / 2
+        if x == arr[mid]:
+            return mid
+        elif x < arr[mid]:
+            right = mid - 1
+        elif x > arr[mid]:
+            left = mid + 1
+    return -1
+
+def recursiveBinarySearch(arr, l, r, x):
+
+    if l <= r:
+        mid = l + (r - l) / 2
+        if x == arr[mid]:
+            return mid
+        elif x < arr[mid]:
+            return recursiveBinarySearch(arr, l, mid-1, x)
+        elif x > arr[mid]:
+            return recursiveBinarySearch(arr, mid+1, r, x)
+    return -1
+
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
+x = 40
   
 # Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
-if result != -1: 
-    print "Element is present at index % d" % result 
+result = iterativeBinarySearch(arr, 0, len(arr)-1, x)
+result1 = recursiveBinarySearch(arr, 0, len(arr)-1, x)
+
+if result != -1 and result1 != -1:
+    print "For Iterative search element is present at index % d" % result
+    print "For Recursive search element is present at index % d" % result1
 else: 
     print "Element is not present in array"
