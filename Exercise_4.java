8f23226b180eac9bb189b398634c72acd20a7d37
Meghna Singhal <meghnasinghal2@gmail.com>
2020-05-23 15:53:40 -0500

    Completed PreCourse_2

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,9 @@
+// Time Complexity : O(nlogn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Running on eclipse
+// Any problem you faced while coding this : None
+// Your code here along with comments explaining your approach
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -6,6 +12,24 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+       int i=l,j=m+1; // two pointers i and j for tracking two parts of array, both initialized at starting of each lower and upper part respectively
+       while(i<=m && j<=r) // while both pointers do not cross the end point of each lower and upper part of array respectively
+       {
+            if(arr[j]<arr[i]) // checking if value at j is smaller than that at i
+            {   int temp = arr[i]; // swapping the two values as the value at j is smaller
+                arr[i]= arr[j];
+                arr[j]= temp;
+                j++; // incrementing j as value at j has been checked and next value has to be checked
+            }
+            else
+            {
+                i++; // otherwise incrementing i as it is already smaller and at its right position
+            }  
+
+       }
+
+
+
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +38,15 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+        if(l<r) // checking that left should not cross right
+        {
+            int mid = (l+r)/2; // finding mid value each split
+            sort(arr,l,mid); // calling sort recursively for first half from l to mid i.e lower half of new split
+            sort(arr,mid+1,r); // calling sort recursively for second half from mid+1 to r i.e upper half of new split
+            merge(arr,l,mid,r); //  calling merge for merging arrays
+
+        }
+
     } 
   
     /* A utility function to print array of size n */
