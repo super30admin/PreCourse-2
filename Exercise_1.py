a7ac591bc12c8687121bedb48c43eeeb4ba1e219
Akshata Sawhney <akshatasawhney@gmail.com>
2020-05-23 15:44:31 -0500

    Completed PreCourse2 May2020

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,3 +1,10 @@
+"""
+// Time Complexity :  O(log n), where n is the number of elements
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+"""
+
 # Python code to implement iterative Binary  
 # Search. 
   
@@ -6,9 +13,17 @@
 def binarySearch(arr, l, r, x): 
   
   #write your code here
-  
-    
-  
+  while l<=r:
+    mid = l + (r-l)//2 #getting the middle index
+            
+    if arr[mid]==x:
+      return mid
+    elif x > arr[mid]: #if x is greater than the mid value, then we update l to mid+1 to consider the second half of the array
+      l = mid+1
+    elif x < arr[mid]: #if x is smaller than the mid value, then we update r to mid-1 to consider the second half of the array
+      r = mid-1
+  return -1
+
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
@@ -17,6 +32,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print ("Element is present at index % d" % result) 
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,13 +1,57 @@
+"""
+// Time Complexity :  O(nlogn), where n is the number of elements in the array
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : not on leetcode
+// Any problem you faced while coding this : No
+"""
+
+
 # Python program for implementation of MergeSort 
+def merge(arr,a,b): # function to merge 2 sorted lists a and b
+  i=0
+  j=0
+  k=0
+  
+  while i<len(a) and j<len(b): #loop runs until one of the lists exhausts 
+    if a[i] < b[j]: #checks if ith element in 'a' is smaller than jth element in 'b' and adds it to the merged list if true
+      arr[k] = a[i] 
+      i+=1
+    else:
+      arr[k] = b[j] #else add b[j] to the merged list
+      j+=1
+    k+=1
+
+  while i < len(a): #loop for when there are elements remaining in the 'a' list, adds remaining values to the merged list
+    arr[k] = a[i] 
+    i+=1
+    k+=1
+          
+  while j < len(b): #loop for when there are elements remaining in the 'b' list, adds remaining values to the merged list
+    arr[k] = b[j] 
+    j+=1
+    k+=1
+  
 def mergeSort(arr):
   
   #write your code here
-  
+    l=0
+    h=len(arr)-1
+
+    if l<h:
+      #print("here")
+      mid = len(arr)//2 #find mid
+      a=arr[:mid] #create 2 lists, from low to mid-1 and mid to high
+      b=arr[mid:]
+      mergeSort(a)
+      mergeSort(b)
+      merge(arr,a,b) #call merge() to merge the 2 lists
+
 # Code to print the list 
 def printList(arr): 
     
     #write your code here
-  
+    print(arr)
+    
 # driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
