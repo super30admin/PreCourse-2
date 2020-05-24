0e989953da38c8768aad7a41ad885214e389f75a
KrishSJ <55265817+KrishSJ@users.noreply.github.com>
2020-05-24 16:53:58 -0700

    Precourse2 completed

--- Exercise_4.java
+++ Exercise_4.java
@@ -6,6 +6,25 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+    	int[] temp = new int[arr.length];
+    	for(int i=0;i<arr.length;i++) {
+    		temp[i] = arr[i];
+    	}
+    	int i = l, j = m+1, k = l;
+    	while(i<=m && j<=r) {
+    		if(temp[i] <= temp[j]) {
+    			arr[k++] = temp[i++];
+    		}else {
+    			arr[k++] = temp[j++];
+    		}
+    	}
+    	
+    	for(; i<=m;i++) {
+    		arr[k++] = temp[i];
+    	}
+    	for(; j<=r;j++) {
+    		arr[k++] = temp[j];
+    	}
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +33,12 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+    	if(l < r) {
+    		int mid = l +(r-l)/2;
+        	sort(arr, l, mid);
+        	sort(arr,mid+1,r);
+        	merge(arr,l,mid,r);
+    	}
     } 
   
     /* A utility function to print array of size n */
@@ -23,7 +48,7 @@ class MergeSort
         for (int i=0; i<n; ++i) 
             System.out.print(arr[i] + " "); 
         System.out.println(); 
-    } 
+    }
   
     // Driver method 
     public static void main(String args[]) 
@@ -40,3 +65,8 @@ class MergeSort
         printArray(arr); 
     } 
 } 
+
+//Time Complexity : O(nlog(n))
+//Space Complexity : O(n) 
+//Did this code successfully run on Leetcode :
+//Any problem you faced while coding this : No
