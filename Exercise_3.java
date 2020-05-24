b0dd6c2827dde99d74edaf7f83860f0de871d21b
KrishSJ <55265817+KrishSJ@users.noreply.github.com>
2020-05-24 11:36:43 -0700

    Excercise2 completed

--- Exercise_3.java
+++ Exercise_3.java
@@ -60,3 +60,8 @@ class LinkedList
         } 
     } 
 } 
+
+//Time Complexity : O(n)
+//Space Complexity : O(1)
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
