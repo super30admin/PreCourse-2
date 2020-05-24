2aea1bd11ff88106473d57ed46f21ad115afc8ae
GreeshmaReddy <greeshma.naredla@gmail.com>
2020-05-24 09:20:26 +0530

    Completed Sprint PreCourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,29 @@
+// Time Complexity : O(log(N))
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this : No
+
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        int index = -1;    //default index is -1 as it represents that element is not found.
+        while(l <= r){
+            int mid = (l+r)/2;   //searching only the satisfied space as the array is sorted.
+            if(arr[mid] == x){
+                index = mid;
+                break;
+            }
+            else if(arr[mid] < x){
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+        return index;
     } 
   
     // Driver method to test above 
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,7 +1,16 @@
 class IterativeQuickSort { 
+      // Time Complexity : O(N*N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this : Did not get any idea for swapping and using stack, have looked up for solution.
     void swap(int arr[], int i, int j) 
     { 
 	//Try swapping without extra variable 
+      if(arr[i]!=arr[j]){
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+       }
     } 
   
     /* This function is same in both iterative and 
@@ -9,17 +18,51 @@ class IterativeQuickSort {
     int partition(int arr[], int l, int h) 
     { 
         //Compare elements and swap.
+       int p = arr[h];
+       int i=l-1;
+       int j = l;
+       while(j< h){
+           if(arr[j]<=p){
+               i = i+1;
+               swap(arr, i,j);
+           }
+           j++;
+       }
+       i++;
+       swap(arr,i,h);
+       return i; 
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+        int[] stack = new int[h-l+1];
+        int top = -1;
+
+        stack[++top] = l;
+        stack[++top] = h;
+        
+         while(top >= 0){
+            h = stack[top--];
+            l = stack[top--];
+
+            int p = partition(arr,l,h);
+
+            if(p-1 > l){
+                stack[++top] = l;
+                stack[++top] = p-1;
+            }
+
+            if(p+1 < h){
+                stack[++top] = p+1;
+                stack[++top] = h;
+            }      
+        }
     } 
   
     // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
+    void printArr(int arr[], int n){
         int i; 
         for (i = 0; i < n; ++i) 
             System.out.print(arr[i] + " "); 
