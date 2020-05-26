1fc7a483b1699f8528b4ca94f5e67f6d1233dc2b
nileshph <nileshpharate94@gmail.com>
2020-05-25 19:39:01 -0700

    Added iterative binary search

--- Exercise_1.java
+++ Exercise_1.java
@@ -20,6 +20,26 @@ class BinarySearch {
         		
         } else
         		return -1;
+    }
+    
+    int binarySearchItr(int arr[], int l, int r, int x) 
+    { 
+        while(l <= r) {
+        		int mid = (l + r)/2;
+        		
+        		if(arr[mid] == x)
+        			return mid;
+        		
+        		if(l == r)
+        			break;
+        		
+        		if(x < arr[mid])
+        			 r = mid - 1;
+        		else
+        			 l = mid + 1;
+        		
+        }
+        	return -1;
     } 
   
     // Driver method to test above 
@@ -29,7 +49,7 @@ class BinarySearch {
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
         int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
+        int result = ob.binarySearchItr(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
         else
