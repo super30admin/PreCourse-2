c430f0bbd88051b50d04242c3191e6a08b10ce31
s-tintin <sneha_r_nair@hotmail.com>
2020-07-04 13:30:50 -0500

    Completed Exercise_2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,19 +1,28 @@
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
+# quicksort is a recursive function. Each iteration you find pivot using partition - pivot is that element in the array
+# where all elements to left of it is less than pivot and all elements to right of it is greater than pivot.
+# Then, we recursively call quicksort on both the halves.
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+    greater_ind = low
+    pivot = arr[high]
+    for curr_ind in range(low, high):
+        if arr[curr_ind] < pivot:
+            arr[curr_ind], arr[greater_ind] = arr[greater_ind], arr[curr_ind]
+            greater_ind += 1
+    arr[greater_ind], arr[high] = arr[high], arr[greater_ind]
+    return greater_ind
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
+def quickSort(arr,low,high):
+    if low < high:
+        pivot_ind = partition(arr, low, high)
+        quickSort(arr, low, pivot_ind - 1)
+        quickSort(arr, pivot_ind + 1, high)
   
 # Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
+arr = [10, 7, 8, 9, 1, 5]
 n = len(arr) 
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
