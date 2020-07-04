a58a675f975b043771a01821a734abf20b245227
Arpit Sharma <arpit317@gmail.com>
2020-07-04 16:16:45 -0700

    solved question 4

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,36 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
+  if len(arr) < 2:
+    return
+  def merge(arr, left, right):
+    i, j, k = 0,0,0
+    while i < len(left) and j < len(right):
+      if left[i] < right[j]:
+        arr[k] = left[i]
+        i += 1
+      else:
+        arr[k] = right[j]
+        j += 1
+      k += 1
+      
+    while i < len(left):
+      arr[k] = left[i]
+      i += 1
+      k += 1
+    while j < len(right):
+      arr[k] = right[j]
+      j += 1
+      k += 1
   
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
-    #write your code here
+  l = []
+  r = []
+  for x in range(len(arr)//2):
+    l.append(arr[x])
+  for y in range(len(arr)//2, len(arr)):
+    r.append(arr[y])
+  mergeSort(l)
+  mergeSort(r)
+  merge(arr, l, r)
   
 # driver code to test the above code 
 if __name__ == '__main__': 
