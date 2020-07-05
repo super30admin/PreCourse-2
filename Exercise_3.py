cc4ad47fe94bb9575029b840dac48b31e32dcb1d
akamboj9034 <45693755+akamboj9034@users.noreply.github.com>
2020-07-05 15:30:03 -0700

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,57 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+class ListNode:
+    """
+    A node in a singly-linked list.
+    """
+    def __init__(self, data=None, next=None):
+        self.data=data
+        self.next=None
+    
+class SinglyLinkedList:
+    def __init__(self):
+        """
+        Create a new singly-linked list.
+        Takes O(1) time.
+        """
+        self.head = None
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+    def append(self, data):
+        """
+        Insert a new element at the end of the list.
+        Takes O(n) time.
+        """
+        node=ListNode(data,None)
+        if self.head==None:
+            self.head=node
+        else:
+            temp=self.head
+            while(temp.next!=None):
+                temp=temp.next
+            temp.next=node
+        return "node inserted"
+    
+    def mid(self):
+        slow=self.head
+        fast= self.head
+
+        while(fast != None):
+            
+            if fast.next==None or fast.next.next==None:
+                break
+            slow=slow.next
+            fast= fast.next.next
+        
+        return slow.data
+
+
+
+singly=SinglyLinkedList()
+
+print(singly.append(1))
+print(singly.append(2))
+print(singly.append(3))
+# print(singly.append(4))
+# print(singly.append(5))
+# print(singly.append(6))
+# print(singly.append(7))
+
+print(singly.mid())
