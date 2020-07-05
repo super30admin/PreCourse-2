653b89c85dcb3c137586b98b0e60fcad56d48fa5
Arpit Sharma <arpit317@gmail.com>
2020-07-04 21:44:00 -0700

    Update Exercise_5.py

--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,49 @@
 # Python program for implementation of Quicksort
 
-# This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+  pIndex = l
+  pivot = arr[h]
+  for i in range(l, h):
+    if arr[i] < pivot:
+      arr[pIndex], arr[i] = arr[i], arr[pIndex]
+      pIndex += 1
+  arr[h], arr[pIndex] = arr[pIndex], arr[h]
+  return pIndex
+
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+  stack = [0] * (h-l+1)
+  top = -1
+  top += 1
+  stack[top] = l
+  top += 1
+  stack[top] = h
+
+  while top >= 0:
+    h = stack[top]
+    top -= 1
+    l = stack[top]
+    top -= 1
+
+    pIndex = partition(arr, l, h)
+
+    if pIndex - 1 > l:
+      top += 1
+      stack[top] = l
+      top += 1
+      stack[top] = pIndex - 1
+
+    if pIndex + 1 < h:
+      top += 1
+      stack[top] = pIndex + 1
+      top += 1
+      stack[top] = h
+
+if __name__ == "__main__":
+  arr = [39, 119, 4,1,6,8,2, 100, 52]
+  n = len(arr)
+  quickSortIterative(arr,0,n-1)
+  print(arr)
+
 
