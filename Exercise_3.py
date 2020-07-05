567ae23c67c036deec60954874f82eb839740a9f
Arpit Sharma <arpit317@gmail.com>
2020-07-04 21:47:49 -0700

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,3 +1,8 @@
+#Time Complexity : O(n)
+#Space Complexity : O(1)
+#Did this code successfully run on Leetcode : yes
+#Any problem you faced while coding this : No
+
 # Node class  
 class Node:  
   
