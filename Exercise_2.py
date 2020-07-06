1d0bc49bc4a5662c359ba75950e140d06fd891e7
satishsolanki <satishsolanki1990@gmail.com>
2020-07-05 20:37:54 -0700

    Ex3,4,5 completed

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,5 +1,4 @@
 # Python program for implementation of Quicksort Sort
-import numpy.random as random
 # give you explanation for the approach
 def partition(arr,low,high):
     #write your code here
@@ -7,34 +6,28 @@ def partition(arr,low,high):
     :param arr: a list or segment of list
     :param low: start index
     :param high: end index
-    :return: left,pivot,right
+    :return: pivot
     '''
-    pivot=random.randint(low,high+1)
-    left=[]
-    right=[]
-    for i in range(low,high+1):
-        if i != pivot:
-            if arr[i]<=arr[pivot]:
-                left.append(arr[i])
-            else:
-                right.append(arr[i])
-    return left,[arr[pivot]],right
-
-  
-
+    ptr=low
+    pivot=high
+    for i in range(low,high):
+        if arr[i]<arr[pivot]:
+            arr[i], arr[ptr] = arr[ptr],arr[i] # swap
+            ptr += 1
+    arr[ptr] , arr[pivot] = arr[pivot], arr[ptr]
+    return ptr
 # Function to do Quick sort 
 def quickSort(arr,low,high):
     #write your code here
     if low < high:
-        left,value,right = partition(arr,low,high)
-        return  quickSort(left,0,len(left)-1)+value+quickSort(right,0,len(right)-1)
-    else:
-        return arr
-  
+        p=partition(arr,low,high)
+        quickSort(arr,low,p-1)
+        quickSort(arr,p+1,high)
+
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
-arr=quickSort(arr,0,n-1)
+quickSort(arr,0,n-1)
 print ("Sorted array is:")
 for i in range(n): 
     print ("%d" %arr[i])
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,46 @@
+import numpy.random as random
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
   #write your code here
-
+    ptr=l
+    pivot=h
+    for i in range(l,h):
+        if arr[i]<arr[pivot]:
+            arr[i], arr[ptr] = arr[ptr],arr[i] # swap
+            ptr += 1
+    arr[ptr] , arr[pivot] = arr[pivot], arr[ptr]
+    return ptr
 
 def quickSortIterative(arr, l, h):
   #write your code here
+    stack=[l,h]
+    while True:
+        print(stack)
+        if len(stack) == 0:
+            break
+        if (stack[-1]-stack[-2]) > 1:
+            print(arr)
+            p=partition(arr,stack[-2],stack[-1])
+            # pop last two and insert l, p-1, p+1,h
+            h=stack.pop(-1)
+            l=stack.pop(-1)
+            stack.append(l)
+            stack.append(p-1)
+            stack.append(p+1)
+            stack.append(h)
+        else:
+            stack.pop(-1)
+            stack.pop(-1)
+
+arr = [1,1,4,4,6,21,5,7, 8,21, 9, 1,6,5,56,32,21,86,88]
+n = len(arr)
+quickSortIterative(arr,0,n-1)
+print ("Sorted array is:")
+for i in range(n):
+    print ("%d" %arr[i])
+
+
+
 
