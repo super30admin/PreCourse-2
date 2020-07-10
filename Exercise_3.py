b5e1795dd4e2240a730ee9705b88232f18865e35
Enna Sachdeva <ennasachdeva@gmail.com>
2020-07-10 01:28:40 -0700

    Quick sort remaining

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,19 +2,38 @@
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
-    def __init__(self): 
-        
+    def __init__(self):
+        node = Node(None)
+        self.head = node
   
-    def push(self, new_data): 
+    def push(self, new_data):
+        if self.head == None:
+            self.head = Node(new_data)
+        else:
+            ptr = self.head
+            while ptr.next:
+                ptr = ptr.next
+            ptr.next =  Node(new_data)
         
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        slow_ptr = self.head
+        fast_ptr = self.head
+
+        while fast_ptr:
+            fast_ptr = fast_ptr.next.next
+            slow_ptr = slow_ptr.next
+
+        print(slow_ptr.data)
+
 
 # Driver code 
 list1 = LinkedList() 
