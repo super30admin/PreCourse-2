9f02c8a8bf266309f8d773581354040e14b142e5
shreyas <shreyasb2@gmail.com>
2020-07-21 18:55:25 -0400

    Middle element

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,21 +1,42 @@
 # Node class  
 class Node:  
-  
     # Function to initialise the node object  
     def __init__(self, data):  
-        
+        self.val = data
+        self.next = None
+
 class LinkedList: 
-  
     def __init__(self): 
-        
+        self.head = None
   
     def push(self, new_data): 
-        
-  
+        if self.head == None:
+            self.head = Node(new_data)
+        else:
+            new_node = Node(new_data)
+            new_node.next = self.head
+            self.head = new_node
+
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        if self.head == None:
+            print('LinkedList is empty')
+            return
 
+        slow = self.head
+        fast = self.head 
+
+        while True:
+            if fast == None or fast.next == None:
+                print(slow.val)
+                return
+            # if fast.next == None:
+            #     return slow
+
+            slow = slow.next
+            fast = fast.next.next
+            
 # Driver code 
 list1 = LinkedList() 
 list1.push(5) 
