3b34eda6a01f412d81621fbf065bfcb53eb5a854
Sindhu <sindhusha.vf12@gmail.com>
2020-08-08 17:45:45 -0500

    Completed Sprint PreCourse2

--- Exercise_2.java
+++ Exercise_2.java
@@ -8,11 +8,23 @@ class QuickSort
        of pivot */
     void swap(int arr[],int i,int j){
         //Your code here   
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
     
     int partition(int arr[], int low, int high) 
     { 
-   	//Write code here for Partition and Swap 
+       //Write code here for Partition and Swap 
+       int pivot=arr[high],i=low-1;
+       for(int j=low;j<high;j++){
+           if(arr[j]<pivot){
+               i++;
+               swap(arr, i, j);
+           }
+       }
+        swap(arr, i+1, high);
+        return i+1;
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
@@ -20,8 +32,11 @@ class QuickSort
       high  --> Ending index */
     void sort(int arr[], int low, int high) 
     {  
-            // Recursively sort elements before 
-            // partition and after partition 
+        if(low<high){
+            int p=partition(arr, low, high);
+            sort(arr, low, p-1);        // Recursively sort elements before
+            sort(arr, p+1, high);       // partition and after partitio
+        }
     } 
   
     /* A utility function to print array of size n */
--- Exercise_4.java
+++ Exercise_4.java
@@ -5,16 +5,48 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
-    } 
+       //Your code here 
+       int n1=m-l+1, n2=r-m;
+       int Left[] = new int[n1];
+       int Right[] = new int[n2];
+       for (int i = 0; i < n1; ++i) 
+            Left[i] = arr[l + i]; 
+        for (int j = 0; j < n2; ++j) 
+            Right[j] = arr[m + 1 + j];
+       int i=0,j=0,k=l;
+       while(i<n1&&j<n2){
+           if(Left[i]<=Right[j]) {
+               arr[k]=Left[i];
+               i++;
+       } else {
+           arr[k]=Right[j];
+           j++;
+       }
+       k++;
+    }
+       while(i<n1){
+        arr[k]=Left[i];
+        k++;i++;
+       }
+       while(j<n2){
+        arr[k]=Right[j];
+        k++;j++;
+       }
+    }
+ 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
     { 
-	//Write your code here
-        //Call mergeSort from here 
+    //Write your code here
+    if(l<r){int m=(r+l)/2;
+    sort(arr,l,m);
+    sort(arr,m+1,r);
+    merge(arr,l,m,r);//Call mergeSort from here
+
     } 
+}
   
     /* A utility function to print array of size n */
     static void printArray(int arr[]) 
