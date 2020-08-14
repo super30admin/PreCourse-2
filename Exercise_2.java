86b2f9e9fb5291fecbdc8b77e184195ca8959ece
bhanuk97 <43115610+bhanuk97@users.noreply.github.com>
2020-08-14 12:56:48 -0400

    Completed Precourse2

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+//import java.util.Arrays;
 
 class QuickSort 
 { 
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,21 +1,68 @@
-class IterativeQuickSort { 
+import java.util.Stack;
+
+class IterativeQuickSort {
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+    //Try swapping without extra variable 
+        int temp = arr[i]; 
+        arr[i] = arr[j]; 
+        arr[j] = temp; 
     } 
   
     /* This function is same in both iterative and 
        recursive*/
-    int partition(int arr[], int l, int h) 
+    int partition(int arr[], int low, int high) 
     { 
         //Compare elements and swap.
-        return 0;
+        int pivot = arr[high];  
+        int i = (low-1); 
+        for (int j=low; j<high; j++) 
+        { 
+             
+            if (arr[j] < pivot) 
+            { 
+                i++; 
+  
+                swap(arr, i, j);
+                
+            } 
+        } 
+        swap(arr, i+1, high);
+  
+        return i+1; 
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+        Stack<Integer> stack = new Stack<Integer>();
+  
+        stack.push(l);
+        stack.push(h);
+  
+        while (!stack.empty()) { 
+            
+            h = stack.pop();
+            l = stack.pop();
+  
+            
+            int p = partition(arr, l, h); 
+  
+            
+            if (p - 1 > l) { 
+                
+                stack.push(l);
+                stack.push(p-1);
+            } 
+  
+            
+            if (p + 1 < h) { 
+                
+                stack.push(p+1);
+                stack.push(h);
+            } 
+        } 
     } 
   
     // A utility function to print contents of arr 
