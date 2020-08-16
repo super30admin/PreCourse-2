6840b434bfa0d6fed23a5b0c75127e3b1e2c8f31
Kavya B <kavyabalihallimath@gmail.com>
2020-08-15 17:12:59 -0700

    Completed Sprint Precourse2

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -2,46 +2,66 @@
 using namespace std;  
   
 // A utility function to swap two elements  
-void swap(int* a, int* b)  
-{  
+void swap(int* a, int* b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
     //Your Code here 
-}  
-  
-/* This function takes last element as pivot, places  
-the pivot element at its correct position in sorted  
-array, and places all smaller (smaller than pivot)  
-to left of pivot and all greater elements to right  
+}
+
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
 of pivot */
-int partition (int arr[], int low, int high)  
-{  
-    //Your Code here 
-}  
-  
-/* The main function that implements QuickSort  
-arr[] --> Array to be sorted,  
-low --> Starting index,  
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int j = (low-1);
+    for (int i = low;i <= high - 1;i++) {
+        if (arr[i] < pivot) {
+            j++;
+            swap(&arr[i],&arr[j]);
+        }   
+    }
+    swap(&arr[j + 1], &arr[high]);
+    return (j + 1);
+}
+
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
 high --> Ending index */
-void quickSort(int arr[], int low, int high)  
-{  
-    //Your Code here 
-}  
-  
+void quickSort(int arr[], int low, int high)
+{
+       //Your Code here 
+    if (low < high) {
+        int mid = partition(arr, low, high);
+
+        quickSort(arr, low, mid - 1);
+        quickSort(arr, mid+1, high);
+    }
+}
+
 /* Function to print an array */
-void printArray(int arr[], int size)  
-{  
-    int i;  
-    for (i = 0; i < size; i++)  
-        cout << arr[i] << " ";  
-    cout << endl;  
-}  
-  
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;   
+}
+
 // Driver Code 
-int main()  
-{  
-    int arr[] = {10, 7, 8, 9, 1, 5};  
-    int n = sizeof(arr) / sizeof(arr[0]);  
-    quickSort(arr, 0, n - 1);  
-    cout << "Sorted array: \n";  
-    printArray(arr, n);  
-    return 0;  
-}  
+int main()
+{
+    int arr[] = { 10, 7, 8, 9, 1, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+   // printArray(arr, n);
+    quickSort(arr, 0, n - 1);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
