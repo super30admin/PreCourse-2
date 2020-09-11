7011d55fc3de05cb5cd20717e703e1acd1b6aea7
PR <pr@PRs-MacBook-Pro.local>
2020-09-11 15:42:08 -0700

    123 completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,12 +1,26 @@
 # Python code to implement iterative Binary  
 # Search. 
+#Time Complexity: O(log(n))
+#Space Complexity: O(log(n)) for the recursion based call stack
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
   
   #write your code here
-  
+  if r >= l == 0:
+    return -1
+
+  mid = l + (r - l) // 2
+
+  if arr[mid] == x:
+    return mid
+
+#Since the array is sorted, elements lesser than the middle element would be on the left, and greater elements on the right
+  if arr[mid] >= x:
+    return binarySearch(arr, l, mid, x)
+  else:
+    return binarySearch(arr, mid + 1, r, x)
     
   
 # Test array 
@@ -17,6 +31,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,21 +1,59 @@
+import math
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
-        
+        self.data = data
+        self.next = None
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.node = None
   
     def push(self, new_data): 
-        
-  
+        new_node = Node(new_data)
+        if self.node == None:
+            self.node = new_node
+        else:
+            head = self.node
+
+            while head.next:
+                head = head.next
+            head.next = new_node
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
 
+        # Approach 1: Time Complexity: O(N)
+        # Iterate till the end of the linked list. Find out how many elements are there and find the mid point.
+        # Start iterating again till the mid point and print the value
+        # pointer = self.node
+        # i = -1
+        # while pointer:
+        #     pointer = pointer.next
+        #     i += 1
+
+        # pointer = self.node
+        # mid = math.ceil(i / 2)
+        # j = 0
+        # while j <= mid:
+        #     pointer = pointer.next
+        #     j += 1
+        
+        # print(pointer.data)
+        # return pointer
+
+        # Approach 1: Time Complexity: O(N)
+        # Fast and slow pointer approach where you iterate one pointer twice as fast as the other. By the end the slow pointer is pointing
+        # to the middle node.
+        slow, fast = self.node, self.node
+        
+        while fast and fast.next:
+            
+            fast = fast.next.next
+            slow = slow.next
+        print(slow.data)
 # Driver code 
 list1 = LinkedList() 
 list1.push(5) 
