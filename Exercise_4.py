15c1352acba893e6d3de224b61ee90e4c63b2364
Lakshya0744 <lgarg@asu.edu>
2020-09-11 19:43:47 -0700

    Merge Sort Asg 4

--- Exercise_4.py
+++ Exercise_4.py
@@ -3,24 +3,48 @@ def mergeSort(arr):
   mid = len(arr)//2
   left = arr[:mid]
   right = arr[mid:]
-  print(left)
-  print(right)
-  mergeSort(left)
-  mergeSort(right)
+  k=0
+  i=0
+  j=0
+  while left<right:
+    mergeSort(left)
+    mergeSort(right)
+  while i<len(left) and j<len(right):
+    if left[i]<right[j]:
+      arr[k] = left[i]
+      i+=1
+    else:
+      arr[k] = right[j]
+      j+=1
+    k+=1
+
+  while i <len(left):
+    arr[k]=left[i]
+    i+=1
+    k+=1
+  while j<len(right):
+    arr[k] = right[j]
+    j+=1
+    k+=1
+  return arr
+
+
+
+
 
   
   #write your code here
   
 # Code to print the list 
-# def printList(arr): 
-    
+def printList(arr): 
+    print(arr)
     #write your code here
   
 # driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end="\n")  
-    # printList(arr) 
+    printList(arr) 
     mergeSort(arr) 
     print("Sorted array is: ", end="\n") 
-    # printList(arr) 
+    printList(arr) 
