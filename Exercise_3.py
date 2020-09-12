c6df605b3ff4f9b9344cc4a5c1f7db0863f86f31
Piyush Kurkure <piyushk10@gmail.com>
2020-09-12 09:49:32 -0700

    Completed Sprint Precourse2

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,18 +3,31 @@ class Node:
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
   
     def push(self, new_data): 
-        
+        new_node = Node(new_data)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.head = new_node
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        slow = fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        print(slow.data)
+
 
 # Driver code 
 list1 = LinkedList() 
@@ -23,4 +36,4 @@ list1.push(4)
 list1.push(2) 
 list1.push(3) 
 list1.push(1) 
-list1.printMiddle() 
+list1.printMiddle()
