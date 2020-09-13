6ce210fc4b6d8e015bf5a115fd9e721e46d911e3
Neharika Khera <neharikakhera@ufl.edu>
2020-09-13 16:37:53 -0400

    Completed Sprint PreCourse_2

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,6 @@
+    // Time complexity : O(nlogn) -> T(n) = 2T(n)/2 + O(n)
+    //space complexity O(n) -> we used auxillary space to perform merge operation
+    // merge sort is recursive algorithm implemented using divide an dconquer approach
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -5,7 +8,35 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+       //Your code here 
+       int n1 = m-l+1;
+       int n2 = r-m; 
+       int[] arr1 = new int[n1];
+       int[] arr2 = new int[n2];
+
+       for(int i=0; i<n1; i++)
+            arr1[i] = arr[l+i]; // elements upto middle element
+        for(int j=0; j<n2; j++)
+            arr2[j] = arr[m+1+j]; //elements after middle element
+
+      //  int[] mergeArr = new int[n1+n2];
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while(i < n1 && j < n2){
+
+            if(arr1[i] <  arr2[j])
+                arr[k++] = arr1[i++];
+            else
+                arr[k++] = arr2[j++];
+          
+        }
+        while(i < n1){
+            arr[k++] = arr1[i++];
+        }
+        while(j < n2){
+            arr[k++] = arr2[j++];
+        }
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +45,14 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+        if(l < r){
+        int m = l + (r-l)/2;
+        sort(arr, l, m);
+        sort(arr, m+1, r);
+
+        merge(arr, l, m, r);
+        }
+
     } 
   
     /* A utility function to print array of size n */
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,20 +1,61 @@
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+    //Try swapping without extra variable 
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
     } 
   
     /* This function is same in both iterative and 
        recursive*/
-    int partition(int arr[], int l, int h) 
+    int partition(int arr[], int low, int high) 
     { 
         //Compare elements and swap.
+        int i = low-1;
+        int pivot = arr[high];
+        for(int j = low; j < high; j++){
+            if(arr[j] < pivot){
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i+1, high);
+        return i+1;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+        int[] stack = new int[h-l+1]; // we implement stack as an array
+       // Stack<LinkdeList> stack = new Stack<>();
+
+       // initialize the stack
+        int top= -1;
+        stack[++top] = l;
+        stack[++top] = h;
+
+        while(top >= 0){// we do until stack is not empty
+            h = stack[top--];
+            l = stack[top--];
+            // we find partition index
+            int p = partition(arr, l, h);
+
+            //we push left side to the stack
+            if(p-1 > l){
+                stack[++top] = l;
+                stack[++top] = p-1;
+            }
+            //we push right side to the stack
+            if(p+1 < h){
+                stack[++top] = p+1;
+                stack[++top] = h;
+            }
+        }
+
+        
+
     } 
   
     // A utility function to print contents of arr 
