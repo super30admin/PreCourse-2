762ee890932a6799783e07e224e1377a66e00b35
Meet Dave <meetdave06@gmail.com>
2020-09-14 02:21:37 -0400

    Precourse 2 Submission

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,20 +3,34 @@
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
+
+# Time complexity O(log n)
+# Space complexity O(1)
+
+
 def binarySearch(arr, l, r, x): 
   
-  #write your code here
+  while l <= r:
+    mid = (l + r)//2
+    if x == arr[mid]:
+      return mid
+    elif x < arr[mid]:
+      r = mid - 1
+    elif x > arr[mid]:
+      l = mid + 1
+
+  return -1
   
     
   
 # Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
+arr = [ 0,1,2,3,4,5] 
+x = 5
   
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print(f"Element is present at index {result}")
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,61 @@
 # Python program for implementation of MergeSort 
-def mergeSort(arr):
-  
+
+# Time Complexity : O(nlogn)
+# Space Complexity : O(1)
+
+def merge(arr1, arr2,arr):
+
+
+    n1 = len(arr1)
+    n2 = len(arr2)
+
+    i = 0
+    j = 0
+    k = 0
+
+    while( i<n1 and j<n2):
+        if arr1[i] <= arr2[j]:
+            arr[k] = arr1[i]
+            k += 1
+            i += 1
+        else:
+            arr[k] = arr2[j]
+            k += 1
+            j += 1
+
+    while i<n1:
+        arr[k] = arr1[i]
+        k += 1
+        i += 1
+    while j<n2:
+        arr[k] = arr2[j]
+        k += 1
+        j += 1
+
+    return arr
+
+def mergeSort(arr): 
+    n = len(arr) 
+
+    if n <=1:
+        return arr
+    else:
+        mid = n//2
+        left = arr[:mid]
+
+        arr1 = mergeSort(left)
+
+        right = arr[mid:]
+        arr2 = mergeSort(right)
+        arr = merge(arr1,arr2,arr)
+        return arr
   #write your code here
   
 # Code to print the list 
 def printList(arr): 
     
     #write your code here
+    print(arr)
   
 # driver code to test the above code 
 if __name__ == '__main__': 
