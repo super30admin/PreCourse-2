eeca5ff47922e156490a0ecce7a1c4e571622732
Raj Jiyani <patelrajj777@gmail.com>
2020-09-13 19:28:59 -0700

    Precourse 2

--- Exercise_2.java
+++ Exercise_2.java
@@ -7,21 +7,50 @@ class QuickSort
        pivot and all greater elements to right 
        of pivot */
     void swap(int arr[],int i,int j){
-        //Your code here   
+        int temp = arr[i]; 
+        arr[i] = arr[j]; 
+        arr[j] = temp; 
     }
     
     int partition(int arr[], int low, int high) 
     { 
-   	//Write code here for Partition and Swap 
+   	 int pivot = arr[high];  
+        int i = (low-1); // index of smaller element 
+        for (int j=low; j<high; j++) 
+        { 
+            // If current element is smaller than the pivot 
+            if (arr[j] < pivot) 
+            { 
+                i++; 
+  
+                // swap arr[i] and arr[j] 
+                swap(arr,i,j) ;
+            } 
+        } 
+  
+        // swap arr[i+1] and arr[high] (or pivot) 
+       swap(arr,i+1,high);
+  
+        return i+1; 
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
       low  --> Starting index, 
       high  --> Ending index */
-    void sort(int arr[], int low, int high) 
+    void sort(int arr[], int low, int high) // recursive call can happen n * n-1 time
+                  // so time complexity O(n^2) and space complexity O(n) (recursion stack)
     {  
-            // Recursively sort elements before 
-            // partition and after partition 
+            if (low < high) 
+            { 
+                /* pi is partitioning index, arr[pi] is  
+                  now at right place */
+                int pi = partition(arr, low, high); 
+      
+                // Recursively sort elements before 
+                // partition and after partition 
+                sort(arr, low, pi-1); 
+                sort(arr, pi+1, high); 
+            } 
     } 
   
     /* A utility function to print array of size n */
