bf7c6089d53ff3b636aee4b0054637330e137459
Aakanksha Patek <aakanksha2696@gmail.com>
2020-09-29 18:05:44 +0530

    Completed Sprint PreCourse2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,30 @@
-# Python program for implementation of Quicksort Sort 
-  
+# Python program for implementation of Quicksort Sort
+
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
     #write your code here
-  
+    pivot = arr[high]
+    i = low - 1
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i + 1
+
+# Function to do Quick sort
+def quickSort(arr,low,high):
     #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+    if low < high:
+        p = partition(arr, low, high)
+        quickSort(arr, low, p-1)
+        quickSort(arr, p+1, high)
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr,0,n-1)
+print ("Sorted array is:")
+for i in range(n):
+    print ("%d" %arr[i]),
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,46 @@
-# Python program for implementation of MergeSort 
+# Python program for implementation of MergeSort
 def mergeSort(arr):
-  
   #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
+    if len(arr) > 1:
+        m = len(arr)//2
+        left = arr[:m]
+        right = arr[m:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i, j, k, = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+              arr[k] = left[i]
+              i += 1
+            else:
+              arr[k] = right[j]
+              j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = (left[i])
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = (right[j])
+            j += 1
+            k += 1
+
+# Code to print the list
+def printList(arr):
     #write your code here
-  
-# driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+    for i in range(arr):
+        print(arr[i])
+
+# driver code to test the above code
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    print ("Given array is", end="\n")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
