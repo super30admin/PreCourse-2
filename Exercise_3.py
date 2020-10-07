cbd1a8d9df7496156966a0a415187003a85fefc0
PrajaktaKhandve <khandve@usc.edu>
2020-10-06 18:28:26 -0700

    Completing Precourse_2

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,19 +2,33 @@
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
-    def __init__(self): 
+    def __init__(self):
+        self.root = None
         
-  
-    def push(self, new_data): 
-        
-  
+    def push(self, new_data):
+        if not self.root:
+            self.root = Node(new_data)
+        else:
+            temp = Node(new_data)
+            temp.next = self.root
+            self.root = temp
+
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        slow = self.root
+        fast = self.root
+        while fast.next is not None and fast.next.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+        return slow.data
+
 
 # Driver code 
 list1 = LinkedList() 
@@ -22,5 +36,10 @@ list1.push(5)
 list1.push(4) 
 list1.push(2) 
 list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+list1.push(1)
+list1.push(7) 
+list1.push(10)
+print("Middle Elemenet is:",list1.printMiddle())
+
+#TC: O(n) we traverse the linkedlist with 2 pointers at the same time
+# SC: O(1) we do not need extra space to find the middle element
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,62 @@
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+def partition(arr, low, high):
+    pivot = arr[high]
+    pIndex = low
+    for i in range(low,high):
+        if arr[i]<=pivot:
+            swap(arr,i,pIndex)
+            pIndex+=1
+    swap(arr,pIndex,high)
+    return pIndex
 
+def swap(arr,x,y):
+   arr[x],arr[y] = arr[y],arr[x]
+ 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    #write your code here
+    size = h-l+1
+    stack = [0]*size
+    t = -1
+    
+    t+=1
+    stack[t] = l
+    t+=1
+    stack[t] = h
 
+
+    while t>=0:
+
+      h = stack[t]
+      t-=1
+      l = stack[t]
+      t-=1
+
+      p = partition(arr,l,h)
+
+      if p-1>l:
+        t+=1
+        stack[t] = l
+        t+=1
+        stack[t] = p-1
+
+      if p+1<h:
+        t+=1
+        stack[t] = p+1
+        t+=1
+        stack[t]=h
+
+# Driver code to test above 
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
+
+
+
+#TC: O(nlogn)
+#TC: O(n)->stack
