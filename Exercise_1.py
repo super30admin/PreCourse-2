702942e3b2014218e2a55b474c750187b9a7cd1e
nishnareddy1 <aletinishna0723@gmail.com>
2020-10-10 12:34:08 -0700

    completed sprint precourse 2

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,30 @@
 # Python code to implement iterative Binary  
 # Search. 
-  
-# It returns location of x in given array arr  
-# if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
-# Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
-  
-# Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
-if result != -1: 
-    print "Element is present at index % d" % result 
-else: 
-    print "Element is not present in array"
+
+# It returns location of x in given array arr
+# if present, else returns -1
+def binarySearch(arr, l, r, x):
+    while (l <= r):
+        mid = (l + r) // 2
+        if x > arr[mid]:
+            l = mid + 1
+        elif x < arr[mid]:
+            r = mid - 1
+        else:
+            return mid
+
+
+# write your code here
+
+
+# Test array
+arr = [2, 3, 4, 10, 40]
+x = 40
+
+# Function call
+result = binarySearch(arr, 0, len(arr) - 1, x)
+
+if result != -1:
+    print("Element is present at index ", result)
+else:
+    print("Element is not present in array")
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,41 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
+    partition_index=0
+    for i in range(l,h):
+        if arr[i]<=arr[h]:
+            arr[i],arr[partition_index]=arr[partition_index],arr[i]
+            partition_index+=1
+    arr[h],arr[partition_index]=arr[partition_index],arr[h]
+    return partition_index
   #write your code here
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
-
+    size = h - l + 1
+    stack = [0] * (size)
+    top = -1
+    top = top + 1
+    stack[top] = l
+    top = top + 1
+    stack[top] = h
+    while top >= 0:
+        h = stack[top]
+        top = top - 1
+        l = stack[top]
+        top = top - 1
+        p = partition(arr, l, h)
+        if p - 1 > l:
+            top = top + 1
+            stack[top] = l
+            top = top + 1
+            stack[top] = p - 1
+        if p + 1 < h:
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = h
+            #write your code here
+arr=[2,1,5,3,9,6]
+quickSortIterative(arr,0,len(arr)-1)
+print(arr)
