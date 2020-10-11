92582e8a59bc4b5a433d1c576f8d3cd76416c8f6
Amrutha2509 <56801612+Amrutha2509@users.noreply.github.com>
2020-10-10 21:05:35 -0400

    Implemented

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,47 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# Node class
+class Node:
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head=None
+
+    def push(self, new_data):
+        node= Node(new_data)
+        if self.head is None:
+            self.head = node
+        else:
+            temp=self.head
+            while(temp.next):
+                temp=temp.next
+            temp.next = node
+
+
+
+    # Function to get the middle of
+    # the linked list
+    def printMiddle(self): #use fast and slow pointers. slow pointer moves by one and fast pointer increments by two.
+        fast=self.head
+        slow=self.head
+        while fast is not None and fast.next is not None: # when fast pointer reaches the end, it means slow pointer would have reached the middle.
+            slow=slow.next
+            fast = fast.next.next
+        print(slow.data)
+
+
+
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2)
+list1.push(3)
+list1.push(1)
+list1.printMiddle()
+
+#Time complexity: O(N)
