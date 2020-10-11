54055c4df52fa6aa5fbc47f62b87e13afc52be27
Achyuth72 <68582633+Achyuth72@users.noreply.github.com>
2020-10-11 17:10:32 -0600

    Update Exercise_4.java

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,42 +1,32 @@
-class MergeSort 
-{ 
-    // Merges two subarrays of arr[]. 
-    // First subarray is arr[l..m] 
-    // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
-    } 
-  
-    // Main function that sorts arr[l..r] using 
-    // merge() 
-    void sort(int arr[], int l, int r) 
-    { 
-	//Write your code here
-        //Call mergeSort from here 
-    } 
-  
-    /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
-  
-    // Driver method 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
-  
-        System.out.println("Given Array"); 
-        printArray(arr); 
-  
-        MergeSort ob = new MergeSort(); 
-        ob.sort(arr, 0, arr.length-1); 
-  
-        System.out.println("\nSorted array"); 
-        printArray(arr); 
-    } 
-} 
+// Time Complexity :O(nlogn)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :
+
+public class RecSort {
+	public static void main(String args[])
+	{
+		int arr[]= {5,7,1,2,4};
+		int ans[]=mergeSort(arr,0,arr.length-1);
+		for(int index:ans)
+		{
+			System.out.println("Val"+index);
+		}
+	}
+public static int[] mergeSort(int arr[],int lo,int hi)
+	{
+		if(lo==hi)
+		{
+			int[] base=new int[1];
+			base[0]=arr[lo];
+			return base;
+			
+		}
+		int mid=(lo+hi)/2;
+		int f[]=mergeSort(arr,lo,mid);
+		int h[]=mergeSort(arr,mid+1,hi);
+		
+		int merge[]=mergeJoin(f,h);
+		return merge;
+	}
+}
