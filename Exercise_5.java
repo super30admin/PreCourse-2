783a1f2150c0ed4f345b43d0890dae56e0e6957a
Achyuth72 <68582633+Achyuth72@users.noreply.github.com>
2020-10-11 17:12:29 -0600

    Update Exercise_5.java

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,52 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
-    /* This function is same in both iterative and 
-       recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
-        //Compare elements and swap.
-    } 
-  
-    // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
-        //Try using Stack Data Structure to remove recursion.
-    } 
-  
-    // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
-    // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
-} 
+// Time Complexity :O(nlogn)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :
+
+public class Demo{
+   void swap_vals(int arr[], int i, int j){
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+   }
+   int partition(int arr[], int l, int h){
+      int x = arr[h];
+      int i = (l - 1);
+      for (int j = l; j <= h - 1; j++){
+         if (arr[j] <= x){
+            i++;
+            swap_vals(arr, i, j);
+         }
+      }
+      swap_vals(arr, i + 1, h);
+      return (i + 1);
+   }
+   void quick_sort(int arr[], int l, int h){
+      int my_list[] = new int[h - l + 1];
+      int top = -1;
+      my_list[++top] = l;
+      my_list[++top] = h;
+      while (top >= 0){
+         h = my_list[top--];
+         l = my_list[top--];
+         int p = partition(arr, l, h);
+         if (p - 1 > l){
+            my_list[++top] = l;
+            my_list[++top] = p - 1;
+         }  
+         if (p + 1 < h){
+            my_list[++top] = p + 1;
+            my_list[++top] = h;
+         }
+      }
+   }
+   public static void main(String args[]){
+      Demo my_ob = new Demo();
+      int my_arr[] = { 34, 76, 41, 32, 11, 0 , 91, 102, -11};
+      my_ob.quick_sort(my_arr, 0, my_arr.length - 1);
+      int i;
+      System.out.println("After iteratively performing quick sort, the array is ");
+      for (i = 0; i < my_arr.length; ++i)
+      System.out.print(my_arr[i] + " ");
+   }
+}
