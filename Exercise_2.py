ff3bb7c24108270bab72a3e67fc4eefb7c5e32d3
ashrit deebadi <dashrit@yahoo.com>
2020-10-11 23:42:36 -0700

    First commit , will submit 4 and 5 by tomorrow night

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,50 @@
-# Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+# Python program for implementation of Quicksort Sort
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+
+# give you explanation for the approach
+
+"""
+Time Complexity :O(nLog(n))
+"""
+
+
+def partition(arr, low, high):
+
+    # write your code here
+
+    myP = arr[high]
+
+    PIndex = low
+    for i in range(low, high):
+        if arr[i] <= myP:
+            temp = arr[i]
+            arr[i] = arr[PIndex]
+            arr[PIndex] = temp
+            PIndex += 1
+    temp = arr[high]
+    arr[high] = arr[PIndex]
+    arr[PIndex] = temp
+
+    return PIndex
+
+# Function to do Quick sort
+
+
+def quickSort(arr, low, high):
+
+    # write your code here
+
+    if low >= high:
+        return
+    PIndex = partition(arr, low, high)
+    quickSort(arr, low, PIndex-1)
+    quickSort(arr, PIndex+1, high)
+
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]),
