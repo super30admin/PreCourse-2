f83175921407c32809d490b65952d81a93661934
harsh <harshsaini697@gmail.com>
2020-10-12 23:40:50 -0500

    Submission

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,19 @@
-class BinarySearch { 
-    // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
+class BinarySearch {
+    // Returns index of x if it is present in arr[l.. r], else return -1
+    int binarySearch(int arr[], int l, int r, int x)
+    {
         //Write your code here
+        while(l<=r){
+            int mid = l + ( r -l )/ 2;
+            if(arr[mid] == x){
+                return mid;
+            }
+            else if(arr[mid] > x)
+                r--;
+            else
+                l++;
+        }
+        return -1;
     } 
   
     // Driver method to test above 
@@ -19,3 +30,7 @@ class BinarySearch {
             System.out.println("Element found at index " + result); 
     } 
 } 
+
+
+// Time Complexity : O(Log N)
+// Space Complexity: O(1) - as no auxillary space used except variables
