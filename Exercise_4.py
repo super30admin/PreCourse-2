bf0d71c9216cc0cdde6ee789bfef1ea2e93d5d88
Sarvesh Kulkarni <30865213+snk95@users.noreply.github.com>
2020-11-15 01:40:32 -0800

    Precourse 2 done

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,57 @@
 # Python program for implementation of MergeSort 
-def mergeSort(arr):
+"""
+Time - O(nlogn) logn to divide and n to merge
+Space - O(n) as we are making copy of array
+Idea - First we keep on dividing the array into half and then it is merged again in ascending order
+"""
+def mergeSort(arr): 
+  divide(arr, 0, len(arr)-1)
+
+def divide(arr, start, end): # time - O(logn)
+  if start >= end:
+    return
+
+  midpoint = (start + end) // 2 + 1
+
+  divide(arr, start, midpoint-1)
+  divide(arr, midpoint, end)
+
+  merge(arr, start, midpoint, end)
+
+def merge(arr, start, start2, end): # time - O(n)
+  p1 = start
+  p2 = start2
+  cur = start
+  copy = arr[:]
+
+  while cur <= end:
+    if p1 < start2 and p2 <= end:
+      if copy[p1] < copy[p2]:
+        arr[cur] = copy[p1]
+        p1 += 1
+      
+      else:
+        arr[cur] = copy[p2]
+        p2 += 1
+    
+    elif p1 < start2:
+      arr[cur] = copy[p1]
+      p1 += 1
+
+    else:
+      arr[cur] = copy[p2]
+      p2 += 1
+    
+    cur += 1
+
+      
   
-  #write your code here
   
 # Code to print the list 
 def printList(arr): 
+  print(arr)
     
-    #write your code here
+    
   
 # driver code to test the above code 
 if __name__ == '__main__': 
