3109e764a58867c7e3689790b18a5eeb36f84c34
bhargavajonnalagadda <74167176+bhargavajonnalagadda@users.noreply.github.com>
2020-11-15 18:09:24 -0500

    Add files via upload

---
+++ Precourse2_exercise5.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Precourse2_xercise5
+{
+    class Precourse2_exercise5
+    {
+        void swap(int[] arr,int i,int j)
+        {
+            if (arr[i] != arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            
+            /* arr[i] = arr[i] + arr[j];
+            arr[j] = arr[i] - arr[j];
+            arr[i] = arr[i] - arr[j];*/
+        }
+        int partition(int[] arr,int l,int h)
+        {
+            int temp;
+            int pivot = arr[h];
+            int i = (l - 1);
+            for(int j=l;j<=h;j++)
+            {
+                if(arr[j]<pivot)
+                {
+                    i++;
+                    swap(arr, i, j);
+                }
+                
+            }
+
+            swap(arr, i + 1, h);
+            return i+1;
+        }
+        void quicksort(int[] arr,int l, int h)
+        {
+            if(l<h)
+            {
+                int pi = partition(arr, l, h);
+                quicksort(arr, l, pi - 1);
+                quicksort(arr, pi + 1, h);
+            }
+        }
+
+        void printarr(int[] arr,int n)
+        {
+            for(int i=0;i<n; i++)
+            {
+                Console.WriteLine("element is {0}", arr[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Precourse2_exercise5 p = new Precourse2_exercise5();
+            int[] arr = { 4, 3, 5, 2, 1, 3, 2, 3 };
+            p.quicksort(arr, 0, arr.Length - 1);
+            p.printarr(arr, arr.Length);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ precourse2_exercise3.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace precourse2_exercise3
+{
+    class precourse2_exercise3
+    {
+        static Node head;
+        static int n=1;
+        public class Node
+        {
+            public int data;
+            public  Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+
+        public void printmiddle()
+        {
+
+            Node tnode = head.next;
+            //n = 1;
+            printmiddleutil(tnode);
+
+        }
+
+        public void printmiddleutil(Node no)
+        {
+            if(no==null)
+            {
+                n = (n) / 2;
+                return;
+            }
+
+            n = n + 1;
+            printmiddleutil(no.next);
+            n = n - 1;
+            if(n==0)
+            {
+                head = no;
+                Console.WriteLine("midpoint node is {0}", head.data);
+            }
+
+        }
+
+        public void Push(int newdata)
+        {
+            Node _newnode = new Node(newdata);
+            _newnode.next = head;
+            head = _newnode;
+        }
+
+        public void printlist()
+        {
+            Node tnode = head;
+            while (tnode!=null)
+            {
+                Console.WriteLine("printlist {0}", tnode.data);
+                tnode=tnode.next;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            precourse2_exercise3 ll = new precourse2_exercise3();
+            for (int i = 15; i > 0; --i)
+
+            {
+                ll.Push(i);
+            }
+                ll.printlist();
+                ll.printmiddle();
+            
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
