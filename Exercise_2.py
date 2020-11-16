758bb155ab387ccdbf3800f56d7d572bd9202e48
apurva <apurva@compgeom.com>
2020-11-15 21:21:44 -0800

    Completed precourse 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,15 +2,30 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
+    i = low - 1
+    pivot = arr[high]
+    
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i+1
   
-  
-    #write your code here
+
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
-    #write your code here
+    if len(arr) == 1:
+        return arr
+        
+    if low < high:
+        mid = partition(arr, low, high)
+        quickSort(arr, low, mid-1)
+        quickSort(arr, mid+1, high)
+
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
