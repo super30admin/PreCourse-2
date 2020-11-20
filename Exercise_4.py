24935126fcee3dce8530aff3fe628ddea97500d6
unknown <kritagyan@gmail.com>
2020-11-19 18:58:32 -0600

    Completed Sprint PreCourse_2

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,47 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
-  #write your code here
-  
+  if len(arr) > 1:
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    # Recursive call on each half
+    mergeSort(left)
+    mergeSort(right)
+
+    # Two iterators for traversing the two halves
+    i = 0
+    j = 0
+    
+    # Iterator for the main list
+    k = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+          # The value from the left half has been used
+          arr[k] = left[i]
+          # Move the iterator forward
+          i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        # Move to the next slot
+        k += 1
+
+    # For all the remaining values
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        arr[k]=right[j]
+        j += 1
+        k += 1
+
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
+  print(arr)
   
 # driver code to test the above code 
 if __name__ == '__main__': 
