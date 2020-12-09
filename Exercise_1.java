6663b83002e761095f1002d90b8cc84c3767ff2a
Shashwath Kamath <kamathshashwath1991@gmail.com>
2020-12-08 21:40:43 -0500

    Binary Search and Quick Sort done.

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,6 +3,19 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        if (l > r){
+            return -1;
+        }
+        int mid = l + (r - l)/2;
+        if (arr[mid] == x){
+            return mid;
+        }
+        else if(arr[mid] > x){
+            binarySearch(arr,l,mid-1,x);
+        }
+        else{
+            binarySearch(arr,mid+1,r,x);
+        }
     } 
   
     // Driver method to test above 
