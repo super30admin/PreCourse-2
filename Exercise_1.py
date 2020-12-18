1c795c6b5c9d697421512b027d82a481f88a9252
Anshul Atriek <atriekak@gmail.com>
2020-12-18 01:11:16 -0500

    Completed Sprint PreCourse-2

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,9 +4,16 @@
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
+  if r >= l:
+    mid = (l+r) // 2
+    if arr[mid] == x:
+      return mid
+    elif x < arr[mid]:
+      return binarySearch(arr, l, mid-1, x)
+    else:  
+      return binarySearch(arr, mid+1, r, x)
+  else:
+    return -1
     
   
 # Test array 
@@ -17,6 +24,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+  print("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+  print("Element is not present in array")
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,18 +3,31 @@ class Node:
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
   
     def push(self, new_data): 
-        
+        newnode = Node(new_data)
+        newnode.next = self.head
+        self.head = newnode
+        return
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        i = self.head
+        j = self.head
+
+        while i and i.next:
+            i = i.next.next
+            j = j.next
+        print("Middle element:" ,j.data)
+        return
 
 # Driver code 
 list1 = LinkedList() 
