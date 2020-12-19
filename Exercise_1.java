0f095edd65e867fa4c845592bebcd6be656aa784
unknown <eo8935@gmail.com>
2020-12-19 14:37:03 -0500

    Completed precourse 2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,21 +1,43 @@
-class BinarySearch { 
+class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
-    } 
-  
+    int binarySearch(int arr[], int l, int r, int x)
+    {
+        //create variables and set index value to -1
+        int mid;
+        int index = -1;
+
+        while(l <= r){
+            //calculate mid point of array
+            mid = (l+r)/2;
+
+            //set l value to one more than mid point valye if x is not in left side
+            if(arr[mid] < x){
+                l=mid+1;
+                //set r value to 1 less than mid value if x is not in the right side
+            } else if(arr[mid] > x){
+                r = mid -1;
+                //set index value if x is found in array
+            } else if(arr[mid] == x){
+                index = mid;
+                break;
+            }
+        }
+        return index;
+    }
+
+    //1 2 3 4 5 69 703 309 52
+
     // Driver method to test above 
-    public static void main(String args[]) 
-    { 
-        BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
-        int n = arr.length; 
-        int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
-        if (result == -1) 
-            System.out.println("Element not present"); 
+    public static void main(String args[])
+    {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 10;
+        int result = ob.binarySearch(arr, 0, n - 1, x);
+        if (result == -1)
+            System.out.println("Element not present");
         else
-            System.out.println("Element found at index " + result); 
-    } 
+            System.out.println("Element found at index " + result);
+    }
 } 
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,48 +1,78 @@
-class QuickSort 
-{ 
-    /* This function takes last element as pivot, 
-       places the pivot element at its correct 
-       position in sorted array, and places all 
-       smaller (smaller than pivot) to left of 
-       pivot and all greater elements to right 
+// Time Complexity :0(n^2)
+// Space Complexity :O(logn)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class QuickSort
+{
+    /* This function takes last element as pivot,
+       places the pivot element at its correct
+       position in sorted array, and places all
+       smaller (smaller than pivot) to left of
+       pivot and all greater elements to right
        of pivot */
     void swap(int arr[],int i,int j){
-        //Your code here   
+        //Your code here
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    
-    int partition(int arr[], int low, int high) 
-    { 
-   	//Write code here for Partition and Swap 
-    } 
-    /* The main function that implements QuickSort() 
-      arr[] --> Array to be sorted, 
-      low  --> Starting index, 
+
+    int partition(int arr[], int low, int high)
+    {
+        //Write code here for Partition and Swap
+        //Compare elements and swap.
+        int pivot = arr[high];
+        int lowestIndex = low-1;
+
+        for(int i=low; i<high; i++){
+            if(arr[i]<pivot){
+                lowestIndex++;
+                swap(arr, lowestIndex, i);
+            }
+        }
+
+        swap(arr, lowestIndex + 1, high);
+
+        return lowestIndex + 1;
+    }
+    /* The main function that implements QuickSort()
+      arr[] --> Array to be sorted,
+      low  --> Starting index,
       high  --> Ending index */
-    void sort(int arr[], int low, int high) 
-    {  
-            // Recursively sort elements before 
-            // partition and after partition 
-    } 
-  
+    void sort(int arr[], int low, int high)
+    {
+        // Recursively sort elements before
+        // partition and after partition
+        if(low<high+1){
+            int partition = partition(arr, low, high);
+            sort(arr, low, partition-1);
+            sort(arr, partition+1, high);
+        }
+    }
+
     /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
-        System.out.println(); 
-    } 
-  
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
     // Driver program 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {10, 7, 8, 9, 1, 5}; 
-        int n = arr.length; 
-  
-        QuickSort ob = new QuickSort(); 
-        ob.sort(arr, 0, n-1); 
-  
-        System.out.println("sorted array"); 
-        printArray(arr); 
-    } 
+    public static void main(String args[])
+    {
+        int arr[] = {10, 7, 8, 9, 1, 5};
+        int n = arr.length;
+
+        QuickSort ob = new QuickSort();
+        ob.sort(arr, 0, n-1);
+
+        System.out.println("sorted array");
+        printArray(arr);
+    }
 } 
