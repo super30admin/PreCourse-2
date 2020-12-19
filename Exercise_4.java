0487b281b7ed3c189678483cc5f086c4f88c805d
Nikhita Sagi <saginikhita98@gmail.com>
2020-12-19 18:53:57 -0500

    Added all the solutions

--- Exercise_4.java
+++ Exercise_4.java
@@ -5,7 +5,31 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+       //Your code here
+        if(arr[m] <= arr[m+1]){
+            return;
+        }
+       int[] temp = new int[r-l+1];
+
+       int i = l;
+       int j = m+1;
+       int index = 0;
+
+       while(i < m+1 && j < r+1){
+           temp[index++] = arr[i] <= arr[j] ? arr[i++] : arr[j++];
+       }
+
+       if(i<=m){
+           for(int k=l+index;k<r+1;k++){
+               temp[k] = arr[i++];
+           }
+       }
+
+       int ind = 0;
+       for(int k=l;k<r+1;k++){
+           arr[k] = temp[ind++];
+       }
+
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -13,7 +37,14 @@ class MergeSort
     void sort(int arr[], int l, int r) 
     { 
 	//Write your code here
-        //Call mergeSort from here 
+        //Call mergeSort from here
+        if(l == r){
+            return;
+        }
+        int mid = l + (r-l)/2;
+        sort(arr,l,mid);
+        sort(arr,mid+1,r);
+        merge(arr,l,mid,r);
     } 
   
     /* A utility function to print array of size n */
@@ -28,7 +59,7 @@ class MergeSort
     // Driver method 
     public static void main(String args[]) 
     { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
+        int arr[] = {12, 11, 13, 5, 6};
   
         System.out.println("Given Array"); 
         printArray(arr); 
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,7 +1,10 @@
-class IterativeQuickSort { 
+import java.util.Stack;
+
+class IterativeQuickSort {
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+	//Try swapping without extra variable
+        arr[i] = arr[j];
     } 
   
     /* This function is same in both iterative and 
@@ -9,12 +12,66 @@ class IterativeQuickSort {
     int partition(int arr[], int l, int h) 
     { 
         //Compare elements and swap.
+        int pivot = arr[l];
+        int i = l;
+        int j = h;
+
+        while(i < j){
+
+            while(i < j && arr[j] >= pivot){
+                j--;
+            }
+
+            if(i < j){
+                swap(arr,i,j);
+                i++;
+                //arr[i] = arr[j];
+
+            }
+            while(i < j && arr[i] <= pivot){
+                i++;
+            }
+
+            if(i < j){
+                swap(arr,j,i);
+                j--;
+                //arr[j] = arr[i];
+            }
+        }
+
+        arr[j] = pivot;
+        return j;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+        if(l >= h){
+            return;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(l);
+        stack.push(h);
+
+        while(!stack.isEmpty()){
+
+            int high = stack.pop();
+            int low = stack.pop();
+
+            if(low >= high){
+                continue;
+            }
+            int pivot = partition(arr,low,high);
+
+            stack.push(low);
+            stack.push(pivot-1);
+
+            stack.push(pivot+1);
+            stack.push(high);
+        }
+
     } 
   
     // A utility function to print contents of arr 
@@ -29,7 +86,7 @@ class IterativeQuickSort {
     public static void main(String args[]) 
     { 
         IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 };
         ob.QuickSort(arr, 0, arr.length - 1); 
         ob.printArr(arr, arr.length); 
     } 
