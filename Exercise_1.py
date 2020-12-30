0f23c9988373b3f51857e06a5e35cd8afab41463
Vaikhari Bongulwar <51323924+vbongulwar@users.noreply.github.com>
2020-12-30 12:52:48 -0700

    Completed PreCourse2

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,19 +4,37 @@
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
+  # Time comp : O(logn)
+  # Space comp : O(n)
   #write your code here
-  
-    
-  
+  mid = 0
+
+  if l>r:
+    return -1
+   
+  while l <= r:
+    mid = (r + l) //2
+
+    if arr[mid] == x:
+      return mid
+
+    elif arr[mid]<x:
+      l = mid+1
+
+    else:
+      r = mid-1
+
+  return -1
+
+
 # Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
+arr = [ 2, 3, 4,5, 10, 40 ] 
 x = 10
   
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index % d" % result) 
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,18 +3,34 @@ class Node:
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head = None
         
   
-    def push(self, new_data): 
+    def push(self, new_data):
+        n_node = Node(new_data)
+        n_node.next = self.head
+        self.head = n_node
         
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        s_ptr = self.head
+        f_ptr = self.head
+
+        if self.head is not None:
+            while(f_ptr is not None and f_ptr.next is not None):
+                f_ptr = f_ptr.next.next
+                s_ptr = s_ptr.next
+
+            print("The middle element:",s_ptr.data)
+
 
 # Driver code 
 list1 = LinkedList() 
