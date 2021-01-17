e80a7b35dc2506543a855db2d28fc2f83987d638
Priyanka <pzar1@asu.edu>
2021-01-16 21:23:03 -0700

    Completed Pre-course2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,44 @@
+# Overall time complexity: O(n/2)
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
-        
+    def __init__(self, data): 
+        self.data = data
+        self.next = None
+
 class LinkedList: 
   
-    def __init__(self): 
-        
+    def __init__(self):
+        # initailize the head node
+        self.head = None
   
-    def push(self, new_data): 
-        
+    # add new node to the linked list
+    def push(self, new_data):
+        # insert at the beginning of the list
+        # create a Node with new_data
+        new_n = Node(new_data)
+        # point the new node to head
+        new_n.next = self.head
+        # make the new node the head of the list
+        self.head = new_n
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        # point the slow_ptr and fast_ptr to the head of the list
+        slow_ptr = self.head
+        fast_ptr = self.head
+        # check if the list is empty or not
+        if self.head is not None:
+            # check if fast_ptr and fast_ptr.next has reached the end of list or not
+            while(fast_ptr is not None and fast_ptr.next is not None):
+                # increment the fast_ptr by 2
+                fast_ptr = fast_ptr.next.next
+                # increment the slow_ptr by 1
+                slow_ptr = slow_ptr.next
+            # print the slow_ptr data which is the middle element of the linked list
+            print(slow_ptr.data)
 
 # Driver code 
 list1 = LinkedList() 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,62 @@
 # Python program for implementation of Quicksort
 
+# Overall complexity: 
+# Bestcase: O(nlogn)
+# Worst case: O(n^2)
+# Average case: O(nlogn)
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
   #write your code here
-
+  i = l -1
+  # last element as pivot
+  pivot = arr[h]
+  for k in range(l,h):
+    # if the element is less than the pivot then
+    if arr[k] <= pivot:
+      # increment i by 1
+      i += 1
+      # swap the elements
+      arr[i], arr[k] = arr[k], arr[i]
+  # swap the element at i+1 position with the pivot
+  arr[i + 1], arr[h] = arr[h], arr[i + 1]
+  return (i + 1)
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  # size of the arr
+  n = h - l - 1
+  # initialize a stack of length n
+  stack = [0] * n
+  top = 0
+  # initialize the first element as l
+  stack[top] = l
+  # increment the top index by 1
+  top += 1
+  # initialize the next element as h
+  stack[top] = h
+
+# check if stack is empty or not
+  while top >= 0:
+    h = stack[top]
+    top -= 1
+    l = stack[top]
+    top -= 1
+
+    # partition index
+    p = partition(arr, l, h)
+
+    # check if end of list is reached or not
+    if p-1 > l:
+      top += 1
+      stack[top] = l
+      top += 1
+      stack[top] = p-1
+
+    # check if end of list is reached or not
+    if p+1 < h:
+      top += 1
+      stack[top] = p + 1
+      top += 1
+      stack[top] = h
+
 
