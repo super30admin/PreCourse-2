384e352ad45eada0c9da00eebbbbceae967ef648
Hardik3172 <44345045+Hardik3172@users.noreply.github.com>
2021-01-18 00:40:37 +0530

    Update Exercise_2.cpp

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -5,6 +5,9 @@ using namespace std;
 void swap(int* a, int* b)  
 {  
     //Your Code here 
+    int x = *a;  
+    *a = *b;  
+    *b = x;  
 }  
   
 /* This function takes last element as pivot, places  
@@ -15,6 +18,20 @@ of pivot */
 int partition (int arr[], int low, int high)  
 {  
     //Your Code here 
+    int pivot = arr[high],x = (low - 1);  
+   
+  
+    for (int i=low;i<=high-1;i++)  
+    {  
+        
+        if (arr[i]<pivot)  
+        {  
+            x++; 
+            swap(&arr[x], &arr[i]);  
+        }  
+    }  
+    swap(&arr[x + 1], &arr[high]);  
+    return (x + 1);  
 }  
   
 /* The main function that implements QuickSort  
@@ -24,6 +41,12 @@ high --> Ending index */
 void quickSort(int arr[], int low, int high)  
 {  
     //Your Code here 
+    if (low<high)  
+    {  
+        int index = partition(arr, low, high);  
+        quickSort(arr, low, index - 1);  
+        quickSort(arr, index + 1, high);  
+    }  
 }  
   
 /* Function to print an array */
