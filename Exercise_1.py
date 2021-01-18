0a954d226896e1db179649dbce46650e52267838
Janhavi Dahihande <janhavidahihande@gmail.com>
2021-01-17 20:34:48 -0800

    Add iterative approach

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,24 +2,50 @@
 # Search. 
   
 # It returns location of x in given array arr  
-# if present, else returns -1 
+# if present, else returns -1
+
+# Recursive Approach:
+# 1. Get middle index of the array
+# 2. x == arr[mid] return mid
+# 3. x > arr[mid] recurse over right half
+# 4. x < arr[mid] recurse over left half
+
+# Iterative approach
+# 1. Repeat while low <= high
+#   1.1 Get middle index of the array
+#   1.2 x > arr[mid] low = mid + 1
+#   1.3 x < arr[mid] high = mid - 1
+#   1.4 else return mid
+
 def binarySearch(arr, l, r, x): 
   
   #write your code here
-  if (r >= l):
-    mid = (l + r) // 2
-    if (x > arr[mid]):
-      return binarySearch(arr, mid + 1, r, x)
-    if (x < arr[mid]):
-      return binarySearch(arr, l, mid - 1, x)
-    if (x == arr[mid]):
+  # Iterative approach
+  while l <= r:
+    mid = ( l + r ) // 2
+    if x > arr[mid]:
+      l = mid + 1
+    elif x < arr[mid]:
+      r = mid - 1
+    else:
       return mid
-  else:
-    return -1 
+  return -1
+  
+  # Recursive approach
+  # if (r >= l):
+  #   mid = (l + r) // 2
+  #   if (x > arr[mid]):
+  #     return binarySearch(arr, mid + 1, r, x)
+  #   if (x < arr[mid]):
+  #     return binarySearch(arr, l, mid - 1, x)
+  #   if (x == arr[mid]):
+  #     return mid
+  # else:
+  #   return -1 
   
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
+x = 3
   
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
