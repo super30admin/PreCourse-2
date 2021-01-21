12d79bea7920632a729fe389496ee8832ac598bb
Satya-Kumar-S <ssurend3@asu.edu>
2021-01-21 00:55:53 -0700

    Pre course

--- Exercise_1.py
+++ Exercise_1.py
@@ -6,6 +6,15 @@
 def binarySearch(arr, l, r, x): 
   
   #write your code here
+    while l<=r:
+        mid = (l + r) // 2
+        if arr[mid] == x:
+            return mid
+        if arr[mid] > x:
+            r = mid - 1
+        else:
+            l = mid + 1
+    return -1
   
     
   
@@ -17,6 +26,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,42 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
+    if len(arr) > 1:
+        mid = len(arr) // 2 
+        Leftarray = arr[0:mid]
+        Rightarray = arr[mid:]
+        
+        mergeSort(Leftarray)
+        mergeSort(Rightarray)
+        
+        i = 0
+        j = 0
+        k = 0
+        
+        while(i < len(Leftarray) and j < len(Rightarray)):
+            if Leftarray[i] < Rightarray[j]:
+                arr[k] = Leftarray[i]
+                i += 1
+            else:
+                arr[k] = Rightarray[j]
+                j += 1
+            k += 1
+        
+        while i < len(Leftarray):
+            arr[k] = Leftarray[i]
+            i+=1
+            k+=1
+        while j < len(Rightarray):
+            arr[k] = Rightarray[j]
+            j+=1
+            k+=1
   #write your code here
-  
-# Code to print the list 
-def printList(arr): 
+# Code to print the list
+            
+
+def printList(arr):
     
+    for i in arr:
+        print(i)
     #write your code here
   
 # driver code to test the above code 
