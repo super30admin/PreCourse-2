de33fd6591820a320eb90e249197b3f89858e4a4
rogen-george <rogenp8@gmail.com>
2021-01-24 12:27:30 -0800

    Precourse 2 Python

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,44 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# Node class
+class Node:
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        if self.head == None :
+            self.head = Node( new_data )
+        else:
+            ptr = self.head
+            while ( ptr.next is not None ):
+                ptr = ptr.next
+            ptr.next = Node( new_data )
+
+
+    # Function to get the middle of
+    # the linked list
+    def printMiddle(self):
+
+        slow = self.head
+        fast = self.head
+
+        while ( fast is not None and fast.next is not None ):
+            slow = slow.next
+            fast = fast.next.next
+
+        print ( slow.data )
+
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2)
+list1.push(3)
+list1.push(1)
+list1.printMiddle()
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,48 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
-
+    #write your code here
+    pivot = arr[l]
+    while ( l < h ):
+        if ( arr[l] < pivot ):
+            l += 1
+        elif ( arr[h] > pivot ):
+            h -= 1
+        if ( l < h ):
+            arr[l], arr[h] = arr[h], arr[l]
+    return l
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    #write your code here
+    stack = []
+    stack.append( l )
+    stack.append( h )
 
+    while ( stack ):
+        high = stack.pop()
+        low = stack.pop()
+
+        if ( high - low < 2 ):
+            continue
+        else:
+            pivot = partition( arr, low , high )
+            # If there are elements on the left side of pivot push
+            # low and pivot - 1 into stack
+            if ( pivot - 1 > l ):
+                stack.append( low )
+                stack.append( pivot - 1 )
+
+            # If there are elements on the right side of pivot push
+            # pivot + 1 to high into stack
+            if ( pivot + 1 < h ):
+                stack.append( pivot + 1 )
+                stack.append( high )
+
+    return arr
+
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSortIterative(arr,0,n-1)
+print ("Sorted array is:")
+for i in range(n):
+    print ("%d" %arr[i]),
