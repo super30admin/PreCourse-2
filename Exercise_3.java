bff809863cdbcd188c6c86a822cf23826f21a312
Lokeswari Umakanthan <lokeswari.umakanthan@utdallas.edu>
2021-02-11 13:11:10 -0600

    Update Exercise_3.java

--- Exercise_3.java
+++ Exercise_3.java
@@ -18,6 +18,13 @@ class LinkedList
    //Complete this function
     void printMiddle() 
     { 
+	Node slow = head;
+    	Node fast = head;
+    	while(fast != null && fast.next != null) {
+    		slow = slow.next;
+    		fast = fast.next.next;
+    	}
+    	System.out.println(slow.data);
         //Write your code here
 	//Implement using Fast and slow pointers
     } 
