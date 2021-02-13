22832c0e3d4a6d530d7ae44629fad72611285ee9
Lokeswari Umakanthan <lokeswari.umakanthan@utdallas.edu>
2021-02-13 12:46:02 -0600

    Update Exercise_5.java

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,6 +1,17 @@
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
+	if(arr[i] == arr[j]) return;
+    	else if(arr[i] > arr[j]) {
+    		arr[i] = arr[i] + arr[j];
+    		arr[j] = arr[i] - arr[j];
+    		arr[i] = arr[i] - arr[j];
+    	}
+    	else {
+    		arr[j] = arr[j] + arr[i];
+    		arr[i] = arr[j] - arr[i];
+    		arr[j] = arr[j] - arr[i];
+    	}
 	//Try swapping without extra variable 
     } 
   
@@ -8,20 +19,46 @@ class IterativeQuickSort {
        recursive*/
     int partition(int arr[], int l, int h) 
     { 
+	int pivot = arr[h];
+    	int a = l-1;
+    	for(int i=l;i<h;i++) {
+    		if(arr[i] < pivot) {
+    			a++;
+    			swap(arr,a,i);
+    		}
+    	}
+    	swap(arr,a+1,h);
+    	return a+1;
         //Compare elements and swap.
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
+	Stack st = new Stack();
+    	st.push(l);
+    	st.push(h);
+    	while(!st.isEmpty()) {
+    		h = st.pop();
+    		l = st.pop();
+    		
+    		int mid = partition(arr,l,h);
+    		if(mid+1 < h) {
+    			st.push(mid+1);
+    			st.push(h);
+    		}
+    		if (mid-1 > l) {
+    			st.push(l);
+    			st.push(mid-1);
+    		}
+    	}
         //Try using Stack Data Structure to remove recursion.
     } 
   
     // A utility function to print contents of arr 
     void printArr(int arr[], int n) 
     { 
-        int i; 
-        for (i = 0; i < n; ++i) 
+        for (int i = 0; i < n; ++i) 
             System.out.print(arr[i] + " "); 
     } 
   
