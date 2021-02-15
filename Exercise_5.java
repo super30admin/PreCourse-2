a38746284e26508ea97c1fb4957cbd46b94d719c
sharathn29 <nsharathn@gmail.com>
2021-02-15 10:32:15 -0800

    Completed Precourse2

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,77 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
-    /* This function is same in both iterative and 
-       recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
-        //Compare elements and swap.
-    } 
-  
-    // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
-        //Try using Stack Data Structure to remove recursion.
-    } 
-  
-    // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
-    // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
-} 
+// Time Complexity:
+// Best Case : Assuming the partition is always done in the middle, the time complexity is O(nlogn). At each partition level it takes 
+// O(n) time and there are log n such levels. So the overall time complexity is O(nlogn)
+// Worst Case : If the partition always happen at the beginning or end of the array, then the time complexity is O(n^2)
+
+// Space Complexity : O(1), but I am confused as to add the space occupied by the stack to hold the low and high bounds for each partition
+
+import java.util.Stack;
+
+class IterativeQuickSort {
+    void swap(int arr[], int i, int j) {
+        // Try swapping without extra variable
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+    }
+
+    /*
+     * This function is same in both iterative and recursive
+     */
+    int partition(int arr[], int low, int high) {
+        // Compare elements and swap.
+        int pivot = arr[low];
+        int start = low, end = high;
+
+        while (start < end) {
+            while (start <= high && arr[start] <= pivot)
+                start++;
+            while (end >= low && arr[end] > pivot)
+                end--;
+            if (start < end) {
+                swap(arr, start, end);
+            }
+        }
+        swap(arr, low, end);
+        return end;
+    }
+
+    // Sorts arr[l..h] using iterative QuickSort
+    void QuickSort(int arr[], int l, int h) {
+        // Try using Stack Data Structure to remove recursion.
+        Stack<Integer> stack = new Stack();
+        stack.push(l);
+        stack.push(h);
+
+        while (!stack.empty()) {
+            h = stack.pop();
+            l = stack.pop();
+
+            int partitionIndex = partition(arr, l, h);
+            if (partitionIndex - 1 > l) {
+                stack.push(l);
+                stack.push(partitionIndex - 1);
+            }
+
+            if (partitionIndex + 1 < h) {
+                stack.push(partitionIndex + 1);
+                stack.push(h);
+            }
+        }
+    }
+
+    // A utility function to print contents of arr
+    void printArr(int arr[], int n) {
+        int i;
+        for (i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
+    // Driver code to test above
+    public static void main(String args[]) {
+        IterativeQuickSort ob = new IterativeQuickSort();
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 };
+        ob.QuickSort(arr, 0, arr.length - 1);
+        ob.printArr(arr, arr.length);
+    }
+}
