60faada7ab6df1041701498abe1cec4109d84e4e
Sagar Rikame <sagarrikame@Sagars-MBP.lan>
2021-02-15 15:11:38 -0600

    Completed Excercise 4

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,50 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
-    #write your code here
-  
-# driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+    # write your code here
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        Left = arr[:mid]
+        Right = arr[mid:]
+
+        mergeSort(Left)
+        mergeSort(Right)
+
+        i = j = k = 0
+
+        while i < len(Left) and j < len(Right):
+            if Left[i] < Right[j]:
+                arr[k] = Left[i]
+                i += 1
+            else:
+                arr[k] = Right[j]
+                j += 1
+            k += 1
+
+        while i < len(Left):
+            arr[k] = Left[i]
+            i += 1
+            k += 1
+
+        while j < len(Right):
+            arr[k] = Right[j]
+            j += 1
+            k += 1
+
+
+# Code to print the list
+def printList(arr):
+    # write your code here
+
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+
+# driver code to test the above code
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
