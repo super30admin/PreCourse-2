9563a5dd4017d0d91ee9bb37bdf87c5f34df11c9
Shilpa Catherine <Shilpa.j.catherine@gmail.com>
2021-02-14 20:20:26 -0500

    Binary Search solution done

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,6 +3,19 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        int mid = l + (r-l)/2;
+        if (arr.length == 0 || arr == null || l < 0 || r > arr.length-1 || mid < 0 || mid > arr.length -1){
+            return -1;
+        }
+
+
+        if(x == arr[mid]){
+            return mid;
+        }else if(x > arr[mid]){
+            return binarySearch(arr, mid+1, r ,x);
+        }else {
+            return binarySearch(arr, l, mid-1 ,x);
+        }
     } 
   
     // Driver method to test above 
