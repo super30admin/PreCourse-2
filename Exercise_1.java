b28ace570b75821f4a5c7dec08b38499c9465180
Bagirathan <bagirathan96@gmail.com>
2021-03-12 09:44:11 -0800

    Completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,21 +1,29 @@
-class BinarySearch { 
-    // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
-    } 
-  
-    // Driver method to test above 
-    public static void main(String args[]) 
-    { 
-        BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
-        int n = arr.length; 
-        int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
-        if (result == -1) 
-            System.out.println("Element not present"); 
+class BinarySearch {
+    // Returns index of x if it is present in arr[l.. r], else return -1
+    int binarySearch(int arr[], int l, int r, int x) {
+        // Write your code here
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (x == arr[mid])
+                return mid;
+            else if (x < arr[mid])
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return -1;
+    }
+
+    // Driver method to test above
+    public static void main(String args[]) {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 10;
+        int result = ob.binarySearch(arr, 0, n - 1, x);
+        if (result == -1)
+            System.out.println("Element not present");
         else
-            System.out.println("Element found at index " + result); 
-    } 
-} 
+            System.out.println("Element found at index " + result);
+    }
+}
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,69 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
-    /* This function is same in both iterative and 
-       recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
-        //Compare elements and swap.
-    } 
-  
-    // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
-        //Try using Stack Data Structure to remove recursion.
-    } 
-  
-    // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
-    // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
-} 
+import java.util.*;
+
+class IterativeQuickSort {
+    void swap(int arr[], int i, int j) {
+        // Try swapping without extra variable
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+    }
+
+    /*
+     * This function is same in both iterative and recursive
+     */
+    int partition(int arr[], int l, int h) {
+        // Compare elements and swap.
+        int left = l - 1;
+        int right = h;
+        int pivot = arr[h];
+        while (true) {
+            while (arr[++left] < pivot && left < right)
+                ;
+            while (arr[--right] > pivot && right > 0)
+                ;
+            if (left <= right)
+                swap(arr, left, right);
+            else
+                break;
+        }
+        swap(arr, left, h);
+        return left;
+    }
+
+    // Sorts arr[l..h] using iterative QuickSort
+    void QuickSort(int arr[], int l, int h) {
+        // Try using Stack Data Structure to remove recursion.
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(l);
+        stack.push(h);
+        while (!stack.isEmpty()) {
+            int end = stack.pop();
+            int start = stack.pop();
+            int partionIndex = partition(arr, start, end);
+            if (partionIndex - 1 > start) {
+                stack.push(start);
+                stack.push(partionIndex - 1);
+            }
+            if (partionIndex + 1 < end) {
+                stack.push(partionIndex + 1);
+                stack.push(end);
+            }
+        }
+    }
+
+    // A utility function to print contents of arr
+    void printArr(int arr[], int n) {
+        int i;
+        for (i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
+    // Driver code to test above
+    public static void main(String args[]) {
+        IterativeQuickSort ob = new IterativeQuickSort();
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 };
+        ob.QuickSort(arr, 0, arr.length - 1);
+        ob.printArr(arr, arr.length);
+    }
+}
