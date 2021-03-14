3a2a25328365c81c25c68c7e3abb74f302090caf
Yash Shah <yash2396@hotmail.com>
2021-03-14 23:27:41 +0530

    Completed Sprint PreCourse2

---
+++ LinkedList.java
@@ -0,0 +1,71 @@
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+// I will be using two pointers
+// fast pointer and slow pointer, the fast pointer jumps two nodes at a time and theslow pointer jumps one node at a time
+// Hence when the fast pointer is a the end of the list the slow pointer will be at the middle
+class LinkedList
+{
+    Node head; // head of linked list
+
+    /* Linked list node */
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    /* Function to print middle of linked list */
+    //Complete this function
+    void printMiddle()
+    {
+        Node slowPtr = head;
+        Node fastPtr = head;
+        while(fastPtr !=null && fastPtr.next != null){
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+
+        }
+        System.out.println(slowPtr.data);
+        //Write your code here
+        //Implement using Fast and slow pointers
+    }
+
+    public void push(int new_data)
+    {
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node;
+    }
+
+    public void printList()
+    {
+        Node tnode = head;
+        while (tnode != null)
+        {
+            System.out.print(tnode.data+"->");
+            tnode = tnode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String [] args)
+    {
+        LinkedList llist = new LinkedList();
+        for (int i=15; i>0; --i)
+        {
+            llist.push(i);
+            llist.printList();
+            llist.printMiddle();
+        }
+    }
+
