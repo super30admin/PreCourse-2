4ce6a21e9943c05cb5870c0b32af7db785956ae8
ripan.halder <ripan.halder@mindera.com>
2021-03-13 23:33:11 -0500

    Completed Exercise 5 - Iterative Quick Sort

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,7 +1,20 @@
-class IterativeQuickSort { 
+// Time Complexity : O(NLogN)
+// Space Complexity : O(N/2)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Had to refer GFG. Did it for the first time
+
+
+// Your code here along with comments explaining your approach
+// Iterative Quick Sort.
+
+class Exercise_5 { 
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+	    //Try swapping without extra variable 
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
     } 
   
     /* This function is same in both iterative and 
@@ -9,12 +22,44 @@ class IterativeQuickSort {
     int partition(int arr[], int l, int h) 
     { 
         //Compare elements and swap.
+        int pivot = arr[h];
+        int i = l -1;
+
+        for(int j = l; j < h; j++){
+            if(arr[j] <= pivot){
+                i++;
+                swap(arr, i , j);
+            }
+        }
+        swap(arr, i+1, h);
+        return i+1;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+        int[] stack = new int[h-l+1];
+
+        int top = -1;
+
+        stack[++top] = l;
+        stack[++top] = h;
+
+        while(top>=0){
+            h = stack[top--];
+            l = stack[top--];
+
+            int pi = partition(arr, l, h);
+            if(pi-1 > l){
+                stack[++top] = l;
+                stack[++top] = pi-1;
+            }
+            if(pi+1 < h){
+                stack[++top] = pi+1;
+                stack[++top] = h;
+            }
+        }
     } 
   
     // A utility function to print contents of arr 
@@ -28,7 +73,7 @@ class IterativeQuickSort {
     // Driver code to test above 
     public static void main(String args[]) 
     { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
+        Exercise_5 ob = new Exercise_5(); 
         int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
         ob.QuickSort(arr, 0, arr.length - 1); 
         ob.printArr(arr, arr.length); 
