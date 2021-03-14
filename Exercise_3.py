2a1d1f28474dfb8f3bf71e963b268dd01416cf12
Prathamesh Kumkar <prathameshkumkar@gmail.com>
2021-03-13 21:08:38 -0800

    Completed PreCourse-2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,50 @@
 # Node class  
-class Node:  
-  
+class Node:
+
     # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
+    def __init__(self, data):
+        self.val = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.curr_ptr = None
+
+    def push(self, new_data):
+        if self.head is None:
+            self.head = Node(new_data)
+            self.curr_ptr = self.head
+        else:
+            new_node = Node(new_data)
+            self.curr_ptr.next = new_node
+            self.curr_ptr = new_node
+
+
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        if not self.head.next:
+            return self.head
+
+        ptr1 = self.head
+        ptr2 = self.head
+
+        while ptr2.next and ptr2.next.next:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next.next
+
+        print("The middle element of the list is", ptr1.val)
 
 # Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+
+
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2)
+list1.push(3)
+list1.push(1)
+list1.printMiddle()
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,50 @@
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+def partition(arr, low, high):
+    # write your code here
+
+    pivot = arr[low]
+    i, j = low, high
+
+    while i < j:
+        while i <= high and arr[i] <= pivot:
+            i += 1
+        while j >= low and arr[j] >= pivot:
+            j -= 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+
+    if j >= low:
+        arr[low], arr[j] = arr[j], arr[low]
+        return j
+
+    return low
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    # write your code here
+    stack = [l, h]
 
+    while stack:
+        y = stack.pop()
+        x = stack.pop()
+
+        p = partition(arr, x, y)
+
+
+        if p - 1 > x:
+            stack.append(x)
+            stack.append(p-1)
+
+        if p + 1 < y:
+            stack.append(p+1)
+            stack.append(y)
+
+# Driver code to test above
+arr = [1, 5, 8, 9, 7, 10]
+n = len(arr)
+quickSortIterative(arr, 0, n - 1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i])
