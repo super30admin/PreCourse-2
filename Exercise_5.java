eafff2a2d72fc45a7c31004d413c70bdcd39e4ca
SaurabhEdge <ss7586@rit.edu>
2021-04-16 20:23:13 -0400

    Updated swap function in quick sort

--- Exercise_5.java
+++ Exercise_5.java
@@ -11,7 +11,7 @@ class IterativeQuickSort {
     void swap(int arr[], int i, int j) 
     { 
 	//Try swapping without extra variable 
-        if(i!=j){
+        if(arr[i]!=arr[j]){
             arr[i] = arr[j] - arr[i];
             arr[j] = arr[j] - arr[i];
             arr[i] = arr[i] + arr[j];
