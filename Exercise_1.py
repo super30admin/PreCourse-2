597ba1e48cdcabbe92dfd243e4433c309a3f5d73
Tanvi <thakurtanvi94@gmail.com>
2021-04-17 21:39:11 -0400

    Completed Sprint Precourse2

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,13 +3,21 @@
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
-# Test array 
+def binarySearch(arr, l, r, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+# Test array
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
   
