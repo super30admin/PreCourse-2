78d0e95ab0a8813bb8910025fe51eed5e5b2e02b
virendra rathore <virendrasingh26894@gmail.com>
2021-04-19 00:10:17 +0530

    Completed PreCourse2 except Quick Sort with iterative method as I am not aware of that

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,17 +1,31 @@
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
+    int binarySearch(int[] arr, int l, int r, int x)
+    {
+
+
         //Write your code here
+        while (l<=r){
+            int mid=(l+r)/2;
+            if (arr[mid]==x){
+                return mid;
+            }else if(x<arr[mid]){
+                r=mid-1;
+            }else if(x>arr[mid]){
+                l=mid+1;
+            }
+        }
+        return -1;
+
     } 
   
     // Driver method to test above 
-    public static void main(String args[]) 
+    public static void main(String[] args)
     { 
         BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
+        int[] arr = { 2, 3, 4, 10, 40 };
         int n = arr.length; 
-        int x = 10; 
+        int x = 100;
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
