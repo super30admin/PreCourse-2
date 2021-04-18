b1a509ee22d6b57637551b071275b1603d26a89c
prajakta jalisatgi <prajaktaganesh.jalisatgi@mavs.uta.edu>
2021-04-17 21:26:46 -0500

    Completed PreCourse-2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,34 @@
+// Slack ID: Prajakta Ganesh Jalisatgi_RN38APR2021
+// Exercise_1 : Binary Search.
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+  
+            // If the element is present at the middle
+            if (arr[mid] == x)
+                return mid;
+  
+            // If element is smaller than mid, then
+            // it can only be present in left subarray
+            if (arr[mid] > x)
+                return binarySearch(arr, l, mid - 1, x);
+  
+            // Else the element can only be present
+            // in right subarray
+            return binarySearch(arr, mid + 1, r, x);
+        }
+  
+        // We reach here when element is not present in an array
+        return -1;
     } 
   
     // Driver method to test above 
