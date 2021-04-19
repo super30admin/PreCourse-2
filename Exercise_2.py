ffb2185f017cad5dd888aaaf5f0bc429dda863e7
Akanksha <kanksha1992@gmail.com>
2021-04-18 19:36:06 -0500

    wip quick sort

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,44 @@
-# Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+# Python program for implementation of Quicksort Sort
+import random
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+# give you explanation for the approach
+def partition(arr, low, high):
+    # pivot_idx = random.randrange(low, high)
+    pivot_idx = low
+    pivot = arr[pivot_idx]
+    swap(arr, pivot_idx, low)
+    curr_idx = high
+
+    while high > low:
+        if arr[high] > pivot:
+            swap(arr, high, curr_idx)
+            high -= 1
+            curr_idx -= 1
+        elif arr[high] < pivot:
+            swap(arr, high, low)
+            low += 1
+        else: # arr[high] == pivot
+            high -= 1
+
+    return curr_idx
+
+# Function to do Quick sort
+def quickSort(arr, low, high):
+    if high <= low:
+        return
+    pivot = partition(arr, low, high)
+    quickSort(arr, low, pivot - 1)
+    quickSort(arr, pivot + 1, high)
+
+# Driver code to test above
+# arr = [10, 7, 8, 9, 1, 5]
+arr = [10, 7, 8, 9, 1, 5, 7, 5]
+n = len(arr)
+quickSort(arr,0,n-1)
+print ("Sorted array is:")
+for i in range(n):
+    print ("%d" %arr[i], end=" ")
+print()
