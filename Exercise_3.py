e6bdbef24bf1d8873bf0097c55708b907a0b8dfb
Akanksha <kanksha1992@gmail.com>
2021-04-18 21:10:24 -0500

    mid element

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,50 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# Node class
+class Node:
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        ptr = self.head
+        while ptr:
+            print(ptr.data, end="->")
+            ptr = ptr.next
+        print()
+
+    # Function to get the middle of
+    # the linked list
+    def printMiddle(self):
+        if not self.head:
+            print("list is empty")
+            return
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        print("middle element is %d" % slow.data)
+
+
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2)
+list1.push(3)
+list1.push(1)
+list1.printList()
+list1.printMiddle()
