1e040a179a5f217fa776d86010d851bb83b7c3e0
sonalgupta276 <sonalgupta276@gmail.com>
2021-05-20 13:20:52 -0500

    Complete Sprint Precourse-2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,39 @@
 # Python program for implementation of Quicksort Sort 
-  
+#  Time Complexity: nlog(n)
+#  Space Complexity: O(1)
 # give you explanation for the approach
+# 1. Take first element as the pivot element.
+# 2. Increment the low pointer until while traversing you find an element greater than pivot.
+# 3. Decrement the high pointer until while traversing you find an element smaller than pivot.
+# 4. If low and high elements don't cross each other, then swap both.
+# 5. If low and high elements cross each other, swap high element with pivot and return high index.
+
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+
+    index = low
+    pivot = arr[index]
+
+    while low < high:
+
+        while low < len(arr) and arr[low] <= pivot:
+            low += 1
+    
+        while arr[high] > pivot:
+            high -= 1
+
+        if low < high:
+            arr[low], arr[high] = arr[high], arr[low]
+
+    arr[index], arr[high] = arr[high], arr[index]
+
+    return high
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
-    #write your code here
+    if low < high:
+        p = partition(arr,low,high)
+        quickSort(arr,low,p-1)
+        quickSort(arr,p+1,high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
@@ -18,6 +41,6 @@ n = len(arr)
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
+    print ("%d" %arr[i], end = " "), 
   
  
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,40 @@
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Approach: Take two pointers, fast and slow. Traverse the linked list by making fast pointer move by two and slow pointer by one.
+# When fast pointer reaches end of linked list, slow pointer will at the middle of linked list.
+
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.next = None
+        self.data = data
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head = None
         
   
     def push(self, new_data): 
-        
+        newNode = Node(new_data)
+        newNode.next = self.head
+        self.head = newNode
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        print("Middle element is ", slow.data)
+
 
 # Driver code 
 list1 = LinkedList() 
