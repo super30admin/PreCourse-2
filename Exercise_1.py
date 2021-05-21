8e009584dd727b733527f37b3eb92fcdfd6a594a
pavankumar.karkekopp <pavankumar.karkekopp@ucalgary.ca>
2021-05-21 16:00:12 -0600

    Completed Binary Search

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,10 +3,44 @@
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
+def binarySearch(arr, l, r, x):
+    #arr: input array
+    #x  : queried element
+    #l  : "leftmost" element
+    #r  : "rightmost" element 
+
+    #By definition of left and right, in a valid searcg array: l<r
+    #Step 1: Iterate until search array shrinks to size 1, i.e, l==r
+    while l<=r:
+        #Step 2: Check if query element (x) is at the midpoint of arr
+        #Floor division operator "//" to find midpoint
+        mid = (l + r) // 2
+        # print(f"mid:{mid}")
+
+        #By law of trichotomy, only 3 possible conditions:
+        #Either x == mid or x > mid or x < mid
+        
+        #Query element at midpoint
+        if(arr[mid] == x):
+            # print("Found")
+            return mid #Query element found
+        
+        #Query element is greater than mid point
+        elif x > arr[mid]:
+            #Drop the search array left of the midpoint
+            #By moving "left" to midpoint + 1
+            l = mid + 1
+            # print(f"l: {l}, r: {r}")
+
+        #Query element has to be lesser than mid point
+        else:
+            #Drop the search array right of the midpoint
+            #By moving "right" to midpoint - 1
+            r = mid - 1
+            # print(f"l: {l}, r: {r}")
+
+    #If conditional loop terminates, element not found
+    return -1  
     
   
 # Test array 
@@ -14,9 +48,12 @@ arr = [ 2, 3, 4, 10, 40 ]
 x = 10
   
 # Function call 
+# The assumption here is that first element is at index 0
+# So array of length "n" has max index = n-1
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    #Changed print statements to Python3 compliant code.
+    print("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
