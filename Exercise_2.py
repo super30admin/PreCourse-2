8e618b2ab5b327ad4debef31a3c17c6c29b017a3
SheetalMore9878 <sheetal.sm212@gmail.com>
2021-05-21 11:17:05 -0700

    Precourse 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -16,8 +16,6 @@ def partition(arr,low,high):
     arr[j], arr[low] = arr[low], arr[j]
     return j
 
-    #write your code here
-
 # Function to do Quick sort 
 def quickSort(arr,low,high):
     if(low<high):
@@ -25,14 +23,11 @@ def quickSort(arr,low,high):
         quickSort(arr,low,j-1)
         quickSort(arr,j+1,high)
 
-    
-    #write your code here
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5]
 n = len(arr) 
 quickSort(arr,0,n-1)
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+    print ("%d" %arr[i]),
+
