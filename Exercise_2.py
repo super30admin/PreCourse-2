32417b589e35dbe1f6006d78388721c10a8b1eb1
rohitsonawane <rohit2790@gmail.com>
2021-05-21 18:48:39 -0700

    Complete Sprint Precourse-2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,20 @@
+# // Time Complexity : O(NlogN)
+# // Space Complexity : O(1)
+# // Did this code successfully run on Leetcode :
+# // Any problem you faced while coding this : No 
+
+
+# // Your code here along with comments explaining your approach
+"""
+1. get the right postition for the ptivot element by calling partition function 
+2. partition function will calculate the pivot index as midpoint of the given array 
+3. swap the pivot element with rightmost element 
+4. traverse through an array compare each element with if its less than right most element swap it with low and each 
+5. at the end swap low with rightmost element , here low is right position for the pivot element return low 
+6. using pivot index value call quick sort for left and right halves of the array Recursively 
+"""
+
+
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
@@ -5,12 +22,27 @@ def partition(arr,low,high):
   
   
     #write your code here
+    pivot = (low + high) //2 
+    arr[pivot] , arr[high] = arr[high] , arr[pivot]
+
+    for each in range(low,high):
+        if arr[each] <= arr[high]:
+            arr[each],arr[low] = arr[low], arr[each]
+            low += 1
+    arr[low], arr[high] = arr[high], arr[low]
+    return low 
+
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
     #write your code here
+    if low >= high:
+        return 
+    pivot = partition(arr, low, high)
+    quickSort(arr, low , pivot -1 )
+    quickSort(arr, pivot +1 , high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,55 @@
+# // Time Complexity : O(NlogN)
+# // Space Complexity : O(N)
+# // Did this code successfully run on Leetcode :
+# // Any problem you faced while coding this : No 
+
+
+# // Your code here along with comments explaining your approach
+"""
+1. calculate mid index of the array , and call mergesort with left and right halves recursively untill length of array is 1 
+2. As single element is sorted , it will start comparing left side and right side array , merge them 
+3. Need to merge residual elements from left or right array 
+"""
+
+
+
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
   
   #write your code here
+  if len(arr) == 1:
+    return 
+  mid = len(arr) // 2
+  leftSub = arr[:mid]
+  rightSub = arr[mid:]
+  mergeSort(leftSub)
+  mergeSort(rightSub)
+
+  i=j=k= 0 
+
+  while i <  len(leftSub) and j < len(rightSub) :
+    if leftSub[i] < rightSub[j]:
+      arr[k] = leftSub[i]
+      i += 1
+    else:
+      arr[k] = rightSub[j]
+      j += 1
+    k += 1
+  
+  while i < len(leftSub):
+    arr[k] = leftSub[i]
+    i +=1 
+    k += 1
+
+  while j < len(rightSub):
+    arr[k] = rightSub[j]
+    j +=1 
+    k += 1
+    
   
 # Code to print the list 
 def printList(arr): 
-    
+    print("Elements in an array {}".format(arr))
     #write your code here
   
 # driver code to test the above code 
