4d33b7456c03b00463db43327067532a6d8bab63
pavankumar.karkekopp <pavan1011@gmail.com>
2021-05-22 16:22:43 -0600

    Fixed comments.

--- Exercise_3.py
+++ Exercise_3.py
@@ -5,11 +5,38 @@ Email       : pavan1011@gmail.com
 S30 SlackID : RN32MAY2021
 =================================
 '''
+#-------------  
+# Explanation:
+#-------------
+
+# Strategy is to use two pointers. 
+
+# One will traverse through the linked list twice as fast as the other. 
+
+# Therefore, the slower pointer will always refer to the midpoint 
+# of the currently traversed part of the linked list
+
+# When the linked list is fully traversed by the faster pointer, the 
+# slower pointer will point to the middle element of the linked list
+
+
+#-----------------
+# Time Complexity:
+#-----------------
+
+# O(n) - Need to traverse the entire linked list of n elements once
+
+#-----------------
+# Space Complexity: 
+#-----------------
+
+# Average case: O(1) - Need to store only two pointers
 
 # Node class  
 class Node:  
     # Function to initialise the node object  
-    # Renamed "next" to next_node since next is a keyword - 
+
+    # Renamed "next" to next_node since next is a keyword
     def __init__(self, data, next_node = None):
 
         # Data module of the linked list node 
@@ -17,7 +44,10 @@ class Node:
         # Pointer to next_node node in the linked list
         self.next_node = next_node 
 
-    #Prints the node and the following linked list
+    #Helper function to print a node in the linked list.
+    # NOTE: Since current node has a next_node
+    #       this method prints the data of the node
+    #       and the following linked list pointed to by the node via next_node
     def __str__(self):
         str_repr = ""
         str_repr = f"{self.data}->"
@@ -32,6 +62,7 @@ class Node:
         
 class LinkedList: 
 
+    # Initialize empty linked list with head pointing to NULL
     def __init__(self): 
         self.head = None
         
@@ -40,7 +71,7 @@ class LinkedList:
         
         # print(f"Pushing data: {new_data}")
         
-        # Create a new node with next = NULL
+        # Create a new node with data = input data and next_node = NULL
         new_node = Node(new_data)
 
         # print(f"    Created new node: {new_node}")
@@ -50,14 +81,14 @@ class LinkedList:
         
         # print(f"    new node: {new_node}")
 
-        # Update head to point to new node
+        # Update head of the linked list to point to new node
         self.head = new_node
         
         # print(f"    Pushed node. Head is now:{self.head}")
 
     
-    # Function to get the middle of  
-    # the linked list 
+    # Function to get the middle of the linked list 
+    # This solution is compliant with leetcode problem #876.
     def printMiddle(self):
         # Initialization
 
@@ -91,6 +122,9 @@ list1.push(4)
 list1.push(2) 
 list1.push(3) 
 list1.push(1)
+
+# Print entire linked list if you call print on head
 print(f"Input: {list1.head}")
 
+# Middle node and the following linked list
 list1.printMiddle() 
--- Exercise_4.py
+++ Exercise_4.py
@@ -6,51 +6,116 @@ S30 SlackID : RN32MAY2021
 =================================
 '''
 
+# 
+# Python program for implementation of MergeSort
 
-# Python program for implementation of MergeSort 
+#-------------  
+# Explanation:
+#-------------
+# MergeSort also uses the Divide and Conquer strategy.
+# Divide: Divide the input array in half and obtain 2 subsequences (n -> n/2)
+# Conquer: Sort the two subsequences recursively
+# Combine: the individually sorted subsequences into a sorted sequence
+#                         
+# As opposed to Quick Sort, the main logic is implemented in the 
+# combine stage rather than the divide stage. 
+
+#-----------------
+# Time Complexity:
+#-----------------
+
+# T(n) = theta(1) if n = 1, <- Constant time to return single element (sorted)
+#      = 2 T(n/2) + theta (n) if n> 1 <- Recursion 
+
+# Therefore, average case: O(n log n) <- Master theorem
+# No worst or best case. 
+
+#-----------------
+# Space Complexity: 
+#-----------------
+# Average case: O(n) - Space inefficient. Quicksort worst case = Mergesort
+
 def mergeSort(arr):
+    # arr: Input array of "n" elements 
+    # Termination condition: Array of 1 element (which is already sorted)
     if len(arr)>1:
         
+        # Obtain the index of the mid element
         mid = len(arr)//2
         
+        # Divide the input array into left and right with n/2 elements each
         left = arr[:mid]
         
         right = arr[mid:]
         
+        # Conquer via recursion
         mergeSort(left)
         
         mergeSort(right)
 
+        # The next stage is to Combine the sorted left and right subsequences
+        
+        # An example of the simplest case: left and right have 1 element each:
+        # - compare element from left to element on right
+        # - combine them s.t. the smallest element added to output array first
+
+        # A non-trivial example: 
+        #   Left and Right subsequences have "l" and "r" elements respectively 
+        #       - Even though Left and Right are sorted, 
+        #       - Combine them s.t the combined list is also sorted
+        
+        # Logic: 
+        # Step 1: Iterate over Left (L) and Right (R). 
+        # Step 2: At every step, compare each element in L with that of R
+        # Step 3: If left is smaller, add to output, else add right to o/p
+        # Step 4: When either L or R runs out of elements,
+        #         if there are any remaining elements in the other array 
+        #         add them to output.
+        # Step 5: Output array is sorted.
+        
+        # Initialize iterators. i-> left, j-> right, k-> output array
         i = j = k = 0
 
+        # Iterate until we run out of elements on either left or right
         while i < len(left) and j < len(right):
+            
+            # compare each element in L with that of R
             if left[i] < right[j]:
+                
+                # If left is smaller, add to output 
                 arr[k] = left[i]
+                
+                # Increment the iterator for left since we added element
+                # from left to output
                 i += 1
+            
             else:
+                # If right is smaller, add right to o/p
                 arr[k] = right[j]
+                
+                # Increment the iterator for right since we added element
+                # from right to output
                 j += 1
+            
+            # Increment the iterator for output at every step since we add
+            # one element at each step
             k += 1
 
+        # Check if any elements are remaining in left
         while i < len(left):
             arr[k] = left[i]
             i += 1
             k += 1
 
+        # Check if any elements are remaining in right
         while j < len(right):
             arr[k] = right[j]
             j += 1
-            k += 1  
+            k += 1 
   
 # Code to print the list 
 def printList(arr):
-    list_str = ""
-    list_str += "["
-    for i in range(len(arr)):
-        list_str += str(arr[i]) + ","
-    list_str = list_str[:len(list_str)-1]
-    list_str += "]"
-    print(list_str)
+    print(arr)
 
   
 # driver code to test the above code 
