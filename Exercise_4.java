11a90c588a4da134dfa5e442a91b591913971507
Snehal Mundhe <m.snehal106@gmail.com>
2021-05-22 19:58:16 -0400

    Completed Exercise 4

--- Exercise_4.java
+++ Exercise_4.java
@@ -5,15 +5,50 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+       int size1 = m - l + 1;
+        int size2 = r - m;
+        int arr1[] = new int[size1];
+        int arr2[] = new int[size2];
+        
+        for (int i = 0; i < size1; ++i)
+            arr1[i] = arr[l + i];
+        for (int j = 0; j < size2; ++j)
+            arr2[j] = arr[m + 1 + j];
+         int i = 0, j = 0;
+         int k = l;
+         while (i < size1 && j < size2) {
+            if (arr1[i] <= arr2[j]) {
+                arr[k] = arr1[i];
+                i++;
+            }
+            else {
+                arr[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < size1) {
+            arr[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while (j < size2) {
+            arr[k] = arr2[j];
+            j++;
+            k++;
+        }
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
     { 
-	//Write your code here
-        //Call mergeSort from here 
+        if (l < r) {
+            int mid = l + (r - l)/2;
+            sort(arr, l, mid);
+            sort(arr, mid + 1, r);
+            merge(arr, l, mid, r);
+        }
     } 
   
     /* A utility function to print array of size n */
