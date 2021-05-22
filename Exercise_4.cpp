2bed4afb44ca45e4f59a61ca5a59587758aebe40
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2021-05-22 09:52:48 -0400

    Completed Precourse_2
    Exercise_2 complete

--- Exercise_4.cpp
+++ Exercise_4.cpp
@@ -7,6 +7,43 @@
 void merge(int arr[], int l, int m, int r) 
 { 
     //Your code here
+    int num_1 = m-l+1;
+    int num_2 = r-m;
+    //the above 2 should add to r-l+1; 
+    int Left_array[num_1];
+    int Right_array[num_2];
+    
+    
+    // Copy data to temp arrays L[] and R[]
+    for (int i = 0; i < num_1; i++)
+        Left_array[i] = arr[l + i];
+    for (int j = 0; j < num_2; j++)
+        Right_array[j] = arr[m + 1 + j];
+    
+        int i = 0;
+ 
+    int j = 0;
+    int k = l;
+ 
+    while (i < num_1 && j < num_2) {
+        if (Left_array[i] <= Right_array[j]) {
+            arr[k] = Left_array[i++];
+        }
+        else {
+            arr[k] = Right_array[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    while (i < num_1) {
+        arr[k++] = Left_array[i++];
+    }
+    
+    while (j < num_2) {
+        arr[k++] = Right_array[j++];
+    }
+    
 } 
   
 /* l is for left index and r is right index of the 
@@ -14,6 +51,18 @@ void merge(int arr[], int l, int m, int r)
 void mergeSort(int arr[], int l, int r) 
 { 
     //Your code here
+    if(l>=r){
+        return;
+    }
+    
+    int mid = l+(r-l)/2;
+    //I did not want to change existing code, else Id put low/hi for better understading
+    // Merges two subarrays of arr[]. 
+    // First subarray is arr[l..m] 
+    // Second subarray is arr[m+1..r] 
+    mergeSort(arr,l,mid);
+    mergeSort(arr,mid+1,r);
+    merge(arr,l,mid,r);
 } 
   
 /* UTILITY FUNCTIONS */
