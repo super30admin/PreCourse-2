c70b4977e513f75a550c6714c48530e5d9a96d9e
Sumit Chavan <chavansumit13@gmail.com>
2021-05-23 04:29:50 +0530

    Completed Sprint PreCourse-2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,21 +1,79 @@
-class BinarySearch { 
-    // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
+// TC : O(log n). This is because after k iterations the size of the array is n/2^k and hence the array becomes half at every iteration
+//SC : For iterative approach : O(1) and for recursive approach O(lon N) due to recursive call stack.
+class BinarySearch {
+    // Returns index of x if it is present in arr[l.. r], else return -1
+
+    //recursive approach for BinarySearch
+    int binarySearch(int[] arr, int left, int right, int x)
+    {
         //Write your code here
-    } 
-  
-    // Driver method to test above 
-    public static void main(String args[]) 
-    { 
-        BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
-        int n = arr.length; 
-        int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
-        if (result == -1) 
-            System.out.println("Element not present"); 
+        if(right>=left)
+        {
+            //mid = (left+right)/2 but we use this expression to avoid integer overflow
+            int mid = (right-left)/2 + left;
+
+            //target element is smaller than the middle element that means, the target element lies in the left half
+            if(x<arr[mid])
+                return binarySearch(arr,left, mid-1,x);
+
+                //target element is greater than the middle element that means it lies in the second half of the array
+            else if(x>arr[mid])
+                return binarySearch(arr,mid+1, right,x);
+            else
+                return mid;
+        }
+
+        //if the target element does not belong to the array then return -1
+        return -1;
+
+    }
+
+
+    //iterative approach to binarySearch
+    int binarySearch(int[] arr,int x)
+    {
+        int left=0, right = arr.length-1;
+
+        while (left<=right)
+        {
+            //mid = (left+right)/2 but we use this expression to avoid integer overflow
+            int mid = (right-left)/2 + left;
+
+            //target element is smaller than the middle element that means, the target element lies in the left half
+            if(x < arr[mid])
+                right = mid -1;
+
+                //target element is greater than the middle element that means it lies in the second half of the array
+            else if(x >arr[mid])
+                left = mid +1;
+            else
+                return mid;
+
+        }
+
+        //if the target element does not belong to the array then return -1
+        return -1;
+
+    }
+
+    // Driver method to test above
+    public static void main(String args[])
+    {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 10;
+        int result = ob.binarySearch(arr, 0, n - 1, x);
+        if (result == -1)
+            System.out.println("Element not present");
         else
-            System.out.println("Element found at index " + result); 
-    } 
-} 
+            System.out.println("Element found at index " + result);
+
+        x = 3;
+        result = ob.binarySearch(arr, x);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,53 +1,71 @@
-class LinkedList 
-{ 
-    Node head; // head of linked list 
-  
-    /* Linked list node */
-    class Node 
-    { 
-        int data; 
-        Node next; 
-        Node(int d) 
-        { 
-            data = d; 
-            next = null; 
-        } 
-    } 
-  
-    /* Function to print middle of linked list */
-   //Complete this function
-    void printMiddle() 
-    { 
-        //Write your code here
-	//Implement using Fast and slow pointers
-    } 
-  
-    public void push(int new_data) 
-    { 
-        Node new_node = new Node(new_data); 
-        new_node.next = head; 
-        head = new_node; 
-    } 
+// TC : O(n/2)
+// SC : O(1)
+class LinkedList
+{
+    Node head; // head of linked list
 
-    public void printList() 
-    { 
-        Node tnode = head; 
-        while (tnode != null) 
-        { 
-            System.out.print(tnode.data+"->"); 
-            tnode = tnode.next; 
-        } 
-        System.out.println("NULL"); 
-    } 
-  
-    public static void main(String [] args) 
-    { 
-        LinkedList llist = new LinkedList(); 
-        for (int i=15; i>0; --i) 
-        { 
-            llist.push(i); 
-            llist.printList(); 
-            llist.printMiddle(); 
-        } 
-    } 
-} 
+    /* Linked list node */
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    /* Function to print middle of linked list */
+    //Complete this function
+    void printMiddle()
+    {
+        //Write your code here
+        //Implement using Fast and slow pointers
+
+        Node slow = head;
+        Node fast = head;
+
+        if (head != null)
+        {
+            //incase the fast pointer reaches last element or goes beyond exit
+            while (fast != null && fast.next != null)
+            {
+
+                //fast pinter moves ahead by two steps, slow pointer by single step
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            System.out.println("The middle element is [" + slow.data + "] \n");
+        }
+    }
+
+    public void push(int new_data)
+    {
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node;
+    }
+
+    public void printList()
+    {
+        Node tnode = head;
+        while (tnode != null)
+        {
+            System.out.print(tnode.data+"->");
+            tnode = tnode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String [] args)
+    {
+        LinkedList llist = new LinkedList();
+        for (int i=15; i>0; --i)
+        {
+            llist.push(i);
+            llist.printList();
+            llist.printMiddle();
+        }
+    }
+}
