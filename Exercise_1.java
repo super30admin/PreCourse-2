13973dfc33677938671418ef784ff310a371d1cc
krishnaattapu <krishna.attapu@gmail.com>
2021-05-22 17:45:27 -0500

    Pre Course 2 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,21 +1,43 @@
+/**
+ *	Time Complexity : O(logn)
+ *
+ * 1. Binary search input array should be sorted
+ * 2. If array size is zero return -1
+ * 3. find mid element of array and compare with search element
+ * 4. if greater search right tree otherwise search left tree
+ */
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+    	if(r >= l) {
+    		int mid= l+(r-1)/2;
+    		if(arr[mid]==x) {
+    			return mid;
+    		} else if(arr[mid]> x) {
+    			return binarySearch(arr, l, mid-1, x);
+    		}else {
+    			return binarySearch(arr, mid+1, r, x);
+    		}
+    	}
+    	return -1;
     } 
   
-    // Driver method to test above 
-    public static void main(String args[]) 
-    { 
-        BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
-        int n = arr.length; 
-        int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
-        if (result == -1) 
-            System.out.println("Element not present"); 
-        else
-            System.out.println("Element found at index " + result); 
-    } 
 } 
+
+public class Exercise_1{
+	// Driver method to test above 
+	public static void main(String args[]) 
+	{ 
+		BinarySearch ob = new BinarySearch(); 
+		int arr[] = { 2, 3, 4, 10, 40 }; 
+		int n = arr.length; 
+		int x = 10; 
+		int result = ob.binarySearch(arr, 0, n - 1, x); 
+		if (result == -1) 
+			System.out.println("Element not present"); 
+		else
+			System.out.println("Element found at index " + result); 
+	} 
+	
+}
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,3 +1,11 @@
+/**
+ *	Time Complexity : O(n)
+ *
+ * 1. initialize midIndex to starting index
+ * 2. when index is odd move midIndex by one
+ * 3. print the midIndex item from the LinkedList
+ */
+
 class LinkedList 
 { 
     Node head; // head of linked list 
@@ -18,8 +26,23 @@ class LinkedList
    //Complete this function
     void printMiddle() 
     { 
-        //Write your code here
-	//Implement using Fast and slow pointers
+    	int midIndex = 0;
+        Node midElement = head;
+        
+        Node headTemp = head;
+        
+     
+        while (headTemp != null)
+        {
+            if ((midIndex % 2) == 1)
+            	midElement = midElement.next;
+     
+            ++midIndex;
+            headTemp = headTemp.next;
+        }
+     
+        if (midElement != null)
+            System.out.println("middle element="+midElement.data);
     } 
   
     public void push(int new_data) 
@@ -40,14 +63,17 @@ class LinkedList
         System.out.println("NULL"); 
     } 
   
-    public static void main(String [] args) 
-    { 
-        LinkedList llist = new LinkedList(); 
-        for (int i=15; i>0; --i) 
-        { 
-            llist.push(i); 
-            llist.printList(); 
-            llist.printMiddle(); 
-        } 
-    } 
 } 
+public class Exercise_3{
+	public static void main(String [] args) 
+	{ 
+		LinkedList llist = new LinkedList(); 
+		for (int i=15; i>0; --i) 
+		{ 
+			llist.push(i); 
+			llist.printList(); 
+			llist.printMiddle(); 
+		} 
+	} 
+	
+}
