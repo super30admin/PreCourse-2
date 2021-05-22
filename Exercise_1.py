4fd59a945191625de69e647534072ecb13bbd1c7
unknown <yangd01234567@gmail.com>
2021-05-22 10:39:02 -0700

    completed precourse-2 python

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,9 +4,20 @@
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
+    '''
+    Time Complexity: O(log n)
+    Space Complexity: O(1)
+    '''
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid] == x: return mid
+        print(arr[mid])
+        if x < arr[mid]:
+            r = mid - 1
+        else:
+            l = mid + 1
+
+    return -1
     
   
 # Test array 
@@ -17,6 +28,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,36 @@
 # Python program for implementation of Quicksort Sort 
-  
+'''
+Time complexity: O(n)
+Space complexity O(n)
+'''
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
+    
+    # we need to make sure to return the point of partition
+    pivot_point = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot_point:
+            # you need to swap out i first before assigning
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    # one last pass
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+
+    return i+1
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
+    if len(arr) == 1: return arr
+
     #write your code here
+    if low < high:
+        pivot = partition(arr, low, high)
+        quickSort(arr, low, pivot-1)
+        quickSort(arr, pivot+1, high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
@@ -18,6 +38,6 @@ n = len(arr)
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
+    print("%d" % arr[i])
   
  
