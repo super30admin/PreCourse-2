e4e1fd7b1f9d4e47ba07e79ca317a2e4a6e67255
Yash Dhawan <84486109+yashdhawan-FSD@users.noreply.github.com>
2021-05-22 16:35:47 -0400

    yash_precourse02: Exercise_1

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,6 +3,19 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        if(r>1){
+        int mid = l+(r-1)/2;
+        if(arr[mid]==x){
+            return mid;
+        }
+        if(arr[mid]>x){
+            return binarySearch(arr, l, mid-1, x);
+        }
+        if (arr[mid]<x){
+            return binarySearch(arr, mid+1, r, x);
+        }
+    }   
+        return -1;
     } 
   
     // Driver method to test above 
