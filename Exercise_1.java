ce768e7916f32cf15823ccfd3d29b87fe586c1dc
Kavya Gupta <gupta.kavya@gmail.com>
2021-05-22 00:15:56 -0700

    Completed Sprint PreCourse 2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,21 +1,38 @@
-class BinarySearch { 
-    // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
-    } 
-  
-    // Driver method to test above 
-    public static void main(String args[]) 
-    { 
-        BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
-        int n = arr.length; 
-        int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
-        if (result == -1) 
-            System.out.println("Element not present"); 
-        else
-            System.out.println("Element found at index " + result); 
-    } 
-} 
+// Time Complexity : O(LogN)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this : No
+
+class BinarySearch {
+  // Returns index of x if it is present in arr[l.. r], else return -1
+
+  //Considering the input provided is sorted
+  int binarySearch(int arr[], int l, int r, int x)
+  {
+    while(l < r){
+      int mid = (l+r)/2;
+
+      if(arr[mid] == x)
+        return mid;
+      else if(arr[mid] < x)
+        return binarySearch(arr, mid + 1, r, x);
+      else
+        return binarySearch(arr, l, mid - 1, x);
+    }
+    return -1;
+  }
+
+  // Driver method to test above
+  public static void main(String args[])
+  {
+    BinarySearch ob = new BinarySearch();
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int n = arr.length;
+    int x = 10;
+    int result = ob.binarySearch(arr, 0, n - 1, x);
+    if (result == -1)
+      System.out.println("Element not present");
+    else
+      System.out.println("Element found at index " + result);
+  }
+}
