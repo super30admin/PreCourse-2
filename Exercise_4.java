f48b14ac56446864a0c02f1be5af6ab10c465279
sumit-chavan <avantidorle24@gmail.com>
2021-05-23 09:43:30 +0530

    Completed precourse 2 please review

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,5 @@
+//Time Complexity :O(nLogn) 
+//Space Complexity : O(n) 
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -5,15 +7,64 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+        // Sizes of the 2 sub arrays to merge
+        int s1 = m - l + 1;
+        int s2 = r - m;
+ 
+        int left[] = new int[s1];
+        int right[] = new int[s2];
+ 
+        for (int i = 0; i < s1; ++i)
+            left[i] = arr[l + i];
+        for (int j = 0; j < s2; ++j)
+            right[j] = arr[m + 1 + j];
+ 
+        //Merge 2 arrays
+        int i = 0, j = 0;
+ 
+        int k = l;
+        while (i < s1 && j < s2) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            }
+            else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        // Copy remaining elements of left[] if any
+        while (i < s1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+ 
+        // Copy remaining elements of right[] if any
+        while (j < s2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
     { 
-	//Write your code here
-        //Call mergeSort from here 
+    	 if (l < r) {
+            // Find the middle index
+            int m =l+(r-l)/2;
+ 
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+ 
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
     } 
   
     /* A utility function to print array of size n */
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,20 +1,62 @@
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     } 
   
     /* This function is same in both iterative and 
        recursive*/
     int partition(int arr[], int l, int h) 
     { 
-        //Compare elements and swap.
+        int pivot = arr[h];     
+        int i = (l - 1);
+ 
+        for(int j = l; j <= h - 1; j++)
+        {
+            if (arr[j] <= pivot)
+            {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, h);
+        return (i + 1);
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
-        //Try using Stack Data Structure to remove recursion.
+        int[] stack = new int[h - l + 1];
+  
+        // Initialize top of stack
+        int top = -1;
+  
+        // Push initial values of l and h to stack
+        stack[++top] = l;
+        stack[++top] = h;
+  
+        // Pop from the stack untill it is empty
+        while (top >= 0) {
+            // Pop h and l
+            h = stack[top--];
+            l = stack[top--];
+  
+            int p = partition(arr, l, h);
+  
+            // If there are elements on left side of pivot, then push left side to stack
+            if (p - 1 > l) {
+                stack[++top] = l;
+                stack[++top] = p - 1;
+            }
+  
+            // If there are elements on right side of pivot, then push right side to stack
+            if (p + 1 < h) {
+                stack[++top] = p + 1;
+                stack[++top] = h;
+            }
+        }
     } 
   
     // A utility function to print contents of arr 
