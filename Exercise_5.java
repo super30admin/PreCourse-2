2169aa3047843142f8829685f4543ccded1a58a9
Sumit Chavan <chavansumit13@gmail.com>
2021-05-23 13:05:45 +0530

    Added the 5th file

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,20 +1,80 @@
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+	//Try swapping without extra variable
+
+        //Your code here
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     } 
   
     /* This function is same in both iterative and 
        recursive*/
-    int partition(int arr[], int l, int h) 
+    int partition(int arr[], int low, int high)
     { 
         //Compare elements and swap.
+
+        //choose last element as pivot
+        int pivot = arr[high];
+
+        //this indicates the most recent smallest element found so far and also the right position of pivot
+        int i = low-1;
+
+        for(int j=low; j<=high-1;j++)
+        {
+
+            //if the current element is smaller than the pivot, that means it should be moved to the correct index (the index that has the elements smaller than the pivot until now)
+            if(arr[j]<pivot)
+            {
+                i++;
+                swap(arr,i,j);
+            }
+
+        }
+
+        //at the last swap the pivot with i+1 index element as that is its correct location
+        swap(arr,i+1,high);
+        return(i+1);
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+
+        // Create an auxiliary stack
+        int[] stack = new int[h - l + 1];
+
+        // initialize top of stack
+        int top = -1;
+
+        // push initial values of l and h to stack
+        stack[++top] = l;
+        stack[++top] = h;
+
+        // Keep popping from stack while is not empty
+        while (top >= 0) {
+            // Pop h and l
+            h = stack[top--];
+            l = stack[top--];
+
+            // Set pivot element at its correct position in the sorted array
+            int p = partition(arr, l, h);
+
+            // If there are elements on left side of pivot, then push left side to stack
+
+            if (p - 1 > l) {
+                stack[++top] = l;
+                stack[++top] = p - 1;
+            }
+
+            // If there are elements on right side of pivot, then push right side to stack
+            if (p + 1 < h) {
+                stack[++top] = p + 1;
+                stack[++top] = h;
+            }
+        }
     } 
   
     // A utility function to print contents of arr 
