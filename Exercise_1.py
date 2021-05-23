68854e4d57de9daaa663644d2dac6f8a7699abe0
Devarsh Amin <devarhamin@gmail.com>
2021-05-23 02:34:16 -0500

    completed sprint precourse 2

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,20 +3,40 @@
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
+def binarySearch(arr, l, r, x):
+
+        if (l>r):
+            return -1
+
+
+        m=int((l+r)/2)
+        if (arr[m]==x):
+
+            return m
+
+        elif arr[m]<x:
+          return  binarySearch(arr,l+1,r,x)
+
+        else:
+           return binarySearch(arr,l,r-1,x)
+
+
   
   #write your code here
   
     
   
-# Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
-  
-# Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
-if result != -1: 
-    print "Element is present at index % d" % result 
-else: 
-    print "Element is not present in array"
+# Test array
+def m():
+    arr = [ 2, 3, 4, 10, 40 ]
+    x = 10
+
+    # Function call
+    result = binarySearch(arr, 0, len(arr)-1, x)
+
+
+    if result != -1:
+        print (f"Element is present at index {result}")
+    else:
+        print ("Element is not present in array")
+m()
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,64 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
+    piv=low
+    i,j=low,high
+    while i<j:
+
+        while arr[i]<arr[piv]:
+            i+=1
+        while arr[j]>arr[piv]:
+            j-=1
+        arr[i],arr[j]=arr[j],arr[i]
+    arr[piv],arr[j]=arr[j],arr[piv]
+    return j
+
+
+
+    #write your code here
+
   #write your code here
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+
+    size = h - l + 1
+    stack = [ 0 ] * (size)
+
+
+    top = -1
+
+
+    top = top + 1
+    stack[ top ] = l
+    top = top + 1
+    stack[ top ] = h
+
+
+    while top >= 0:
+
+
+        h = stack[ top ]
+        top = top - 1
+        l = stack[ top ]
+        top = top - 1
+
+
+
+        p = partition( arr, l, h )
+
+        if p - 1 > l:
+            top = top + 1
+            stack[ top ] = l
+            top = top + 1
+            stack[ top ] = p - 1
+
+       
+        if p + 1 < h:
+            top = top + 1
+            stack[ top ] = p + 1
+            top = top + 1
+            stack[ top ] = h
+
+
 
