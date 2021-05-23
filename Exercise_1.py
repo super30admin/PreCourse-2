28c1cba732ab65cd1dd2191fac53e35cca196c32
XDsubhash <subhashxd@gmail.com>
2021-05-22 20:58:55 -0700

    Completed Spring Precourse2 - please review
    Signed-off-by: XDsubhash <subhashxd@gmail.com>

--- Exercise_1.py
+++ Exercise_1.py
@@ -5,10 +5,18 @@
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
   
-  #write your code here
-  
+  while l <= r:
+    mid = (l+r)//2
+    if arr[mid]==x:
+      return mid
+    elif arr[mid]>x:
+      r = mid-1
+    else:
+      l = mid+1
+  if l>r:
+    return -1
     
-  
+ 
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
@@ -17,6 +25,12 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
+
+"""
+the complexity of the binary search tree is O(logn) 
+as we divide the array into two halves and search 
+for the element in a single half.
+""" 
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,66 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+   #  take the last element as pivot and
+    #  place the elements less than pivot on left side and
+    #  all elements greater than pivot on right side of it in the array
+    pivot = arr[h]
+    #lets say index of smallest element be low-1
+    k = l -1
+    for j in range(l, h):
+        if arr[j]<pivot:
+            k += 1
+            arr[k], arr[j] = arr[j], arr[k]
+        # print(arr)
+    arr[k+1], arr[h] = arr[h], arr[k+1]
+    return (k+1)
+
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+  size = h-l+1
+  arr1 = [0]*(size)
+  top = -1
 
+  top = top+1
+  arr1[top]=l
+  top = top+1
+  arr1[top]=h
+  print(arr1)
+  
+  while top>=0:
+    h = arr1[top]
+    top = top-1
+    l = arr1[top]
+    top = top-1
+    print(arr1)
+
+    p = partition(arr, l, h)
+
+    if p-1 >l:
+      top = top+1
+      arr1[top]=l
+      top = top+1
+      arr1[top]=p-1
+      print(arr1)
+
+    if p+1 < h:
+      top = top+1
+      arr1[top]=p+1
+      top = top+1
+      arr1[top]=h
+      print(arr1)
+
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
+
+    
+
+"""
+Quick sort has O(nlogn) complexity in the best case where median element is selected as pivot.
+it has O(n^2) complexity when smallest or largest element is picked as pivot.
+"""
