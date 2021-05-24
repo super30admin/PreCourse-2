5f3a6897ae609298b71adef5792b2ba45059c597
mahesh-9 <umamaheshnov9@gmail.com>
2021-05-23 22:26:34 -0500

    Complete all Precourse-2 java exercises

--- Exercise_1.java
+++ Exercise_1.java
@@ -2,6 +2,20 @@ class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
+        int mid;
+        while(l<=r){
+            mid = l + (r-l)/2;
+            if(arr[mid] == x){
+                return mid;
+            }
+            else if(arr[mid] < x){
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+        return -1;
         //Write your code here
     } 
   
--- Exercise_4.java
+++ Exercise_4.java
@@ -5,7 +5,33 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+       //Your code here
+       int[] arr2 = new int[arr.length];
+       System.arraycopy(arr, 0, arr2, 0, arr.length);
+       int i = l, k = l, j = m + 1;
+       while (i <= m && j <= r) {
+           if (arr2[i] <= arr2[j]) {
+               arr[k] = arr2[i];
+               k++;
+               i++;
+           } else {
+               arr[k] = arr2[j];
+               k++;
+               j++;
+           }
+       }
+
+       while (i <= m) {
+           arr[k] = arr2[i];
+           k++;
+           i++;
+       }
+
+       while (j <= r) {
+           arr[k] = arr2[j];
+           k++;
+           j++;
+       } 
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -13,7 +39,13 @@ class MergeSort
     void sort(int arr[], int l, int r) 
     { 
 	//Write your code here
-        //Call mergeSort from here 
+        //Call mergeSort from here
+        if(l<r){
+            int mid = l +(r-l)/2;
+            sort(arr,l,mid);
+            sort(arr,mid+1,r);
+            merge(arr,l,mid,r);
+        } 
     } 
   
     /* A utility function to print array of size n */
