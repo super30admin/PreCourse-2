f9b97326efdbee8ab3ac9b851c78a69d56fe8e5f
Arjun Hegde <arjunhegde372@gmail.com>
2021-05-25 18:12:20 -0500

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,20 @@
+//Time Complexity: O(logN)
+//Space Complexity: O(1)
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+        while (l <= r) {
+            int m = l + (r - l) / 2;  // to avoid overflow
+             if (arr[m] == x)
+                return m;
+             if (arr[m] < x)
+                l = m + 1;
+             else
+                r = m - 1;
+        }
+        return -1;
     } 
   
     // Driver method to test above 
