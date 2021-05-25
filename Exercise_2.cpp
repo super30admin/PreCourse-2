236887a65548f8b4dc25400f967c480df53cab5c
Zaiba Fathima <zaibafathima999@gmail.com>
2021-05-24 20:04:01 -0700

    PreCourse-2 completed

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,10 +1,15 @@
-#include <bits/stdc++.h> 
+/* Quick sort. */
+
+#include <iostream> 
 using namespace std;  
   
 // A utility function to swap two elements  
 void swap(int* a, int* b)  
 {  
     //Your Code here 
+    int t = *a;  
+    *a = *b;  
+    *b = t; 
 }  
   
 /* This function takes last element as pivot, places  
@@ -15,6 +20,21 @@ of pivot */
 int partition (int arr[], int low, int high)  
 {  
     //Your Code here 
+    int pivot = arr[high]; // pivot  
+    int i = (low - 1); // Index of smaller element  
+  
+    for (int j = low; j <= high - 1; j++)  
+    {  
+        // If current element is smaller than the pivot  
+        if (arr[j] < pivot)  
+        {  
+            i++; // increment index of smaller element  
+            swap(&arr[i], &arr[j]);  
+        }  
+    }  
+    swap(&arr[i + 1], &arr[high]);  
+    return (i + 1);  
+
 }  
   
 /* The main function that implements QuickSort  
@@ -24,6 +44,17 @@ high --> Ending index */
 void quickSort(int arr[], int low, int high)  
 {  
     //Your Code here 
+    if (low < high)  
+    {  
+        /* pi is partitioning index, arr[p] is now  
+        at right place */
+        int pi = partition(arr, low, high);  
+  
+        // Separately sort elements before  
+        // partition and after partition  
+        quickSort(arr, low, pi - 1);  
+        quickSort(arr, pi + 1, high);  
+    }  
 }  
   
 /* Function to print an array */
