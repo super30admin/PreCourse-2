bd0593337e5358bf7cf2d62ccd19468ed5038afc
Seetha Yanamandra <s_yanamandra@apple.com>
2021-05-24 18:08:59 -0700

    Completed Sprint PreCourse2

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,42 +1,96 @@
 class MergeSort 
-{ 
-    // Merges two subarrays of arr[]. 
-    // First subarray is arr[l..m] 
-    // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
-    } 
-  
-    // Main function that sorts arr[l..r] using 
-    // merge() 
-    void sort(int arr[], int l, int r) 
-    { 
-	//Write your code here
-        //Call mergeSort from here 
-    } 
-  
+{
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r)
+    {
+        //Your code here
+        // Create 2 sub-arrays out of arr[]  by finding the sizes
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create 2 sub-arraays using the calculated size
+        int leftArray[] = new int[n1];
+        int rightArray[] = new int[n2];
+
+        //copy data to the sub-arrays
+        for (int i = 0; i < n1; i++)
+            leftArray[i] = arr[l + i];
+        for (int j = 0; j < n2; j++)
+            rightArray[j] = arr[m + 1 + j];
+
+        // Initial indexes of first and second sub-arrays
+        int i = 0, j = 0;
+
+        // Initial index of merged sub-array array
+        int k = l;
+        while (i < n1 && j < n2) {
+            //compare leftArray[i] with rightArray[j] i
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            }
+            else {
+                arr[k] = rightArray[j];
+                j++;            }
+            k++;
+        }
+
+        // Copy if there are any remaining elements left in leftArray[] to arr[k]
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Copy if there are any remaining elements in  right array to arr
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sort(int arr[], int l, int r)
+    {
+        //Write your code here
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first half using start(left) to middle
+            sort(arr, l, m);
+            //Sort second half from middle+1 as middle already done to end(right)
+            sort(arr, m + 1, r);
+            //Call mergeSort from here
+            merge(arr, l, m, r);
+        }
+    }
+
     /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
-  
-    // Driver method 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
-  
-        System.out.println("Given Array"); 
-        printArray(arr); 
-  
-        MergeSort ob = new MergeSort(); 
-        ob.sort(arr, 0, arr.length-1); 
-  
-        System.out.println("\nSorted array"); 
-        printArray(arr); 
-    } 
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String args[])
+    {
+        int arr[] = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Given Array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length-1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
 } 
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,85 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
-    /* This function is same in both iterative and 
+class IterativeQuickSort {
+    void swap(int arr[], int i, int j)
+    {
+        //Try swapping without extra variable
+        arr[i] = (arr[i] + arr[j]) - (arr[j] = arr[i]);
+    }
+
+    /* This function is same in both iterative and
        recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
+    int partition(int arr[], int l, int h)
+    {
         //Compare elements and swap.
-    } 
-  
-    // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
+        //Write code here for Partition and Swap
+        int pivot = arr[h];
+        int i = l -1;
+        for(int j = l; j <=h-1; j++) {
+            if(arr[j] <= pivot) {
+                i++;
+                swap (arr, i, j);
+            }
+        }
+        // By now the elements which are up  to i position on left hand side are guaranteed that they are smaller than pivot
+        // And the elements from i to the h-1 position are greater than pivot.
+        // We know that pivot's location is i+1 position in the array.  So now swap the elements at i+1 and h.
+        // By this step the pivot is at right location in the array
+        swap(arr, i+1, h);
+        return i+1;
+    }
+
+    // Sorts arr[l..h] using iterative QuickSort
+    void QuickSort(int arr[], int l, int h)
+    {
         //Try using Stack Data Structure to remove recursion.
-    } 
-  
-    // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
-    // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
+        // Create an auxiliary  array
+        int[] stack = new int[h - l + 1];
+
+        // initialize top of stack
+        int top = -1;
+
+        // push initial values of l and h to stack
+        stack[++top] = l;
+        stack[++top] = h;
+
+        // Keep popping from stack while is not empty
+        while (top >= 0) {
+            // Pop h and l
+            h = stack[top--];
+            l = stack[top--];
+
+            // Call the partition to set the pivot in correct position
+            // get the pivot position in p
+            int p = partition(arr, l, h);
+
+            // Now check and push the l and pivot position-1 positions to stack,
+            if (p - 1 > l) {
+                stack[++top] = l;
+                stack[++top] = p - 1;
+            }
+
+            // Now check and push the h and pivot position+1 positions to stack,
+            if (p + 1 < h) {
+                stack[++top] = p + 1;
+                stack[++top] = h;
+            }
+        }
+
+    }
+
+    // A utility function to print contents of arr
+    void printArr(int arr[], int n)
+    {
+        int i;
+        for (i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
+    // Driver code to test above
+    public static void main(String args[])
+    {
+        IterativeQuickSort ob = new IterativeQuickSort();
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 };
+        ob.QuickSort(arr, 0, arr.length - 1);
+        ob.printArr(arr, arr.length);
+    }
 } 
