5bd78c2023aff4bd967537945080bad76c21dedd
Shashank Singh <shashank.singh9193@gmail.com>
2021-05-26 02:32:05 -0700

    Completed Precourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,9 +1,25 @@
+// Time complexity: O(logn)
+// Space Complexity: O(1)
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
-    } 
+        while(l <= r) {
+            int mid = l + (r - l)/2;
+
+            if(arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] > x) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return -1;
+    }
   
     // Driver method to test above 
     public static void main(String args[]) 
@@ -11,7 +27,7 @@ class BinarySearch {
         BinarySearch ob = new BinarySearch(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
-        int x = 10; 
+        int x = 2; 
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,6 @@
+// Time Complexity: O(nlogn)
+// Space Complexity: O(n)
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -5,15 +8,73 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+        //Your code here
+
+        int left_length = m - l + 1;
+        int right_length = r - m;
+
+        int[] left = new int[left_length];
+        int[] right = new int[right_length];
+
+        // construct the left and right array
+        for (int i=0;i<left_length;i++) {
+            left[i] = arr[l+i];
+        }
+
+        for(int j=0;j<right_length;j++) {
+            right[j] = arr[j + m  + 1];
+        }
+
+        // Merge the two aubarrays and modify the original Array
+        int a = 0;
+        int b = 0;
+
+        int k = l;
+
+        while (a < left_length && b < right_length) {
+            if (left[a] <= right[b]) {
+                arr[k] = left[a];
+                a++;
+                k++;
+            } else {
+                arr[k] = right[b];
+                b++;
+                k++;
+            }
+        }
+
+        while (a < left_length) {
+            arr[k++] = left[a++];
+        }
+
+        while (b < right_length) {
+            arr[k++] = right[b++];
+        }
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
     { 
-	//Write your code here
-        //Call mergeSort from here 
+        //Write your code here
+
+        // Base case
+        // if there is only one element, array is considered sorted
+        if (r-l < 1) {
+            return;
+        }
+
+        //Call mergeSort from here
+
+        // Divide the array into two halves
+        int mid = l + (r - l)/2;
+
+        // Call sort function recursively for left and right array
+        sort(arr, l, mid);
+        sort(arr, mid+1, r);
+
+        // Call merge function to merge the left and right array into original array
+        merge(arr, l, mid, r);
     } 
   
     /* A utility function to print array of size n */
