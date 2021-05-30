afa4ea4a523a05a5225e119b16f32d481f9163b1
AdithyaHolla <56132230+AdithyaHolla@users.noreply.github.com>
2021-05-29 20:42:48 -0700

    Update Exercise_4.py

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,10 +1,52 @@
+"""
+Time Complexity : O(nlog(n))
+Space Complexity : O(n)
+"""
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
+    
+    if len(arr) == 1:
+        return arr 
+    
+    elif len(arr) > 1:
+         mid = len(arr)//2
+         left = arr[:mid]
+         right = arr[mid:]
+         
+         mergeSort(left)
+         mergeSort(right)
+         
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+        
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i += 1
+             
+        else:
+            arr[k] = right[j]
+            j += 1 
+                
+        k += 1 
+        
+    while i < len(left):
+        arr[k] = left[i]
+        k += 1 
+        i += 1 
+        
+    while j < len(right):
+        arr[k] = right[j]
+        k += 1 
+        j += 1 
+  
   
   #write your code here
   
 # Code to print the list 
 def printList(arr): 
+    for i in arr:
+        print(i, end = " ")
+    print()
     
     #write your code here
   
