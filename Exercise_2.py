001eb7415d96fde0e8c9244a144982378b2ae6c0
AdithyaHolla <56132230+AdithyaHolla@users.noreply.github.com>
2021-05-29 20:42:44 -0700

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,14 +1,31 @@
+"""
+Time Complexity : O(nlog(n)) when pivot is accuretly(always middle element) picked and O(n^2) when array is sorted 
+Space Complexity : O(n) - recursive stack space 
+"""
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
     #write your code here
-  
+    piv = arr[high]
+    i = low - 1 
+    
+    for j in range(low, high):
+        if arr[j] < piv:
+            i += 1 
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[high], arr[i+1] = arr[i+1], arr[high]
+    return i + 1
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
+def quickSort(arr,low,high):
+    if low >= high:
+        return 
+    
+    pivot = partition(arr, low, high)
+    
+    quickSort(arr,low, pivot - 1)
+    quickSort(arr,pivot + 1, high)
     
     #write your code here
   
@@ -18,6 +35,6 @@ n = len(arr)
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
+    print ("%d" %arr[i], end = " "), 
   
  
