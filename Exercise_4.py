34ed4d60c84ba0ac65646dd527570a1f0d9e7e11
sushmakallapur <sushma.kallapur@gmail.com>
2021-05-31 12:41:23 -0700

    Sprint Precourse 2 completed

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,47 @@
-# Python program for implementation of MergeSort 
+# Python program for implementation of MergeSort
 def mergeSort(arr):
-  
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
-    #write your code here
-  
-# driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+  if len(arr) > 1:
+      mid = len(arr) // 2  # Find mid Element
+      left = arr[:mid]
+      right = arr[mid:]
+
+      mergeSort(left)  #Sort the left side
+      mergeSort(right) # Sort the right Side
+
+      i = j = k = 0
+
+      while i < len(left) and j < len(right):
+          if left[i] < right[j]:
+              arr[k] = left[i]
+              i += 1
+          else:
+              arr[k] = right[j]
+              j += 1
+          k += 1
+
+#Checking if any more Elements are left to be sorted towards left and right
+      while i < len(left):
+          arr[k] = left[i]
+          i += 1
+          k += 1
+
+      while j < len(right):
+          arr[k] = right[j]
+          j += 1
+          k += 1
+
+# Code to print the list
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end= " ")
+    print()
+
+# driver code
+if __name__ == '__main__':
+    arr =[5, 4, 6, 3, 2, 5, 4, 2,8]
+    print ("Given array is", end=" ")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is: ", end=" ")
+    printList(arr)
+
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,57 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+  i = l - 1
+  pivot = arr[h]  # Assign last element as pivot value and then compare it with the rest of the array elements
+
+  for j in range(l, h):
+      if(arr[j] <= pivot):
+          i += 1
+          arr[i], arr[j] = arr[j], arr[i]
+  arr[i + 1], arr[h] = arr[h], arr[i + 1]
+  return (i + 1)
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    n = h - l + 1
+    stack = [0] * n #Auxilary Stack
+    top = -1
+
+    top = top +1   # Initial value push() to stack
+    stack[top] = l
+    top = top + 1
+    stack[top] = h
+
+    while top >= 0:
+        h = stack[top]   #Pop() high and Low
+        top = top - 1
+        l = stack[top]
+        top = top - 1
+
+        part = partition(arr, l, h)  # Setting Pivot Element
+
+        #For Elements in the left of pivot
+        if part-1 > l:
+            top = top + 1
+            stack[top] = l
+            top = top + 1
+            stack[top] = part - 1
+
+        # For Elements in the right of pivot
+        if part + 1 < h:
+            top = top + 1
+            stack[top] = part + 1
+            top = top + 1
+            stack[top] = h
+
+
+arr = [5, 4, 6, 3, 2, 5, 4, 2,8]
+n = len(arr)
+quickSortIterative(arr, 0, n-1)
+print ("Sorted array is:")
+for i in range(n):
+    print ("%d" %arr[i]),
+
+
+
 
