96acc949fc138f5450318de0a4db770add64bed6
Mehar Shahani <mehar.shahani@gmail.com>
2021-06-18 18:26:24 -0500

    Completed Precourse 2 - Please review

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,41 @@
-# Python code to implement iterative Binary  
-# Search. 
+# Implement iterative Binary Search. 
+# time complexity - O(log n)
+# Space complexity - O(1)
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
+
+  if r >= 1:
+    # find the mid element: mid is 1, arr[mid] = 3
+    mid = (l + r) // 2  
+
+    # if element is found
+    if arr[mid] == x:
+      return mid
+
+    # if search element is greater than mid 
+    elif x > arr[mid]:
+      l = mid + 1
+      return binarySearch(arr, l, r, x)
     
+    # if search element is lesser than mid 
+    else:
+      r = mid - 1
+      return binarySearch(arr, l, r, x)
+
+  else:
+    return -1
+
   
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
+x = 40
   
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index % d" % result) 
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,54 @@
-# Python program for implementation of Quicksort
+# Implementation of iterative Quicksort
 
 # This function is same in both iterative and recursive
+from typing import Deque
+
+
 def partition(arr, l, h):
   #write your code here
+  i = l - 1
+  pivot = arr[h]
+
+  for j in range(l, h):
+    if arr[j] <= pivot:
+      i += 1
+      arr[i], arr[j] = arr[j], arr[i]
+
+  arr[i+1], arr[h] = arr[h], arr[i+1]
+
+  return i+1
 
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  stack = Deque()
+
+  start = 0
+  end = len(arr) - 1
+
+  stack.append((start, end))
+
+  while stack:
+    start ,end = stack.pop()
+
+    pivot = partition(arr, start, end)
+
+    if pivot - 1 > start:
+      stack.append((start, pivot - 1))
+
+    if pivot + 1 < end:
+      stack.append((pivot + 1, end))
+
+
+# Driver code to test above 
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+
+quickSortIterative(arr,0,n-1) 
+
+print ("Sorted array is:") 
+
+for i in range(n): 
+    print ("%d" %arr[i]),
+  
 
