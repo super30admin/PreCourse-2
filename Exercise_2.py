442ef567cc2fa4e71d2d9ef4cef61d9ee91cc6e5
broy2808 <42786350+broy2808@users.noreply.github.com>
2021-06-19 11:34:06 -0700

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,15 +2,29 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+     pivot=low
+     L=low+1
+     while(L<=high):
+         if (arr[L]>arr[pivot]) and (arr[high]<arr[pivot]) :
+             arr[L],arr[high]=arr[high],arr[L]
+             L+=1
+             high-=1
+         elif (arr[L]<=arr[pivot]):
+             
+             L+=1
+         elif (arr[high]>=arr[pivot]):
+            
+            high-=1
+     arr[pivot],arr[high]=arr[high],arr[pivot]
+     return high
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
+    if (low<high):
+        pElement=partition(arr,low,high)
+        quickSort(arr,low,pElement-1)
+        quickSort(arr,pElement+1,high)
     
-    #write your code here
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
@@ -18,6 +32,4 @@ n = len(arr)
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+    print("%d" %arr[i])
