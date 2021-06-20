d2344d32f5cc2ea1f906f6747e4e535600f9b9a0
Ankita Chikodi <ankitachikodi@gmail.com>
2021-06-19 18:53:55 -0700

    PreCourse-2 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,27 @@
-class BinarySearch { 
+/**
+ * Time Complexity : O(log n)
+ * Space Complexity: 1
+ */
+class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
+    {
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            // Check if x is present at mid
+            if (arr[mid] == x)
+                return mid;
+
+            // Increment mid value if x is greater
+            else if (arr[mid] < x)
+                l = mid + 1;
+
+            // Decrement mid value if x is smaller
+            else
+                r = mid - 1;
+        }
+        return -1;
     } 
   
     // Driver method to test above 
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,42 +1,76 @@
-class MergeSort 
-{ 
-    // Merges two subarrays of arr[]. 
-    // First subarray is arr[l..m] 
-    // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
-    } 
-  
-    // Main function that sorts arr[l..r] using 
-    // merge() 
-    void sort(int arr[], int l, int r) 
-    { 
+/**
+ * Time Complexity : O(n log n)
+ * Space Complexity: O(n)
+ */
+class MergeSort
+{
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r)
+    {
+        int arr1 = m - l + 1;
+        int arr2 = r - m;
+
+        int[] left = new int[arr1];
+        int[] right = new int[arr2];
+
+        for (int i = 0; i < arr1; ++i)
+            left[i] = arr[l + i];
+
+        for (int j = 0; j < arr2; ++j)
+            right[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0, k = l;
+
+        while (i < arr1 && j < arr2) {
+            if (left[i] >= right[j])
+                arr[k++] = right[j++];
+            else
+                arr[k++] = left[i++];
+        }
+        while (i < arr1)
+            arr[k++] = left[i++];
+
+        while (j < arr2)
+            arr[k++] = right[j++];
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sort(int arr[], int l, int r)
+    {
 	//Write your code here
-        //Call mergeSort from here 
-    } 
-  
+        //Call mergeSort from here
+        if (l < r) {
+            int middle = l + (r - l) / 2;
+            sort(arr, l, middle);
+            sort(arr, middle + 1, r);
+            merge(arr, l, middle, r);
+        }
+    }
+
     /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
-  
-    // Driver method 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
-  
-        System.out.println("Given Array"); 
-        printArray(arr); 
-  
-        MergeSort ob = new MergeSort(); 
-        ob.sort(arr, 0, arr.length-1); 
-  
-        System.out.println("\nSorted array"); 
-        printArray(arr); 
-    } 
-} 
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String args[])
+    {
+        int arr[] = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Given Array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length-1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
+}
