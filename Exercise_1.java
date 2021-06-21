58b667516a6a09c14e403c500c2ecae3d09ed788
Nyrikabr <nyrikabr@gmail.com>
2021-06-21 20:40:15 +0530

    Completed Sprint PreCourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,6 +3,23 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        if(r>0) // to check if array is not empty
+        {
+            int m = (l+r)/2; // middle index in array
+            if(arr[m]==x)
+            {
+                return m;
+            }
+            else if(arr[m]<x)
+            {
+                return binarySearch(arr,m+1,r,x);
+            }
+            else
+            {
+                return binarySearch(arr,l,m-1,x);
+            }
+        }
+        return -1;
     } 
   
     // Driver method to test above 
