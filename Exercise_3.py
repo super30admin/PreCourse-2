c4ad93ce1462c6af00278a38e9df9e9fbc6b4973
Sirisha Kona (Student) <skona@asurite.asu.edu>
2021-06-23 18:07:36 -0700

    Completed precourse2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,44 @@
 # Node class  
+#Time Complexity :  O(n)
+#Space Complexity : O(n)
+#Did this code successfully run on Leetcode : yes
+#Any problem you faced while coding this : no
+
+
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head = None
         
   
-    def push(self, new_data): 
-        
+    def push(self, data): 
+        if self.head:
+            current = self.head
+            while current.next :
+                current = current.next
+            current.next = Node(data)
+        else:
+            self.head = Node(data)
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        if self.head:
+            temp1 = self.head
+            temp2 = self.head
+            while temp2 and temp2.next:
+                temp1 = temp1.next
+                temp2 = temp2.next.next
+            return temp1.data
+        else:
+            return
 
 # Driver code 
 list1 = LinkedList() 
@@ -23,4 +47,5 @@ list1.push(4)
 list1.push(2) 
 list1.push(3) 
 list1.push(1) 
-list1.printMiddle() 
+middle_element = list1.printMiddle() 
+print("middle element is {}".format(middle_element))
