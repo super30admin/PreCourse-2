39534c0e7ae2dc05127acf33add3df9b58b75a80
Durgesh Patil <patildurgesh1998@gmail.com>
2021-07-17 18:25:48 -0400

    Update Exercise_01.cpp

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -6,6 +6,18 @@
 int binarySearch(int arr[], int l, int r, int x) 
 {   
     //Your Code here 
+    int mid;
+    while(l<=r)
+    {
+        mid = l+(r-l)/2;
+        if(arr[mid] == x)
+            return mid;
+        else if(arr[mid] < x)
+            l = mid+1;
+        else
+            r = mid-1;
+    }
+    return -1;
 } 
   
 int main(void) 
