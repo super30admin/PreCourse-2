0073722ca19a97a191e42798e86f54c74bf76079
Durgesh Patil <patildurgesh1998@gmail.com>
2021-07-17 18:28:07 -0400

    Update Exercise_04.cpp

--- Exercise_4.cpp
+++ Exercise_4.cpp
@@ -1,12 +1,29 @@
-#include<stdlib.h> 
-#include<stdio.h> 
-  
+#include<bits/stdc++.h>
+using namespace std;
 // Merges two subarrays of arr[]. 
 // First subarray is arr[l..m] 
 // Second subarray is arr[m+1..r] 
-void merge(int arr[], int l, int m, int r) 
+void merge(int arr[], int i1, int j1, int i2, int j2) 
 { 
     //Your code here
+    int i,j,k;
+    i = i1;
+    j = i2;
+    k=0;
+    int temp[100];
+    while(i<=j1 && j<=j2)
+    {
+        if(arr[i]<arr[j])
+            temp[k++]=arr[i++];
+        else
+            temp[k++]=arr[j++];
+    }
+    while(i<=j1)
+        temp[k++]=arr[i++];
+    while(j<=j2)
+        temp[k++]=arr[j++];
+    for(i=i1,j=0;i<=j2;i++,j++)
+        arr[i] = temp[j];
 } 
   
 /* l is for left index and r is right index of the 
@@ -14,6 +31,14 @@ void merge(int arr[], int l, int m, int r)
 void mergeSort(int arr[], int l, int r) 
 { 
     //Your code here
+    int mid;
+    if(l<r)
+    {
+        mid = l+(r-l)/2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        merge(arr,l,mid,mid+1,r);
+    }
 } 
   
 /* UTILITY FUNCTIONS */
