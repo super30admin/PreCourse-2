26380fcb7685c36528e9daa53bbaaae8df573928
Aniket-jadhav <aniketjadhav5398@gmail.com>
2021-07-19 00:44:17 -0500

    Pre Course 2 solved 4 questions

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,15 +2,24 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+    ind = low
+    pivot = arr[ind]
+    while low < high:
+        while low<len(arr) and arr[low]<=pivot:
+            low+=1
+        while high>=0 and arr[high]>pivot:
+            high-=1
+        if low < high:
+            arr[low],arr[high]=arr[high],arr[low] 
+    arr[high], arr[ind] = arr[ind], arr[high]
+    return high
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
+def quickSort(arr,low,high):
+    if low<high:
+        p = partition(arr, low, high)
+        quickSort(arr, low, p-1)
+        quickSort(arr, p+1, high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,10 +1,36 @@
 # Python program for implementation of MergeSort 
+#Time complexity O(nlogn) since we are splitting array and merging it
+#Space complexity O(n)
 def mergeSort(arr):
-  
+  if len(arr)>1:
+    mid = len(arr)//2
+    left = arr[:mid]
+    right = arr[mid:]
+    mergeSort(left)
+    mergeSort(right)
+    i=j=k=0
+    while i<len(left) and j<len(right):
+      if left[i]<right[j]:
+        arr[k] = left[i]
+        i+=1
+      else:
+        arr[k] = right[j]
+        j+=1
+      k+=1
+    while i<len(left):
+      arr[k] = left[i]
+      i+=1
+      k+=1
+    while j<len(right):
+      arr[k] = right[j]
+      j+=1
+      k+=1
+
   #write your code here
   
 # Code to print the list 
-def printList(arr): 
+def printList(arr):
+  print(arr)
     
     #write your code here
   
