ec094a69c79bdc1cdacf381a5e4649e53f9befe8
Milind Gianchandani <gianchandani1@gmail.com>
2021-07-19 13:53:38 -0400

    Precourse-2 Submission

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,22 +2,32 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+    i = (low-1)
+    pvt = arr[high]
+
+    for x in range(low, high):
+        if arr[x] <= pvt:
+            i = i + 1
+            arr[i], arr[x] = arr[x], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
+def quickSort(arr,low,high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+        index = partition(arr, low, high)
+        quickSort(arr, low, index-1)
+        quickSort(arr, index+1, high)
+
+
+
+
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
+quickSort(arr, 0, n-1)
+print("Sorted array is:")
 for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+    print(arr[i])
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,54 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+    i = (l - 1)
+    x = arr[h]
+    for j in range(l, h):
+        if arr[j] <= x:
+            # increment
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    return (i + 1)
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    # Creating stack
+    size = h - l + 1
+    stack = [0] * (size)
 
+    top = -1
+    # Pushing initial values
+    top = top + 1
+    stack[top] = l
+    top = top + 1
+    stack[top] = h
+    # Popping from stack
+    while top >= 0:
+        # Pop
+        h = stack[top]
+        top = top - 1
+        l = stack[top]
+        top = top - 1
+        # Set index element at its correct position
+        index = partition(arr, l, h)
+        # For elements on the left
+        if index - 1 > l:
+            top = top + 1
+            stack[top] = l
+            top = top + 1
+            stack[top] = index - 1
+        # For elements on the right
+        if index + 1 < h:
+            top = top + 1
+            stack[top] = index + 1
+            top = top + 1
+            stack[top] = h
+
+# Added driver code
+arr = [99,12,5,16,121,83,0,53]
+n = len(arr)
+quickSortIterative(arr, 0, n-1)
+print ("Sorted array is:")
+for i in range(n):
+   print(arr[i],end=" ")
