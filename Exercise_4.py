cabb540a80a9bc33add7d81895658ef96d3aabd6
Parth Ambalkar <parth@parths-mbp.myfiosgateway.com>
2021-07-20 19:21:51 -0400

    Completed 4 files ,Implemented quick sort recursively

--- Exercise_4.py
+++ Exercise_4.py
@@ -2,11 +2,36 @@
 def mergeSort(arr):
   
   #write your code here
+  if(len(arr))>1:
+    mid=int(len(arr)/2)
+    left=arr[0:mid]
+    right=arr[mid:]
+    mergeSort(left)
+    mergeSort(right)
+    i,j,k=0,0,0
+    while(i<len(left) and j<len(right)):
+      if(left[i]<right[j]):
+        arr[k]=left[i]
+        i=i+1
+      else:
+        arr[k]=right[j]
+        j=j+1
+      k=k+1
+    while i<len(left):
+      arr[k]=left[i]
+      i+=1
+      k+=1
+    while j<len(right):
+      arr[k]=right[j]
+      j+=1
+      k+=1
   
 # Code to print the list 
 def printList(arr): 
     
     #write your code here
+    for i in range(len(arr)):
+      print(arr[i])
   
 # driver code to test the above code 
 if __name__ == '__main__': 
