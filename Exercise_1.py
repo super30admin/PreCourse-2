a9087c16de8d3ad087b35e947648709d49d4a1b1
mmshivesh-S30 <87630867+mmshivesh-S30@users.noreply.github.com>
2021-07-20 20:15:18 -0700

    Completed Binary Search and Middle of Linked List.

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,11 +4,13 @@
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
+    m = (l+r)//2
+    if x == arr[m]:
+        return m
+    elif x > arr[m]:
+        return binarySearch(arr, m+1, r, x)
+    elif x < arr[m]:
+        return binarySearch(arr, l, m-1, x)
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
@@ -17,6 +19,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print(f"Element is present at index :{result}")
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
