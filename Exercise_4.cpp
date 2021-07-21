87a178d5c7f9b10a2f2a32f44a0c48e3085821f8
Pinak Pathak <pinakpathak@Pinaks-MBP.fios-router.home>
2021-07-20 22:44:22 -0400

    Completed PreCourse-2

--- Exercise_4.cpp
+++ Exercise_4.cpp
@@ -7,6 +7,33 @@
 void merge(int arr[], int l, int m, int r) 
 { 
     //Your code here
+    int i=l,j=m+1,k=l;
+    int temp[r-l+1];
+    while(i<=m&&j<=r){
+        if(arr[i]<=arr[j]){
+            temp[k]=arr[i];
+            i++;
+            k++;
+        }
+        else{
+            temp[k]=arr[j];
+            j++;
+            k++;
+        }
+    }
+    while(i<=m){
+        temp[k]=arr[i];
+        i++;
+        k++;
+    }
+    while(j<=r){
+        temp[k]=arr[j];
+        j++;
+        k++;
+    }
+    for(i=l;i<=r;i++){
+        arr[i]=temp[i];
+    }
 } 
   
 /* l is for left index and r is right index of the 
@@ -14,6 +41,12 @@ void merge(int arr[], int l, int m, int r)
 void mergeSort(int arr[], int l, int r) 
 { 
     //Your code here
+    if(l<r){
+        int m = l + (r-l)/2;
+        mergeSort(arr,l,m);
+        mergeSort(arr,m+1,r);
+        merge(arr,l,m,r);
+    }
 } 
   
 /* UTILITY FUNCTIONS */
