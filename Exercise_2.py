73366e2609f2261179589e69d4b946555efe1e20
vkhanderao <60457921+vkhanderao@users.noreply.github.com>
2021-07-23 18:13:16 -0400

    Design_2-changes

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,35 @@
-# Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+# Python program for implementation of Quicksort Sort
+def swap(arr,i,idx):
+    temp=arr[i]
+    arr[i]=arr[idx]
+    arr[idx]=temp
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+def partition(arr, low, high):
+    pivot=arr[high];
+    i=low-1
+    for idx in range(low,high):
+        if arr[idx]<pivot:
+            i+=1
+            swap(arr,i,idx)
+    swap(arr,i+1,high)
+    return i+1
+# write your code here
+
+
+# Function to do Quick sort
+def quickSort(arr, low, high):
+    if(low<high):
+        p=partition(arr,low,high)
+        quickSort(arr,low,p-1)
+        quickSort(arr,p+1,high)
+# write your code here
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n - 1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]),
+
+
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,41 @@
 # Python program for implementation of Quicksort
-
+def swap(arr,i,idx):
+    temp=arr[i]
+    arr[i]=arr[idx]
+    arr[idx]=temp
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
+    pivot=arr[h]
+    i=l-1
+    for idx in range(l,h):
+        if(arr[idx]<pivot):
+            i+=1
+            swap(arr,i,idx)
+    swap(arr,i+1,h)
+    return i+1
   #write your code here
 
-
+from queue import LifoQueue
 def quickSortIterative(arr, l, h):
+    stack=LifoQueue()
+    stack.put(l)
+    stack.put(h)
+    while(stack.qsize()>0):
+        high=stack.get()
+        low=stack.get()
+        p=partition(arr,low,high)
+        if p-1>low:
+            stack.put(low)
+            stack.put(p-1)
+        if p+1<high:
+            stack.put(p+1)
+            stack.put(high)
   #write your code here
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSortIterative(arr, 0, n - 1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]),
+
 
