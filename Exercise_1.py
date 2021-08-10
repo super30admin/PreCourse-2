b839f8e915346376c6228ccf60c476919804a403
Unnati Nadupalli <prema.unnati@gmail.com>
2021-08-10 19:29:58 -0400

    Completed Precourse-2 except Exercise 5

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,36 @@
 # Python code to implement iterative Binary  
 # Search. 
-  
+
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
-# Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
-  
-# Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
-if result != -1: 
-    print "Element is present at index % d" % result 
-else: 
-    print "Element is not present in array"
+def binarySearch(arr, l, r, x):
+    while l < r:
+        mid = l + (r - l) // 2
+        if arr[mid] == x:
+            return mid
+        else:
+            if arr[mid] > x:
+                r = mid - 1
+            elif arr[mid] < x:
+                l = mid + 1
+
+
+    return -1
+
+
+    # write your code here
+
+
+if __name__ == '__main__':
+
+    # Test array
+    arr = [2, 3, 4, 10, 40]
+    x = 10
+
+    # Function call
+    result = binarySearch(arr, 0, len(arr) - 1, x)
+
+    if result != -1:
+        print("Element is present at index ", result)
+    else:
+        print("Element is not present in array")
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,24 +3,40 @@ class Node:
   
     # Function to initialise the node object  
     def __init__(self, data):  
-        
+        self.data = data
+        self.next = None
+
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
   
-    def push(self, new_data): 
-        
+    def push(self, new_data):
+        temp = Node(new_data)
+
+        if self.head == None:
+            self.head = temp
+        else:
+            last = self.head
+            while(last.next):
+                last = last.next
+            last.next = temp
+
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
-
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+        slow, fast = self.head, self.head
+        while(fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+        print(slow.data)
+if __name__ == '__main__':
+    # Driver code
+    list1 = LinkedList()
+    list1.push(5)
+    list1.push(4)
+    list1.push(2)
+    list1.push(3)
+    list1.push(1)
+    list1.printMiddle()
