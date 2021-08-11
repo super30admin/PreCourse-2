5552de258e626596d350d06c5f2fcd0803b1ae8b
meghasn <meghoos99@gmail.com>
2021-08-11 23:41:49 +0530

    implemented binary search algo

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,9 +4,21 @@
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
+  if l<=r:
   #write your code here
-  
+      mid=(l+r)//2
+      
+      if x==arr[mid]:
+        
+        return mid
+      if x<arr[mid]:
+        return binarySearch(arr,l,mid-1,x)
+      if x>arr[mid]:
+        return binarySearch(arr,mid+1,r,x)
+      
+  else:
+    return -1
+      
     
   
 # Test array 
@@ -17,6 +29,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index % d" % result) 
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
