1d67c1a431e12cbf0c1a6563f2c45e4385a1a203
Abhirag Nagpure <nagpureabhirag@gmail.com>
2021-08-11 18:08:26 -0500

    Complete Sprint PreCourse-2

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,37 @@
 # Python code to implement iterative Binary  
 # Search. 
-  
+
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
+
+	#write your code here
+	mid = int((l + (r-l)/2))
+
+	while l <= r:
+		mid = int((l + (r-l)/2))
+		if arr[mid] == x:
+			return mid
+		if arr[mid] < x:
+			l = mid + 1
+		else:
+			r = mid - 1
+
+	return -1
+
+
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
+# arr = [10, 15]
+# arr = [1, 10]
+# arr = [10]
+# arr = [10, 122, 123, 12412, 1253, 5454, 54545, 66666, 77777, 88888]
 x = 10
-  
+
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
-  
+
 if result != -1: 
-    print "Element is present at index % d" % result 
+	print("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+	print("Element is not present in array")
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,18 +3,49 @@ class Node:
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head = None
         
   
     def push(self, new_data): 
-        
+        if not self.head:
+            self.head = Node(new_data)
+        else:
+            temp = self.head
+            while temp.next:
+                temp = temp.next
+            temp.next = Node(new_data)
+
+    # def display(self):
+    #     """
+    #     Debugging func to display contents
+    #     """
+    #     ans = []
+    #     temp = self.head
+    #     while temp:
+    #         ans.append(temp.data)
+    #         temp = temp.next
+    #     print(ans)
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        # two pointer runner technique
+        # first, second pointer, first inc by 1, second inc by 2
+        # of course, assuming the linked list has an odd number of elements
+        first, second = self.head, self.head
+        while second.next:
+            print((first.data, second.data))
+            second = second.next.next
+            first = first.next
+            if not second:
+                break
+        print(first.data)
 
 # Driver code 
 list1 = LinkedList() 
@@ -23,4 +54,6 @@ list1.push(4)
 list1.push(2) 
 list1.push(3) 
 list1.push(1) 
+# list1.push(22)
+# list1.display()
 list1.printMiddle() 
