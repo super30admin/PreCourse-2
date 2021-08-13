a0e29ba93cf06ea925e52d82f062abf30d371c50
rajchoksi1997 <rajchoksi1997@gmail.com>
2021-08-13 15:20:19 -0700

    Binary search corrected

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,20 +1,24 @@
 class BinarySearch { 
-   // Time Complexity : O(N)
+   // Time Complexity : O(LogN)
     // Space Complexity : O(1)
     // Did this code successfully run on Leetcode : Yes
     // Any problem you faced while coding this : No
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //using loop to search required number
-        
-        for(int i =0;i< arr.length;i++){
-           if(arr[i]==x){
-               //returns the required index
-               return i;
-           }
-        }
-        // return -1 if the number is not found
-        return -1;
+        if(l>r) {
+			return -1;
+		}
+		int mid = (l+r)/2;
+		if(arr[mid]== x) {
+			return mid;
+		}else if(arr[mid]>x) {
+			return binarySearch(arr,l,mid-1,x);			
+		}else {
+			return binarySearch(arr,mid+1,r,x);
+		}
+
+      
     } 
   
     // Driver method to test above 
@@ -23,7 +27,7 @@ class BinarySearch {
         BinarySearch ob = new BinarySearch(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
-        int x = 3; 
+        int x = 10; 
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
