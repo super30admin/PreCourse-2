ebb19f5b7781bda5ccd8c5fc5838078e45ec8ef5
meghasn <41339621+meghasn@users.noreply.github.com>
2021-08-15 01:16:50 +0530

    not sure about the iiterative part

--- Exercise_5.py
+++ Exercise_5.py
@@ -3,8 +3,21 @@
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
   #write your code here
+  pivot=arr[l]
+  i=l+1
+  j=h
+  while True:
+    while(i<=j and pivot>=arr[i]):
+      i=i+1
+    while(i<=j andpivot<=arr[j]):
+      j=j-1
+    if i<=j:
+      arr[i],arr[j]=arr[j],arr[i]
+    else:
+      break
+  arr[j],arr[l]=arr[l],arr[j]
 
-
+  return j
 def quickSortIterative(arr, l, h):
   #write your code here
 
