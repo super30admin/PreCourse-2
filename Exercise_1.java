e1196d50c66c47816ca4491dc8acccd6bc5a1cfb
bhesa@DESKTOP-JUA2FT2 <bhesaniayatha@gmail.com>
2021-08-14 12:38:15 -0500

    Completed Sprint PreCourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,6 +3,15 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        if(r>=l){
+            int mid=l+(r-l)/2;
+            if(arr[mid]==x)
+                return mid;
+            if(arr[mid]>x)
+                binarySearch(arr, l, mid-1, x);
+            return binarySearch(arr, mid+1, r, x);
+        }
+        return -1;
     } 
   
     // Driver method to test above 
