2ac4dd443107c0c928f77c4fca0201f696cc4b36
meghasn <41339621+meghasn@users.noreply.github.com>
2021-08-14 19:29:32 +0530

    completed merge sort

--- Exercise_4.py
+++ Exercise_4.py
@@ -9,11 +9,36 @@
 def mergeSort(arr):
   
   #write your code here
+  if len(arr)>1:
+    mid=len(arr)//2
+    l=arr[:mid]
+    r=arr[mid:]
+    mergeSort(l)
+    mergeSort(r)
+    i=j=k=0
+    while i<len(l) and j<len(r):
+      if l[i]<r[i]:
+        arr[k]=l[i]
+        i=i+1
+      else:
+        arr[k]=r[j]
+        j=j+1
+      k=k+1
+    while i<len(l):
+      arr[k]=l[i]
+      i=i+1
+      k=k+1
+    while j<len(r):
+      arr[k]=r[j]
+      k=k+1
+      j=j+1
   
 # Code to print the list 
 def printList(arr): 
     
     #write your code here
+    for i in range(len(arr)):
+      print(arr[i])
   
 # driver code to test the above code 
 if __name__ == '__main__': 
