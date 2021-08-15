6e2906be822c874010b3661040fb8bdc614e1d20
Sampada Samaka <sampadasamaka@gmail.com>
2021-08-14 23:18:06 -0500

    Added PreCourse-2

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,11 +4,25 @@
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
+  l = 0
+  r = len(arr) - 1
+  mid = 0
   
-  #write your code here
-  
+  while l < r: 
+    mid = (l + r) // 2
     
-  
+    if arr[mid] < x: 
+      l = mid + 1
+      
+    elif arr[mid] > x:
+      r = mid - 1
+      
+    else:
+      return mid 
+      
+  return -1 
+
+   
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
@@ -17,6 +31,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,18 +3,35 @@ class Node:
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None 
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head = None
         
   
     def push(self, new_data): 
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        temp = self.head 
+        count = 0 
+
+        while self.head: 
+
+            if(count & 1):
+                temp = temp.next
+
+            self.head = self.head.next
+            count += 1
+        print(temp.data)
 
 # Driver code 
 list1 = LinkedList() 
