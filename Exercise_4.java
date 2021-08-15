31b1190d116eff97e5b48bcb060c3f30830fd45f
Madhulika Lekkala <madhulikareddy88@gmail.com>
2021-08-15 00:18:21 -0500

    PreCourse2

--- Exercise_4.java
+++ Exercise_4.java
@@ -6,6 +6,50 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+       //int n=arr.length;
+       int nL = m-l+1;
+       int nR = r-m;
+       int[] left = new int[nL];
+       int[] right = new int[nR];
+    //    for(int i=0;i<=m-1;i++){
+    //        left[i]=arr[i];
+    //    }
+    //    12 11 13 5 6 7
+    //             m
+    //    for(int j=m;j<=n-1;j++){
+    //     right[j-m]=arr[j];
+    //    }
+
+    for (int i = 0; i < nL; ++i)
+     left[i] = arr[l + i];
+    for (int j = 0; j < nR; ++j)
+     right[j] = arr[m + 1 + j];
+
+    int k = l;
+    int i = 0, j = 0;
+    while (i < nL && j < nR) {
+        if (left[i] <= right[j]) {
+
+            arr[k] = left[i];
+            i++;
+            k++;
+        } else {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+    while (i < nL) {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+    while (j < nR) {
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +58,12 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+        if(l<r){
+            int mid = l + (r-l)/2;
+            sort(arr,l,mid-1);
+            sort(arr,mid+1,r);
+            merge(arr,l,mid,r);
+        }
     } 
   
     /* A utility function to print array of size n */
