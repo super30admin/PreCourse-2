0ef49d045eb7f2dda9e59031e204b8c603207553
Gaurav Jaisinghani <47674146+gauravha@users.noreply.github.com>
2021-08-14 22:22:26 -0400

    Completed sprint precourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,7 +1,28 @@
-class BinarySearch { 
+
+// Time Complexity: O(log n)
+// Space Complexity: O(log n)
+// Did this code successfully run on Leetcode:
+//Any problems you faced while coding this:
+
+class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
-    { 
+    {
+
+        int m = l + (r-l) / 2;
+        while(l<r){
+            if (x == arr[m]){
+                return m;
+            }
+            if (x < arr[m]){
+                return binarySearch(arr, l, m-1, x);
+            }
+            else{
+                return binarySearch(arr,m+1, r, x);
+
+            }
+        }
+        return -1;
         //Write your code here
     } 
   
@@ -9,9 +30,9 @@ class BinarySearch {
     public static void main(String args[]) 
     { 
         BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
+        int arr[] = { 2, 3, 4, 30, 40 };
         int n = arr.length; 
-        int x = 10; 
+        int x = 2;
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
