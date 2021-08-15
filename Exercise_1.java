1043d19ed54d5eadf7de249bb8cbc77f8e65c4a7
ghodkesujay <ghodke.su@northeastern.edu>
2021-08-14 22:59:55 -0400

    Completed Sprint PreCourse-2

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,6 +3,20 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        while(l<=r){
+            int middle = (l+r)/2;
+
+            if(x < arr[middle]){
+                r = middle - 1;
+            }
+            else if(x > arr[middle]) {
+                l = middle + 1;
+            }
+            else {
+                return middle;
+            }
+        }
+        return -1;
     } 
   
     // Driver method to test above 
--- Exercise_4.java
+++ Exercise_4.java
@@ -5,7 +5,56 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+       //Your code here
+        int size1 = m - l + 1;
+        int size2 = r - m;
+
+        /* Create temp arrays */
+        int leftArr[] = new int [size1];
+        int rightArr[] = new int [size2];
+
+        /*Copy data to temp arrays*/
+        for (int i=0; i<size1; ++i)
+            leftArr[i] = arr[l + i];
+        for (int j=0; j<size2; ++j)
+            rightArr[j] = arr[m + 1+ j];
+
+
+        /* Merging arrays */
+        int i = 0, j = 0;
+
+
+        int k = l;
+        while (i < size1 && j < size2)
+        {
+            if (leftArr[i] <= rightArr[j])
+            {
+                arr[k] = leftArr[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* remaining elements of leftArray */
+        while (i < size1)
+        {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        /* remaining elements of rightArray */
+        while (j < size2)
+        {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -13,7 +62,16 @@ class MergeSort
     void sort(int arr[], int l, int r) 
     { 
 	//Write your code here
-        //Call mergeSort from here 
+        //Call mergeSort from here
+        if(l == r){
+            return;
+        }
+        else {
+            int mid = (l + r)/2;
+            sort(arr, l, mid);
+            sort(arr, mid+1, r);
+            merge(arr, l, mid, r);
+        }
     } 
   
     /* A utility function to print array of size n */
