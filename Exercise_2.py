140323878a94e1f8992ba2da4d5407322f080f8a
Kapil <kdharao19@gmail.com>
2021-08-14 22:29:00 -0700

    Completed Sprint Precourse2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,17 +1,35 @@
+# Time Complexity : O(nlogn)
+# Space Complexity : 
+# Is it on Leetcode: Dont know
+# Problem faced while solving: Was difficult for me to write partition function 
+
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+    i = (low-1)                                         # Index position of smaller element
+    pivot = arr[high]                                   # Declared pivot 
+    
+    for j in range(low, high):                          
+                                                        
+        if(arr[j] <= pivot):                            # When j value is less than or equal to pivot value
+            i = i+1                                     # Increment smaller element index and swap
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
+def quickSort(arr,low,high):                            
+    if(len(arr) == 1):                                  # If array consists of one element 
+        return arr                                      # Will return that element
+    if(low < high):
+
+        pi = partition(arr, low, high)                  # Partitioning index used for sorting
+
+        quickSort(arr, low, pi-1)                       # Used for sorting elements before and after partitioning 
+        quickSort(arr, pi+1, high)
+
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,41 @@
+# Time Complexity : 
+# Space Complexity: 
+# Did you find this problem on Leetcode: Yes
+# Did you face any problem solving it: Logic used for printMiddle function 
+
+# Problem solving approach: Traverse linked list using two pointers. To get the middle value used fast and
+# slow pointer, when fast will reach the end element, slow will reach the middle element.
+
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data): 
+        self.data = data                                # Assigned data
+        self.next = None                                # Initialized next as null
         
-class LinkedList: 
+class LinkedList:                                       # LinkedList class contains node object
   
-    def __init__(self): 
-        
+    def __init__(self):
+        self.head = None
   
-    def push(self, new_data): 
+    def push(self, new_data):                           # Push function will add a new node at start
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node 
         
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        print("Element in the middle is ",slow.data) 
 
 # Driver code 
 list1 = LinkedList() 
