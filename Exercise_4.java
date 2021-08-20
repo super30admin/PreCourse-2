f17ef44e244e9b15d65e179c5d3bf75d4f72372d
sudo-007 <bs@email.com>
2021-08-19 19:42:20 -0500

    Completed Aug2021 PreCourse-2

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(n log n)
+// Space Complexity : O(n log n)
+// Any problem you faced while coding this : None.
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -5,15 +9,79 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+       int subArr1;
+       int subArr2;
+       
+       subArr1 = m - l + 1;
+       subArr2 = r - m;
+
+       // Temp arrays to store the right and left sides of the arrays
+       int leftSide[]  = new int[subArr1];
+       int rightSide[] = new int[subArr2];
+
+       // Move elements to temp array
+
+       for(int i = 0; i < subArr1; ++i){
+           leftSide[i] = arr[l + i];
+        }
+       for(int j = 0; j < subArr2; ++j){
+            rightSide[j] = arr[m + 1 + j];
+        }
+
+        int i; //subArr1 index
+        int j; //subArr2 index
+        int k; //merged array index
+
+        i = 0;
+        j = 0;
+        k = l;
+
+        // Start merging the indexes from both subarrays
+        while(i < subArr1 && j < subArr2) {
+            if (leftSide[i] <= rightSide[j]) { //left side should not overlap over the right side
+                arr[k] = leftSide[i];
+                i++;
+            }
+            else{
+                arr[k] = rightSide[j];
+                j++;
+            }
+            k++;
+        }
+
+        // copy elements from left side
+        while (i < subArr1) {
+            arr[k] = leftSide[i];
+            i++;
+            k++;
+        }
+
+        // copy elements from right side
+        while (i < subArr2) {
+            arr[k] = rightSide[j];
+            j++;
+            k++;
+        }
+
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
     { 
-	//Write your code here
-        //Call mergeSort from here 
+        // left index must be lower than the right index
+        if (l < r) {
+            int m; 
+            m = l + (r - l) / 2;
+            
+            //sort left
+            sort(arr, l, m);
+            //sort right
+            sort(arr, m+1, r);
+
+            // Merge subarrays
+            merge(arr, l, m, r);
+        }
     } 
   
     /* A utility function to print array of size n */
