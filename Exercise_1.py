c6244b0ce6e7218754490e363244430fc9c7f670
ameeshaS11 <shrivastav.ameesha1@gmail.com>
2021-08-23 17:21:21 -0700

    Completed Precourse2

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,9 +3,27 @@
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
+
+#TimeComplexity: O(log n)
+#SpaceComplexity: O(n)
+
 def binarySearch(arr, l, r, x): 
   
-  #write your code here
+    #check if the array has any more elements left to check otherwise return -1
+    if r>=l:
+        mid = (l+r)//2
+        #if x matched the middle element return middle index
+        if x == arr[mid]:
+            return mid
+        #if x in smaller search in the left side of the array
+        elif arr[mid]>x:
+            return binarySearch(arr, l, mid-1, x)
+        #if x is greater search in the right side of the array
+        else:
+            return binarySearch(arr, mid+1, r, x)
+    else:
+        return -1
+    
   
     
   
@@ -17,6 +35,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print ("Element is present at index % d" % result )
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,64 @@
 # Python program for implementation of Quicksort
+  
+#TimeComplexity: O(n log n)
+#SpaceComplexity: O(n)
 
-# This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+    
+    #set last element as pivot
+    pivot = arr[h]
+    #keep track of the index of swaps when the element is smaller than pivot element
+    small_iter = l-1
+    
+    #for all element before pivot element
+    for i in range(l,h):
+        #if the element is smaller take it to the front of array else do nothing
+        if arr[i]<pivot:
+            small_iter += 1
+            arr[small_iter], arr[i] = arr[i], arr[small_iter]
+    #place the pivot element just after the element swapped as they are smaller
+    #we obtain correct index for pivot element
+    arr[small_iter + 1], arr[h] = arr[h], arr[small_iter + 1]
+    
+    #return the partition index where we placed the pivot element
+    return small_iter+1
+  
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    #create a stack
+    stack = [0]*len(arr)
+    top_of_stack = -1
+    
+    #push l and h
+    top_of_stack += 1
+    stack[top_of_stack] = l
+    top_of_stack += 1
+    stack[top_of_stack] = h
+    
+    while top_of_stack > -1:
+        
+        #remove l and h
+        h = stack[top_of_stack]
+        top_of_stack -= 1
+        l = stack[top_of_stack]
+        top_of_stack -= 1
+        
+        #get partition index
+        p = partition(arr, l, h)
+        
+        #if there are small element push left index
+        if p-1>l:
+            top_of_stack += 1
+            stack[top_of_stack] = l
+            top_of_stack += 1
+            stack[top_of_stack] = p-1
+        
+        #if there are larger elements push right index
+        if p+1<h:
+            top_of_stack += 1
+            stack[top_of_stack] = p+1
+            top_of_stack += 1
+            stack[top_of_stack] = h
+    
 
