37841215c205351f2ca9b36376829aa2dfbe0501
Shreya <shreya.cogsci@gmail.com>
2021-09-09 01:11:19 -0500

    completed pre-course 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,44 @@
 # Python program for implementation of Quicksort Sort 
-  
+
+# // Time Complexity : O(lg n) where n is the number of elements in the array
+  # // Space Complexity : O(lg n) where n is the number of elements in the array 
+  # for which recursive calls were made
+  # // Did this code successfully run on Leetcode : yes
+  # // Any problem you faced while coding this : a little when writing the 'terminating condition'
+  # if l > r:
+  #       return -1
+
+  # // Your code here along with comments explaining your approach
+
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
     #write your code here
-  
+    pivot = arr[high]
+    p_index = low
+    for i in range(low,high):
+        if arr[i] <= pivot:
+            arr[i],arr[p_index] = arr[p_index],arr[i]
+            p_index += 1
+    arr[high],arr[p_index] = arr[p_index],arr[high]
+    return p_index
+
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
     #write your code here
-  
+    if low < high:
+
+        p_index = partition(arr,low,high)
+        quickSort(arr,low,p_index-1)
+        quickSort(arr,p_index+1,high)
+
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
+print ("Array is:",arr) 
 n = len(arr) 
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
+    print ("%d" %arr[i],end=', '), 
   
  
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,47 @@
+# // Time Complexity : O(n) where n is the number of elements added in the list, TC for checking middle of the list is O(lg n)
+  # // Space Complexity : O(n) where n is the number of elements added in the list 
+  # // Did this code successfully run on Leetcode : yes
+  # // Any problem you faced while coding this : none
+
+  # // Your code here along with comments explaining your approach
+
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
-        
+        self.data = data 
+        self.next = None
+
 class LinkedList: 
   
     def __init__(self): 
-        
+        # taking a dummy node for ease of traversal 
+        # of even and odd len lists
+        self.head = Node(0)
+        self.end = self.head
   
     def push(self, new_data): 
         
+        # adding node at the end 
+        # making end point to the new node
+        node = Node(new_data)
+        self.end.next = node
+        self.end = self.end.next
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        # using 2 ptr approach
+        # slow ptr moves at half the pace of fast ptr
+        ptr1 = ptr2 = self.head
+
+        while ptr2 and ptr2.next:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next.next
+        
+        if ptr1:
+            print(ptr1.data)
 
 # Driver code 
 list1 = LinkedList() 
@@ -22,5 +49,5 @@ list1.push(5)
 list1.push(4) 
 list1.push(2) 
 list1.push(3) 
-list1.push(1) 
+# list1.push(1) 
 list1.printMiddle() 
