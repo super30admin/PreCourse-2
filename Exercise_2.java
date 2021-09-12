d95d283477d83d6c67dd55a02ee7b16828a92a8b
Vishwa Parikh <90078135+vishwaparikh@users.noreply.github.com>
2021-09-11 20:48:00 -0500

    Completed Exercise2- Please Review

--- Exercise_2.java
+++ Exercise_2.java
@@ -7,11 +7,27 @@ class QuickSort
        pivot and all greater elements to right 
        of pivot */
     void swap(int arr[],int i,int j){
+         int temp = arr[i];
+         arr[i] = arr[j];
+         arr[j] = temp;
         //Your code here   
     }
     
     int partition(int arr[], int low, int high) 
     { 
+        int pivot = arr[high]; 
+        int i = (low - 1); 
+  
+        for(int j = low; j <= high - 1; j++)
+        {
+            if (arr[j] < pivot) 
+            {
+                i++; 
+                swap(arr, i, j);
+            }
+        }
+    swap(arr, i + 1, high);
+    return (i + 1);
    	//Write code here for Partition and Swap 
     } 
     /* The main function that implements QuickSort() 
@@ -20,6 +36,12 @@ class QuickSort
       high  --> Ending index */
     void sort(int arr[], int low, int high) 
     {  
+        if (low < high) 
+        {
+        int pivot = partition(arr, low, high);
+        sort(arr, low, pivot - 1);
+        sort(arr, pivot + 1, high);
+        }
             // Recursively sort elements before 
             // partition and after partition 
     } 
@@ -29,7 +51,7 @@ class QuickSort
     { 
         int n = arr.length; 
         for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
+        System.out.print(arr[i]+" "); 
         System.out.println(); 
     } 
   
@@ -46,3 +68,10 @@ class QuickSort
         printArray(arr); 
     } 
 } 
+
+//time complexity : 	O(n*log(n))
+//space complexity :  O(log n)
+// Recursive approach 
+//I have ran this code
+//I faced difficulty in code as well as calculating time and space complexity
+
