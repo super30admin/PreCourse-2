9eecffa505f7e2ef85717d89febfe644aea406dd
akshay <akulkarni10@hawk.iit.edu>
2021-09-11 19:00:55 -0700

    Implement quicksort

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,52 @@
-# Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+# // Time Complexity : O(nLogn)
+# // Space Complexity : O(n)
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : Had to revise some concepts of quicksort from CLRS and revisit basics.
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+
+# // Your code here along with comments explaining your approach
+
+# give you explanation for the approach
+def partition(arr, low, high):
+
+    # Initializing pivot's index to low
+    pivot_index = low
+    pivot = arr[pivot_index]
+
+    # This loop runs till low pointer is greater than
+    # high pointer after that we'll swap the
+    # pivot with element on high pointer
+    while low < high:
+
+        # Increment low pointer till it finds an
+        # element greater than  pivot
+        while low < len(arr) and arr[low] <= pivot:
+            low += 1
+
+        # Decrement the high pointer till it finds an
+        # element less than pivot
+        while arr[high] > pivot:
+            high -= 1
+        if(low < high):
+            arr[low], arr[high] = arr[high], arr[low]
+    arr[high], arr[pivot_index] = arr[pivot_index], arr[high]
+
+    # Returning high pointer to divide the arr into 2
+    return high
+
+
+# Function to do Quick sort
+def quickSort(arr, low, high):
+    if (low < high):
+        p = partition(arr, low, high)
+        quickSort(arr,low, p - 1)
+        quickSort(arr,p + 1, high)
+
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i])
