e97ad9d14e8d55519a9b651e8a5bc38e7670a8e7
anandreddysripathi <anandrdd5@gmail.com>
2021-09-13 00:05:38 -0500

    Completed preCourse-2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,21 +1,19 @@
-class BinarySearch { 
-    // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
-    } 
-  
-    // Driver method to test above 
-    public static void main(String args[]) 
-    { 
-        BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
-        int n = arr.length; 
-        int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
-        if (result == -1) 
-            System.out.println("Element not present"); 
-        else
-            System.out.println("Element found at index " + result); 
-    } 
-} 
+//Binary Search 
+public class Exercise_1{
+    public static void main(String[] args) {
+        int arr[]={1,4,6,9,12,14,16};
+        int searchElement=12;
+        int elementPosition=binarySearch(arr,0,arr.length,searchElement);
+        if(elementPosition==-1) System.out.println("element not found");
+        else System.out.println("element found at position "+elementPosition);
+    }
+    static int binarySearch(int arr[],int low,int high,int element){
+        if(high>=low){
+        int mid=((low+high)/2);
+        if(arr[mid]==element) return mid+1;
+        if(arr[mid]>element) return  binarySearch(arr,low,mid-1,element);
+        else if(arr[mid]<element) return  binarySearch(arr,mid+1, high, element);
+        }
+        return -1;
+    }
+}
