961d7a914382828ea0f84b7d796ce290128a5fd8
psnehas <sneha@sjsu.edu>
2021-09-12 19:18:51 -0500

    Completed Precourse 2 - Please review

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,44 @@
-# Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+# Time complexity: O(n^2) worst case and O(n log n) average case
+# Space complexity: O(n)
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+# Python program for implementation of Quicksort Sort
+from collections import deque
+# give you explanation for the approach
+
+
+def partition(arr, low, high):
+    # mark the low element as pivot for partitioning
+    pivot = arr[low]
+    start, end = low, high
+    while start < end:
+        # move the start pointer forward as long as the elements are less than/equal to the pivot element
+        while start <= end and arr[start] <= pivot:
+            start += 1
+        # move the end pointer backward as long as the elements are greater than pivot
+        while start <= end and arr[end] > pivot:
+            end -= 1
+        # swap start and end elements
+        if start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+    # swap the end and the pivot(marked by arr[low])
+    arr[end], arr[low] = arr[low], arr[end]
+    return end
+
+
+# Function to do Quick sort
+def quickSort(arr, low, high):
+    # retrieve partition index and call quick sort twice with each partition
+    if low < high:
+        partitionIndex = partition(arr, low, high)
+        quickSort(arr, low, partitionIndex-1)
+        quickSort(arr, partitionIndex+1, high)
+    # return arr
+
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]),
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,47 @@
-# Python program for implementation of MergeSort 
+# Time complexity: O(nlog n)
+# Space complexity: O(n)
+
+# Idea is to split the array into two halves sort the two halves while merging them together. Divide and conquer strategy
+# Python program for implementation of MergeSort
 def mergeSort(arr):
-  
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
-    #write your code here
-  
-# driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+    if len(arr) == 1:
+        return arr
+    mid = len(arr)//2
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    left_idx, right_idx = 0, 0
+    result = []
+    # as long as the indices are within limits, add the minimum elements to the result array and advance respective indices
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] < right[right_idx]:
+            result.append(left[left_idx])
+            left_idx += 1
+        else:
+            result.append(right[right_idx])
+            right_idx += 1
+    # if there are any elements left in either left or right array, add the to the result
+    if left_idx < len(left):
+        result += left[left_idx:]
+    if right_idx < len(right):
+        result += right[right_idx:]
+    return result
+
+# Code to print the list
+
+
+def printList(arr):
+    print(arr)
+
+
+# driver code to test the above code
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    printList(arr)
+    arr = mergeSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
