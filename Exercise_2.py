2965fccefc7a8233898dd7f11dc89a8826302b75
MANJARI KULKARNI <manjari.kulkarni386@gmail.com>
2021-09-18 14:46:28 -0500

    Completed PreCourse-2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,39 @@
-# Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+# Python program for implementation of Quicksort Sort
+#Time Complexity = O(n^2) - worst case, O(nlogn) - best case
+#Space Complexity = O(logn)
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
+
+def partition(arr, low, high):
+
+    pivot = arr[high] #assigning pivot as last element
+
+    i = low - 1 # pointer i to index -1
+
+    for j in range(low,high): # run j pointer from start to end of arr
+        if arr[j] < pivot: # each time checking if current element is less than pivot
+            i = i + 1 #if yes keep incrementing i pointer
+            arr[i],arr[j] = arr[j],arr[i] # and swap elements - To make 2 groups (1st grp = elements less then pivot, 2nd grp = elements less then pivot)
+
+    arr[i+1],arr[high] = arr[high],arr[i+1] #At last swap element i+1 index with pivot element - Now pivot element comes in middle
+
+    return i+1 # returning index of pivot
+
+# Function to do Quick sort
+def quickSort(arr, low, high):
+    if low >= high: # sort is complete or nothing to sort
+        return
+
+    p = partition(arr,low,high)
+
+    quickSort(arr,low,p-1) # recursively call QS to left portion of pivot
+    quickSort(arr,p+1,high) # recursively call QS to right portion of pivot
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n - 1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]), 
   
  
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,45 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# Time Complexity = O(n)
+# Space Complexity = O(n)
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
+# Node class
+class Node:
+
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self): # initializing linkedlist object
+        self.head = None
+
+    def push(self, new_data): # code to create new node at front of linkedlist
+
+        newnode = Node(new_data)
+        newnode.next = self.head
+        self.head = newnode
+
+    # Function to get the middle of the linked list - Using 2 ptr approach
+    def printMiddle(self):
+        firstptr = self.head
+        secondptr = self.head
+
+        if self.head is not None: # start only if linked list is not empty
+
+            while(secondptr.next is not None and secondptr.next.next is not None): # loop until secondptr reaches end of linkedlist
+                secondptr = secondptr.next.next
+                firstptr = firstptr.next
+            print("Middle Element =  ",firstptr.data) # when secondptr reaches end, firstptr has to point to middle of linkedlist - because logic is
+            # travel distance of first ptr = exactly 1/2 * travel distance of second ptr
+
+
+
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2)
+list1.push(3)
+list1.push(1)
 list1.printMiddle() 
