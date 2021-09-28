b802fdc7bafdf504a622457c14018e483bb08b7e
Tanmay Patel <86372482+Tanmay0511@users.noreply.github.com>
2021-09-27 20:59:42 -0400

    Update Exercise_3.java

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,53 +1,51 @@
-class LinkedList 
-{ 
-    Node head; // head of linked list 
-  
-    /* Linked list node */
-    class Node 
-    { 
-        int data; 
-        Node next; 
-        Node(int d) 
-        { 
-            data = d; 
-            next = null; 
-        } 
-    } 
-  
-    /* Function to print middle of linked list */
-   //Complete this function
-    void printMiddle() 
-    { 
-        //Write your code here
-	//Implement using Fast and slow pointers
-    } 
-  
-    public void push(int new_data) 
-    { 
-        Node new_node = new Node(new_data); 
-        new_node.next = head; 
-        head = new_node; 
-    } 
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this :
 
-    public void printList() 
-    { 
-        Node tnode = head; 
-        while (tnode != null) 
-        { 
-            System.out.print(tnode.data+"->"); 
-            tnode = tnode.next; 
-        } 
-        System.out.println("NULL"); 
-    } 
-  
-    public static void main(String [] args) 
-    { 
-        LinkedList llist = new LinkedList(); 
-        for (int i=15; i>0; --i) 
-        { 
-            llist.push(i); 
-            llist.printList(); 
-            llist.printMiddle(); 
-        } 
-    } 
-} 
+# Your code here along with comments explaining your approach
+
+# Node class  
+class Node:  
+
+    # Function to initialise the node object  
+    def __init__(self, data):  
+        self.data = data
+        self.next = None
+
+class LinkedList: 
+
+    def __init__(self): 
+        self.head = None
+
+
+    def push(self, new_data): 
+
+
+    def push(self, new_data): #Allocating the Node and putting in the data, making next of new Node as head
+        new_node = Node(new_data)
+        new_node.next = self.head
+
+        self.head = new_node #Moving the head to point to new Node
+
+    # Function to get the middle of  
+    # the linked list 
+    def printMiddle(self): 
+    def printMiddle(self): #Traversing linked list using slow and fast pointers. Move one pointer by one and other pointer by two. When the fast pointer reaches end slow pointer will reach middle of the linked list.
+        slow_ptr = self.head
+        fast_ptr = self.head
+
+        if self.head is not None:
+            while (fast_ptr is not None and fast_ptr.next is not None):
+                fast_ptr = fast_ptr.next.next
+                slow_ptr = slow_ptr.next
+            print("The middle element is: ", slow_ptr.data)
+
+
+# Driver code 
+list1 = LinkedList() 
+list1.push(5) 
+list1.push(4) 
+list1.push(2) 
+list1.push(3) 
+list1.push(1) 
