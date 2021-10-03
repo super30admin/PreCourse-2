f63a03b8d6aebd827c55e89b68aa06c8cd94131a
jaidevmane <devmane.j@northeastern.edu>
2021-10-03 03:20:33 -0400

    Completed Sprint PreCourse-2 - Please review

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,43 @@
-# Python code to implement iterative Binary  
-# Search. 
+# Time Complexity : O(log(N))
+# Space Complexity : O(1)
+
+# Exercise 1 : Binary Search
+# Python code to implement iterative Binary Search. 
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
   
   #write your code here
+  if r >=l:
+
+    #mid element 
+    mid = (l + r) //2
+
+    #if the mid element is equal to x, return x
+    if arr[mid] == x:
+      return mid
+
+    #if x is smaller than mid, then x will be found in left subarray
+    elif arr[mid] > x:
+      return binarySearch(arr,l,mid-1,x)
+
+    #else if not found anywhere x is in right subarray
+    else:
+      return binarySearch(arr,mid+1, r, x)
+
+  #if x is not found return -1
+  return -1
   
-    
   
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
+x = 2
   
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print ("Element is present at index % d" %result) 
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,42 @@
+# Time Complexity : O(N log(N))
+# Space Complexity : O(log(N))
+
+# Exercise 2 : Quick Sort
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
+    #setting pivot to low pointer initially
+    pivot_index = low
+    pivot = arr[pivot_index]
+
+    #base condition, perform loop while low<high, when low overtakes high we will escape while loop and swap high and pivot element
+    while low < high:
+        # perform check while low does not exceed length of array and till we find element that is greater than pivot
+        while low < len(arr) and arr[low] <= pivot:
+            low += 1
+        #perform check till we find element that is lower than pivot
+        while arr[high] > pivot:
+            high -= 1
+        #if the low pointer overtakes high the last time, we will stop decrementing or incrementing, and swap both elements respectively
+        if low < high:
+            arr[low], arr[high] = arr[high], arr[low]
+    #once low overtakes high, we will swap the pivot element with high, and now we have pivot element sorted
+    arr[high],arr[pivot_index] = arr[pivot_index], arr[high]
+    #we simply return the value of the high element that we swapped, so we can proceed to partition
+    return high
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
-    #write your code here
+
+    if(low < high):
+        #this will give us partition element, which is already sorted, we have to sort the division before and after this element
+        p = partition(arr, low, high)
+
+        #run quicksort recursively on the before and after sorted division respectively
+        quickSort(arr, low, p - 1)
+        quickSort(arr, p + 1, high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
