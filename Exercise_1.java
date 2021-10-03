8d277b7dfa48a1c14a6faa1eac37fb4f6e4d12f6
ramkr508 <ramkr508@gmail.com>
2021-10-02 22:15:45 -0500

    Completed Sprint Precourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,27 @@
-class BinarySearch { 
+//time complexity is O(log n)
+//space complexity is O(1)
+class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
+    {
+        //if right is greater or equal to left
+        if( r >= l) {
+            //take mid element , to avoid int overflow subtract r-l to get r
+            int mid = l + (r-l) /2;
+
+            // if  middle element is the target, return middle element
+            if(arr[mid] == x)
+                return mid;
+            // if middle element is greater than target,move right to mid -1,call recursively again
+            else if( arr[mid] > x) {
+              return  binarySearch(arr, l, mid-1, x);
+            }
+            else
+                // if middle element is less  than target,move left  to mid +1,call recursively again
+               return  binarySearch(arr, mid +1, r, x);
+
+        }
+        return -1;
     } 
   
     // Driver method to test above 
