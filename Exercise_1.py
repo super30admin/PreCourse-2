283f2a8c304fa70a9fcfac47a985a88df536f879
Prashant Mittal <prashant.mittal2@globallogic.com>
2021-10-03 22:14:05 -0500

    Assignment2 completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,35 @@
 # Python code to implement iterative Binary  
 # Search. 
-  
+
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
+def binarySearch(arr, l, r, x):
+    """
+        time complexity: O(log(n))
+        space complexity: O(1)
+    """
+    mid = 0
+    while r >= l:
+        mid = (l + r) // 2
+        if arr[mid] < x:
+            l = mid + 1
+        elif arr[mid] > x:
+            r = mid - 1
+        else:
+            return mid
+    else:
+        return -1
+
+
+
 # Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
+arr = [2, 3, 4, 10, 40]
 x = 10
-  
+
 # Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
-if result != -1: 
-    print "Element is present at index % d" % result 
-else: 
-    print "Element is not present in array"
+result = binarySearch(arr, 0, len(arr) - 1, x)
+
+if result != -1:
+    print("Element is present at index % d" % result)
+else:
+    print("Element is not present in array")
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,49 @@
-# Python program for implementation of MergeSort 
+# Python program for implementation of MergeSort
+"""
+    time complexity: O(nlog(n))
+    space complexity: O(n)
+"""
+
+
 def mergeSort(arr):
-  
-  #write your code here
-  
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        arr_left = arr[:mid]
+        arr_right = arr[mid:]
+        mergeSort(arr_left)
+        mergeSort(arr_right)
+
+        i = j = k = 0
+        while i < len(arr_left) and j < len(arr_right):
+            if arr_left[i] < arr_right[j]:
+                arr[k] = arr_left[i]
+                i += 1
+            else:
+                arr[k] = arr_right[j]
+                j += 1
+            k += 1
+        while i < len(arr_left):
+            arr[k] = arr_left[i]
+            i += 1
+            k += 1
+        while j < len(arr_right):
+            arr[k] = arr_right[j]
+            j += 1
+            k += 1
+
+
 # Code to print the list 
-def printList(arr): 
-    
-    #write your code here
-  
+def printList(arr):
+    for x in arr:
+        print(x, end=' ')
+    print()
+
+
 # driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
