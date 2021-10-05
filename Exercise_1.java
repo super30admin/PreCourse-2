76e8620895405fe39c8f0ffe47fe5c0fcc560ac5
Ramya Iyer <ramya.iyer06@gmail.com>
2021-10-04 18:43:40 -0700

    precourse 2 submission

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,21 +1,42 @@
-class BinarySearch { 
+
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
+    int binarySearch(int arr[], int l, int r, int x)
+    {
         //Write your code here
-    } 
-  
+        while(l<=r) {
+            int mid = l + ((r-l)/2); //find midpoint, if target - arr[mid] then return mid else adjust left, right values based on target val
+            if(arr[mid] == x)
+                return mid;
+            else if(arr[mid]<x) {
+                l = mid + 1;
+            } else {
+                r = mid - 1 ;
+            }
+        }
+        return -1;
+    }
+
     // Driver method to test above 
-    public static void main(String args[]) 
-    { 
-        BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
-        int n = arr.length; 
-        int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
-        if (result == -1) 
-            System.out.println("Element not present"); 
+    public static void main(String args[])
+    {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 3;
+        int result = ob.binarySearch(arr, 0, n - 1, x);
+        System.out.println(result);
+        if (result == -1)
+            System.out.println("Element not present");
         else
-            System.out.println("Element found at index " + result); 
-    } 
+            System.out.println("Element found at index " + result);
+    }
 } 
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,42 +1,95 @@
-class MergeSort 
-{ 
-    // Merges two subarrays of arr[]. 
-    // First subarray is arr[l..m] 
-    // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
-    } 
-  
-    // Main function that sorts arr[l..r] using 
-    // merge() 
-    void sort(int arr[], int l, int r) 
-    { 
-	//Write your code here
-        //Call mergeSort from here 
-    } 
-  
+// Time Complexity : O(nlog n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : no
+// Any problem you faced while coding this :
+class MergeSort
+{
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r)
+    {
+        //Your code here
+        
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sort(int arr[], int l, int r)
+    {
+        //Write your code here
+        //Call mergeSort from here
+        if(l >= r)
+            return;
+
+        int mid = l+(r-l)/2;
+      //  int[] left = new int[mid];
+     //   int[] right = new int[len-mid];
+        sort(arr, l, mid);
+        sort(arr, mid+1, r);
+        merge(arr, l,mid,r);
+
+    }
+
     /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
-  
-    // Driver method 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
-  
-        System.out.println("Given Array"); 
-        printArray(arr); 
-  
-        MergeSort ob = new MergeSort(); 
-        ob.sort(arr, 0, arr.length-1); 
-  
-        System.out.println("\nSorted array"); 
-        printArray(arr); 
-    } 
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String args[])
+    {
+        int arr[] = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Given Array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length-1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
 } 
