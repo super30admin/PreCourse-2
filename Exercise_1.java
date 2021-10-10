cc9c1dbc854b552a8f9924e23c4ee16d8b687400
sanyami30 <bhavsarsanyami@gmail.com>
2021-10-09 20:32:14 -0700

    Completed PreCourse 2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,12 +1,25 @@
-class BinarySearch { 
+//Time Complexity: O(log n)
+public class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
-    } 
+        
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+       
+        if (arr[mid] == x)
+             return mid;
   
+        if (arr[mid] > x)
+             return binarySearch(arr, l, mid - 1, x);
+  
+          
+         return binarySearch(arr, mid + 1, r, x);
+    } 
+    return -1;
+    }
     // Driver method to test above 
-    public static void main(String args[]) 
+   public static void main(String args[]) 
     { 
         BinarySearch ob = new BinarySearch(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,4 +1,5 @@
-class LinkedList 
+//Time Complexity: O(n/2)
+public class LinkedList 
 { 
     Node head; // head of linked list 
   
@@ -20,6 +21,17 @@ class LinkedList
     { 
         //Write your code here
 	//Implement using Fast and slow pointers
+	
+        Node slow_ptr = head;
+        Node fast_ptr = head;
+         
+            while (fast_ptr != null && fast_ptr.next != null)
+            {
+                fast_ptr = fast_ptr.next.next;
+                slow_ptr = slow_ptr.next;
+            }
+            System.out.println("The middle element is [" +
+                                slow_ptr.data + "] \n");
     } 
   
     public void push(int new_data) 
