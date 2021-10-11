1ae5bb7e016d24a6c36b956521143341937dfc2b
Kavin T <43201092+Kavint9@users.noreply.github.com>
2021-10-11 07:54:41 -0500

    Completed Pre-course 2 - please review

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,35 @@
+# Time Complexity : O[log(n)]
+# Space Complexity : O(1)
+
 # Python code to implement iterative Binary  
 # Search. 
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
+  if arr[l] > x or arr[r] < x:
+    return -1
+  while l <= r:
+    m = int((l + r)/2)
+    if arr[m] == x:
+      return m
+    elif arr[m] < x:
+      l = m + 1
+    elif arr[m] > x:
+      r = m - 1
+  return -1
+
   
     
   
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
+x = 1
   
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index " + str(result))
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,42 @@
+# Time Complexity : O[nlogn]
+# Space Complexity : O[1]
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
+  if len(arr) > 1:
+    mid = len(arr)//2
   
+    L = arr[:mid]
+    R = arr[mid:]
+  
+    mergeSort(L)
+    mergeSort(R)
+  
+    i = j = k = 0
+    while i < len(L) and j < len(R):
+      if L[i] < R[j]:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+      else:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+  
+      # Checking if any element was left
+      while i < len(L):
+        arr[k] = L[i]
+        i += 1
+        k += 1
+  
+      while j < len(R):
+        arr[k] = R[j]
+        j += 1
+        k += 1
   #write your code here
   
 # Code to print the list 
 def printList(arr): 
-    
+    print(arr)
     #write your code here
   
 # driver code to test the above code 
