ce18b645f94eeb9e4d67e74035c5e5d564cf6f5c
Ashish Kumar <kumar.as@husky.neu.edu>
2021-11-17 01:19:17 -0600

    completed precourse - 2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,21 +1,36 @@
+#Time Complexity : O(N)
+# Space Complexity :O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
   
     def push(self, new_data): 
-        
-  
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+    
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
-
+        #making 2 pointer , one goes two next and other just go tot he next. 
+        # so when the fast on goes to end , the slow points to middle
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        print("the middle element is ",slow.data)    
 # Driver code 
 list1 = LinkedList() 
 list1.push(5) 
