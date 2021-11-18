f3610892576e8e6411f8d050c9557a6915417352
AnuragJacob <69404405+AnuragJacob@users.noreply.github.com>
2021-11-18 07:53:25 -0500

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,7 +2,22 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
+  pivot = arr[low]
+  i = low, j= high
   
+  ##swapping conditions during comparison
+  while i<j:
+    if arr[i]>pivot and arr[j]<pivot:
+      arr[i],arr[j] = arr[j],arr[i]
+    
+    elif arr[i]<pivot:
+      i++
+      
+    elif arr[j]>pivot:
+      j--
+  
+  arr[low],arr[j] = arr[j],arr[low] ##swap pivot with j after i is no longer smaller than j
+  return j ##j is the true position of the pivot. Ensures that values to the left of pivot are smaller and those on the right are larger
   
     #write your code here
   
@@ -11,6 +26,10 @@ def partition(arr,low,high):
 def quickSort(arr,low,high): 
     
     #write your code here
+    if low<high:
+      j = partition(arr,low,high) ##returns correct element of pivot
+      quickSort(arr,low,j) ##elements left of pivot get rearranged
+      quickSort(arr,j+1,high) ##elements right of pivot get rearranged
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
