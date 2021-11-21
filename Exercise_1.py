71a1b5a7cedaf77648d395c7345273b17e27b2f6
ajaydevmane <94388564+ajaydevmane@users.noreply.github.com>
2021-11-22 01:04:17 +0530

    Completed Sprint PreCourse2

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,10 +3,21 @@
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
+
+import math
 def binarySearch(arr, l, r, x): 
   
   #write your code here
+  while l<=r:
+    mid=math.floor((l+r)/2)
+    if x==arr[mid]:
+      return mid
+    if x<arr[mid]:
+      r=mid-1
+    else:
+      l=mid+1
   
+  return -1
     
   
 # Test array 
@@ -17,6 +28,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index", result)
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,52 @@
 # Python program for implementation of MergeSort 
-def mergeSort(arr):
+def mergeSort(arr, l, h):
   
   #write your code here
+  if l < h:
+    mid = (l+h)//2
+    mergeSort(arr, l, mid)
+    mergeSort(arr, mid+1, h)
+    merge(arr, l, mid, h)
   
 # Code to print the list 
 def printList(arr): 
+  for i in arr:
+    print(i)
     
     #write your code here
+
+def merge(arr, l, m, h):
+  i = l
+  j = m+1
+  temp = [0 for i in range(h+1)]
+  k = l
+  while(i<=m and j<=h):
+    if arr[i]<=arr[j]:
+      temp[k] = arr[i]
+      i = i+1
+      k = k+1   
+    else:
+      temp[k] = arr[j]
+      j = j+1
+      k = k+1
+  while(i<=m):
+    temp[k] = arr[i]
+    i = i+1
+    k = k+1
+
+  while(j<=h):
+    temp[k] = arr[j]
+    j = j+1
+    k = k+1
+
+  for i in range(l, h+1):
+    arr[i] = temp[i]
   
 # driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end="\n")  
     printList(arr) 
-    mergeSort(arr) 
+    mergeSort(arr, 0, len(arr)-1) 
     print("Sorted array is: ", end="\n") 
     printList(arr) 
