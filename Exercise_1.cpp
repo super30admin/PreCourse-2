047b46fd58bd182312d09323ae37a24b009dc525
Arjun Chaturvedi <chaturvedi.arjun.07@gmail.com>
2021-11-20 16:43:01 -0800

    Precourse 2 solution.

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,11 +1,34 @@
-#include <stdio.h> 
+#include <bits/stdc++.h> 
   
 // A recursive binary search function. It returns 
 // location of x in given array arr[l..r] is present, 
 // otherwise -1 
+
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+
+/* Check if the given element is present at the mid of array.
+*  If is present then return the mid index.
+*  If mid element is less than the target x then search the elemeent between mid index + 1 and R.
+*  If mid element is greater than the target x then search the elemeent between L and mid index - 1.
+*  If L is greater than r then return -1 as element is not present in array. 
+*/
 int binarySearch(int arr[], int l, int r, int x) 
 {   
-    //Your Code here 
+    //Your Code here
+
+    if (l > r)
+        return -1;
+
+    int mid = (l + r) / 2;
+
+    if (arr[mid] == x)
+        return mid;
+
+    if (arr[mid] < x)
+        return binarySearch(arr, mid + 1, r, x);
+
+    return binarySearch(arr, l, mid - 1, x);     
 } 
   
 int main(void) 
