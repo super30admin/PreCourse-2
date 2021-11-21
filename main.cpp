57269833c264c7112cc5ae2b4e2f427cbd8d8eb7
SaiGowtham-11 <61152047+SaiGowtham-11@users.noreply.github.com>
2021-11-20 22:47:09 -0800

    excercise - 4

--- main.cpp
+++ main.cpp
@@ -1,62 +1,73 @@
-#include<bits/stdc++.h>
-#include <stdio.h>
-using namespace std;
-// Time complexity --> O(Logn)
-// Space Complexity --> O(1)
-// Struct
-struct Node
-{
-    int data;
-    struct Node* next;
-};
+#include<stdlib.h>
+#include<stdio.h>
 
-/* Function to get the middle of the linked list*/
-void printMiddle(struct Node *head)
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
 {
-    Node * fast = head;
-    Node * slow = head;//14
-    while(fast != NULL){
-       fast = fast->next;
-       if(fast != NULL){
-           fast = fast->next;
-       }
-        if(fast != NULL){
-            slow = slow->next;
+    int i = l;
+    int j = m+1;
+    int k = l;
+    int B[r+1];
+    while (i <= m && j <= r){
+        if (arr[i] < arr[j]){
+            B[k++] = arr[i++];
+        } else {
+            B[k++] = arr[j++];
         }
     }
-    cout<<"The middle element is " <<slow->data<<endl;
-}
-
-// Function to add a new node
-void push(struct Node** head_ref, int new_data)
-{
-    struct Node* new_node = new Node;
-    new_node->data = new_data;
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
-}
-
-// A utility function to print a given linked list
-void printList(struct Node *ptr)
-{
-    while (ptr != NULL)
-    {
-        printf("%d->", ptr->data);
-        ptr = ptr->next;
+    while (i <= m){
+        B[k++] = arr[i++];
     }
-    printf("NULL\n");
+    while (j <= r){
+        B[k++] = arr[j++];
+    }
+    for (int i=l; i<=r; i++){
+        arr[i] = B[i];
+    }
 }
 
-// Driver Code
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    int p;
+    for (p=2; p<=r+1; p=p*2){
+        for (int i=0; i+p-1<r+1; i=i+p){
+            int low = i;
+            int high = i+p-1;
+            int mid = (low+high)/2;
+            merge(arr, low, mid, high);
+        }
+    }
+    if (p/2 < r+1){
+        merge(arr, 0, p/2-1, r);
+    }
+}
+
+/* UTILITY FUNCTIONS */
+/* Function to print an array */
+void printArray(int A[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+/* Driver program to test above functions */
 int main()
 {
-    struct Node* head = NULL;
-    for (int i=15; i>0; i--)
-    {
-        push(&head, i);
-        printList(head);
-        printMiddle(head);
-    }
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
 
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
     return 0;
 }
