07a6a03bec322c22491e6fd0ac6fa4a7c77afb09
Parangi Patel <49344179+parangi98@users.noreply.github.com>
2021-11-22 13:56:45 -0600

    Update Exercise_4.java

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,29 +1,58 @@
 class MergeSort 
 { 
-    // Merges two subarrays of arr[]. 
-    // First subarray is arr[l..m] 
-    // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
-    } 
-  
-    // Main function that sorts arr[l..r] using 
-    // merge() 
-    void sort(int arr[], int l, int r) 
-    { 
-	//Write your code here
-        //Call mergeSort from here 
-    } 
-  
-    /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
+    void merge(int arr[], int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int leftArray[] = new int[n1];
+        int rightArray[] = new int[n2];
+        for (int i = 0; i < n1; ++i)
+        	leftArray[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+        	rightArray[j] = arr[m + 1 + j];
+        
+        int i = 0, j = 0;
+        int k = l;
+        
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <=rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            }
+            else {
+                arr[k] =rightArray[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+    void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            int m =l+ (r-l)/2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+    
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
   
     // Driver method 
     public static void main(String args[]) 
