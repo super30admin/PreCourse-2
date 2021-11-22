560853e9e7fdc6bbca5a78be7931d82bce05981e
Suhas Reddy <suhassuchi@gmail.com>
2021-11-21 20:44:05 -0500

    Completed code changes

--- Exercise_5.cpp
+++ Exercise_5.cpp
@@ -1,5 +1,12 @@
-#include <bits/stdc++.h> 
-using namespace std; 
+// Time Complexity : O(NlogN)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : NA
+// Any problem you faced while coding this : None
+
+//#include<bits/stdc++.h>  
+#include<iostream>
+#include<stack>
+using namespace std;
   
 // A utility function to swap two elements 
 void swap(int* a, int* b) 
@@ -10,9 +17,23 @@ void swap(int* a, int* b)
 } 
   
 /* This function is same in both iterative and recursive*/
-int partition(int arr[], int l, int h) 
+int partition(int arr[], int low, int high) 
 { 
     //Do the comparison and swapping here 
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
 } 
   
 /* A[] --> Array to be sorted,  
@@ -21,6 +42,29 @@ h --> Ending index */
 void quickSortIterative(int arr[], int l, int h) 
 { 
     //Try to think that how you can use stack here to remove recursion.
+    stack<pair<int, int>> s;
+
+    s.push(make_pair(l, h));
+
+    while (!s.empty())
+    {
+
+        int start = s.top().first;
+        int end = s.top().second;
+        s.pop();
+
+        int pivot = partition(arr, start, end);
+
+        if (pivot - 1 > start) 
+        {
+            s.push(make_pair(start, pivot - 1));
+        }
+
+        if (pivot + 1 < end) 
+        {
+            s.push(make_pair(pivot + 1, end));
+        }
+    }
 } 
   
 // A utility function to print contents of arr 
