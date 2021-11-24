29a6d6a6c1ea0596d6013cf479478d84c66c6993
Aditya <abhogte@gmail.com>
2021-11-24 15:25:13 -0600

    Completed Pre-course

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,15 +1,38 @@
+#Time complexity: O(n * n)
+#space complexity: O(n)
+
+#The lowest value is taken is taken as pivot and is  then sorted based on the pivot. Recursion technique is used here to solve for this one.
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
+    if low < high:
+        rec = partition(low, high, arr)
+
+        quickSort(arr, rec - 1, arr)
+        quickSort(rec + 1, high, arr)
   
     #write your code here
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
+    pivotI = low
+    pivot = arr[pivotI]
+
+    while low < high:
+        while low < len(arr) and arr[low] <= pivot:
+            low += 1
+        while arr[high] > pivot:
+            high -= 1
+        if (low < high):
+            arr[low], arr[high] = arr[high], arr[low]
+
+        arr[high], arr[pivotI] = arr[pivotI], arr[high]
+        return high
+        
+
+
     #write your code here
   
 # Driver code to test above 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,26 @@
+# Time complexity = o(nlog(n))
+#space complexity: O(n)
 # Python program for implementation of Quicksort
+# iterative quick sort follows the same technique as divide and concure. This question made me to think the right approach and is willing to learn more in class.
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+  pivot = arr[h]
+  small = (l - 1)
+
+  for i in range(l, h):
+    if arr[i] <= pivot:
+      small += 1
+      arr[small], arr[i] = arr[i], arr[small]
+  arr[small + 1], arr[h] = arr[h], arr[small + 1]
+  return(small + 1)
+
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+  if l < h:
+    part = partition(arr, l, h)
+
+    quickSortIterative(arr, l, part-1)
+    quickSortIterative(arr, part + 1, h)
 
