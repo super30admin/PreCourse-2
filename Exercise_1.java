4d6174d0c5bbad26d720bc11f01964b01ee71b52
ehassah10 <ehassah10@gmail.com>
2021-12-14 18:32:08 -0800

    exercise 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,32 @@
+/*
+ Time complexity: O(logN) where N is size of array. Assumption made that the array is sorted in ascending order
+ Scpace complexity: O(1)
+ Did this code successfully run on Leetcode : Yesß
+ Any problem you faced while coding this : No
+*/
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        int idx = -1;
+        if(arr == null || arr.length == 0){
+            return idx;
+        }
+
+        while(l <= r){
+            int mid = l  + (r - l)/2;
+            if(arr[mid] == x){
+                return mid;
+            }else if(x < arr[mid]){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+
+        return idx;
     } 
   
     // Driver method to test above 
