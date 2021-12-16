499a8a150d8da237aef85aecc3e17915a94882f2
ehassah10 <ehassah10@gmail.com>
2021-12-15 20:35:03 -0800

    ex 4

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,10 @@
+/*
+ Time complexity: O(NlogN) where N is size of array.
+ Scpace complexity: O(N) to maintaing the call recursive call stack
+ Did this code successfully run on Leetcode : Yes
+ Any problem you faced while coding this : Yes, i had to brushup merge sort algorithm
+*/
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -6,6 +13,30 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+       int[] tempArr = new int[r - l + 1];
+       int i = l, j = m + 1, pos = 0;
+
+       while(i <= m && j <= r && pos < tempArr.length){
+           if(arr[i] < arr[j]){
+               tempArr[pos++] = arr[i++];
+           }else{
+               tempArr[pos++] = arr[j++];
+           }
+       }
+
+       //below while loop will handle cases when either of i or j reaches it's end
+       while(i <= m){
+        tempArr[pos++] = arr[i++];
+       }
+       while(j <= r ){
+        tempArr[pos++] = arr[j++];
+       }
+       
+       //we copy sorted value to the actual array space
+       pos = l;
+       for(i = 0; i < tempArr.length; i++){
+            arr[pos++] = tempArr[i];
+        }
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +45,17 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+        if(l < r){
+            // more than one elements in current array space
+            // split the array space into two from middle and call sort function on them
+            int mid = l + (r - l)/2;
+            sort(arr, l, mid); // perform sort operation on half of array
+            sort(arr, mid + 1, r); // perform sort operarion on other half of array
+            
+            // after sort function returns, we have two arrays space which are sorted among them
+            // we need to merge two sorted array
+            merge(arr, l, mid, r);
+        }
     } 
   
     /* A utility function to print array of size n */
