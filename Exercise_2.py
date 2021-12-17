11a2b0a52f0ea11c23f5671bbeb432de01baecd6
Aniruddha Ingle <aniruddhaingle@aniruddhas-mbp.lan>
2021-12-17 12:44:42 -0600

    Sprint: Problem ID2 Completed. Problem ID4 Tweaked.

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,24 +2,56 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
-    pass
-  
+    '''Always pass valid index positions in low and high'''
+
+
+    pivot = arr[low] #Since, we use first element as pivot. The placement position will be dictated by right.
+                     #This is because right will end on an element smaller than pivot. Hence, we can swap that element
+                     #to the beginning and maintain the orderedness of the left sub array and the right subarray.
+    left = low + 1
+    right = high
+
+    while left <= right:
+
+        while left <= high and arr[left] < pivot:
+            left += 1
+        while right > low and arr[right] >= pivot:  # Any implications of the equality in this condition?
+            right -= 1                              # Not really. If pivot is 5. 5 can be on the right or left of the pivot.
+                                                    # This equality in the incr of left or right will only decide 
+                                                    # which subproblem to add this equal element to. Outcome is the same.
+            
+        
+        if left < right: #perform swap
+            arr[left], arr[right] = arr[right], arr[left]
+
+    
+    arr[low], arr[right] = arr[right], arr[low]
+
+    return right
   
     #write your code here
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    pass
     
-    #write your code here
-  
+    if low >= high: # if low > high, invalid sub problem. if low == high, already solved sub problem.
+        return
+    idx = partition(arr, low, high)
+    quickSort(arr, low, idx - 1)
+    quickSort(arr, idx + 1, high)
+    
 # Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
+arr = [10, 7, 8, 9, 1, 5]
+print("Original Array: {}".format(arr))
 n = len(arr) 
 quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
+print ("Sorted array is: {}".format(arr)) 
+
+arr = [-1,2,3,1,2,3]
+print("Original Array: {}".format(arr))
+n = len(arr) 
+quickSort(arr,0,n-1) 
+print ("Sorted array is: {}".format(arr)) 
   
  
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,36 +1,37 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
 
-  if len(arr) == 1:
-    print(arr)
-    return arr
+  def recur(arr):
+    if len(arr) == 1:
+      return arr
 
-  left = 0
-  right = len(arr) - 1
-  middle = left + (right - left) // 2
+    left = 0
+    right = len(arr) - 1
+    middle = left + (right - left) // 2
 
-  leftSort = mergeSort(arr[left:middle + 1])
-  rightSort = mergeSort(arr[middle + 1:right + 1]) 
+    leftSort = recur(arr[left:middle + 1])
+    rightSort = recur(arr[middle + 1:right + 1]) 
 
-  p = 0
-  q = 0
-  currSorted = []
-  while p < len(leftSort) and q < len(rightSort):
-    if leftSort[p] <= rightSort[q]:
+    p = 0
+    q = 0
+    currSorted = []
+    while p < len(leftSort) and q < len(rightSort):
+      if leftSort[p] <= rightSort[q]:
+        currSorted.append(leftSort[p])
+        p += 1
+      else:
+        currSorted.append(rightSort[q])
+        q += 1
+    while p < len(leftSort):
       currSorted.append(leftSort[p])
       p += 1
-    else:
+    while q < len(rightSort):
       currSorted.append(rightSort[q])
       q += 1
-  while p < len(leftSort):
-    currSorted.append(leftSort[p])
-    p += 1
-  while q < len(rightSort):
-    currSorted.append(rightSort[q])
-    q += 1
-  
-  print(currSorted)
-  return currSorted
+      
+    return currSorted
+    
+  arr[:] = recur(arr)
 
   #write your code here
   
