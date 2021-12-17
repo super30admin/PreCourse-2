f016bce7dd6bd70ef2f8af10ca24a90c11e02fb4
Aniruddha Ingle <aniruddhaingle@aniruddhas-mbp.lan>
2021-12-17 17:52:19 -0600

    Problem ID4: Space Optimisation

--- Exercise_4.py
+++ Exercise_4.py
@@ -2,44 +2,42 @@
 # Best Case: O(N(log(N))) 
 # Worst Case: O(N(log(N)))  
 
-# Space Complexity : (Recursion Stack = log(N) + Auxiliarry Array = N)
-# Best, Worst, Average Case = O(N)
+# Space Complexity : (Recursion Stack = log(N) + Auxiliarry Array = 1)
+# Best, Worst, Average Case = O(log(N))
 
 # Did this code successfully run on Leetcode : N/A
 # Any problem you faced while coding this : No
 def mergeSort(arr):
 
-  def recur(arr):
-    if len(arr) == 1:
-      return arr
+  def recur(left, right):
+    if left == right:
+      return (left, right)
 
-    left = 0
-    right = len(arr) - 1
-    middle = left + (right - left) // 2
+    mid = left + (right - left) // 2
 
-    leftSort = recur(arr[left:middle + 1])
-    rightSort = recur(arr[middle + 1:right + 1]) 
-
-    p = 0
-    q = 0
-    currSorted = []
-    while p < len(leftSort) and q < len(rightSort):
-      if leftSort[p] <= rightSort[q]:
-        currSorted.append(leftSort[p])
-        p += 1
-      else:
-        currSorted.append(rightSort[q])
-        q += 1
-    while p < len(leftSort):
-      currSorted.append(leftSort[p])
-      p += 1
-    while q < len(rightSort):
-      currSorted.append(rightSort[q])
-      q += 1
-      
-    return currSorted
+    pBounds = recur(left, mid)    
+    qBounds = recur(mid + 1, right)
     
-  arr[:] = recur(arr)
+    p = pBounds[0]
+    q = qBounds[0]
+    qEnd = qBounds[1]
+
+    while p < q and q <= qEnd:
+      if arr[p] <= arr[q]: #if ele in left subarray less than ele in right subarray move left pointer up
+        p += 1
+      else: # rotate the subarray from index [p + 1] to q in the right direction. 
+            # Put element that was at q in index location p.
+        temp = arr[q] 
+        for i in range(q, p, -1):
+          arr[i] = arr[i - 1]
+        arr[p] = temp
+        p += 1
+        q += 1
+    
+    return (left, right) 
+    # Always return the left and right because in the call above we want to use these bounds to move elements in place.
+  
+  recur(0, len(arr) - 1)
 
   #write your code here
   
@@ -49,7 +47,7 @@ def printList(arr):
   
 # driver code to test the above code 
 if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
+    arr = [4, 2, 1, 4, 3,12, 11, 13, 6, 5, 7]  
     print ("Given array is", end="\n")  
     printList(arr) 
     mergeSort(arr) 
