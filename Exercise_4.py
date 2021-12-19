91c5a2b819c558e3c791852abe108e6295377426
LiberoPat <patrick.trias@gmail.com>
2021-12-18 23:41:09 -0800

    precourse 2 complete

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,50 @@
 # Python program for implementation of MergeSort 
+
+# Time Complexity : O(nlogN)
+# Space Complexity : O(N)
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this : maximum recursion depth exceeded error, had to fix print function
+# Find Mid Point of a Singly Linked List.
+
 def mergeSort(arr):
-  
-  #write your code here
+  if len(arr) > 1:
+    #write your code here
+    mid = len(arr) // 2
+    #left and right array, recursively then merge
+    L = arr[:mid]
+    R = arr[mid:]
+
+    mergeSort(L)
+    mergeSort(R)
+
+    i =0; j =0 ; k = 0
+
+    while i < len(L) and j < len(R):
+        if L[i] < R[j]:
+              arr[k] = L[i]
+              i += 1
+        else:
+              arr[k] = R[j]
+              j += 1
+        k += 1
+      
+            # Checking if any element was left
+    while i < len(L):
+        arr[k] = L[i]
+        i += 1
+        k += 1
+      
+    while j < len(R):
+          arr[k] = R[j]
+          j += 1
+          k += 1
+
   
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
   
 # driver code to test the above code 
 if __name__ == '__main__': 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,53 @@
 # Python program for implementation of Quicksort
+# Time Complexity : O(nlog(n))
+# Space Complexity : O(n), 
+# Did this code successfully run on Leetcode : Yes,
+# Any problem you faced while coding this : no
 
-# This function is same in both iterative and recursive
-def partition(arr, l, h):
+
+def partition(arr, low, high):
   #write your code here
+    i = (low-1)         # index of smaller element
+    pivot = arr[high]     # pivot
+ 
+    for j in range(low, high):
+ 
+        # If current element is smaller than or
+        # equal to pivot
+        if arr[j] <= pivot:
+ 
+            # increment index of smaller element
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+ 
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
 
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  stacklist = []
 
+  stacklist.append(l)
+  stacklist.append(h)
+
+  while stacklist:
+      high=stacklist.pop()
+      low=stacklist.pop()
+
+      p=partition(arr,low, high) #gets the pivot element
+
+      if p-1>low: #if elements are present on the left side of the pivot, we add the end points from low to p-1 to the stack
+        stacklist.append(low)
+        stacklist.append(p-1)
+
+      if p+1<high: #if elements are present on the right side of the pivot, we add the end points from p+1 to high to the stack
+        stacklist.append(p+1)
+        stacklist.append(high)
+
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
