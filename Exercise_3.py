a7e140f7ecd7006f4268ad9742c452bf0ed11782
shrivatsav <kumarshrivatsav98@gmail.com>
2021-12-18 18:52:07 -0600

    Fix Repo error and update all .py files

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,41 @@
+#  Time Complexity : O(n)
+#  Space Complexity : O(1) as we are using 2 variables.
+#  Did this code successfully run on Leetcode : Yes
+#  Any problem you faced while coding this : No
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data
+        self.next = None 
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head = None
         
   
-    def push(self, new_data): 
+    def push(self, new_data):
+        if(self.head == None):
+            self.head = Node(new_data)
+        else:
+            current = self.head
+            while(current.next != None):
+                current = current.next
+            current.next = Node(new_data)
         
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        i = self.head
+        j = self.head
+        # use 2 variables. i jumps 2 steps and j jumps 1 step each time till the end of the linked list.
+        while(i and i.next != None and i.next.next != None):
+            i = i.next.next
+            j = j.next
+        print(j.data)
 
 # Driver code 
 list1 = LinkedList() 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,16 +1,50 @@
-# Python program for implementation of MergeSort 
+#  Time Complexity : O(nlogn)
+#  Space Complexity : O(n)
+#  Did this code successfully run on Leetcode : Yes
+#  Any problem you faced while coding this : No
+# Python program for implementation of MergeSort
+
+def merge(a,b,arr):
+  # a and b are two sub lists which are to be merged.
+  i = 0
+  j = 0
+  k = 0
+  while(i< len(a) and j < len(b)): # until i or j reaches end of their respective lists.
+    if(a[i]<=b[j]):
+      arr[k] = a[i]
+      k += 1
+      i += 1
+    else:
+      arr[k] = b[j]
+      k += 1
+      j += 1
+  # add remaining elements from either a or b to the resultant list.
+  while(i<len(a)):
+    arr[k] = a[i]
+    i += 1
+    k += 1
+  while(j<len(b)):
+    arr[k] = b[j]
+    j += 1
+    k += 1
+
 def mergeSort(arr):
-  
-  #write your code here
+  # Split at the middle and recursively call mergeSort until each we get a list of single element.
+  if(len(arr) > 1):
+    mid = len(arr)//2
+    left_array = arr[:mid]
+    right_array = arr[mid:]
+    mergeSort(left_array)
+    mergeSort(right_array)
+    merge(left_array,right_array,arr)
   
 # Code to print the list 
-def printList(arr): 
-    
-    #write your code here
+def printList(arr):
+  print(arr)
   
 # driver code to test the above code 
 if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
+    arr = [12, 11, 13, 5, 6, 7]
     print ("Given array is", end="\n")  
     printList(arr) 
     mergeSort(arr) 
