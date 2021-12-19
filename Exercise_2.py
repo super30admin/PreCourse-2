ff2e19f43984fa0c108f7f9a6ae8965d0a91323e
Akshar Patel <aksharpatel144@gmail.com>
2021-12-19 00:25:10 -0500

    Completed PreCourse2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,15 +1,28 @@
+# Time Complexity : O(nlog(n)) Average case, O(n^2) worst case
+#  Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Also worked locally
+# Any problem you faced while coding this : No
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
+    j = low + 1
+    for i in range(low+1, high+1): 
+        if arr[i] < arr[low]:
+            arr[j], arr[i] = arr[i], arr[j] 
+            j +=1 
+    arr[low], arr[j-1] =  arr[j-1], arr[low]
+    return j-1
+
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
+    if low >= high: return 
+    pivot = partition(arr, low, high)
+    quickSort(arr, low, pivot-1)
+    quickSort(arr, pivot+1, high)
     #write your code here
   
 # Driver code to test above 
