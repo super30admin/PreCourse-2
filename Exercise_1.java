670a3681721f703da6209ef7ba94f09cca3d74e0
unknown <svkannawar@gmail.com>
2021-12-18 23:17:19 -0800

    Completed PreCourse-2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,36 @@
+// Time Complexity : O(1)
+// Space Complexity : O(log n)
+// Did this code successfully run on Leetcode : NA
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+		//initialize start and end
+		int start=l;
+		int end=r;
+		
+		//loop to check through the array
+		while(start<=end) {
+			int mid=(start+end)/2;
+		if(arr[mid]==x) {
+            //return index on finding the element
+			return mid;
+		}else if(x>arr[mid]) {
+			start=mid+1;
+			
+		}else if(x<arr[mid]) {
+			end=mid-1;
+		}
+		
+		}
+		//return -1 if loop is passed
+		return -1;
     } 
   
     // Driver method to test above 
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,53 +1,103 @@
+//Time Complexity : O(n)
+//Space Complexity :O(1)
+//Did this code successfully run on Leetcode : NA
+//Any problem you faced while coding this : got exception while writing condition for
+//fast pointer but resolved it quickly.
+
+
+//Your code here along with comments explaining your approach
+
 class LinkedList 
 { 
-    Node head; // head of linked list 
-  
-    /* Linked list node */
-    class Node 
-    { 
-        int data; 
-        Node next; 
-        Node(int d) 
-        { 
-            data = d; 
-            next = null; 
-        } 
-    } 
-  
-    /* Function to print middle of linked list */
-   //Complete this function
-    void printMiddle() 
-    { 
-        //Write your code here
-	//Implement using Fast and slow pointers
-    } 
-  
-    public void push(int new_data) 
-    { 
-        Node new_node = new Node(new_data); 
-        new_node.next = head; 
-        head = new_node; 
-    } 
+ Node head; // head of linked list 
 
-    public void printList() 
-    { 
-        Node tnode = head; 
-        while (tnode != null) 
-        { 
-            System.out.print(tnode.data+"->"); 
-            tnode = tnode.next; 
-        } 
-        System.out.println("NULL"); 
-    } 
-  
-    public static void main(String [] args) 
-    { 
-        LinkedList llist = new LinkedList(); 
-        for (int i=15; i>0; --i) 
-        { 
-            llist.push(i); 
-            llist.printList(); 
-            llist.printMiddle(); 
-        } 
-    } 
+ /* Linked list node */
+ class Node 
+ { 
+     int data; 
+     Node next; 
+     Node(int d) 
+     { 
+         data = d; 
+         next = null; 
+     } 
+ } 
+
+ public LinkedList() {
+	 head=null;
+ }
+ /* Function to print middle of linked list */
+//Complete this function
+ void printMiddle() 
+ { 
+	 //Write your code here
+		//Implement using Fast and slow pointers
+	 if(this.head==null) {
+		 System.out.println("No elements");
+		 return;
+	 }
+	 if(this.head!=null && this.head.next==null) {
+		 System.out.println("Middle element is "+this.head.data);
+		 return;
+	 }
+    
+	 Node sp= this.head;
+	 Node fp=this.head;
+	 
+	 while(fp.next!=null && fp.next.next!=null) {
+		 sp=sp.next;
+		 fp=fp.next.next;
+	 }
+	
+		 System.out.print("Middle of the Linked List is "+sp.data);
+	 
+	 
+ } 
+
+ public void push(int data) 
+ { 
+     // Create a new node with given data 
+     Node NewNode= new Node(data);
+ 
+         if(head==null) {
+				head=NewNode;
+			}else {
+				
+				Node temp=head;
+				while(temp.next!=null) {
+					temp=temp.next;
+					
+				}
+				temp.next=NewNode;
+				
+			}
+
+ }
+	
+ public void printList() 
+ {  
+     // Traverse through the LinkedList 
+     // Print the data at current node 
+     // Go to next node 
+ 		Node temp = head;
+         while(head!=null) {
+			System.out.print(head.data+" ");
+			head=head.next;
+		}
+		System.out.println();
+		head=temp;
+ } 
+
+ public static void main(String [] args) 
+ { 
+     LinkedList llist = new LinkedList(); 
+     for (int i=15; i>0; --i) 
+     { 
+         llist.push(i); 
+         System.out.println("\nlist is");
+         llist.printList(); 
+        
+        llist.printMiddle(); 
+     } 
+ } 
 } 
