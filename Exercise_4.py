b7dad89526efc258d825f446cdffeeb5e988ee6c
alexanderjac <alexander.jac@northeastern.edu>
2021-12-19 14:25:37 -0500

    Completed Sprint PreCourse2

--- Exercise_4.py
+++ Exercise_4.py
@@ -2,9 +2,41 @@
 def mergeSort(arr):
   
   #write your code here
+  if len(arr) > 1:
+         mid = len(arr)//2
+         left_list = arr[:mid]
+         right_lst = arr[mid:]
+         mergeSort(left_list)
+         mergeSort(right_lst)
+         i = 0
+         j = 0
+         k = 0
+         if left_list[i] < right_lst[j]:
+             arr[k] = left_list[i]
+         else:
+             arr[k] = right_lst[j]
+             while i<len(left_list) and j<len(right_lst):
+                 if left_list[i] < right_lst[j]:
+                     arr[k] = left_list[i]
+                     i = i+1
+                     k = k+1
+                 else:
+                     arr[k] = right_lst[j]
+                     j = j+1
+                     k = k+1
+             while i<len(left_list):
+                 arr[k] = left_list[i]
+                 i = i+1
+                 k = k+1
+             while j<len(right_lst):
+                 arr[k] = right_lst[j]
+                 j = j+1
+  return arr
+
   
 # Code to print the list 
 def printList(arr): 
+    return arr
     
     #write your code here
   
@@ -15,4 +47,4 @@ if __name__ == '__main__':
     printList(arr) 
     mergeSort(arr) 
     print("Sorted array is: ", end="\n") 
-    printList(arr) 
+    print(printList(arr))
