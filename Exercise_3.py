e3413579734df1c5aaf56d4a7f71b89f955ee249
Neelharia97 <nharia@stevens.edu>
2021-12-20 19:07:20 -0500

    Submission

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,51 @@
 # Node class  
+#Exercise_3 : Find Mid Point of a Singly Linked List.
+
+#O(n) to find length and push elements
+
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = Node(None)
   
     def push(self, new_data): 
-        
+        temp = self.head
+        new_node = Node(new_data)
+        while(temp.next):
+            temp =temp.next
+        temp.next = new_node
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        length = 0
+        runner = self.head
+        while(runner):
+            length += 1
+            runner =runner.next
+        mid = (length//2)-1
+        temp_runner = self.head
+        while(mid>=0):
+            temp_runner = temp_runner.next
+            mid -= 1
+        return temp_runner.data
+
+# 0 1 2 3 4 5 6 7 8 9
 
 # Driver code 
 list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+# list1.push(5) 
+# list1.push(4) 
+# list1.push(2) 
+# list1.push(3) 
+# list1.push(1) 
+for i in range(10):
+    list1.push(i)
+print(list1.printMiddle())
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,56 @@
-# Python program for implementation of Quicksort
+#Time Complexity: O(n^2) worst case
+def partition(arr,l,h):
+    i = ( l - 1 )
+    x = arr[h]
+  
+    for j in range(l , h):
+        if   arr[j] <= x:
+  
+            i = i+1
+            arr[i],arr[j] = arr[j],arr[i]
+  
+    arr[i+1],arr[h] = arr[h],arr[i+1]
+    return (i+1)
 
-# This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+def quickSortIterative(arr,l,h):
+  
+    size = h - l + 1
+    aux_array = [0] * (size)
+  
+    top = -1
+  
+    top = top + 1
+    aux_array[top] = l
+    top = top + 1
+    aux_array[top] = h
+  
+    while top >= 0:
+  
+        h = aux_array[top]
+        top = top - 1
+        l = aux_array[top]
+        top = top - 1
+  
+        p = partition( arr, l, h )
+  
+        if p-1 > l:
+            top = top + 1
+            aux_array[top] = l
+            top = top + 1
+            aux_array[top] = p - 1
+  
+        if p+1 < h:
+            top = top + 1
+            aux_array[top] = p + 1
+            top = top + 1
+            aux_array[top] = h
 
 
-def quickSortIterative(arr, l, h):
-  #write your code here
+arr = [4, 3, 5, 2, 1, 3, 2, 3]
+print(arr)
+size=len(arr)
+quickSortIterative(arr,0,size-1)
+print(arr)
+
+
 
