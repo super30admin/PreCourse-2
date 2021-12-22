ba99020d14c304babd8b73bc46833e246b7a1119
shirin <shirin32@gmail.com>
2021-12-22 11:49:52 -0800

    Completed Precourse-2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,63 @@
-# Python program for implementation of Quicksort Sort 
+# Python program for implementation of Quicksort  
+
+# Time Complexity : O(n log n), O(n^2) worst case
+# Space Complexity : O(log n)
+# Did this code successfully run on VScode : Yes
+# Any problem you faced while coding this : No
   
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
+# In this function, we choose a pivot and partition the array
+# In the end of this function, the elements smaller than pivot will be on left of pivot
+# All elements greater will be on the right of pivot
+def partition(arr,low,high):            
+
+    # choosing first element as pivot
+    pivot_value = arr[low]
+
+    # Since first element is pivot, array will start at the next element and end at last element index
+    left = low + 1
+    right = high
+
+    finished = False
+    while not finished:
+
+        # As long as there are elements in arr and
+        # And value of first element next to pivot is smaller than pivot
+        # No need to do anything as smaller element is on the left. So just to increment to the next position
+        while left <= right and arr[left] <= pivot_value:       
+            left = left + 1
+
+        # Same as above, as long as elements greater than pivot are on the right just go the the previous index to compare
+        while right >= left and arr[right] >= pivot_value:
+            right = right - 1
+
+        # When no more elements to compare, exit loop
+        if right < left:
+            finished = True
+
+        # First smaller element on right or higher element on the lelft we encouter, 
+        # swap values with corresponding right/lelft elements
+        else:
+            temp = arr[left]
+            arr[left] = arr[right]
+            arr[right] = temp
+
+    # When split point is reached, swap pivot value with the right index value
+    temp = arr[low]
+    arr[low] = arr[right]
+    arr[right] = temp
+
+    return right
   
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
+def quickSort(arr,low,high):
+
+    if(low < high):
+
+        splitpoint = partition(arr, low, high)
+
+        quickSort(arr, low, splitpoint - 1)
+        quickSort(arr, splitpoint + 1, high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,69 @@
 # Python program for implementation of MergeSort 
+
+# Time Complexity : O(n log n) (as divides into 2 halves and merges)
+# Space Complexity : O(n) (as we create temp arrays based on n elements of original array)
+# Did this code successfully run on VScode : Yes
+# Any problem you faced while coding this : No
+
 def mergeSort(arr):
+
+  # In MergeSort we divide array in 2 halves, sort the 2 halves and merge them both
+  # This way we get 2 sorted arrays merged into 1 sorted array
+
+  # As long as there are multiple elements in the array
+  if (len(arr) > 1):
+
+    # Dividing array into 2 halves. Calline left half L and right half R
+    mid = len(arr) // 2
+    L = arr[:mid]
+    R = arr[mid:]
+
+    # Sorting the 2 arrays using recursive call
+    mergeSort(L)
+    mergeSort(R)
+
+    # Actual logic to sort array
   
-  #write your code here
+    # Declaring variables for the loop
+    i = j = k = 0
+
+    # as long as there are elements in both halves
+    while i < len(L) and j < len(R):
+
+      # if first element in the left half is smaller than first element in right half
+      if L[i] < R[j]:
+
+        # Add smallest element from left half as first element of array and increment index
+        arr[k] = L[i]
+        i += 1
+
+      # if first element in right half is smaller than first element of left half  
+      else:
+
+        # Add smallest element from right half as first element and increment index
+        arr[k] = R[j]
+        j += 1
+
+      # increment index to sort and store next element    
+      k += 1
+
+    # After this, it may be possible that right or left array is smaller and loop breaks. 
+    # In that case, need to check remaining elements in either of the half array's and add them to sorted array
+    while i < len(L):
+      arr[k] = L[i]
+      i += 1
+      k += 1
+
+    while j < len(R):
+      arr[k] = R[j]
+      j += 1 
+      k += 1
   
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
+  for i in range (len(arr)):
+    print(arr[i], end=" ")
+  
   
 # driver code to test the above code 
 if __name__ == '__main__': 
@@ -14,5 +71,5 @@ if __name__ == '__main__':
     print ("Given array is", end="\n")  
     printList(arr) 
     mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
+    print("\nSorted array is: ", end="\n") 
     printList(arr) 
