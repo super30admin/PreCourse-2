4ad48a40d78918a50adb2c77fe330930ce9ac0d5
Mitali Bhandari <mail8mitali@gmail.com>
2021-12-24 19:53:09 +0530

    Completed precourse2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,47 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# Node class
+class Node:
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None  # head
+
+    def push(self, new_data):
+        """
+        A function to insert new node at beginning 
+        """
+        newNode = Node(new_data)
+        newNode.next = self.head
+        self.head = newNode
+
+    # Function to get the middle of
+    # the linked list
+    def printMiddle(self):
+        """
+        return middle element in the linked list 
+        """
+        # When the fast pointer reaches the end 
+        # slow pointer will reach the middle of the linked list.
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow.data
+
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2) # middle 
+list1.push(3)
+list1.push(1)
+print(list1.printMiddle())
