bac4b87af631a894d4f45e6129ef2902d4623962
rohitsonawane <rohit2790@gmail.com>
2021-12-25 11:41:01 -0800

    Complete Pre-course 2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,55 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# // Time Complexity : O(N)
+# // Space Complexity : O(1)
+# // Did this code successfully run on Leetcode :
+# // Any problem you faced while coding this : No
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
+
+# // Your code here along with comments explaining your approach
+"""
+1. Use two pointers slow and fast , make slow traverse one element and fast by two elements at a time
+2. When fast reaches end of the linked list , slow pointers reaches at middle of linkedlist
+"""
+
+
+# Node class
+class Node:
+
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        if not self.head:
+            self.head = Node(new_data)
+            return
+        pos = self.head
+        while pos.next:
+            pos = pos.next
+        pos.next = Node(new_data)
+
+    # Function to get the middle of
+    # the linked list
+    def printMiddle(self):
+        slow = fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        print("Middle element is {}".format(slow.data))
+
+
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2)
+list1.push(3)
+list1.push(1)
 list1.printMiddle() 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,57 @@
+# // Time Complexity : O(NlogN)
+# // Space Complexity : O(1)
+# // Did this code successfully run on Leetcode :
+# // Any problem you faced while coding this : No
+
+
+# // Your code here along with comments explaining your approach
+"""
+1. Partition function will calculate the pivot index as middle index and find right position for it , return the index
+2. will maintain a stack , loop through untill its empty
+3. will pop two elements from stack l and h , once we get index for the pivot from partition func , will push new l and h based and the pivot index
+
+"""
+
+
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+    # write your code here
+    pivot = (l + h) // 2
+    arr[pivot], arr[h] = arr[h], arr[pivot]
+
+    for each in range(l, h):
+        if arr[each] <= arr[h]:
+            arr[each], arr[l] = arr[l], arr[each]
+            l += 1
+    arr[l], arr[h] = arr[h], arr[l]
+    return l
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    # write your code here
+    stack = []
 
+    stack.append(l)
+    stack.append(h)
+
+    while len(stack):
+        l = stack.pop(0)
+        h = stack.pop(0)
+        pivot = partition(arr, l, h)
+        if pivot - 1 > l:
+            stack.append(l)
+            stack.append(pivot - 1)
+        if pivot + 1 < h:
+            stack.append(pivot + 1)
+            stack.append(h)
+
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSortIterative(arr, 0, n - 1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]),
