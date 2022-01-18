506dd72a51c17e7c8f113b10a53fc8757a046a41
Aashish Kumar <vats96ashu@gmail.com>
2022-01-18 18:54:14 +0530

    completed-Sprint-PreCourse-2

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,53 +1,56 @@
-class LinkedList 
-{ 
-    Node head; // head of linked list 
-  
-    /* Linked list node */
-    class Node 
-    { 
-        int data; 
-        Node next; 
-        Node(int d) 
-        { 
-            data = d; 
-            next = null; 
-        } 
-    } 
-  
-    /* Function to print middle of linked list */
-   //Complete this function
-    void printMiddle() 
-    { 
-        //Write your code here
-	//Implement using Fast and slow pointers
-    } 
-  
-    public void push(int new_data) 
-    { 
-        Node new_node = new Node(new_data); 
-        new_node.next = head; 
-        head = new_node; 
-    } 
+class LinkedList {
+    Node head; // head of linked list
 
-    public void printList() 
-    { 
-        Node tnode = head; 
-        while (tnode != null) 
-        { 
-            System.out.print(tnode.data+"->"); 
-            tnode = tnode.next; 
-        } 
-        System.out.println("NULL"); 
-    } 
-  
-    public static void main(String [] args) 
-    { 
-        LinkedList llist = new LinkedList(); 
-        for (int i=15; i>0; --i) 
-        { 
-            llist.push(i); 
-            llist.printList(); 
-            llist.printMiddle(); 
-        } 
-    } 
-} 
+    /* Linked list node */
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    /* Function to print middle of linked list */
+    // Complete this function
+    void printMiddle() {
+        // Write your code here
+        // Implement using Fast and slow pointers
+        // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
+        Node fast = head;
+        if (head != null) {
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                head = head.next;
+
+            }
+            System.out.println(head.data);
+        }
+
+    }
+
+    public void push(int new_data) {
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node;
+    }
+
+    public void printList() {
+        Node tnode = head;
+        while (tnode != null) {
+            System.out.print(tnode.data + "->");
+            tnode = tnode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        LinkedList llist = new LinkedList();
+        for (int i = 15; i > 0; --i) {
+            llist.push(i);
+        }
+        llist.printList();
+        llist.printMiddle();
+    }
+}
