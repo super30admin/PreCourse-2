08499ae711b494eaa975f27a48722777d840d51c
Kartik Ghangare <kartikghangare@Kartiks-MacBook-Air.local>
2022-01-20 03:57:17 -0700

    PreCourse-2 Complete

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -6,6 +6,22 @@
 int binarySearch(int arr[], int l, int r, int x) 
 {   
     //Your Code here 
+    int indexMiddleElement= l + (r-l)/2;
+    int middleElement=arr[indexMiddleElement];
+    if(x==middleElement)
+    {
+        return indexMiddleElement;
+    }
+
+    if(r<=l)
+    {
+        return -1;
+    }
+    if(x>middleElement)
+    {
+        return binarySearch(arr,indexMiddleElement+1,r,x);
+    }
+    return binarySearch(arr,l,indexMiddleElement-1,x);
 } 
   
 int main(void) 
@@ -13,7 +29,7 @@ int main(void)
     int arr[] = { 2, 3, 4, 10, 40 }; 
     int n = sizeof(arr) / sizeof(arr[0]); 
     int x = 10; 
-    int result = binarySearch(arr, 0, n - 1, x); 
+    int result = binarySearch(arr, 0, n - 1, 30); 
     (result == -1) ? printf("Element is not present in array") 
                    : printf("Element is present at index %d", 
                             result); 
