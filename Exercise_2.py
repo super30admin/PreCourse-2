729fe70129ff27db74afe0de40203af266d69804
shiva-pa <shivagpandey31@gmail.com>
2022-01-22 09:22:02 -0800

    Completed PreCourse-2, please review

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,19 +1,48 @@
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
+#time complexity O(nlog n)
+#space complexity:O(1)
+
 def partition(arr,low,high):
-  
-  
-    #write your code here
+    l= low
+    r= high - 1
+    pivot=arr[high]
+
+    while l < r:
+     # All the elements less than to the
+    # pivot go before or at l
+        while l< high and arr[l]< pivot:
+            #l moves to the right
+            l += 1
+        while r > low and arr[r] >= pivot:
+            #r moves to the left 
+            r -= 1
+        
+        if l < r:
+            #swap the positions
+            arr[l],arr[r]=arr[r],arr[l]
+
+    #if l and r crossed each other
+    if arr[l]> pivot:
+        #swap
+        arr[l],arr[high] = arr[high],arr[l]
+#partion function returns the index of the pivot elemenet #after the first step of quick sort
+    return l
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
-    #write your code here
+#if sub array contains atleast 2 elements
+    if low < high:
+        partion_position=partition(arr,low,high)
+        #recursively call to quick sort on elemets less than to pivot element
+        quickSort(arr,low,partion_position- 1)
+        #call to the elemts greater to pivot
+        quickSort(arr,partion_position+ 1,high)
   
 # Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
+arr = [10, 7, 8, 9, 1, 5, 4] 
 n = len(arr) 
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,41 @@
-# Node class  
+# Node class 
+# # Time complexity O(n)
+# Space complexity O(1) 
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
-        
+        self.data=data
+        self.next=None
+
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head=None
   
     def push(self, new_data): 
-        
+#creating a node
+        node=Node(new_data)
+       #set new node.next to point to head
+        node.next=self.head
+    # set the head to point to new node
+        self.head=node
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+    #using fast and slow pointer initially pointing to head
+        slow=self.head
+        fast=self.head
+
+        if self.head is not None:
+            while (fast is not None and fast.next is not None):
+                #fast pointer pointer will be twice ahead of the slow pointer
+                fast=fast.next.next
+                slow=slow.next
+            
+            # fast pointerslow pointer reahes to end of the list by then slow pointer will be pointing to the middle of the linked list
+            print("middle of linked list: ", slow.data)
 
 # Driver code 
 list1 = LinkedList() 
@@ -22,5 +43,5 @@ list1.push(5)
 list1.push(4) 
 list1.push(2) 
 list1.push(3) 
-list1.push(1) 
+list1.push(1)
 list1.printMiddle() 
