8e0d5ec9f609c7e90ef0713fc06ef2a5b7e60033
priyanka-bhansali <priyanka.bhansali@aleks.com>
2022-01-22 16:46:50 -0800

    PreCourse 2

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,6 @@
+//time complexity = O(nlogn)
+//space complexity = O(n)
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -6,6 +9,23 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+        int k = 0;
+		int i = l;
+		int j = m + 1;
+		int t[] = new int[r - l + 1];
+
+		while (i <= m && j <= r)
+			// compare a[i] with a[j] and then assign value to t[k].
+			//Last increment value of k(k++)
+			t[k++] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+
+			while (i <= m)
+				t[k++] = arr[i++];
+
+			while (j <= r)
+				t[k++] = arr[j++];
+
+			System.arraycopy(t, 0, arr, l, t.length);
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -13,7 +33,15 @@ class MergeSort
     void sort(int arr[], int l, int r) 
     { 
 	//Write your code here
-        //Call mergeSort from here 
+        //Call mergeSort from here
+        if(l < r)
+		{
+			int mid = (l + r) / 2;
+			System.out.println("mid " + mid);
+			sort(arr, l, mid);
+			sort(arr, mid + 1, r);
+			merge(arr, l, mid, r);
+		}
     } 
   
     /* A utility function to print array of size n */
