af62ac440379fd1c736889d24224fd625f6d0a56
Jay Bharadva <b.jay2040@gmail.com>
2022-01-24 00:34:44 -0800

    Completed Precourse2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,38 @@
-# Python program for implementation of Quicksort Sort 
+#Time Complexity : O(n^2)
+#Space Complexity : O(n)
+#The code runs fine as it supposed to be
+#I had to re-read the algorithm since its little bit complex for me to understand
+
+# Python program for implementation of Quicksort Sort
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+    pivot = arr[high] #assigning the value at high index as pivot
+    i = (low-1) #keeping the smaller element aside from the list
+    for j in range(low,high):
+        if arr[j] < pivot: #if pivot is greater than the element then
+            # reassign the smaller element and swap the smaller element with the current one
+            i = i+1
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+    #swapping the smaller element pointer with the pivot
+    # so that it create a partition in such a way that
+    # all the elements smaller than the pivot stays on left side of it
+    # and the rest on the right side of it
+    temp = arr[i+1]
+    arr[i+1] = arr[high]
+    arr[high] = temp
+
+    #return the partition index
+    return i+1
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
+def quickSort(arr,low,high):
+    if low<high:
+        part = partition(arr,low,high) #get the partitioning index
+        quickSort(arr,low,part-1) #recurssively sort the left hand side of the list
+        quickSort(arr,part+1,high) #recurssively sort the right hand side of the list
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,50 @@
-# Node class  
+#Time Complexity : O(n)
+#Space Complexity : O(1)
+#This code runs fine with desired output
+
+
+# Node class
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
         
 class LinkedList: 
   
-    def __init__(self): 
+    def __init__(self):
+        self.head = None
         
   
-    def push(self, new_data): 
+    def push(self, new_data):
+        if self.head == None:
+            self.head = Node(new_data)
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = Node(new_data)
+
+    def printSLinkedList(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.data)
+            temp = temp.next
         
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        #creating two pointers for to travers the linkedlist
+        fast = self.head #increament by 2 nodes
+        slow = self.head #increament by 1 node
+
+        while fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        print(slow.data)
 
 # Driver code 
 list1 = LinkedList() 
@@ -22,5 +52,8 @@ list1.push(5)
 list1.push(4) 
 list1.push(2) 
 list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+list1.push(1)
+# list1.push(0)
+# list1.push(-1)
+# list1.printSLinkedList()
+list1.printMiddle()
