80cb049bde7152e7dad4b9abfbea231cb523a0fe
vinayak sonandkar <sonandkarvinayak@gmail.com>
2022-01-23 21:35:40 -0800

    Vinayak_PreCourse-2_review

--- Exercise_5.py
+++ Exercise_5.py
@@ -3,8 +3,46 @@
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
   #write your code here
+  pivot = arr[h]
+  i = l - 1
+
+  for j in range(l, h):
+      if arr[j] < pivot :
+          i += 1
+          arr[i], arr[j] = arr[j], arr[i]
+  arr[i+1], arr[h] = arr[h], arr[i+1] 
+  return (i+1)
+
 
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  arr_size = (h - l) + 1
+  stack = [0]*arr_size
 
+  top = -1
+  top += 1
+  stack[top] = l
+  top += 1
+  stack[top] = h
+
+  while top >= 0:
+    h = stack[top]
+    top -= 1
+    l = stack[top]
+    top -= 1
+    
+    p = partition(arr, l, h)
+
+    if (p-1) > l:
+      top += 1
+      stack[top] = l
+      top += 1
+      stack[top] = p - 1
+
+    if (p+1) < h:
+      top += 1
+      stack[top] = p + 1
+      top += 1
+      stack[top] = h
+      
