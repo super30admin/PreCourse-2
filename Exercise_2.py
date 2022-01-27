de4a85adaca3d52ca0cd0fe8d1b0aa40d313a26f
Yogesh Narigapalli <ynarigapalli@dataeconomy.io>
2022-01-27 11:35:21 -0500

    Completed

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,6 +2,16 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
+    i=(low-1)
+    pivot=arr[high]
+
+    for j in range(low,high):
+        if arr[j]<=pivot:
+            i=i+1
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i+1
   
   
     #write your code here
@@ -9,6 +19,14 @@ def partition(arr,low,high):
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
+    if len(arr) == 1:
+        return arr
+    if low < high:
+        pi = partition(arr, low, high)
+ 
+
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
     
     #write your code here
   
