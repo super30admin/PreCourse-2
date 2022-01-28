3065ee9765caa5105d9be01b6030f9f605c6d954
Mounica Kamireddy <mounicakamimreddy5@gmail.com>
2022-01-27 16:18:38 -0800

    Completed PreCourse-2 in Python

--- Exercise_2.py
+++ Exercise_2.py
@@ -5,13 +5,33 @@ def partition(arr,low,high):
   
   
     #write your code here
+    i = low
+    j = high-1
+    pivot = arr[high]
+    
+    while i<j:
+        while i < high and arr[i] < pivot:
+            i += 1
+        while j > low and arr[j] >= pivot:
+            j -= 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+    if arr[i] > pivot:
+        arr[i], arr[high] = arr[high], arr[i]
+    return i 
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
     #write your code here
-  
+    if len(arr) == 1:
+        return arr
+    if low<high:
+        x = partition(arr, low, high)
+        quickSort(arr, low, x-1)
+        quickSort(arr, x+1, high)
+    
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
