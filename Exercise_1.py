45c718b122a09e5241f17079231e7e64041c2799
sujana <sujana99j@gmail.com>
2022-01-27 23:18:26 -0800

    Precourse2_submission

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,50 @@
-# Python code to implement iterative Binary  
-# Search. 
-  
-# It returns location of x in given array arr  
-# if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
+# 1. Python code to implement iterative Binary Search.
+
+# It returns location of x in given array arr if present, else returns -1
+
+
+# Time Complexity  : o(logn)
+# Space Complexity :  o(1)
+# Algorithm
+'''
+1. Search the list using first and last
+2. if value at mid point is greater than x search the list in first half
+   else second half
+3. keep track of element present or not prsent using found parameter
+4. if entire list is traversed return -1
+'''
+
+def binarySearch(arr, l, r, x):
     
-  
-# Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
-  
-# Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
-if result != -1: 
-    print "Element is present at index % d" % result 
-else: 
-    print "Element is not present in array"
+    first=l
+    last=r
+    found=False
+    mid =(first+last)//2
+    while first<last and mid<= r and not found:
+        val=arr[mid]
+        
+        if val==x:
+            found=True
+            return x
+        else:
+            if val<x:
+                first=mid
+            else:
+                last=mid  
+        mid=(first+last)//2
+    return -1
+            
+            
+    
+
+# Test array
+arr = [ 2, 3, 4, 10, 40 ]
+x = 1
+
+# Function call
+result = binarySearch(arr, 0, len(arr)-1, x)
+
+if result != -1:
+    print ("Element is present at index % d" % result )
+else:
+    print ("Element is not present in array")
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,59 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# 3. Python program for implementation for finding Mid Point of a Singly Linked List
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+# tc - o(n/2) 
+# sc - o(1)
+
+class Node:
+
+    # Function to initialise the node object
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self,newdata):
+        self.data=newdata
+
+    def set_next(self,newnext):
+        self.next=newnext
+
+class LinkedList:
+
+    def __init__(self):
+        self.head=None
+        self.size=0
+
+    #the linkedlist will be like this-- head -> 5-> 4-> 2-> 3-> 1 ->none
+    def push(self, new_data):
+            temp=Node(new_data)
+            temp.set_next(self.head)
+            self.head=temp
+            self.size=self.size+1
+
+    # Function to get the middle of the linked list
+    def printMiddle(self):
+        current=self.head
+        middle=self.size//2
+        size=0
+        while (size !=middle) &(size<=middle):
+            current=current.get_next()
+            size=size+1
+        return current.get_data()
+
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+print(list1.printMiddle())
+list1.push(4)
+print(list1.printMiddle())
+list1.push(3)
+print(list1.printMiddle())
+list1.push(2)
+print(list1.printMiddle())
+list1.push(1)
+print(list1.printMiddle())
