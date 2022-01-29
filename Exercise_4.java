9846e6519f6a13af1c92463ed2bf9b9c09639198
PRINCE CHOUDHARY <universe.mars@gmail.com>
2022-01-29 04:53:27 -0800

    Completed Sprint Precourse2

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,42 +1,70 @@
-class MergeSort 
-{ 
+class MergeSort
+{
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
     // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
-    } 
-  
+    void merge(int arr[], int l, int m, int r)
+    {
+        //Your code here
+        int lLen = m - l + 1;
+        int rLen = r - m;
+        int[] left = new int[lLen];
+        int[] right = new int[rLen];
+        System.arraycopy(arr, l, left, 0, lLen);
+        System.arraycopy(arr, m + 1, right, 0, rLen);
+        int k = l, j = 0, i = 0;
+        while(i < lLen && j < rLen){
+            if(left[i] <= right[j]){
+                arr[k++] = left[i++];
+            }
+            else{
+                arr[k++] = right[j++];
+            }
+        }
+        while(i < lLen){
+            arr[k++] = left[i++];
+        }
+        while(j < rLen){
+            arr[k++] = right[j++];
+        }
+    }
+
     // Main function that sorts arr[l..r] using 
     // merge() 
-    void sort(int arr[], int l, int r) 
-    { 
-	//Write your code here
-        //Call mergeSort from here 
-    } 
-  
+    void sort(int arr[], int l, int r)
+    {
+        //Write your code here
+        //Call mergeSort from here
+
+        if(l < r){
+            int m = l + (r - l) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
     /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
-  
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
     // Driver method 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
-  
-        System.out.println("Given Array"); 
-        printArray(arr); 
-  
-        MergeSort ob = new MergeSort(); 
-        ob.sort(arr, 0, arr.length-1); 
-  
-        System.out.println("\nSorted array"); 
-        printArray(arr); 
-    } 
+    public static void main(String args[])
+    {
+        int arr[] = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Given Array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length-1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
 } 
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,67 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
+class IterativeQuickSort {
+    void swap(int arr[], int i, int j)
+    {
+        //Try swapping without extra variable
+        if(i != j) {
+            arr[i] = (arr[j] + arr[i]);
+            arr[j] = (arr[i] - arr[j]);
+            arr[i] = (arr[i] - arr[j]);
+        }
+    }
+
     /* This function is same in both iterative and 
        recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
+    int partition(int arr[], int l, int h)
+    {
         //Compare elements and swap.
-    } 
-  
+        int pivot = arr[h];
+        int i = l;
+        for(int j = l; j < h; j++) {
+            if (arr[j] < pivot) {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        swap(arr,i, h);
+        return i;
+    }
+
     // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
+    void QuickSort(int arr[], int l, int h)
+    {
         //Try using Stack Data Structure to remove recursion.
-    } 
-  
+        Stack st = new Stack();
+        st.push(l);
+        st.push(h);
+        while(!st.isEmpty()){
+            h = st.pop();
+            l = st.pop();
+            int parti = partition(arr, l, h);
+            if((parti - 1) > l){
+                st.push(l);
+                st.push(parti-1);
+            }
+            if((parti + 1) < h){
+                st.push(parti + 1);
+                st.push(h);
+            }
+        }
+    }
+
     // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
+    void printArr(int arr[], int n)
+    {
+        int i;
+        for (i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
     // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
+    public static void main(String args[])
+    {
+        IterativeQuickSort ob = new IterativeQuickSort();
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 };
+        ob.QuickSort(arr, 0, arr.length - 1);
+        ob.printArr(arr, arr.length);
+    }
 } 
