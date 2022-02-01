b9ef0e66d514cbd2b50e069a6568ccc53a191e99
lawzeem <lawzeeml@buffalo.edu>
2022-01-31 21:31:29 -0600

    Commit
    Finished pre-course 2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,58 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# Node class
+class Node:
+    # Time Complexity : O(1)
+    # Space Complexity : O(1)
+    # Any problem you faced while coding this : No
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+class LinkedList:
+    # Time Complexity : O(1)
+    # Space Complexity : O(1)
+    # Any problem you faced while coding this : No
+    # Function to initialise the list
+    def __init__(self):
+        self.head = None
+
+    # Time Complexity : O(1)
+    # Space Complexity : O(1)
+    # Any problem you faced while coding this : No
+    # Function to create a new node and set it to the head
+    def push(self, new_data):
+        nodeToAdd = Node(new_data)
+        nodeToAdd.next = self.head
+        self.head = nodeToAdd
+
+    # Time Complexity : O(n/2)
+    # Space Complexity : O(1)
+    # Any problem you faced while coding this : No
+    # This function gets the middle of the list, two pointers are created. One iterates slowly, one node at a time. The other iterates faster, two nodes at a time.
+    # When the fast node reaches the end, the slow node is at the middle and we can return the middle
+    def printMiddle(self):
+        slowPtr = fastPtr = self.head
+        if self.head is not None:
+            while fastPtr is not None and fastPtr.next is not None:
+                slowPtr = slowPtr.next
+                fastPtr = fastPtr.next.next
+            print(slowPtr.data)
+
+    # Time Complexity : O(n)
+    # Space Complexity : O(1)
+    # Any problem you faced while coding this : No
+    # Helper function to print the list
+    def printList(self):
+        ptr = self.head
+        while ptr.next:
+            print(ptr.data)
+            ptr = ptr.next
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2)
+list1.push(3)
+list1.push(1)
+list1.printList()
+list1.printMiddle()
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,59 @@
 # Python program for implementation of Quicksort
 
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Any problem you faced while coding this : No
+# The code finds the element between low and high that is larger than pivot and swaps them
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+    index = (l - 1)
+    pivot = arr[h]
+    for j in range(l, h):
+        if arr[j] <= pivot:
+            index += 1
+            arr[index], arr[j] = arr[j], arr[index]
+    arr[index+1], arr[h] = arr[h], arr[index+1]
+    return (index + 1)
 
-
+# Time Complexity : O(nlogn)
+# Space Complexity : O(n)
+# Any problem you faced while coding this : No
+# This is the iterative quicksort algorithm. We maintain a stack with the same size as the array
+# We push in values larger than the pivot from the right array and smaller than the pivot from the left array and pop until empty
 def quickSortIterative(arr, l, h):
-  #write your code here
+    size = (h - l) + 1
+    # Create a stack of size array
+    stack = [0] * size
 
+    top = 0
+    stack[top] = l
+    top += 1
+    stack[top] = h
+
+    # While stack is not empty
+    while top > 0:
+        h = stack[top]
+        top -= 1
+        l = stack[top]
+
+        pivot = partition(arr, l, h)
+
+        if pivot - 1 > l:
+            top += 1
+            stack[top] = l
+            top += 1
+            stack[top] = pivot - 1
+
+        if pivot + 1 < h:
+            top += 1
+            stack[top] = pivot + 1
+            top += 1
+            stack[top] = h
+
+# Driver code
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSortIterative(arr,0,n-1)
+print ("Sorted array is:")
+for i in range(n):
+    print ("%d" %arr[i]),
