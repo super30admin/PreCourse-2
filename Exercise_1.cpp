92eb1720a56844b5f7c39f445e18b1afd84de902
Puneet Rehal <pr1663@nyu.edu>
2022-02-08 13:58:12 -0500

    PreCourse-2 Completed

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,3 +1,27 @@
+/**
+ * Time complexity:
+ * O(logn)
+ */
+
+/**
+ * Space Complexity:
+ * The space complexity is O(1) as we are not creating any extra space.
+ * 
+ */
+
+/**
+ * Approach: 
+ * The entire sorted array is our search space. In binary search we repeatedly
+ * divide the search space in half. If the value we have to find is less than
+ * the element in the middle we will reduce the search space to the left half.
+ * Otherwise, the search space will be reduced to right half.
+ * 
+ */
+
+// The code ran perfectly
+
+
+
 #include <stdio.h> 
   
 // A recursive binary search function. It returns 
@@ -6,7 +30,19 @@
 int binarySearch(int arr[], int l, int r, int x) 
 {   
     //Your Code here 
-} 
+    
+    while(l<r){
+        int mid  =  l + (r -l)/2;
+        if(arr[mid] == x) return mid;
+        if(arr[mid] > x){
+            return binarySearch(arr, l, mid, x);
+        } 
+        if(arr[mid] < x){
+            return binarySearch(arr, mid +1, r, x);
+        }
+    }
+        return -1;
+}
   
 int main(void) 
 { 
