7232f41ef916961f71cfb07788b1421ec5c1a717
shwetaterkar <81641786+shwetaterkar@users.noreply.github.com>
2022-02-20 15:08:04 -0500

    Update Exercise_5.py

--- Exercise_5.py
+++ Exercise_5.py
@@ -3,8 +3,56 @@
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
   #write your code here
+  i = (low - 1)
+    x = array[high]
+
+    for j in range(low, high):
+        if array[j] <= x:
+            i = i + 1
+            
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[high] = array[high], array[i + 1]
+    
+    return (i + 1)
 
 
 def quickSortIterative(arr, l, h):
   #write your code here
 
+  size = high - low + 1
+    stack = [0] * (size)
+
+    top = -1
+
+    top = top + 1
+    stack[top] = low
+    top = top + 1
+    stack[top] = high
+
+    # Keep popping from stack while is not empty
+    while top >= 0:
+
+        # Pop high and low
+        high = stack[top]
+        top = top - 1
+        low = stack[top]
+        top = top - 1
+        
+
+        # sorted array
+        p = partition(array, low, high)
+       
+        # push left side to stack
+        if p - 1 > low:
+            top = top + 1
+            stack[top] = low
+            top = top + 1
+            stack[top] = p - 1
+        #  push right side to stack
+        if p + 1 < high:
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = high
+       
