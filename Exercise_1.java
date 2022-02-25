1250eb0ed429fa68c510cc999f625efcfb77cd66
Prakash Kantheti <prakashkantheti@gmail.com>
2022-02-24 22:48:06 -0500

    Precourse 2 submission

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,11 +1,32 @@
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
+
+    /*
+    Time Complexity: O(log(n))
+    Space Complexity: O(1), since we are only considering 1 element at any given time.
+    Did this code successfully run on Leetcode : NA
+    Any problem you faced while coding this : No
+    */
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+      int left = l, right = r;
+
+      
+      while(left <= right){
+        int mid = left + (right - left)/2;
+
+        if(arr[mid] == x){
+          return mid;
+        } else if (arr[mid] > x) {
+          right = mid-1;
+        } else {
+          left = mid + 1;
+        }
+      }
+
+      return -1;
     } 
-  
-  "hey"
+
     // Driver method to test above 
     public static void main(String args[]) 
     { 
