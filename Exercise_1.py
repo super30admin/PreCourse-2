5443440bb5408d7f466b2e403decbbf0831aac4d
Sai Varma Penmatsa <67587377+SAIVARMA571@users.noreply.github.com>
2022-02-25 10:56:46 -0500

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,11 +1,20 @@
+Time complexity: O(logn)
+
 # Python code to implement iterative Binary  
 # Search. 
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
+ def binarySearch(arr, l, r, x):
+    while(l <= r):
+        mid = (l+r)//2
+        if(arr[mid] == x):
+            return mid
+        elif(arr[mid] > x):
+            return binarySearch(arr,l,mid-1,x)
+        else:
+            return binarySearch(arr,mid+1,r,x)
+    return -1
   
     
   
