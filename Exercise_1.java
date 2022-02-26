6f5d65c2885e3565ac04bcc7cdf1342522d056f2
Jeel Patel <JeelPatel@Jeels-MacPro.attlocal.net>
2022-02-26 14:17:16 -0600

    PreCourse-2 in Java Completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,24 @@
+// Time Complexity : O(Log n)
+// Space Complexity : O(1)
+
+// Did this code successfully run on Leetcode : n/a
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+/*
+Check for left and right indices that left <= right
+
+Find mid point of left and right: If element at mid point = x return that mid point(index)
+If element at mid point < x => X is located in the right side of mid point
+If element at mid point > x => X is located in the left side of mid point
+
+If element not found then simply return -1
+
+*/
+
+
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
