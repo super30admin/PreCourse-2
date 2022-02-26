76cdcc69f6b738335f1e0207940e8409634b562a
nisargushah <58926548+nisargushah@users.noreply.github.com>
2022-02-26 17:21:13 -0500

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,49 @@
+# Time Complexity -> O(n) to add n elements and to find the middle element -> O(n/2)
+#Space complexity -> O(n)
+
+
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
+        #self.count = 0
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
+        self.count = 0
+
   
     def push(self, new_data): 
-        
+        if self.head == None:
+            node = Node(new_data)
+            self.head = node
+            self.count += 1
+
+        else:
+            node = Node(new_data)
+            self.count += 1
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = node
+
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        current = self.head
+        mid = self.count // 2
+        i = 0
+        while i != mid:
+            i += 1
+            current = current.next
+        print(current.data)
+
 
 # Driver code 
 list1 = LinkedList() 
