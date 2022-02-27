b2a486fc2ff703a0e821e0443f9cefba837c5e51
knatarasan <kannappan_natarasan@yahoo.com>
2022-02-26 18:08:27 -0800

    Completed all 5 problems in python

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,20 +3,49 @@
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
+'''
+// Time Complexity : O(logN)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : little round about to get mid point
+
+
+// Your code here along with comments explaining your approach
+'''
+
+'''
+1. calc mid point on array repeat following until searched is found and right is greater than or equal to left
+  2. if array value at mid is searched return
+        array value is greater searched is left side so chop right part
+        array value is smaller searched is right side , chop left side
+
+'''
+def bsi(arr,l,r,x):
+  while True:
+    mid=(l+r)//2
+
+    if r>=l:
+      if arr[mid]==x:
+        return mid
+      elif arr[mid]<x:
+        l=mid+1
+      else:
+        r=mid-1
+    else:
+      return -1  
   
-  #write your code here
-  
-    
   
 # Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
+arr = [ 2, 3, 4, 10, 40 ,55,67,89] 
+x = [10,3,0,-4,67]
   
-# Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
-if result != -1: 
-    print "Element is present at index % d" % result 
-else: 
-    print "Element is not present in array"
+# Function call
+def test(func):
+  for to_be_find in x: 
+    result=func(arr, 0, len(arr)-1, to_be_find)
+    print(str(to_be_find)+' : '+str(result))
+
+test(bsi)
+
+
+
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,73 @@
 # Python program for implementation of Quicksort
 
+'''
+// Time Complexity : BEST WORST AVG O( N Log(N))
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode :  Time limit exceeded , other cases succeeded though
+// Any problem you faced while coding this : Tricky to convert recursion to iteration
+
+
+// Your code here along with comments explaining your approach
+
+'''
+'''
+This sort has following two portions ,
+  1. Took first element as pivot , 
+  iterate remaining with two pointer one from left and other from right use partition helper function this retruns right which is partition point
+  2. Stack is used to call partition until all subarrays are called for sorting
+
+- Partition
+  1. When left is greter than pivot and right is less than pivot left and right are swapped
+      When left is smaller than or equal to pivot increment left
+      When right is greate or equal to pivot decrement right
+  2. Swap pivot and right 
+
+'''
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+def partition(arr,start,end):
+    p=start
+    left=start+1
+    right=end
+
+    while right>=left:
+        if arr[left]>arr[p] and arr[right]<arr[p]:
+            arr[left],arr[right]=arr[right],arr[left]
+        if arr[left]<=arr[p]:
+            left+=1
+        if arr[right]>=arr[p]:
+            right-=1
+    
+    arr[p],arr[right]=arr[right],arr[p]
+    return right
 
 
-def quickSortIterative(arr, l, h):
-  #write your code here
+def quickSortIterative(arr, low, high):
+  stack = []
+  stack.append(low)
+  stack.append(high)
+  # print("Before",temp)
+  while stack:  #while len(stack) is not 0:
+    high=stack.pop()
+    low= stack.pop()
+    # print("In loop",temp, low,high)
+    partition_idx = partition(arr,low,high)
+    # print('Partition',pi,arr)
+    if low < partition_idx -1:
+        stack.append(low)
+        stack.append(partition_idx -1)
 
+    if partition_idx +1 < high:
+        stack.append(partition_idx+1)
+        stack.append(high)
+  
+
+
+  
+# Driver code to test above 
+# arr = [10, 7, 8, 9, 1, 5] 
+arr=[-4,0,7,4,9,-5,-1,0,-7,-1]
+print(arr)
+quickSortIterative(arr,0,len(arr)-1)
+print(arr)
+  
+ 
