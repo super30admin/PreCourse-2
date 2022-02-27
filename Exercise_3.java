241417f38986845a4eaf0845ca52d0e9a2dd52d0
Soumya Bhatnagar <soumya11207@gmail.com>
2022-02-26 18:10:58 -0800

    Precourse-2 completed

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,53 +1,69 @@
-class LinkedList 
-{ 
-    Node head; // head of linked list 
-  
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+class LinkedList
+{
+    Node head; // head of linked list
+
     /* Linked list node */
-    class Node 
-    { 
-        int data; 
-        Node next; 
-        Node(int d) 
-        { 
-            data = d; 
-            next = null; 
-        } 
-    } 
-  
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
     /* Function to print middle of linked list */
    //Complete this function
-    void printMiddle() 
-    { 
+    void printMiddle()
+    {
         //Write your code here
 	//Implement using Fast and slow pointers
-    } 
-  
-    public void push(int new_data) 
-    { 
-        Node new_node = new Node(new_data); 
-        new_node.next = head; 
-        head = new_node; 
-    } 
+        Node fast = head, slow = head;
 
-    public void printList() 
-    { 
-        Node tnode = head; 
-        while (tnode != null) 
-        { 
-            System.out.print(tnode.data+"->"); 
-            tnode = tnode.next; 
-        } 
-        System.out.println("NULL"); 
-    } 
-  
-    public static void main(String [] args) 
-    { 
-        LinkedList llist = new LinkedList(); 
-        for (int i=15; i>0; --i) 
-        { 
-            llist.push(i); 
-            llist.printList(); 
-            llist.printMiddle(); 
-        } 
-    } 
-} 
+        if(head != null)
+        {
+          while(fast != null && fast.next != null)
+          {
+            fast = fast.next.next;
+            slow = slow.next;
+          }
+          System.out.println(slow.data);
+        }
+    }
+
+    public void push(int new_data)
+    {
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node;
+    }
+
+    public void printList()
+    {
+        Node tnode = head;
+        while (tnode != null)
+        {
+            System.out.print(tnode.data+"->");
+            tnode = tnode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String [] args)
+    {
+        LinkedList llist = new LinkedList();
+        for (int i=15; i>0; --i)
+        {
+            llist.push(i);
+            llist.printList();
+            llist.printMiddle();
+        }
+    }
+}
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,42 +1,98 @@
-class MergeSort 
-{ 
-    // Merges two subarrays of arr[]. 
-    // First subarray is arr[l..m] 
-    // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
-    } 
-  
-    // Main function that sorts arr[l..r] using 
-    // merge() 
-    void sort(int arr[], int l, int r) 
-    { 
-	//Write your code here
-        //Call mergeSort from here 
-    } 
-  
+// Time Complexity : O(nlogn)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+public class Exercise_4
+{
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r)
+    {
+       //Your code here
+       int[] left = new int[m-l+1];
+       int[] right = new int[r-m];
+
+       for(int i=0; i<m-l+1; i++)
+       {
+         left[i] = arr[l+i];
+       }
+
+       for(int i=0; i<r-m; i++)
+       {
+         right[i] = arr[m+1+i];
+       }
+
+       int i=0, j=0, k=l;
+
+       while(i<left.length && j<right.length)
+       {
+         if(left[i] <= right[j])
+         {
+           arr[k] = left[i];
+           i++;
+         }
+        else
+        {
+          arr[k] = right[j];
+          j++;
+        }
+          k++;
+       }
+
+       while(i<left.length)
+       {
+         arr[k] = left[i];
+         k++;
+         i++;
+       }
+
+       while(j<right.length)
+       {
+         arr[k] = right[j];
+         k++;
+         j++;
+       }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sort(int arr[], int l, int r)
+    {
+	      //Write your code here
+        //Call mergeSort from here
+        if(l<r)
+        {
+          int mid = l + (r-l)/2;
+
+          sort(arr, l, mid);
+          sort(arr, mid+1, r);
+
+          merge(arr, l, mid, r);
+        }
+    }
+
     /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
-  
-    // Driver method 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
-  
-        System.out.println("Given Array"); 
-        printArray(arr); 
-  
-        MergeSort ob = new MergeSort(); 
-        ob.sort(arr, 0, arr.length-1); 
-  
-        System.out.println("\nSorted array"); 
-        printArray(arr); 
-    } 
-} 
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String args[])
+    {
+        int arr[] = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Given Array");
+        printArray(arr);
+
+        Exercise_4 ob = new Exercise_4();
+        ob.sort(arr, 0, arr.length-1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
+}
