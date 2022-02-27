c89301aa9adb37c2500d173afc85a8539afc8a51
architakale <architakale@knights.ucf.edu>
2022-02-27 17:26:37 -0500

    fixed indentation

--- Exercise_2.py
+++ Exercise_2.py
@@ -9,7 +9,7 @@ def partition(arr,low,high):
         if arr[j] <= pivot:
             i = i+1
             (arr[i],arr[j]) = (arr[j],arr[i])
-        (arr[i+1],arr[high]) = (arr[high],arr[i+1])
+    (arr[i+1],arr[high]) = (arr[high],arr[i+1])
     return i+1
 
 # Function to do Quick sort 
--- Exercise_5.py
+++ Exercise_5.py
@@ -10,10 +10,10 @@ def partition(arr, l, h):
   pivot = arr[h]
   i = l-1
   for j in range(l,h):
-      if arr[j] <= pivot:
-          i = i+1
-          (arr[i],arr[j]) = (arr[j],arr[i])
-      (arr[i+1],arr[h]) = (arr[h],arr[i+1])
+    if arr[j] <= pivot:
+      i = i+1
+      (arr[i],arr[j]) = (arr[j],arr[i])
+  (arr[i+1],arr[h]) = (arr[h],arr[i+1])
   return i+1
 
 
