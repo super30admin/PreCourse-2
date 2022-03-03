4de8e6dc8c7811318f8af6e69891a39417ba781a
Arteegupta <arteegupta@gmail.com>
2022-03-03 07:22:55 -0500

    Precourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,9 +1,23 @@
-class BinarySearch { 
+/**Time Complexity: O(log n)
+ * Space Complexity: O(1)**/
+class BinarySearch { 	
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
-    } 
+        //Write your code here 	
+    	while (l <= r){
+    		int mid= l+ (r-l)/2;
+    		
+    		if(arr[mid] == x) {
+    			return mid;
+    		}else if(x > arr[mid]) {
+    			l=mid+1;
+    		}else {
+    			r=mid-1;
+    		}
+    	}
+    	return -1;
+    }
   
     // Driver method to test above 
     public static void main(String args[]) 
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,5 @@
+/**Time Complexity: O(n log n)
+ * Space Complexity: O(n)**/
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -5,16 +7,66 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+    	//Your code here  
+    	int len1= m-l+1;
+    	int len2= r-m;
+    	
+    	int left[]= new int[len1];
+    	int right[]= new int[len2];
+    	
+    	//copy data to both arrays
+    	for(int i=0; i<len1; i++) {
+    		left[i]=arr[l+i];
+    	}
+    	for(int i=0; i<len2; i++) {
+    		right[i]=arr[m+1+i];
+    	}
+    	
+    	//merge both the sorted arrays
+    	int i=0; //begin index of first array
+    	int j=0; //begin index of second array
+    	int k=l; //begin index of merged array
+    	
+    	while(i< len1 && j<len2) {
+    		if(left[i] <= right[j]) {
+    			arr[k]= left[i];
+    			i++;
+    		}else {
+    			arr[k]= right[j];
+    			j++;
+    		}
+    		k++;
+    	}
+    	
+    	//copy remaining items of first array if exists
+    	while(i < len1) {
+    		arr[k]=left[i];
+    		i++;
+    		k++;
+    	}
+    	
+    	//copy remaining items of second array if exists
+    	while(j < len2) {
+    		arr[k]=right[j];
+    		j++;
+    		k++;
+    	}
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
     { 
-	//Write your code here
-        //Call mergeSort from here 
-    } 
+    	if(l < r) {
+	    	//Write your code here
+	    	int mid= l+(r-l)/2;    	
+	    	sort(arr, l, mid);
+	    	sort(arr, mid+1, r);
+	    	
+	        //Call mergeSort from here 
+	    	merge(arr,l,mid,r);
+    	}
+    }
   
     /* A utility function to print array of size n */
     static void printArray(int arr[]) 
