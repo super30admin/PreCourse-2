bbdd12fbb5715320671234186651d4a04de10dab
gunjalgupta <78285301+gunjalgupta@users.noreply.github.com>
2022-03-22 02:28:12 -0700

    completed

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,18 +1,27 @@
 class QuickSort 
 { 
-    /* This function takes last element as pivot, 
-       places the pivot element at its correct 
-       position in sorted array, and places all 
-       smaller (smaller than pivot) to left of 
-       pivot and all greater elements to right 
-       of pivot */
+    
     void swap(int arr[],int i,int j){
-        //Your code here   
+         int temp = arr[i];
+         arr[i] = arr[j];
+         arr[j] = temp; 
     }
     
     int partition(int arr[], int low, int high) 
-    { 
-   	//Write code here for Partition and Swap 
+    { int pivot = arr[high];
+
+         int i = (low - 1);
+
+         for(int j = low; j <= high - 1; j++)
+         {
+             if (arr[j] < pivot)
+             {
+                 i++;
+                 swap(arr, i, j);
+             }
+         }
+         swap(arr, i + 1, high);
+         return (i + 1);
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
@@ -22,6 +31,13 @@ class QuickSort
     {  
             // Recursively sort elements before 
             // partition and after partition 
+	if (low < high)
+        {
+            int pi = partition(arr, low, high);
+        
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
     } 
   
     /* A utility function to print array of size n */
@@ -46,3 +62,49 @@ class QuickSort
         printArray(arr); 
     } 
 } 
+//=============================================
+class Solution {
+    public int[] sortArray(int[] nums) {
+        
+        if (nums.length == 0) { return nums;}
+        
+        QuickSort(nums, 0, nums.length - 1);
+        return nums;
+        
+    }
+    
+    private void QuickSort(int[] nums, int start, int end) {
+        
+        if(start >= end) return;
+        
+        int pivot = nums[(start + end)/2];
+        int left = start, right = end;
+
+        while (left <= right) {
+            while (left <= right && nums[left] < pivot ) {
+            left++;
+            }
+        
+            while (left <= right && nums[right] > pivot) {
+            right--;
+            }
+            
+            if(left <= right) {
+                int temp;
+                temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                
+                left++;
+                right--;
+            }
+        }
+        
+       QuickSort(nums, start, right);
+       QuickSort(nums, left, end);
+    }
+        
+  }
+//===============================
+//time complexity- O(nlogn)
+//space complexity- O(1)
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,20 +1,41 @@
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+	arr[i]= arr[i]+arr[j];
+	arr[j]= arr[i]-arr[j];
+	arr[i]= arr[i]- arr[j];
     } 
   
     /* This function is same in both iterative and 
        recursive*/
     int partition(int arr[], int l, int h) 
     { 
-        //Compare elements and swap.
+        int pivot = arr[high];
+        int i = (low - 1); // index of smaller element
+        for (int j = low; j <= high - 1; j++) {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot) {
+                i++;
+ 
+                swap(arr, i, j) 
+            }
+        }
+ 
+        swap(arr, i+1, high); 
+ 
+        return i + 1;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
-        //Try using Stack Data Structure to remove recursion.
+        if (low < high) {
+            int pi = partition(arr, low, high);
+ 
+            QuickSort(arr, low, pi - 1);
+            QuickSort(arr, pi + 1, high);
+        }
     } 
   
     // A utility function to print contents of arr 
