d64231c1b9aac59db6d03744259f53d1017091e5
Vaishnavi Kannan <v.kannan@salesforce.com>
2022-03-22 23:51:29 -0700

    pre-course 2 - exercise 1, 3, 4

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(n log n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Did not run it on leetcode
+// Any problem you faced while coding this : No
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -5,7 +9,46 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+       //Your code here
+        int leftSize = m - l + 1;
+        int rightSize = r - m;
+
+        int left[] = new int[leftSize];
+        int right[] = new int[rightSize];
+
+
+        for (int it = 0; it < leftSize; ++it) {
+            left[it] = arr[l + it];
+        }
+        for (int it = 0; it < rightSize; ++it) {
+            right[it] = arr[m + 1 + it];
+        }
+
+        int i = 0, j = 0, k = l;
+
+        while (i < leftSize && j < rightSize) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                ++k;
+                ++i;
+            } else {
+                arr[k] = right[j];
+                ++k;
+                ++j;
+            }
+        }
+
+        while (i < leftSize) {
+            arr[k] = left[i];
+            ++k;
+            ++i;
+        }
+
+        while (j < rightSize) {
+            arr[k] = right[j];
+            ++k;
+            ++j;
+        }
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,7 +57,15 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
-    } 
+
+        if (l < r) {
+            int mid = l + (r-l)/2;
+            sort(arr, l, mid);
+            sort(arr, mid+1, r);
+
+            merge(arr, l, mid, r);
+        }
+    }
   
     /* A utility function to print array of size n */
     static void printArray(int arr[]) 
