12020cf622ed2bf7cc988e8183895e91b61704b2
Veda Nandan Gandi <vedanandan@VNs-MacBook-Pro.local>
2022-03-27 19:20:55 -0500

    Completed Sprint Precourse. Please Review

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,6 @@
+# Time Complexity: O(n*n)
+# Space Comlexity: O(n)
+
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
@@ -5,12 +8,26 @@ def partition(arr,low,high):
   
   
     #write your code here
-  
+    i = low-1
+    pivot = arr[high]                           # Initial pivot is the last element.
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i+=1
+            arr[i], arr[j] = arr[j], arr[i]     # Swapping elements.
+            
+    arr[i+1], arr[high] = arr[high], arr[i+1]       # Swapping the pivot element.
+    return i+1
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
     #write your code here
+    if len(arr) == 1:
+        return arr
+    if low < high:
+        pivot = partition(arr, low, high)       # Finding the pivot element
+        quickSort(arr, low, pivot-1)            # For the left subarray.
+        quickSort(arr, pivot+1, high)           # For the right subarray.
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,42 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None                # Initialize the head pointer.
   
     def push(self, new_data): 
-        
+        node = Node(new_data)
+        if self.head == None:           # If head is empty.
+            self.head = node
+            return
+        if self.head.next == None:      # If head is the only element.
+            self.head.next = node
+            return
+        curr = self.head
+        while curr.next != None:        # Traversing the list
+            curr = curr.next
+        curr.next = node
+        return
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        slow = fast = self.head                         # Initialize slow and fast pointers.
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        print("Middle Element: " + str(slow.data))
+
 
 # Driver code 
 list1 = LinkedList() 
