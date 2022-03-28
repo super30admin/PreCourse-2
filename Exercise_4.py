25959e44784e936e417abbf3272827ffd489d37f
Sanskar Gyawali <sanskargyawali@Sanskars-MacBook-Pro.local>
2022-03-27 18:44:33 -0700

    Completed Precourse 2

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,16 +1,51 @@
+# // Time Complexity : O(N)
+# // Space Complexity : O(N)
+# // Did this code successfully run on Leetcode :yes
+# // Any problem you faced while coding this : had to research mergesort before starting, as a review
+
+
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
-  #write your code here
-  
+  if(len(arr)>1):
+    middle = len(arr)//2
+    left = arr[:middle]         #getting the left side of the array
+    right= arr[middle:]         #getting the right side of the array
+
+    mergeSort(left)             #recursively merge the left side
+    mergeSort(right)            #recursively merge the right side
+
+    index1 = index2 = arrayindex = 0
+
+    while(index1 < len(left) and index2 <len(right)):       #  compare the left to the right number and put them in the correct order to the main array starting with two elements
+      if(left[index1] <right[index2]):
+        arr[arrayindex] = left[index1]
+        index1+=1
+      else:
+        arr[arrayindex] = right[index2]
+        index2+=1
+      arrayindex+=1
+
+    if (index1<len(left)):        #if there are still elements remaining in the left array, add them to the main array
+      while(index1 < len(left)):
+        arr[arrayindex] = left[index1]
+        index1+=1
+        arrayindex+=1
+    elif(index2<len(right)):      #if there are still elements remaining in the right array, add them to the main array
+      while(index2 <len(right)):
+        arr[arrayindex] = right[index2]
+        index2+=1
+        arrayindex+=1
+    
+
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
+    for i in arr:
+      print(i)
+   
   
 # driver code to test the above code 
 if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
+    arr = [12, 11, 13, 5, 6, 7,]  
     print ("Given array is", end="\n")  
     printList(arr) 
     mergeSort(arr) 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,49 @@
+# // Time Complexity :O(N^2) - O(N) for partitioning and O(N) for the iteration
+# // Space Complexity :O(N)
+# // Did this code successfully run on Leetcode :yes
+# // Any problem you faced while coding this : had some trouble figuring out this one because I hadn't done it iteratively before, so had to do a lot of research
+
+
 # Python program for implementation of Quicksort
 
-# This function is same in both iterative and recursive
+# This function is same in both iterative and recursive version
 def partition(arr, l, h):
   #write your code here
+  #partition should be the same for recursive and iteration
+  pivot = arr[h]       # we are using the last element as the pivot
+  index=l               # we always start at low
+  while l <h:
+      if(arr[l]<=pivot):        # if the number is less than the pivot, then swap it with the first greater number in the array
+          arr[l], arr[index] = arr[index] , arr[l]
+          index+=1
+      l+=1
+  arr[h], arr[index] = arr[index], arr[h]       #at the end, swap the pivot next to the last number smaller than it
+  return index
+
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+  sublist = []            # to store all the sublist we will be partitioning
+  first, last = 0, h      #starting with the whole array
 
+  sublist.append((first, last))             #add the index of the whole array to partition first as a tuple
+
+  while (sublist != []):
+    first, last = sublist.pop()         #get the elements you want to partition first
+
+    p = partition(arr, first, last)
+
+    if first < p -1:                    # if there are elements on the left of pivot, keep pushing them to the sublist to partition
+      sublist.append((first, p-1))
+
+    if last > p+1:                       # if there are elements on the right of pivot, keep pushing them to the sublist to partition
+      sublist.append((p+1, last))
+
+#added the test case from exercise 2
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
+  
