d35f745a25b39d5e72faae02d029a1ef6e34adf6
priyank <ppatel631@gmail.com>
2022-04-04 17:36:00 -0400

    Added time and space complexity

--- Sample.java
+++ Sample.java
@@ -5,3 +5,14 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+//Exercise_1.java has O(log n) time complexity and O(N) space complexity
+
+//Exercise_2.java has O(n^2) time complexity in worst case otherwise O(n log n) and O(N) space complexity
+
+//Exercise_3.java has O(n) time complexity and O(1) space complexity
+
+//Exercise_4.java has O(n log n) time complexity and O(N) space complexity
+
+//Exercise_5.java has O(log n) time complexity and O(log n) space complexity
