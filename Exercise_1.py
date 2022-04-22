5a4271f04ff3fe82dde3082e5f5fd084b2685e07
lohitb009 <45014992+lohitb009@users.noreply.github.com>
2022-04-22 13:56:13 +0530

    PreCourse 2
    Completed PreCourse 2 will all exercises
    Exercise_2 and Exercise_5 were quick sort exercises

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,57 @@
+'''
+Approach:
+
+1. This is a binary search, so we will assume that our array is sorted (here ascending order)
+2. We will perform recursion
+3. We will pass the left boundary, right boundary and search the element in that boundary.
+4. We will find th mid of the boundary, if 'x' matches the mid, return the index
+5. If the value of x is less than mid, search on the lhs. Adjust the right boundary to mid-1
+6. If the value of x is greater than mid, search on the rhs. Adjust the left boundary to mid+1
+7. Our recursion stops if our left boundary is greater than right boundary.
+8. Time Complexity:
+   Since we are finding the mid and iterating the half of the list
+   Our complexity will be 0(nlogn)
+9. I faced the problem of infinite recursion call. On debug I realized that I need to adjust the boundary either by
+    +1 or -1 to solve this problem
+10. Didn't check on leet code, need leet code link to check it out
+'''
 # Python code to implement iterative Binary  
 # Search. 
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
+    
+    if r < l:
+        # It's invalid case
+        return -1
+    
+    # 1. Find th mid pt
+    mid = l + (((r-l)+1)//2)
+    
+    # 2. Compare
+    if arr[mid] == x:
+        # It's a match
+        return mid
+    
+    elif arr[mid] >  x:
+        # On the lhs of the list
+        return binarySearch(arr, l, mid-1, x)
+    
+    elif arr[mid] < x:
+        # On the rhs of the  list
+        return binarySearch(arr, mid+1, r, x)
     
   
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
-  
+
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
+
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print ("Element is present at index % d" % result) 
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,89 @@
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+
+'''
+Approach:
+1. Used Lomuto Algorithm for partitioning
+2. First we will partition the list
+3. During partitioning we have to decide the pivot. Here my pivot will be arr[high]
+4. In partitioning, I have created 2 ptr's
+    4.1 jth ptr for "R2" region. All elements in R2 region will be greater than "pivot".
+    4.2 ith ptr for "R1" region. All elements in R1 region will be less than "pivot".
+    4.3 Our initial partition will look like following [<---R1--->,<---R2--->,PIVOT]
+    4.4.Will will swap the pivot with 1st element of R2 region. Now our list will look as follows
+        [<---R1--->,PIVOT,<---R2--->]
+    4.5 Our partition finction will return the pivot index
+5. Will will again perform quick sort on the lhs of the pivot and rhs of the pivot
+6. Our quick-sort and partition function will be called recursively till our array is not sorted
+7. Time Compexity is 0(nlogn)
+'''
+# Use LOMUTO Algo
+def partition(arr,low,high):  
+    
+    # 1. Set pivot
+    pivot = arr[high]
+    
+    
+    # For region R2 i.e. values greater than 'pivot'
+    j = low
+    
+    # For region R1 i.e. values less than 'pivot'
+    i = j - 1
+    
+    
+    # Perform Iteration check
+    while j != high:
+        
+        if arr[j]<pivot:
+            # Add to R1
+            
+            #1. Perform swap
+            arr[i+1], arr[j] = arr[j], arr[i+1]
+            
+            #2. Increment i counter
+            i+=1
+        
+        # For other case simply increase counter of j 
+        # It will be added to R2 region
+        j+=1
+        
+        continue
+    
+    # Adjust the pivot
+    arr[i+1],arr[high] = arr[high], arr[i+1]
+    
+    # Return the pivot index
+    return i+1    
+    
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
+    if high <= low:
+        # We have only 1 element
+        return
+    
     #write your code here
-  
+    # 1. Perform partition
+    pivotIndex = partition(arr, low, high)
+    
+    # Quick Sort LHS of the list
+    quickSort(arr, low, pivotIndex-1)
+    
+    # Quick Sort RHS of the list
+    quickSort(arr, pivotIndex+1, high)
+    
+    return arr
+
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
-quickSort(arr,0,n-1) 
+print(quickSort(arr,0,n-1)) 
+
 print ("Sorted array is:") 
 for i in range(n): 
     print ("%d" %arr[i]), 
-  
+
  
+
