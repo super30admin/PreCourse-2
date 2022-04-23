77dd6414169decee048f9c9047065374c80d7089
Gautham Rajsimha Pulipati <gauthamrajsimhapulipati@Gauthams-Air.lan>
2022-04-23 16:27:51 -0400

    Submitting precourse 2

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,48 +1,76 @@
-class QuickSort 
-{ 
-    /* This function takes last element as pivot, 
-       places the pivot element at its correct 
-       position in sorted array, and places all 
-       smaller (smaller than pivot) to left of 
-       pivot and all greater elements to right 
-       of pivot */
-    void swap(int arr[],int i,int j){
-        //Your code here   
+// complexity is O(nlogn) average case and O(n^2) worst case
+class QuickSort {
+    /*
+     * This function takes last element as pivot,
+     * places the pivot element at its correct
+     * position in sorted array, and places all
+     * smaller (smaller than pivot) to left of
+     * pivot and all greater elements to right
+     * of pivot
+     */
+    void swap(int arr[], int i, int j) {
+        // Your code here
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
     }
-    
-    int partition(int arr[], int low, int high) 
-    { 
-   	//Write code here for Partition and Swap 
-    } 
-    /* The main function that implements QuickSort() 
-      arr[] --> Array to be sorted, 
-      low  --> Starting index, 
-      high  --> Ending index */
+
+    // complexity is O(n)
+    int partition(int arr[], int low, int high) {
+        // Write code here for Partition and Swap
+        int pivot = arr[high];
+        int new_pivot = low;
+
+        // swapping the values as many times as the count of numbers less than the last number of the subarray
+
+        for (int i = low; i <= high-1; i++) {
+            if (arr[i] <= pivot) {
+                this.swap(arr, i, new_pivot);
+                new_pivot++;
+            }
+        }
+        this.swap(arr, new_pivot, high);
+        return new_pivot;
+    }
+
+    /*
+     * The main function that implements QuickSort()
+     * arr[] --> Array to be sorted,
+     * low --> Starting index,
+     * high --> Ending index
+     */
+    // complexity is O(log n) avg and O(n) worse case * complexity of partition function
     void sort(int arr[], int low, int high) 
     {  
             // Recursively sort elements before 
             // partition and after partition 
-    } 
-  
+        if(low >= high){
+            return;
+        }
+        int pivot = this.partition(arr, low, high);
+        // find the pivot and split apply the same function on either sides of the pivot
+        this.sort(arr, low, pivot-1);
+        this.sort(arr, pivot+1, high);
+    }
+
     /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
-        System.out.println(); 
-    } 
-  
-    // Driver program 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {10, 7, 8, 9, 1, 5}; 
-        int n = arr.length; 
-  
-        QuickSort ob = new QuickSort(); 
-        ob.sort(arr, 0, n-1); 
-  
-        System.out.println("sorted array"); 
-        printArray(arr); 
-    } 
-} 
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver program
+    public static void main(String args[]) {
+        // int arr[] = { 10, 7, 8, 9, 1, 5 };
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
+        int n = arr.length;
+
+        QuickSort ob = new QuickSort();
+        ob.sort(arr, 0, n - 1);
+
+        System.out.println("sorted array");
+        printArray(arr);
+    }
+}
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,42 +1,81 @@
-class MergeSort 
-{ 
-    // Merges two subarrays of arr[]. 
-    // First subarray is arr[l..m] 
-    // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
-    } 
-  
-    // Main function that sorts arr[l..r] using 
-    // merge() 
-    void sort(int arr[], int l, int r) 
-    { 
-	//Write your code here
-        //Call mergeSort from here 
-    } 
-  
+class MergeSort {
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+
+    // Complexity is O(n log n)
+    void merge(int arr[], int l, int m, int r) {
+        // Your code here
+        int[] aux = new int[arr.length];
+        for(int i=0; i<aux.length; i++){
+            aux[i] = arr[i];
+        }
+        // using an auxillary array to compare and swap the values in the main array
+        int index_left = l;
+        int index_right = m+1;
+
+        int mode = 0;
+        for(int i=l; i<=r; i++){
+            if(mode == 0){
+                if(aux[index_left] <= aux[index_right]){
+                    index_left++;
+                }
+                else{
+                    arr[i] = aux[index_right];
+                    index_right++;
+                }
+                if(index_left > m){
+                    mode = 1;
+                }
+                else if(index_right > r){
+                    mode = 2;
+                }
+            }
+            else if(mode == 1){
+                arr[i] = aux[index_right];
+                index_right++;
+            }
+            else if(mode == 2){
+                arr[i] = aux[index_left];
+                index_left++;
+            }
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    // complexity log n
+    void sort(int arr[], int l, int r) {
+        // Write your code here
+        // Call mergeSort from here
+        if(l>=r){
+            return;
+        }
+        int m = l+ (r-l)/2;
+        this.sort(arr, l, m);
+        this.sort(arr, m+1, r);
+        this.merge(arr, l, m, r);
+    }
+
     /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
-  
-    // Driver method 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
-  
-        System.out.println("Given Array"); 
-        printArray(arr); 
-  
-        MergeSort ob = new MergeSort(); 
-        ob.sort(arr, 0, arr.length-1); 
-  
-        System.out.println("\nSorted array"); 
-        printArray(arr); 
-    } 
-} 
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String args[]) {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+
+        System.out.println("Given Array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
+}
