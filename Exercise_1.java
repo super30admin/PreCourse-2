bb1405828c2f0c65fd2ce42add1b74e490d24e5e
Neha Mishra <neha79091@gmail.com>
2022-04-24 03:11:17 +0530

    Precourse-2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,21 +1,76 @@
-class BinarySearch { 
-    // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
-    } 
-  
-    // Driver method to test above 
-    public static void main(String args[]) 
-    { 
-        BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
-        int n = arr.length; 
-        int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
-        if (result == -1) 
-            System.out.println("Element not present"); 
+// Time Complexity :O(logn)(base 2) as we are dividing our array to half everytime;where n =no.of elements in array
+// Space Complexity :O(1)constant(we are not using any extra space)
+// Did this code successfully run on Leetcode :NA
+// Any problem you faced while coding this :no
+class BinarySearch {
+    // Returns index of x if it is present in arr[l.. r], else return -1
+    int binarySearch(int arr[], int l, int r, int x) {
+        int start = l;
+        int end = r;
+        // assigning values to start and end(we can use l and r directly too)
+        while (start <= end) {// making sure we dont get caught up in infinite loop using this condition
+            // and all indices will be checked at start==end
+
+            int mid = start + (end - start) / 2;// using this instead of start+end to avoid integer out of range error
+            if (arr[mid] == x) {// if we found the element, we'll simply return the index
+                return mid;
+            }
+            if (arr[mid] < x) {// else there are these two conditions, if element at index mid is greater than
+                               // target,
+                // means we have to go left so we'll set our end to mid-1 else set start to
+                // mid+1 as target is smaller
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        // Write your code here
+        return -1;// return -1 if element is not found
+    }
+
+    // Driver method to test above
+    public static void main(String args[]) {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 10;
+        int result = ob.binarySearch(arr, 0, n - 1, x);
+        if (result == -1)
+            System.out.println("Element not present");
         else
-            System.out.println("Element found at index " + result); 
-    } 
-} 
+            System.out.println("Element found at index " + result);
+    }
+}
+
+// ------------------BRUTE
+// FORCE----------------------------------------------------
+// Time Complexity :O(n) as we are traversing through the while array
+// Space Complexity :O(1)constant(we are not using any extra space)
+// Did this code successfully run on Leetcode :NA
+// Any problem you faced while coding this :no
+class BinarySearch {
+    // Returns index of x if it is present in arr[l.. r], else return -1
+    int binarySearch(int arr[], int l, int r, int x) {
+        // we'll traverse until target is found
+        for (int i = 0; i < r; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        // Write your code here
+        return -1;// return -1 if element is not found
+    }
+
+    // Driver method to test above
+    public static void main(String args[]) {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 10;
+        int result = ob.binarySearch(arr, 0, n - 1, x);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,90 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
-    /* This function is same in both iterative and 
-       recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
-        //Compare elements and swap.
-    } 
-  
-    // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
-        //Try using Stack Data Structure to remove recursion.
-    } 
-  
-    // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
-    // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
-} 
+// Time Complexity :O(nlogn) where n is no. of elements
+// Space Complexity :O(h) where h is no of elements in stack at a time 
+// Did this code successfully run on Leetcode :NA
+// Any problem you faced while coding this :in partition method, first I was using two pointers but that 
+//didn't work and how to use stack was kind of tricky
+
+import java.util.Stack;
+
+class IterativeQuickSort {
+    void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        return;
+        // Try swapping without extra variable
+    }
+
+    class Range {// making new range class to store in stack
+        int x;// start range
+        int y;// end range
+
+        Range(int X, int Y) {
+            x = X;
+            y = Y;
+        }
+
+        int getX() {
+            return x;
+        }// getter methods for both properties
+
+        int getY() {
+            return y;
+        }
+    }
+
+    /*
+     * This function is same in both iterative and
+     * recursive
+     */
+    int partition(int arr[], int l, int h) {
+        int pivot = arr[h];// making last element the pivot
+        int lowptr = l;// starting a pointer from low
+        for (int i = l; i <= h - 1; i++) {// swapping lowPtr with pivot if element at j is smaller than pivot
+            if (arr[i] <= pivot) {
+                swap(arr, lowptr, i);
+                lowptr++;
+            }
+        } // at last , the right location of pivot is found after swapping
+        swap(arr, lowptr, h);
+        return lowptr;// returning the position of pivot
+        // Write code here for Partition and Swap
+        // Write code here for Partition and Swap
+        // Compare elements and swap.
+    }
+
+    // Sorts arr[l..h] using iterative QuickSort
+    void QuickSort(int arr[], int l, int h) {
+        Stack<Range> st = new Stack<>();
+
+        st.push(new Range(l, h));
+        while (!st.isEmpty()) {
+            int x = st.peek().getX();
+            int y = st.peek().getY();
+            st.pop();
+            int pivot = partition(arr, x, y);
+            if (x < pivot - 1) {
+                st.push(new Range(x, pivot - 1));
+            }
+            if (pivot + 1 < y) {
+                st.push(new Range(pivot + 1, y));
+            }
+        }
+        // Try using Stack Data Structure to remove recursion.
+    }
+
+    // A utility function to print contents of arr
+    void printArr(int arr[], int n) {
+        int i;
+        for (i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
+    // Driver code to test above
+    public static void main(String args[]) {
+        IterativeQuickSort ob = new IterativeQuickSort();
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 };
+        ob.QuickSort(arr, 0, arr.length - 1);
+        ob.printArr(arr, arr.length);
+    }
+}
