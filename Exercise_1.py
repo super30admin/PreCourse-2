9681bd9d7af2642fbc16567a83489f1cd5d8e8b4
Chandra sagar Bhogadi <chandrasagarbhogadi@Chandras-MacBook-Air.local>
2022-04-24 09:20:32 -0400

    Completed Sprint Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -6,6 +6,30 @@
 def binarySearch(arr, l, r, x): 
   
   #write your code here
+  #recursive binary search
+  #checking base case 
+  if r>=l:
+    #finding the mid-point 
+    mid=l+(r-l)//2
+    #if element found return index
+    if arr[mid]==x:
+      return mid
+    #if element is smaller than mid element, then element will be present in left subarray
+    elif arr[mid]>x:
+      return binarySearch(arr,l,mid-1,x)
+    #if element is larger than mid element, then element will be present in right subarray
+    else:
+      return binarySearch(arr,mid+1,r,x)
+  #if base condition fails , return -1 (element not found)
+  else:
+    return -1
+
+
+
+  
+  
+
+  
   
     
   
@@ -17,6 +41,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print ("Element is present at index % d" % result )
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,62 @@
 # Node class  
+from platform import node
+
+
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data=data
+        self.next=None
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head=None
+        self.tail=self.head
+        self.length=0
+
         
   
     def push(self, new_data): 
+        #initialize new node
+        newnode=Node(new_data)
+        #case-1 if list is empty
+        if self.head==None:
+            self.head=newnode
+            self.tail=self.head
+        #case-2 if list is not empty
+        else:
+            #append at the tail
+            self.tail.next=newnode
+            #set tail as newnode
+            self.tail=newnode
+        self.length=self.length+1
         
   
     # Function to get the middle of  
     # the linked list 
+    #method-1
     def printMiddle(self): 
+        current_node=self.head
+        middle=self.length//2 #find the middle index
+        #traverse until the middle index and return the node.
+        for i in range(0,middle):
+            current_node=current_node.next
+        return current_node.data
+
+
+    #method-2
+    def printMiddle_2(self):
+        #initalize two pointers
+        slow=self.head  #goes one step
+        fast=self.head  #goes two steps
+        #loop until fast reaches end of linked-list
+        while fast!=None and fast.next!=None:#check both cause(even and odd case,and fast.next.next is being used)
+            slow=slow.next
+            fast=fast.next.next
+        #when fast reaches end , slow will be at middle
+        return slow.data
 
 # Driver code 
 list1 = LinkedList() 
@@ -23,4 +65,5 @@ list1.push(4)
 list1.push(2) 
 list1.push(3) 
 list1.push(1) 
-list1.printMiddle() 
+print(list1.printMiddle() )
+print(list1.printMiddle_2() )
