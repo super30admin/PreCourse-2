1a815edb87294967a2991acad9d5c6d4d0939de5
Mrigesh Dasgupta <dasgupta.m@northeastern.edu>
2022-04-25 19:06:22 -0400

    Precourse-2 Code Completed

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,8 @@
+// Time Complexity :o(nlogn)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :NO
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -5,15 +10,56 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+        //Your code here
+        int n1=m-l+1;
+        int n2=r-m;
+
+        int left[] = new int[n1];
+        int right[] = new int[n2];
+
+        for(int i =0;i<n1;++i){
+            left[i]=arr[l+i];
+        }
+        for(int j =0;j<n2;++j){
+            right[j]=arr[m+1+j];
+        }
+        int i =0,j=0;
+        int k=l;
+        while(i<n1 && j<n2){
+            if(left[i]<=right[j]){
+                arr[k]=left[i];
+                i++;
+            }else{
+                arr[k]=right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i<n1) {
+            arr[k]=left[i];
+            i++;
+            k++;
+        }
+        while(j<n2){
+            arr[k]=right[j];
+            j++;
+            k++;
+        }
+
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
     { 
-	//Write your code here
-        //Call mergeSort from here 
+	 //Write your code here
+    //Call mergeSort from here
+        if(l<r){
+           int mid = l+(r-l)/2 ;
+           sort(arr,l,mid);
+           sort(arr,mid+1,r);
+           merge(arr,l,mid,r);
+        }
     } 
   
     /* A utility function to print array of size n */
