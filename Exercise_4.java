3f81fc09373663c4ab622dd313d2015fe37786c6
Manaswi20 <89878086+Manaswi20@users.noreply.github.com>
2022-04-24 21:33:14 -0700

    Completed EX4

--- Exercise_4.java
+++ Exercise_4.java
@@ -6,6 +6,32 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+       int[] marray = new int[r-l];
+       int i = l, j = m, k = 0;
+       while(i < m && j < r) {
+            // if the left array has the small value then copy the left one
+            if(arr[i] < arr[j]) {               
+                marray[k++] = arr[i++];
+             // if the right array has the smallest value then copy the right one
+            } else {                            
+                marray[k++] = arr[j++];
+            }
+       }
+
+       // Copy all the remaining elements from left or right arrays
+       while(i < m) {
+           marray[k++] = arr[i++];
+       }
+
+       while(j < r) {
+            marray[k++] = arr[j++];
+       }
+
+       // Copy the result into the original array
+        for(int ctr = 0; ctr < marray.length; ctr++) {
+            arr[l + ctr] = marray[ctr];
+        }
+
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +40,18 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+        if(r - l == 1) {
+            return;
+        }
+
+        // Dividing array into half recursively until there's only one element
+        int m = l + (r - l) / 2;
+        sort(arr, l, m);
+        sort(arr, m, r);
+
+        // Merging two unsorted arrays
+        merge(arr, l, m, r+1);
+
     } 
   
     /* A utility function to print array of size n */
