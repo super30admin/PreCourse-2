a7c72cdc55d9f19831094675eb4fbbac098541ca
Tony Mathen <tonymathen@Tonys-MacBook-Pro.local>
2022-04-27 18:45:08 -0700

    PreCourse2 completed Tony Mathen_RN40APR2022

--- Exercise_1.py
+++ Exercise_1.py
@@ -6,17 +6,30 @@
 def binarySearch(arr, l, r, x): 
   
   #write your code here
+
+
+
+  while l <= r:
+    m = (l+r)//2
+    if arr[m] == x:
+      return m
+    elif arr[m] < x:
+      l = m + 1     
+    else:
+      r = m - 1
+
+  return -1
   
     
   
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
-  
+
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
-  
+
 if result != -1: 
-    print "Element is present at index % d" % result 
+  print("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+  print("Element is not present in array")
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,67 @@
 # Python program for implementation of MergeSort 
+
 def mergeSort(arr):
   
   #write your code here
+
+  low = 0
+  high = len(arr)-1
+  res = [-1]*len(arr)
+
+  def merge(arr, low, high, mid):
+
+    i = low
+    j = mid+1
+    idx = low
+
+    while i <= mid and j <= high:
+      if arr[i] <= arr[j]:
+        res[idx] = arr[i]
+        i += 1
+      else:
+        res[idx] = arr[j]
+        j += 1
+
+      idx += 1
+
+    if i < mid:
+      while i <= mid:
+        res[idx] = arr[i]
+        idx += 1
+        i += 1
+
+    elif j < high:
+      while j <= high:
+        res[idx] = arr[j]
+        idx += 1
+        j += 1
+
+    return res
+  
+  def mergesort(arr, low, high):
+      if low >= high:
+        return
+      
+      mid = (low+high)//2
+
+      mergesort(arr, low, mid)
+      mergesort(arr, mid+1, high)
+      merge(arr, low, high, mid)
+
+  mergesort(arr, low, high)
+
+  return res
+
+  
+
+    
+  
+#mergeSort([])
   
 # Code to print the list 
 def printList(arr): 
-    
+    for num in arr:
+      print(num, end=' ')
     #write your code here
   
 # driver code to test the above code 
@@ -13,6 +69,6 @@ if __name__ == '__main__':
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end="\n")  
     printList(arr) 
-    mergeSort(arr) 
+    arr = mergeSort(arr) 
     print("Sorted array is: ", end="\n") 
     printList(arr) 
