71ebb3a0cb6a26be2e7a94dc0603277f200063f3
Maharshi Kondapaneni <maharshi@ou.edu>
2022-05-12 19:54:41 -0500

    Completed mergeSort,binarySearch and middle of Linkedlist

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,45 @@
 # Python code to implement iterative Binary  
 # Search. 
-  
+# Time Complexity : O(log(size of array))
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this :
+# NO
+#
+# Your code here along with comments explaining your approach :
+# array always needs to be sorted is one major assumption I took into consideration.
+# Since its sorted and we need to find only one elment we need to narrow down our search if the mid element is not
+# matching then there is no chance that the value with less that that matches.
+# so we go for the side which we thing there might be one value present
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
-# Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
+def binarySearch(arr, l, r, x):
+    # write your code here
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            r = mid - 1
+        else:
+            l = mid + 1
+    return -1
+
+
+# Test array
+arr = [2, 3, 4, 10, 40]
 x = 10
-  
-# Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
-if result != -1: 
-    print "Element is present at index % d" % result 
-else: 
-    print "Element is not present in array"
+arr = [2]
+x = 2
+
+arr = []
+
+arr = [2, 4]
+
+# Function call
+result = binarySearch(arr, 0, len(arr) - 1, x)
+
+if result != -1:
+    print("Element is present at index ", result)
+else:
+    print("Element is not present in array")
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,51 @@
-# Python program for implementation of MergeSort 
+# Python program for implementation of MergeSort
+# // Time Complexity : O(log(N))
+# // Space Complexity :  O(N)
+# // Did this code successfully run on Leetcode :
+# // Any problem you faced while coding this : Not able to figure out how to do in place merge Sort so for now did using extra space
+# // Your code here along with comments explaining your approach
 def mergeSort(arr):
-  
   #write your code here
-  
+    return divide(arr)
+
+def divide(arr):
+    mid = (len(arr)) // 2
+    if len(arr) == 1:
+        return arr
+    left = divide(arr[:mid])
+    right = divide(arr[mid:])
+    return merge(left, right)
+
+def merge(arrL , arrR):
+    sortedArr = []
+    l = 0
+    r = 0
+    while l < len(arrL) and r < len(arrR):
+        if arrL[l] < arrR[r]:
+            sortedArr.append(arrL[l])
+            l += 1
+        else:
+            sortedArr.append(arrR[r])
+            r += 1
+
+    while l < len(arrL):
+        sortedArr.append(arrL[l])
+        l += 1
+    while r < len(arrR):
+        sortedArr.append(arrR[r])
+        r += 1
+    return sortedArr
 # Code to print the list 
-def printList(arr): 
-    
+def printList(arr):
     #write your code here
+    for i in arr:
+        print(i)
   
 # driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end="\n")  
     printList(arr) 
-    mergeSort(arr) 
+    arr = mergeSort(arr)
     print("Sorted array is: ", end="\n") 
     printList(arr) 
