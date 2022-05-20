797ed709962c47acb3b49c5dac3a97cc12c333d7
vatsalpathak <vatsalpathak22@gmail.com>
2022-05-19 22:59:46 -0700

    Complete Precourse-2

--- Exercise_1.py
+++ Exercise_1.py
@@ -5,7 +5,18 @@
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
   
-  #write your code here
+  if r >= l:
+
+    mid = (r+l) // 2
+
+    if arr[mid] == x:
+      return mid
+    elif arr[mid] > x:
+      return binarySearch(arr, l , mid - 1, x)
+    else:
+      return binarySearch(arr, mid + 1, r, x)
+  else:
+    return -1
   
     
   
