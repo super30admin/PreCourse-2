2967ca78e663f77a2c22679c7205643aa51f7648
akash0218 <92997084+akash0218@users.noreply.github.com>
2022-05-20 14:27:45 -0500

    finished

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,42 @@
-# Python program for implementation of MergeSort 
+# Time Complexity - O(n*log(n)) for best, average and worst cases
+# Space Complexity - O(n)
+
+# Python program for implementation of MergeSort
 def mergeSort(arr):
-  
-  #write your code here
-  
+    n = len(arr)
+    if n > 1:
+        # dividing the given array into two halves left and right
+        # and left array is further divided to left and right until the length of respective left/right array is > 1
+        left = arr[:n//2]
+        right = arr[n//2:]
+        mergeSort(left)
+        mergeSort(right)
+
+        # creating 3 pointers i, j, k for left, right and main array respectively
+        # comparing elements with left and right array and modifying main array according to that
+        i = j = k = 0
+        l = len(left)
+        r = len(right)
+        while i < l and j < r:
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i, k = i+1, k+1
+            else:
+                arr[k] = right[j]
+                j, k = j+1, k+1
+        # checking for any missing items and adding them to final array.
+        while i < l:
+            arr[k] = left[i]
+            i, k = i + 1, k + 1
+        while j < r:
+            arr[k] = right[j]
+            j, k = j + 1, k + 1
+
+            
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
+    print(arr)
+
   
 # driver code to test the above code 
 if __name__ == '__main__': 
