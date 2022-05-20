6663ff0e80c8e8ceeb6ea98728c0dbbe03919844
akash0218 <92997084+akash0218@users.noreply.github.com>
2022-05-20 14:54:40 -0500

    finished

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,36 @@
-# Python code to implement iterative Binary  
-# Search. 
-  
-# It returns location of x in given array arr  
-# if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
+# Python code to implement iterative Binary Search.
+# It returns location of x in given array arr if present, else returns -1
+def binarySearch(arr, l, r, x):
+    if len(arr) == 0 or x < arr[l] or x > arr[r]:
+        return -1
+    while l < r:
+        mid = l + (r-l)//2
+        # while loop to avoid repetitive elements in the array
+        while arr[l] == arr[l+1]:
+            l = l+1
+        while arr[r] == arr[r-1]:
+            r = r-1
+        if arr[l] == x:
+            return l
+        elif arr[r] == x:
+            return r
+        elif arr[mid] == x:
+            return mid
+        else:
+            # if mid-element is greater than the target, set the end pointer to the next element left of mid-element
+            # else start pointer is set next to next element right to the mid-element
+            if arr[mid] > x:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+
 # Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
+arr = [2, 3, 4, 10, 40]
 x = 10
-  
 # Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
+result = binarySearch(arr, 0, len(arr)-1, x)
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is at the index " + str(result))
 else: 
-    print "Element is not present in array"
+    print("Element is not present in the array")
