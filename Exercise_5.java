611d32db0486e39a5636241ea81a4d77e3226618
praneeha <34100736+praneeha@users.noreply.github.com>
2022-05-21 15:24:16 -0500

    Update Exercise_5.java

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,7 +1,17 @@
+import java.util.Stack;
 class IterativeQuickSort { 
+    //Time complexity : O(nlogn)
+    //Space complexity : O(1)
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+	//Try swapping without extra variable
+	    if(arr[i]==arr[j]){
+	        return;
+	    }
+	    arr[i]=arr[i]^arr[j];
+	    arr[j]=arr[i]^arr[j];
+	    arr[i]=arr[i]^arr[j];
+	
     } 
   
     /* This function is same in both iterative and 
@@ -9,12 +19,38 @@ class IterativeQuickSort {
     int partition(int arr[], int l, int h) 
     { 
         //Compare elements and swap.
+        int p=arr[h];
+        int pi=l-1;
+        for(int i=l;i<h;i++){
+            if(arr[i]<=p){
+                pi++;
+                swap(arr,i,pi);
+            }
+        }
+        swap(arr,pi+1,h);
+        return pi+1;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+        Stack<Integer> st = new Stack<>();
+        st.push(l);
+        st.push(h);
+        while(!st.empty()){
+            h=st.pop();
+            l=st.pop();
+            int p=partition(arr,l,h);
+            if(p-1>l){
+                st.push(l);
+                st.push(p-1);
+            }
+            if(p+1<h){
+                st.push(p+1);
+                st.push(h);
+            }
+        }
     } 
   
     // A utility function to print contents of arr 
