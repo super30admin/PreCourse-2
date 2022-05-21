a4231cb78a4e59473b31407d186f9a6403b1b58b
NishanthSayana <snvnishanth99@gmail.com>
2022-05-22 01:08:18 +0530

    PreCourse-2 Java | Please Review

--- Exercise_2.java
+++ Exercise_2.java
@@ -6,22 +6,50 @@ class QuickSort
        smaller (smaller than pivot) to left of 
        pivot and all greater elements to right 
        of pivot */
+
+    //Time Complexity=O(1)
+    //Space Complexity=o(1)
     void swap(int arr[],int i,int j){
-        //Your code here   
+        //Your code here
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
-    
-    int partition(int arr[], int low, int high) 
-    { 
-   	//Write code here for Partition and Swap 
-    } 
-    /* The main function that implements QuickSort() 
-      arr[] --> Array to be sorted, 
-      low  --> Starting index, 
+
+    //Time Complexity=O(n)
+    //Space Complexity=O(1)
+    int partition(int arr[], int low, int high)
+    {
+   	//Write code here for Partition and Swap
+        int i=low-1;
+        int pivot=arr[high];
+        for(int j=low;j<=high-1;j++){
+            if(arr[j]<pivot){
+                //shifting elements less than pivot to the begining of the array
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        //shifting the pivot to a balanced position->(elements<pivot)(pivot)(elements>pivot)
+        swap(arr,i+1,high);
+        return i+1;
+    }
+    /* The main function that implements QuickSort()
+      arr[] --> Array to be sorted,
+      low  --> Starting index,
       high  --> Ending index */
-    void sort(int arr[], int low, int high) 
-    {  
-            // Recursively sort elements before 
-            // partition and after partition 
+    void sort(int arr[], int low, int high)
+    {
+        //Time Complexity=T(k)+T(n-k)+n->Wort Case:O(n^2)|Avg case:O(nlogn)
+        //Space Complexity=O(1)
+        if(low<high){
+            int pivot=partition(arr,low,high);
+            // Recursively sort elements before
+            // partition and after partition
+            sort(arr,low,pivot-1);
+            sort(arr,pivot+1,high);
+        }
+
     } 
   
     /* A utility function to print array of size n */
--- Exercise_3.java
+++ Exercise_3.java
@@ -16,37 +16,55 @@ class LinkedList
   
     /* Function to print middle of linked list */
    //Complete this function
-    void printMiddle() 
-    { 
-        //Write your code here
-	//Implement using Fast and slow pointers
-    } 
-  
-    public void push(int new_data) 
-    { 
-        Node new_node = new Node(new_data); 
-        new_node.next = head; 
-        head = new_node; 
-    } 
 
-    public void printList() 
-    { 
-        Node tnode = head; 
-        while (tnode != null) 
-        { 
-            System.out.print(tnode.data+"->"); 
-            tnode = tnode.next; 
-        } 
-        System.out.println("NULL"); 
-    } 
-  
-    public static void main(String [] args) 
-    { 
-        LinkedList llist = new LinkedList(); 
-        for (int i=15; i>0; --i) 
-        { 
-            llist.push(i); 
-            llist.printList(); 
+    //Time Complexity=O(n)
+    //Space Complexity=O(1)
+    void printMiddle()
+    {
+        //Write your code here
+        if(head==null){
+            System.out.println("Null");
+        }if(head.next==null){
+            System.out.println(head.data);
+        }else{
+            //Implement using Fast and slow pointers
+            Node slow=head;
+            Node fast=head;
+            while(fast.next!=null && fast.next.next!=null){
+                slow=slow.next;
+                fast=fast.next.next;
+            }
+            System.out.println(slow.data);
+        }
+
+    }
+
+    public void push(int new_data)
+    {
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node;
+    }
+
+    public void printList()
+    {
+        Node tnode = head;
+        while (tnode != null)
+        {
+            System.out.print(tnode.data+"->");
+            tnode = tnode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String [] args)
+    {
+        LinkedList llist = new LinkedList();
+        llist.printList();
+        for (int i=15; i>0; --i)
+        {
+            llist.push(i);
+            llist.printList();
             llist.printMiddle(); 
         } 
     } 
