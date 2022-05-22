aa82dfbb8064c60f5934267a64796ba50458e8e0
Ruthwik12 <ratnaruthwikreddy@gmail.com>
2022-05-21 20:34:04 -0700

    Completed Precourse -2

--- Exercise_2.py
+++ Exercise_2.py
@@ -5,12 +5,30 @@ def partition(arr,low,high):
   
   
     #write your code here
+    pivot = arr[low]
+    i = low
+    j = high
+    while(i < j):
+        while(i < high and arr[i] <= pivot):
+            i += 1
+        while(j >= low and arr[j] >= pivot):
+            j -= 1
+        if(i < j):
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[low], arr[j] = arr[j], arr[low]
+    return j
+
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
     #write your code here
+    if(low < high):
+        k = partition(arr, low, high)
+        quickSort(arr, low, k-1)
+        quickSort(arr, k+1, high)
+
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
