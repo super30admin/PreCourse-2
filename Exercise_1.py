0a63b755ee7bb9f6d3502db4ba3d0462e5979ec7
unknown <harshilp402@gmail.com>
2022-05-21 23:15:33 -0400

    Completed Precourse-2

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,34 @@
+'''
+Time Complexity: O(log(n)) as we are spliting
+search space into half.
+Space Complexity: O(n)
+Probelms: to find the middle index of array.
+'''
 # Python code to implement iterative Binary  
 # Search. 
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
+def binarySearch(arr, l, r, x):
+  mid = (r+l)//2
+  if r < l:
+    return -1
+  if arr[mid] == x:
+    return mid 
+  elif arr[mid] < x:
+    return binarySearch(arr, mid+1, r, x)
+  else:
+    return binarySearch(arr, l, mid-1, x)  
     
   
 # Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
+arr = [ 2, 3, 4, 10, 40 ,50] 
+x = 4
   
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index ",result) 
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,45 @@
+'''
+Time Complexity: O(nlog(n))
+Space Complexity: O(n)
+Problem: Merging two lists using 
+        3 pointers.
+'''
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
+  if len(arr) > 1:
+    mid = len(arr)//2
+    Left = arr[0:mid]
+    Right = arr[mid:len(arr)]
+    mergeSort(Left)
+    mergeSort(Right)
+    l = 0
+    r = 0
+    k = 0
+    while l < len(Left) and r < len(Right):
+      if Left[l] <= Right[r]:
+        arr[k] = Left[l]
+        l += 1
+      else:
+        arr[k] = Right[r]
+        r += 1
+      k += 1
     
+    if r == len(Right):
+      while l < len(Left):
+        arr[k] = Left[l]
+        l += 1
+        k += 1
+    else:
+      while r < len(Right):
+        arr[k] = Right[r]
+        r += 1
+        k += 1
+
+
+# Code to print the list 
+def printList(arr):
+  for val in arr:
+    print(val)
     #write your code here
   
 # driver code to test the above code 
