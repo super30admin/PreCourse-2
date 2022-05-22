a2483cc1853949c489373a9a0f56ee1756872869
YashTike <yashtike.usa@gmail.com>
2022-05-22 00:43:41 -0700

    Completed PreCourse-2

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,10 +1,14 @@
+/*Time complexity = O(n * log(n))
+Space complexity = O(n)*/
 #include <bits/stdc++.h> 
 using namespace std;  
   
 // A utility function to swap two elements  
 void swap(int* a, int* b)  
 {  
-    //Your Code here 
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }  
   
 /* This function takes last element as pivot, places  
@@ -14,7 +18,22 @@ to left of pivot and all greater elements to right
 of pivot */
 int partition (int arr[], int low, int high)  
 {  
-    //Your Code here 
+    int pivot = arr[low];       //Choosing first item for pivot
+    int j = low;
+
+    //This loop performs comparisons between the pivot and other elements of the array, and keeps track of j as the pivot position
+    for (int i = low + 1; i <= high; i++)
+    {   
+        if (arr[i] < pivot)
+        {
+            j++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    //item j after the loop is swapped with the actual pivot, and the value of j is returned
+    swap(arr[low], arr[j]);
+
+    return j;
 }  
   
 /* The main function that implements QuickSort  
@@ -22,8 +41,16 @@ arr[] --> Array to be sorted,
 low --> Starting index,  
 high --> Ending index */
 void quickSort(int arr[], int low, int high)  
-{  
-    //Your Code here 
+{   
+    int pivot;
+    if (high > low)
+    {
+        pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+
+    return; 
 }  
   
 /* Function to print an array */
--- Exercise_4.cpp
+++ Exercise_4.cpp
@@ -1,3 +1,5 @@
+/* Time Complexity = O(n * log(n))
+   Space Complexity = O(n) */
 #include<stdlib.h> 
 #include<stdio.h> 
   
@@ -5,15 +7,49 @@
 // First subarray is arr[l..m] 
 // Second subarray is arr[m+1..r] 
 void merge(int arr[], int l, int m, int r) 
-{ 
-    //Your code here
+{   
+    //We create a temporary array to store the merged array, with tempPos as position indicator, and n as the size of array
+    int n = r - l + 1;
+    int tempArr[n];
+    int tempPos = l;
+    int leftEnd = m - 1;
+
+    while (l <= leftEnd && m <= r) 
+    {
+        if (arr[l] <= arr[m])
+            tempArr[tempPos++] = arr[l++];
+        else
+            tempArr[tempPos++] = arr[m++];
+    }
+
+    //Copying rest of left half
+    while (l <= leftEnd)
+        tempArr[tempPos++] = arr[l++];
+
+    //Copying rest of right half
+    while (m <= r)
+        tempArr[tempPos++] = arr[m++];
+
+    /*Copying temporary array back to the original from back to front,
+        because only the rightmost position is an accurate indicator for the edge of the array*/
+    for (int i = 0; i < n; i++, r--)
+    {
+        arr[r] = tempArr[r];
+    }
 } 
   
 /* l is for left index and r is right index of the 
    sub-array of arr to be sorted */
 void mergeSort(int arr[], int l, int r) 
-{ 
-    //Your code here
+{   
+    //We do not do left<=right, because we need atleast two elements in the array to divide and merge later
+    if (l < r) 
+    {
+        int m = (l + r) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m + 1, r);    
+    }
 } 
   
 /* UTILITY FUNCTIONS */
