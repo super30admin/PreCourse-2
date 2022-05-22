165a5b19336831ccea1e36a64c18a466fe10238d
Thirunaa <thirunaavukkarasu.thiru@gmail.com>
2022-05-22 05:39:01 -0400

    Test commit

--- Exercise_1.py
+++ Exercise_1.py
@@ -6,9 +6,9 @@
 def binarySearch(arr, l, r, x): 
   
   #write your code here
-  
+  pass
     
-  
+#Test Commit
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
@@ -17,6 +17,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print ("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
