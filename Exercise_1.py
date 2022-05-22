7d7709e731e43b21f43e0125b20b1bb3e33b854e
Nithish023 <nithishreddy124@gmail.com>
2022-05-22 09:25:08 +0530

    Completed SprintPrecourse2

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,11 +1,30 @@
+#Time complexity is O(logn)
+#Space complexity is O(1)
+
+
 # Python code to implement iterative Binary  
 # Search. 
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
+def binarySearch(arr, l, r, x):
+  l=0
+  r=len(arr)-1
+  middle=0
+  while l<=r:
+    middle=(l+r)//2
+    if arr[middle]>x:
+      r=middle-1
+    elif arr[middle]<x:
+      l=middle+1 
+    else:
+      return middle
+  return -1
   
-  #write your code here
+  
+
+
+
   
     
   
