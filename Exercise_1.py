0e30d0f42015c92f13a7106271c7a7d0b4383b36
mansi8140 <mansi.study8140@gmail.com>
2022-05-22 01:12:26 -0500

    PreCourse2 Completed in Python - Please review

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,10 +1,30 @@
-# Python code to implement iterative Binary  
-# Search. 
+#TimeComplexiry: O(logn)
+ #SpaceComplexity: O(1)
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
+  if r >= l:
+ 
+        mid = l + (r - l) // 2
+ 
+        # If element is present at the middle itself
+        if arr[mid] == x:
+            return mid
+ 
+        # If element is smaller than mid, then it
+        # can only be present in left subarray
+        elif arr[mid] > x:
+            return binarySearch(arr, l, mid-1, x)
+ 
+        # Else the element can only be present
+        # in right subarray
+        else:
+            return binarySearch(arr, mid + 1, r, x)
+ 
+  else:
+        # Element is not present in the array
+        return -1
   #write your code here
   
     
@@ -17,6 +37,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index", result)
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,14 +1,54 @@
-# Python program for implementation of MergeSort 
+#Timecomplexity: O(nlogn)
+#Spacecomplexity: O(1)
 def mergeSort(arr):
+    if len(arr) > 1:
   
-  #write your code here
+         # Finding the mid of the array
+        mid = len(arr)//2
+  
+        # Dividing the array elements
+        L = arr[:mid]
+  
+        # into 2 halves
+        R = arr[mid:]
+  
+        # Sorting the first half
+        mergeSort(L)
+  
+        # Sorting the second half
+        mergeSort(R)
+  
+        i = j = k = 0
+  
+       
+        while i < len(L) and j < len(R):
+            #comparing each element of left and right subarray to create a sorted list
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+  
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+  
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1  
+ 
   
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
+  for i in range(len(arr)):
+        print(arr[i], end=" ")
+  print()
   
-# driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end="\n")  
