877fd06f520dcb8fe3e2af570df3545bc23b92f1
AbinayaSwaminathan <aswaminathan3@horizon.csueastbay.edu>
2022-05-23 11:53:07 -0700

    Completed Sprint PreCourse2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,41 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# Node class
+#Time Complexity : O(N)
+#Space Complexity : O(1)
+#Any problem you faced while coding this : No
+#Traverse linked list with two pointers where we move one pointer by one and other pointer by two. When the  pointer 2 reaches end pointer 1  reaches the middle of the linked list.
+class Node:
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Function to get the middle of
+    # the linked list
+    def printMiddle(self):
+        ptr1 = self.head
+        ptr2 = self.head
+
+        if self.head is not None:
+            while (ptr2 is not None and ptr2.next is not None):
+                ptr2 = ptr2.next.next
+                ptr1 = ptr1.next
+            print("The middle element is: ", ptr1.data)
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2)
+list1.push(3)
+list1.push(1)
+list1.printMiddle()
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,51 @@
 # Python program for implementation of Quicksort
-
+#Time Complexity : O(NLogN)
+#Space Complexity : O(1)
+#Any problem you faced while coding this : No
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+def partition(arr,l,h):
+	i = l - 1
+	x = arr[h]
 
+	for j in range(l , h):
+		if arr[j] <= x:
 
-def quickSortIterative(arr, l, h):
-  #write your code here
+			# increment index of smaller element
+			i = i+1
+			arr[i],arr[j] = arr[j],arr[i]
 
+	arr[i+1],arr[h] = arr[h],arr[i+1]
+	return (i+1)
+
+def quickSortIterative(arr,l,h):
+	size = h - l + 1
+	stack = [0] * (size)
+	top = -1
+	top = top + 1
+	stack[top] = l
+	top = top + 1
+	stack[top] = h
+	while top >= 0:
+		h = stack[top]
+		top = top - 1
+		l = stack[top]
+		top = top - 1
+		p = partition( arr, l, h )
+		if p-1 > l:
+			top = top + 1
+			stack[top] = l
+			top = top + 1
+			stack[top] = p - 1
+		if p+1 < h:
+			top = top + 1
+			stack[top] = p + 1
+			top = top + 1
+			stack[top] = h
+
+# Driver code to test above
+arr = [4, 3, 5, 2, 1, 3, 2, 3]
+n = len(arr)
+quickSortIterative(arr, 0, n-1)
+print ("Sorted array is:")
+for i in range(n):
+	print ("%d" %arr[i]),
