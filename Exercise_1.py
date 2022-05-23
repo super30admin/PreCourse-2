85b4d698d9006801949701eb6460ceefc2a0c1cf
kri25shna99 <48583952+kri25shna99@users.noreply.github.com>
2022-05-22 23:12:21 -0500

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -6,7 +6,23 @@
 def binarySearch(arr, l, r, x): 
   
   #write your code here
-  
+    #write your code here
+  if r >= l:
+      mid = l + (r-1)//2
+
+      # If element is present at the middle
+      if arr[mid] == x:
+        return mid
+      # If element is smaller than mid, then it is present in left subarray
+      elif arr[mid] > x:
+        return binarySearch(arr, l, mid-1, x)
+      # Else the element is present in right subarray
+      else:
+        return binarySearch(arr, mid + 1, r, x)
+  else:
+      #for element not present in the array
+      return -1
+    
     
   
 # Test array 
