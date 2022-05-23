b88d07f2bfea39ac60c0918cd5085e53d0cd4a25
Dash_Cosmatos77 <53261974+Dhairya1999@users.noreply.github.com>
2022-05-22 22:27:39 -0500

    Update Exercise_4.py

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,49 @@
-# Python program for implementation of MergeSort 
-def mergeSort(arr):
-  
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
-    #write your code here
-  
-# driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+"""
+Time Complexity : O( n*log(n) )
+Space Complexity : O(n)
+"""
+
+
+
+def MergeSort(arr):
+
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        MergeSort(left)
+        MergeSort(right)
+
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+
+def PrintArr(arr):
+    print(arr)
+
+
+# driver code to test the above code
+if __name__ == '__main__':
+    arr = [23,13,4,7,9,11]
+    print("Original Array", end="\n")
+    PrintArr(arr)
+    MergeSort(arr)
+    print("Sorted Array", end="\n")
+    PrintArr(arr)
