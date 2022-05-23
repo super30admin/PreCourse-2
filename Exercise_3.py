da52d1fdaf7af8c06cc8a3585b2340607a4b19ce
vineetvk19 <67041692+vineetvk19@users.noreply.github.com>
2022-05-23 00:29:39 -0400

    Precourse-2, Exercise_3 python submission

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,44 @@
+# Author: Vineet Khanna
+
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data 
+        self.next = None 
         
 class LinkedList: 
   
-    def __init__(self): 
+    def __init__(self):
+        self.head = None
         
-  
-    def push(self, new_data): 
-        
-  
+    def push(self, new_data):
+        node1 = Node(new_data)
+        node1.next = self.head
+        self.head = node1
+         
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        p = self.head
+        count = 0
+        
+        while p:
+            count += 1
+            p = p.next
+
+        
+        new_count = count//2
+
+        p = self.head
+        for i in range(new_count):
+            p = p.next
+
+        print(p.data)
+
+
+
 
 # Driver code 
 list1 = LinkedList() 
@@ -24,3 +48,4 @@ list1.push(2)
 list1.push(3) 
 list1.push(1) 
 list1.printMiddle() 
+
