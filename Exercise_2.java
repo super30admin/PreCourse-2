9da6f479c66143a6baed12471f6bbe2a810b543a
Kumudini Sharad Thote <58613043+kumudinithote@users.noreply.github.com>
2022-05-23 15:15:27 -0700

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,10 @@
+/*
+ * Time Complexity :O(nlog(n))
+ * Space Complexity : O(N) N is number of elemnts inserted into the stack.
+ * Did this code successfully run on Leetcode : Didn't see this problem on Leetcode and hence solve locally.
+ * Any problem you faced while coding this : NO
+ * 
+ */
 class QuickSort 
 { 
     /* This function takes last element as pivot, 
@@ -7,21 +14,42 @@ class QuickSort
        pivot and all greater elements to right 
        of pivot */
     void swap(int arr[],int i,int j){
-        //Your code here   
+       
+    	int x = arr[i];
+	    arr[i] = arr[j];
+	    arr[j] = x;
     }
     
     int partition(int arr[], int low, int high) 
-    { 
-   	//Write code here for Partition and Swap 
+    {
+    	int i = (low - 1);
+    	int pivotNumber = arr[high];
+    	
+    	
+    	for(int j = low; j <= high - 1; j++)
+        {
+            if (arr[j] < pivotNumber)
+            {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+    	swap(arr, i + 1, high);
+        return (i + 1);
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
       low  --> Starting index, 
-      high  --> Ending index */
+      high  --> Ending index 
+     */
     void sort(int arr[], int low, int high) 
     {  
-            // Recursively sort elements before 
-            // partition and after partition 
+    	 //sort elements in ae=rray before  and after partition 
+    	if(low<high){
+            int j = partition(arr,low, high);
+            sort(arr, low, j-1);
+            sort(arr, j+1, high);
+        }
     } 
   
     /* A utility function to print array of size n */
@@ -45,4 +73,4 @@ class QuickSort
         System.out.println("sorted array"); 
         printArray(arr); 
     } 
-} 
+}
