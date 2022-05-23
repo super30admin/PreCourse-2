784084fdf71c930b8db49347d391dca74b0bfa13
vineetvk19 <67041692+vineetvk19@users.noreply.github.com>
2022-05-23 00:27:04 -0400

    Precourse-2, Exercise_1 python submission

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,11 +1,30 @@
+# Author: Vineet Khanna
 # Python code to implement iterative Binary  
 # Search. 
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
+
+# TC: O(log(n), as we divide the array in two repeatedly.
+def binarySearch(arr, l, r, x):
   #write your code here
+    middle = 0
+    while l <= r:
+        #calculating middle element of array.
+        middle = (l+r)//2
+
+        #Check if element is on left of middle.
+        if arr[middle]>x:
+            r = middle-1
+
+        #Check if element is on right of middle.
+        elif arr[middle]<x:
+            l = middle+1
+            
+        else:
+            return middle
+    return -1
+
   
     
   
@@ -17,6 +36,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print ("Element is present at index % d" % result) 
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
