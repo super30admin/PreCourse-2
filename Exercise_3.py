be1bb274451a6a2029357c13760e96f4b2a855fa
Dixika Grewal <59345244+dixg@users.noreply.github.com>
2022-05-22 17:26:04 -0700

    q3 - completed

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,19 +2,47 @@
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data=data
+        self.next=None  
         
 class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
+    def __init__(self):
+        self.head=None
+    def push(self, new_data):
+        newnode=Node(new_data)       
+        curr=self.head   
+        if self.head==None:
+            self.head=newnode
+            return
+        else:
+            newnode.next=self.head
+            self.head=newnode
+            print(" Newly added and last itme ",self.head.data)
+            return
+            
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        print(self.head.data)
+        curr=self.head
+        count=0
+        while curr!=None:
+            count+=1
+            curr=curr.next
+        print("count=", count)
+        midnode_indx=count//2
+        print("midnode_indx =",midnode_indx) 
+        ptr=self.head
+        mid_counter=0
+        while mid_counter!=midnode_indx:
+            mid_counter+=1
+            ptr=ptr.next
+        print("@@@", ptr.data)
+        return ptr.data
+            
 
 # Driver code 
 list1 = LinkedList() 
