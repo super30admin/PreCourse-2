413772526b256b8ce22080f7449623c1b38277b9
Jayavani Akkiraju <jayavaniakkiraju@Jayavanis-MacBook-Pro.local>
2022-05-26 11:33:50 -0400

    Completed sprint PreCourse2

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,13 +2,27 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
     #write your code here
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i + 1
+            (arr[i], arr[j]) = (arr[j], arr[i])
+    (arr[i + 1], arr[high]) = (arr[high], arr[i + 1])
+    return i + 1
   
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
+
+def quickSort(arr,low,high):
+
+    if low < high:
+        partit = partition(arr, low, high)
+        quickSort(arr, low, partit - 1)
+        quickSort(arr, partit + 1, high)
+
+    
     
     #write your code here
   
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,10 +1,48 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
+
   
   #write your code here
-  
-# Code to print the list 
-def printList(arr): 
+  if len(arr) > 1:
+
+    mid = len(arr)//2
+
+    Left = arr[:mid]
+    Right = arr[mid:]
+
+    mergeSort(Left)
+    mergeSort(Right)
+
+    i = 0
+    j = 0
+    k = 0
+
+    size_L = len(Left)
+    size_R = len(Right)
+
+    while i < size_L and j < size_R:
+      if Left[i] < Right[j]:
+        arr[k] = Left[i]
+        i += 1
+      else:
+        arr[k] = Right[j]
+        j += 1
+      k += 1
+
+    while i < size_L:
+      arr[k] = Left[i]
+      i += 1
+      k += 1
+
+    while j < size_R:
+      arr[k] = Right[j]
+      j += 1
+      k += 1
+
+def printList(arr):
+  size = len(arr)
+  for i in range(size):
+    print(arr[i], " ")
     
     #write your code here
   
