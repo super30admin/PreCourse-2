e334837782370b541aa49db098bb68ffc0682f39
AkashSJSU <105601154+AkashSJSU@users.noreply.github.com>
2022-05-30 14:09:45 -0700

    Update Exercise_1.cpp

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -5,7 +5,18 @@
 // otherwise -1 
 int binarySearch(int arr[], int l, int r, int x) 
 {   
-    //Your Code here 
+    if(r>=l)
+    {
+        int mid= l+(r-l) / 2 ;
+        if(arr[mid]==x)
+            return mid;
+        if(arr[mid]>x)
+            return binarySearch(arr,l,mid-1,x);
+
+        return binarySearch(arr,mid+1,r,x);
+        
+    }
+    return -1;
 } 
   
 int main(void) 
