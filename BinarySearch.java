8eb558d4d9169a5edf26223d5e586f710e1bf884
V SAIKISHORE CHARY <69071073+Kishore7-7@users.noreply.github.com>
2022-05-30 15:02:20 -0500

    Added function for binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,4 +1,7 @@
-class BinarySearch { 
+class BinarySearch {
+
+    // Time Complexity :O(logn)
+// Space Complexity :O(1)
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     {if(r>=l) {
