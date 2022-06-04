adf0a1463278347438ac7747a7acb70c0520057a
Kedar Kulkarni <kedarvijayk@kedarvijayk-a01.vmware.com>
2022-06-04 16:38:43 -0400

    Added singly linked list mid

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,25 +2,58 @@
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
   
     def push(self, new_data): 
-        
+        n = Node(new_data)
+        if not self.head:
+            self.head = n
+        else:
+            p = self.head
+            while p.next:
+                p = p.next
+            p.next = n
   
+    def print(self):
+        p = self.head
+        while p:
+            print(p.data)
+            p = p.next
+
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        p1 = p2 = self.head
+        while p1 and p1.next:
+            p1 = p1.next.next
+            if p1:
+                p2 = p2.next
+        print(p2.data)
+    
+    def printMiddleTwoPass(self):
+        size = 0
+        p = self.head
+        while p.next:
+            size += 1
+            p = p.next
+        mid = size // 2
+        p = self.head
+        while mid > 0:
+            p = p.next
+            mid -= 1
+        print(p.data)
 
 # Driver code 
 list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
+for i in range(16):
+    list1.push(i+1)
+# list1.print()
 list1.printMiddle() 
+list1.printMiddleTwoPass()
