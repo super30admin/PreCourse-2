6bda35f8c6656c641147c830eb1086a945a51dec
Kareem Ahmed Sheik <ahmedkareem999@gmail.com>
2022-06-16 23:21:34 +0530

    Completed S30 PreCourse-2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,40 @@
-# Python program for implementation of Quicksort Sort 
-  
+# Python program for implementation of Quicksort Sort
+# Time Complexity : O(n*log n)
+# Space Complexity : O(n*log n)
+# Did this code successfully run on Leetcode : N/A
+# Any problem you faced while coding this : None
+
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
     #write your code here
-  
+    pivot = arr[low] # considering first element as pivot
+    left = low + 1
+    right = high
+    while True:
+        while left <= right and arr[left] <= pivot:  # while left element is less than or equal to right and less than or equal to pivot element
+            left += 1
+        while left <= right and arr[right] >= pivot: # while right element is greater than or equal to left and greater than or equal to pivot element
+            right -= 1
+        if right < left: # loop breaks when right element becomes less than left
+            break
+        else:
+            arr[left],arr[right] = arr[right],arr[left] # initialising left to right and vice-versa
+    arr[low],arr[right] = arr[right],arr[low]
+    return right
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
+
+# Function to do Quick sort
+def quickSort(arr,low,high):
+
     #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+    if low < high:
+        part = partition(arr,low,high)
+        quickSort(arr,low,part - 1)  # using recursive method for sorting left part of  the list
+        quickSort(arr,part + 1,high) # using recursive method for sorting right part of the list
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr,0,n-1)
+print ("Sorted array is:")
+print(arr)
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,69 @@
 # Python program for implementation of Quicksort
-
+# Time Complexity : O(n*log n)
+# Space Complexity : O(log n) as partition done similar to tree
+# Did this code successfully run on Leetcode : N/A
+# Any problem you faced while coding this : None
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
   #write your code here
-
+  p = arr[l] # considering l element as p
+  left = l + 1
+  right = h
+  while True:
+      while left <= right and arr[left] <= p:  # while left element is less than or equal to right and less than or equal to p element
+          left += 1
+      while left <= right and arr[right] >= p: # while right element is greater than or equal to left and greater than or equal to p element
+          right -= 1
+      if right < left: # loop breaks when right element becomes less than left
+          break
+      else:
+          arr[left],arr[right] = arr[right],arr[left] # initialising left to right and vice-versa
+  arr[l],arr[right] = arr[right],arr[l]
+  return right
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  # creating an empty stack
+  length_of_stack = (h - l) + 1
+  stack = [0] * (length_of_stack)
 
+  #initialising top of the stack to -1 denoting it as empty
+  top = -1
+
+  #pushing initial values of low and high to stack
+  top += 1 # incrementing top value by 1
+  stack[top] = l # added low value to stack's top
+  top += 1
+  stack[top] = h
+
+  # keep removing from stack if its not empty
+  while top >= 0: # checking if top value is greater than or equal to zero
+      h = stack[top]
+      top -= 1
+      l = stack[top]
+      top -= 1
+
+      pivot = partition(arr,l,h) # choosing the pivot element's index
+
+      # pushing left side elements to stack
+      if pivot - 1 > l:
+          top += 1
+          stack[top] = l
+          top += 1
+          stack[top] = pivot - 1
+
+      # pushing right side of elements to stack
+      if pivot + 1 < h:
+          top += 1
+          stack[top] = pivot + 1
+          top += 1
+          stack[top] = h
+
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+print(f"Original array is: {arr} ")
+quickSortIterative(arr,0,n-1)
+print ("Sorted array is:")
+print(arr)
