4355974a8cb430199f3140af7b98c8a46a65cc46
Rutvik Avaiya <rutvikavaiya01@gmail.com>
2022-06-17 13:54:11 -0500

    Completed Sprint Precourse2

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,25 +2,40 @@
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data=data
+        self.next=None  
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head=None
         
   
     def push(self, new_data): 
+        newnode=Node(new_data)
+        newnode.next=self.head
+        self.head = newnode
         
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        lptr=self.head
+        rptr=self.head
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+        if self.head is not None:
+            while(rptr is not None and rptr.next is not None):
+                rptr=rptr.next.next
+                lptr=lptr.next
+            print("Mid Element: ",lptr) 
+
+if __name__=='__main__': 
+    # Driver code 
+    list1 = LinkedList() 
+    list1.push(5) 
+    list1.push(4) 
+    list1.push(2) 
+    list1.push(3) 
+    list1.push(1) 
+    list1.printMiddle() 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,64 @@
 # Python program for implementation of Quicksort
+#Time Complexity: O(nlogn)
+#Space Complexity: O(logn)
+
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+  k = ( l - 1 )
+  num = arr[h]
+  
+  for j in range(l , h):
+        if   arr[j] <= num:
+            # increment index of smaller element
+            k = k+1
+            arr[k],arr[j] = arr[j],arr[k]
+  
+  arr[k+1],arr[h] = arr[h],arr[k+1]
+  return (k+1)
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+  size = h - l + 1
+  stack = [0] * (size)
+  
+  # initialize top of stack
+  top = -1
+  
+  # push initial values of l and h to stack
+  top = top + 1
+  stack[top] = l
+  top = top + 1
+  stack[top] = h
+  
+  # Keep popping from stack while is not empty
+  while top >= 0:
+        h = stack[top]
+        top = top - 1
+        l = stack[top]
+        top = top - 1
+        p = partition( arr, l, h )
+  
+        # If there are elements on left side of pivot,
+        # then push left side to stack
+        if p-1 > l:
+            top = top + 1
+            stack[top] = l
+            top = top + 1
+            stack[top] = p - 1
+  
+        # If there are elements on right side of pivot,
+        # then push right side to stack
+        if p+1 < h:
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = h
+
+arr = [40, 20, 45, 65, 10, 33, 25, 39]
+n = len(arr)
+quickSortIterative(arr, 0, n-1)
+print ("Sorted array is:")
+for i in range(n):
+    print ("%d" %arr[i]),
 
