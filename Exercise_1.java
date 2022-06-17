1941e8d62c943a86a7d5468d40b794ad81aa8558
Meet Kothiya <meetkothiya96.mk@gmail.com>
2022-06-17 12:06:03 -0500

    Completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,19 @@
+// Time Complexity : O(logN), where N is length of an array. So O(log5 base 2)
+// Space Complexity : O(1)
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            if(x > arr[mid]){
+                l = mid + 1;
+            } else if(x < arr[mid]){
+                r = mid - 1;
+            } else return mid;
+        }
+        return -1;
     } 
   
     // Driver method to test above 
@@ -11,7 +22,7 @@ class BinarySearch {
         BinarySearch ob = new BinarySearch(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
-        int x = 10; 
+        int x = 3; 
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,6 @@
+//Time Complexity: O(N logN)
+//Space Complexity: O(N)
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -5,15 +8,55 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+       //Your code here
+        int leftArraySize = m - l + 1;
+        int rightArraySize = r - m;
+ 
+        int left[] = new int[leftArraySize];
+        int right[] = new int[rightArraySize];
+ 
+        for (int i = 0; i < leftArraySize; i++){
+            left[i] = arr[l + i];
+        }for (int j = 0; j < rightArraySize; ++j){
+            right[j] = arr[m+j+1];
+        }
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while (i < leftArraySize && j < rightArraySize){
+            if(left[i] <= right[j]){
+                arr[k] = left[i];
+                i++;
+            } else{
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        while (i < leftArraySize) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        } while (j < rightArraySize) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
     { 
-	//Write your code here
-        //Call mergeSort from here 
+        //Write your code here
+        if(l<r){
+            int m = (l + r) / 2;
+            sort(arr, l, m);
+            sort(arr, m+1, r);
+            merge(arr, l, m, r);
+        }
     } 
   
     /* A utility function to print array of size n */
