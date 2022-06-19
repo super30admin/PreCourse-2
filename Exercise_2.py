15617412671a44d4e3bef6d5d796e13a2534e7f1
navya0405 <navya.spin@gmail.com>
2022-06-19 16:21:40 -0700

    completed precourse-2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,7 +1,31 @@
 # Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
+#  average time complexity: O(log n)
+#  worst case time complexity: O(n^2)
+
+# approach:
+# initially, low is at index 0, high is at len(arr)-1
+# set pivot as the first element, if element at low is lesser than pivot, increment low. if element at high is gretaer than pivot, decrement high
+# when the element at low > pivot, or element at high < pivot, exit loop and swap both. when low becomes > high, end the outer loop and swap pivot and high
+# high is partition index
+# at this point, pivot would be at the sorted place. 
+# all the elements before pivot would be less than pivot and at right would be greater than pivot
+
+# recursively run the above while low < high.
+# left array would be from low to partiton index-1 and right array would be partition index+1 to high
+
 def partition(arr,low,high):
+    pivot_index = low
+    pivot = arr[pivot_index]
+    while(low<high):
+        while low < len(arr) and arr[low]<= pivot:
+            low += 1
+        while arr[high]> pivot:
+            high -= 1
+        if low < high:
+            arr[low], arr[high] = arr[high], arr[low]
+    arr[high], arr[pivot_index] = arr[pivot_index], arr[high]
+    return high
+    
   
   
     #write your code here
@@ -9,6 +33,11 @@ def partition(arr,low,high):
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
+    if low < high:
+        partition_index = partition(arr, low, high) #get the partition index to partition the array
+        quickSort(arr, low, partition_index-1) # sort the left partition array
+        quickSort(arr,partition_index+1, high) # sort the right partition array
+        
     
     #write your code here
   
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,48 @@
 # Python program for implementation of MergeSort 
+# time complexity: O(n logn)
+
+#approach: divide the elements into two halves each time until all the elements are separated into single array
+# then merge from bottom with mergesort_list function in a sorted manner
 def mergeSort(arr):
+  if(len(arr)==1):
+    return arr
+  mid = len(arr)//2
+  left = arr[:mid]
+  right = arr[mid:]
+  mergeSort(left)
+  mergeSort(right)
+  mergesort_list(left, right, arr)
+  
   
   #write your code here
   
+def mergesort_list(list1, list2, arr):
+  i, j, k = 0, 0, 0
+  while i<len(list1) and j < len(list2):
+    if list1[i]<=list2[j]:
+      arr[k] = list1[i]
+      i += 1
+    else:
+      arr[k]= list2[j]
+      j += 1
+    k += 1
+
+  while i < len(list1):
+    arr[k]=list1[i]
+    i += 1
+    k += 1
+  while j < len(list2):
+    arr[k]=list2[j]
+    j += 1
+    k += 1
+
+
+
 # Code to print the list 
 def printList(arr): 
     
     #write your code here
+    print(arr)
   
 # driver code to test the above code 
 if __name__ == '__main__': 
