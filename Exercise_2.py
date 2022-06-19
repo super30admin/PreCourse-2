46bfd72285222bf10d14ccd3c6d5e4a112b57420
Pranav Singh <46050869+pranavsinghps1@users.noreply.github.com>
2022-06-20 05:20:24 +0530

    PC2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,29 @@
-# Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+"""
+Time complexity for:
+partition of elements = n
+quicksort = O(logn)
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
+Space complexity for quicksort = O(n)
+"""
+
+def partition(arr,low,high):
     #write your code here
+    pivot = arr[high]
+    ip = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            ip += 1
+            arr[j], arr[ip] = arr[ip], arr[j]
+    arr[ip+1], arr[high] = arr[high], arr[ip+1]
+    return ip+1
+
+
+def quickSort(arr,low,high):
+    #write your code here
+    if low < high:
+        partition_index = partition(arr, low, high)
+        quickSort(arr, low, partition_index - 1)
+        quickSort(arr, partition_index + 1, high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
@@ -18,6 +31,12 @@ n = len(arr)
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+    print ("%d" %arr[i])
+
+print("*"*10)
+arr = [1, 6, 56, 9, 1, 53] 
+n = len(arr) 
+quickSort(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i])
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,28 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# Node class
+"""
+TC: O(n)
+SC: O(1)
+
+"""
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+    def printMiddle(self):
+        ptr1 = self.head
+        ptr2 = self.head
+        # ptr1 will catch upto ptr2, while ptr2 checks for the last element.
+        while ptr2 and ptr2.next:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next.next
+        print("The middle element is ", ptr1.data)
 
 # Driver code 
 list1 = LinkedList() 
