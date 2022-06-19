9a51215f122e15b74ca69af90f02ec8fe27cc10a
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-06-19 18:59:47 -0400

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,23 @@
+// Time Complexity : o(log(n)) since we'll elimate half of the elements from the array in each iteration 
+// Space Complexity : o(n) to store the n elements in an array
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+// check if the middle index element is equal to x. if not, check if x is greater than middle element so its on the right sub array so change l to middle+1 otherwise its in the left sub array to change r to middle -1.
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        while(l<=r){
+            int middle = l + ((r-l)/2);
+            if(arr[middle] == x)
+                return middle;
+            else if(arr[middle] < x)
+                l= middle + 1;
+            else 
+                r = middle -1;
+        }
+        return -1;
     } 
   
     // Driver method to test above 
