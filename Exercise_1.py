09cb23253c00b2a67202d8eed1249416c04681a1
Pranjali Seth <pranjaliseth@Pranjalis-MacBook-Air.local>
2022-06-19 19:49:08 -0700

    Updated

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,11 +2,31 @@
 # Search. 
   
 # It returns location of x in given array arr  
-# if present, else returns -1 
+# if present, else returns -1
+
+# Time Complexity: O(logn)
+# Space Complexity: O(1)
+
+import math
+
 def binarySearch(arr, l, r, x): 
   
   #write your code here
-  
+
+    while r < l:
+        return -1
+
+    while r >= l:
+        mid_index = math.floor((l + r) / 2)
+        if x < arr[mid_index]:
+            r = mid_index - 1
+        elif x > arr[mid_index]:
+            l = mid_index + 1
+        elif x == arr[mid_index]:
+            return mid_index
+        else:
+            return -1
+    return -1
     
   
 # Test array 
@@ -17,6 +37,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print ("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,55 @@
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
+
+# Time Complexity = O(logn)
+# Space Complexity = O(1)
+
 def partition(arr, l, h):
-  #write your code here
+    # write your code here
+
+    i = (l - 1)
+    pivot = arr[h]
+    for j in range(l, h):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    return i + 1
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    # write your code here
 
+    size = h - l + 1
+    stack = [0] * (size)
+    top = -1
+    top = top + 1
+    stack[top] = l
+    top = top + 1
+    stack[top] = h
+    while top >= 0:
+        h = stack[top]
+        top = top - 1
+        l = stack[top]
+        top = top - 1
+        p = partition(arr, l, h)
+        if p - 1 > l:
+            top = top + 1
+            stack[top] = l
+            top = top + 1
+            stack[top] = p - 1
+        if p + 1 < h:
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = h
+
+
+# Driver Code
+arr = [4, 2, 6, 9, 8]
+n = len(arr)
+# Calling quickSort function
+quickSortIterative(arr, 0, n - 1)
+for i in range(n):
+    print(arr[i], end=" ")
