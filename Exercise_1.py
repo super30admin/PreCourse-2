04100d524996ff40b3ca65a36f73f9feecf3e231
saiteja1905 <sjavvaji@asu.edu>
2022-06-19 23:01:12 -0700

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,6 +4,18 @@
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
+  mid = int((l+r)/2)
+  if(arr[mid]==x):
+    return mid
+  elif(x<arr[mid]):
+    for i in range(l,mid):
+      if(arr[i]==x):
+        return i
+  else:
+    for j in range(mid,r):
+      if(arr[j]==x):
+        return j
+  return -1
   
   #write your code here
   
@@ -17,6 +29,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index {}".format(result))
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
