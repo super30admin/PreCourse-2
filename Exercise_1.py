00900752785acdc6a02add1b1165b78184104e81
vish1525 <43075323+vish1525@users.noreply.github.com>
2022-06-21 02:18:19 -0400

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,3 +1,15 @@
+"""
+Runtime Complexity:
+O(log n)- The algorithm works by taking the middle element of the array and slices it into two halves, left and right. Then we compare and check whether the target 
+element is in the left half or right half. If target is lesser than mid then we search the left half else we search right half iteratively. Since the iteration goes on ,
+the elements are stores in log levels. This forms a iteration tree. Length of 'n' node tree is log n. Therefore the overall time complexity is O(log n)
+
+Space complexity:
+O(1) - we just compare and assign mid. These operations take O(1) space .
+
+Yes, it worked on leetcode.
+"""
+
 # Python code to implement iterative Binary  
 # Search. 
   
@@ -6,9 +18,17 @@
 def binarySearch(arr, l, r, x): 
   
   #write your code here
-  
-    
-  
+    while l<=r:
+        mid = l+(r-l)//2
+        
+        if arr[mid] ==x:
+            return mid
+        elif arr[mid]<x:
+            l = mid+1
+        else: 
+            r =mid-1
+    return -1
+
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
@@ -17,6 +37,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index % d" % result) 
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
