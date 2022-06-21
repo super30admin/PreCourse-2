c756caa6eb5bb1989ac40d81fa24286ba1ea18fd
Dnbhatt <62647174+Dnbhatt99@users.noreply.github.com>
2022-06-21 13:13:31 -0500

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,12 +1,31 @@
+# Runtime Complexity - O(log(n))
+# Space Complexity - O(1)
+
 # Python code to implement iterative Binary  
 # Search. 
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
+
 def binarySearch(arr, l, r, x): 
   
   #write your code here
-  
+    if l == r:
+        if x == arr[0]:   
+            return 0
+        else:   
+            return -1
+    
+    while l <= r:
+        mid = (l + r) // 2
+        if x not in arr:
+            return -1
+        if arr[mid] == x:
+            return mid
+        elif x < arr[mid]:
+            r = mid - 1
+        else:
+            l = mid + 1
     
   
 # Test array 
@@ -17,6 +36,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index - ", result) 
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
