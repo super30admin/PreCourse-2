b5a89a9740df45e17e5e1c4041128ca2b4b1546f
jayachandravvs <42566670+jayachandravvs@users.noreply.github.com>
2022-06-21 21:50:39 -0500

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,28 +1,48 @@
 class QuickSort 
-{ 
-    /* This function takes last element as pivot, 
-       places the pivot element at its correct 
-       position in sorted array, and places all 
-       smaller (smaller than pivot) to left of 
-       pivot and all greater elements to right 
-       of pivot */
-    void swap(int arr[],int i,int j){
-        //Your code here   
-    }
-    
-    int partition(int arr[], int low, int high) 
-    { 
-   	//Write code here for Partition and Swap 
-    } 
-    /* The main function that implements QuickSort() 
-      arr[] --> Array to be sorted, 
-      low  --> Starting index, 
-      high  --> Ending index */
-    void sort(int arr[], int low, int high) 
-    {  
-            // Recursively sort elements before 
-            // partition and after partition 
-    } 
+ public static int[] quickSort(int[] array) {
+		    quickSort(array,0,array.length-1);
+		    return array;
+		  }
+			
+			public static void quickSort(int[] array,int startIdx, int endIdx){
+		if (startIdx>=endIdx){
+		return;
+			
+		}	
+				int pivotIdx= startIdx;
+			 int leftIdx = startIdx+1;
+				int rightIdx= endIdx;
+				
+				while (rightIdx>=leftIdx){
+					
+					if ( array[leftIdx]> array[pivotIdx] && array[rightIdx] < array[pivotIdx]){
+						swap(leftIdx,rightIdx,array);
+					}if (array[leftIdx] <=array[pivotIdx]){
+						leftIdx+=1;
+					}if ( array[rightIdx] >=array[pivotIdx]){
+						rightIdx-=1;
+					}
+					
+				}
+					swap(pivotIdx,rightIdx,array);
+					boolean leftSubArrayISSmaller= rightIdx-1-startIdx<endIdx-(rightIdx+1);
+				System.out.println("rightIdx "+rightIdx +" "+ "startIdx "+startIdx + " endIdx "+endIdx+ " rightIdx-1-startIdx "+ (rightIdx-1-startIdx) + " endIdx-(rightIdx+1) "+(endIdx-(rightIdx+1)));
+			if(leftSubArrayISSmaller){
+				
+				quickSort(array,startIdx,rightIdx-1);
+					quickSort(array,rightIdx+1,endIdx);
+		}  	else{
+				quickSort(array,rightIdx+1,endIdx);
+					quickSort(array,startIdx,rightIdx-1);
+			}
+		}
+
+		public static void swap(int i , int j , int [] array){
+			int temp=array[j];
+			array[j] = array[i];
+			array[i]= temp;
+			
+		}
   
     /* A utility function to print array of size n */
     static void printArray(int arr[]) 
