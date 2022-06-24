4289ebf50b2f9b8c2c2d9819d7a1f3cd9826448f
saiteja1905 <sjavvaji@asu.edu>
2022-06-23 18:07:57 -0700

    Completed code changes

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,14 +1,36 @@
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
+from tempfile import tempdir
+
+
 def partition(arr,low,high):
+    pivot = arr[high]
+    ptr = low
+    for i in range(low,high):
+        if(arr[i]<pivot):
+            temp = arr[i]
+            arr[i] = arr[ptr]
+            arr[ptr] = temp
+            ptr+=1
+    arr[ptr], arr[high] = arr[high], arr[ptr]
+    return ptr
+            
   
   
     #write your code here
   
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
+def quickSort(arr,low,high):
+    if(len(arr)==1):
+        return arr
+    if(low<high):
+        pt = partition(arr, low, high)
+        quickSort(arr, low, pt-1)
+        quickSort(arr, pt+1, high)
+    return arr
+
     
     #write your code here
   
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,22 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+  swap_index = l
+  pivot_index = l
+  for i in range(pivot_index+1, h+1):
+    if(arr[i]<arr[pivot_index]):
+      swap_index+=1
+      arr[swap_index], arr[i] = arr[i], arr[swap_index]
+  arr[swap_index], arr[pivot_index] = arr[pivot_index], arr[swap_index]
+  return swap_index
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+  if(l<h):
+    pivot_index = partition(arr, l, h)
+    quickSortIterative(arr, l, pivot_index-1)
+    quickSortIterative(arr, pivot_index+1, h)
+  return arr
 
+arr = [2,4,3,1,5]
+print(quickSortIterative(arr,0, len(arr)-1))
