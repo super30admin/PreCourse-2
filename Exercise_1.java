db90b0bda77580fc567fd6a0a72053cbee456e27
Sundeep Akella <68639805+sundeep1299@users.noreply.github.com>
2022-06-25 13:49:51 -0700

    Binary search - Please review

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,28 @@
+//complexity- O(logn)
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+           
+            int mid= (l + (r-l))/2;
+            while(l<r)
+            {
+                if(arr[mid]==x)
+                {
+                    return mid;
+                }
+                else if(x>arr[mid])
+                {
+                    l=mid+1;
+
+                }
+                else 
+                {
+                    r=mid+1;
+                }
+
+            }
+           return -1;
     } 
   
     // Driver method to test above 
