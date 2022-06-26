3bff709a11ddf0700ca7bd492fbfb667eaf97709
saiteja1905 <sjavvaji@asu.edu>
2022-06-25 19:36:59 -0700

    Updated Binary Search Code

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,26 +4,21 @@
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  mid = int((l+r)/2)
-  if(arr[mid]==x):
-    return mid
-  elif(x<arr[mid]):
-    for i in range(l,mid):
-      if(arr[i]==x):
-        return i
-  else:
-    for j in range(mid,r):
-      if(arr[j]==x):
-        return j
-  return -1
-  
-  #write your code here
-  
-    
+  low = l
+  high = r
+  while(low<=high):
+    mid = int(low + (high-low)/2)
+    if(arr[mid]==x):
+      return mid
+    elif(x<arr[mid]):
+      high = mid -1
+    else:
+      low = mid+1
+  return -1 
   
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
+x = 4
   
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
