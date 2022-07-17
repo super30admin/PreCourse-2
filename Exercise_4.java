c3e27021ed88bb972b3e3f7f99cb43bacc44710d
KAPILESHYADAVBONGU <kapileshyadav2000@gmail.com>
2022-07-17 17:38:55 -0500

    completed ex4

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,10 +1,35 @@
-class MergeSort 
+class Exercise_4 
 { 
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
-    {  
+    {  int k;
+        int n1=m-l+1,n2=r-m;
+        int le[]=new int[n1];
+        int ri[]=new int[n2];
+        for(int i=0;i<n1;i++)
+            le[i]=arr[l+i];
+        for(int j=0;j<n2;j++)
+            ri[j]=arr[m+1+j];
+        int j;
+        int i;
+        i=j=0;
+        k=l;
+        while(i<n1 && j<n2){
+            if(le[i]<=ri[j])
+                arr[k]=le[i++];
+            else 
+                arr[k]=ri[j++];
+            k++;
+          }
+
+        while(i<n1)
+            arr[k++]=le[i++];
+        while(j<n2)
+            arr[k++]=ri[j++];
+        
+
        //Your code here  
     } 
   
@@ -13,7 +38,13 @@ class MergeSort
     void sort(int arr[], int l, int r) 
     { 
 	//Write your code here
-        //Call mergeSort from here 
+        //Call mergeSort from here
+        if(l < r) {
+            int mid = (l + r)/2;
+            sort(arr, l, mid);
+            sort(arr, mid + 1, r);
+            merge(arr, l, mid, r);
+        } 
     } 
   
     /* A utility function to print array of size n */
@@ -26,14 +57,14 @@ class MergeSort
     } 
   
     // Driver method 
-    public static void main(String args[]) 
+    public static   void main(String args[]) 
     { 
         int arr[] = {12, 11, 13, 5, 6, 7}; 
   
         System.out.println("Given Array"); 
         printArray(arr); 
   
-        MergeSort ob = new MergeSort(); 
+        Exercise_4 ob = new Exercise_4(); 
         ob.sort(arr, 0, arr.length-1); 
   
         System.out.println("\nSorted array"); 
