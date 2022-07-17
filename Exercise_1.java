27662086edc9c2ead97d408ad5b2d59badfaea8f
Priti Gumaste1 <priti.gumaste1@ibm.com>
2022-07-17 09:07:25 +0530

    Completed Sprint PreCourse2 - Exercise 1.

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,6 +3,27 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+    	//r is the length of array
+    	//checking is array is empty, if so return -1 
+       if(r>=1)
+       {
+        //Write your code here
+    	int mid= (r+l / 2);
+    	
+    	//if element is the middle most element
+    	if(arr[mid] == x)
+    		return mid; 
+    	
+    	//if element is smaller than middle most element,then search the left part of array
+    	
+    	if (arr[mid] > x)
+    		return binarySearch(arr, l, mid-1, x);
+    	
+    	//if x is greater than middle most element then search in right sub-array
+    	else
+    		return binarySearch(arr, mid+1, r, x);
+       }
+       return -1; 
     } 
   
     // Driver method to test above 
