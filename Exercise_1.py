3a290a0ebe22f4909a650eff05af4a3f180d0118
Chandra Kiran Viswanath Balusu <cbalusu@gmu.edu>
2022-07-18 00:47:23 -0400

    Complete `Exercise 2` Which is Quick Sort Using Python - Rewrite everything using a class in `Exercise_2` - Handle an edge case in `Exercise 1`

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,21 +4,23 @@
 # if present, else returns -1 
 def binarySearch(arr, l, r, x):
     if r > l:
-        mid = l + (r - 1) // 2
+        mid = (l+(r - 1)) // 2
         if x == arr[mid]:
             return mid
         if x > arr[mid]:
             return binarySearch(arr, mid + 1, r, x)
         return binarySearch(arr, l, mid - 1, x)
+    elif r == l:
+        return r
     return -1
 
 
 # Test array 
 arr = [2, 3, 4, 10, 40]
-x = 10
+x = 3
 
 # Function call 
-result = binarySearch(arr, 0, len(arr) - 1, x)
+result = binarySearch(arr, 0, len(arr), x)
 
 if result != -1:
     print("Element is present at index % d" % result)
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,38 @@
 # Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+# give you explanation for the approach
+# Well, taking the lowest index as pivot for the partition
+# and sort everything through recursion
+class QuickSort:
+    def __init__(self, array: list):
+        self._a = array
+
+    def _partition(self, low, high) -> int:
+        (_i, _j) = (low, high)
+        _pivot = self._a[low]
+        while _i < _j:
+            while _i < len(arr) and self._a[_i] <= _pivot:
+                _i += 1
+            while _j > 0 and self._a[_j] > _pivot:
+                _j -= 1
+            if _i < _j:
+                self._a[_i], self._a[_j] = self._a[_j], self._a[_i]
+        self._a[low], self._a[_j] = self._a[_j], self._a[low]
+        return _j
+
+    def sort(self, low=None, high=None):
+        if low is None and high is None:
+            low = 0
+            high = len(self._a) - 1
+        if low < high:
+            j = self._partition(low, high)
+            self.sort(low, j - 1)
+            self.sort(j + 1, high)
+        return self._a
+
+
+# Driver code to test above
+if __name__ == "__main__":
+    arr = [10, 7, 8, 9, 1, 5]
+    print("Sorted array is:")
+    print(QuickSort(arr).sort())
