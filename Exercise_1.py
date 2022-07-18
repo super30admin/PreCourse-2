88b3f7782b9788f14502d4946492b407ce41602c
Kameshwari-Soundararajan <kameshwaris99@gmail.com>
2022-07-17 22:15:29 -0500

    Completed Pre course 2

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,14 +1,29 @@
 # Python code to implement iterative Binary  
 # Search. 
-  
+
+# Time Complexity : O(log n)
+# Space Complexity : O(1)
+
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
+  while l <= r:
+    mid = (l+r) //2
+
+    if arr[mid] < x:
+      l = mid + 1
+
+    elif arr[mid] > x:
+      r = mid -1
+
+    else: 
+      return mid
   
-  #write your code here
+  return -1
   
-    
-  
+
+
+
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
@@ -17,6 +32,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print ("Element is present at index % d" % result )
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,40 @@
 # Python program for implementation of Quicksort Sort 
-  
+# Recursive approach 
+
+#  Time complexity : O(nlogn)
+#  Space Complexity : O(1)
+
+
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+#   Arr's right element is the pivot here and arr's low is the ptr
+  #write your code here
+  pivot, ptr = arr[high],low
+  for i in range(low,high):
+    if arr[i] <= pivot:
+      arr[i], arr[ptr] = arr[ptr], arr[i]
+      ptr += 1
+
+    arr[ptr], arr[high] = arr[high], arr[ptr]
+  return ptr
+
+
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
     #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
+    
+  if len(arr) == 1:
+    return arr
+
+  if low < high :
+    part = partition(low,high,arr)
+    # Recursively sort left values
+    quickSort(low, part-1, arr)
+    # Recursively sort right values   
+    quickSort(part+1, high,arr)
+
+  return arr
+
  
