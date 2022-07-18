3f4c48e1d49181b50567cdb3ca761200bc63b58c
lostrider07 <4511saiteja@gmail.com>
2022-07-17 18:15:37 -0700

    completed all exercises

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,39 @@
 # Node class  
+# Time complexity - O(n)  Space Complexity - O(1)
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data
+        self.next = None
         
-class LinkedList: 
-  
+class LinkedList:
     def __init__(self): 
-        
+        self.head = None
+        self.tail = None
   
     def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+        self.tail = new_node
+
+    # Function to get the middle of  the linked list
+    def printMiddle(self):
+        dictu = {}
+        dictu1 = {}
+        temp = self.head
+        count = 0
+        while temp:
+            dictu[count] = temp.data
+            dictu1[count] = temp
+            temp = temp.next
+            count += 1
+        print("middle element of linked list is " + str(dictu[count//2]))
+        print("middle element is at the node ", end=" ")
+        print(dictu1[count//2])
 
 # Driver code 
 list1 = LinkedList() 
@@ -23,4 +42,4 @@ list1.push(4)
 list1.push(2) 
 list1.push(3) 
 list1.push(1) 
-list1.printMiddle() 
+list1.printMiddle()
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,39 @@
 # Python program for implementation of Quicksort
+# Time complexity - O(n^2) (Worst case)  
+# Space complexity - O(logn)
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+#  In this solution, the last element is taken as the pivot from the list and two pointers are defined. 
+# The elements less than pivot and greater than pivot are swapped when the low element is greater than the higher one.
+#  If the element low is greater than the higher one, we then swap the pivot and the higher one. Then we return the high element.
+#  Then the pivot will be at its correct sorted position and its index is pi(partition index) which divides the array into two unsorted arrays to its left and right
 
+def partition(arr, l, h):
+    pivot_index = l
+    pivot = arr[l]
+    while l < h:
+        while l < len(arr) and arr[l] <= pivot:
+            l += 1
+        while arr[h] > pivot:
+            h -= 1
+        if l < h:
+            arr[l], arr[h] = arr[h], arr[l]
+        arr[pivot_index], arr[h] = arr[h], arr[pivot_index]
+    return h
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    stack = [(l, h)]
+    while stack:
+        l, h = stack.pop()
+        pi = partition(arr, l, h)
 
+        if pi - 1 > l:
+            stack.append((l, pi - 1))
+
+        if pi + 1 < h:
+            stack.append((pi + 1, h))
+
+
+arr = [1,4,3,2,5,4,3,6,7]
+quickSortIterative(arr, 0, len(arr) - 1)
+print(arr)
