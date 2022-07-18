31b3a9f4ff180d0ce7a0a8fc65b29666e8d07e15
Jayachander-Rachakonda <109487526+jayachanders30acc@users.noreply.github.com>
2022-07-17 22:12:38 -0700

    completed precourse 2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,46 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# Node class
+class Node:
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head=None
+
+
+    def push(self, new_data):
+        if self.head==None:
+            self.head=Node(new_data)
+        else:
+            node=self.head
+            while node.next:
+                node=node.next
+            node.next=Node(new_data)
+
+
+
+    # Function to get the middle of
+    # the linked list
+    def printMiddle(self):
+        c=1
+        l=self.head
+        while l.next!=None:
+            l=l.next
+            c+=1
+        l=self.head
+        for i in range(c//2):
+            l=l.next
+        print(l.data)
+
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2)
+list1.push(3)
+list1.push(1)
+list1.printMiddle()
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,35 @@
-# Python program for implementation of Quicksort
-
-# This function is same in both iterative and recursive
 def partition(arr, l, h):
+    pivot = arr[h]
+    i = l - 1
+    for j in range(l, h):
+        if arr[j] <= pivot:
+            i = i + 1
+            (arr[i], arr[j]) = (arr[j], arr[i])
+    (arr[i + 1], arr[h]) = (arr[h], arr[i + 1])
+    return i + 1
   #write your code here
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
-
+    stack=[0]*(h-l+1)
+    top=-1
+    top+=1
+    stack[top]=l
+    top+=1
+    stack[top]=h
+    while top>=0:
+        h=stack[top]
+        top-=1
+        l=stack[top]
+        top-=1
+        p=partition(arr,l,h)
+        if p-1>l:
+            top+=1
+            stack[top]=l
+            top+=1
+            stack[top]=p-1
+        if p+1<h:
+            top+=1
+            stack[top]=p+1
+            top+=1
+            stack[top]=h
