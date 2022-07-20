2627ab85668bb48e6a55c340bcb19fbcad5861ad
harshil1709 <harshilprajapati1709@gmail.com>
2022-07-19 22:17:15 -0400

    Completed Sprint PreCourse2

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,51 @@
-# Python program for implementation of MergeSort 
+'''
+Time Complexity - O(nlogn) as we are traversing the whole array and dividing after each iteration
+
+
+Space Complexity - O(n) number of elements in the array
+
+'''
+# Python program for implementation of MergeSort
+
+
 def mergeSort(arr):
-  
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
-    #write your code here
-  
-# driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+        mergeSort(left)
+        mergeSort(right)
+        i, j, k = 0, 0, 0
+        while i < len(left) and j < len(right):
+            if left[i] > right[j]:
+                arr[k] = right[j]
+                j += 1
+            else:
+                arr[k] = left[i]
+                i += 1
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+# Code to print the list
+
+
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i])
+
+
+# driver code to test the above code
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,50 @@
+'''
+Time Complexity - 
+Average Case - O(nlogn) as we are traversing the whole array and dividing after each iteration
+Worst Case - O(n^2) when our array will be sorted and we select smallest or largest indexed element as pivot
+Space Complexity - O(logn) as we need a call to stack
+
+'''
+
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+from re import L
 
 
-def quickSortIterative(arr, l, h):
-  #write your code here
+def partition(arr, low, high):
+    pivot = arr[high]
+    ptr = low
+    for i in range(low, high):
+        if arr[i] <= pivot:
+            arr[i], arr[ptr] = arr[ptr], arr[i]
+            ptr += 1
+    arr[ptr], arr[high] = arr[high], arr[ptr]
+    return ptr
 
+
+def quickSortIterative(arr, low, high):
+    stack = []
+
+    stack.append(low)
+    stack.append(high)
+
+    while stack:
+        stack.pop(0)
+        stack.pop(0)
+        par_index = partition(arr, low, high)
+
+        if low < par_index-1:
+            stack.append(low)
+            stack.append(par_index-1)
+        if high > par_index+1:
+            stack.append(par_index+1)
+            stack.append(high)
+
+    # Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSortIterative(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]),
