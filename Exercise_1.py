a431978c9478fec10dc3c667ebc96ce91d710494
Rohan Acharya <rohachar@cisco.com>
2022-07-24 20:38:40 -0500

    log(n) not n

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,7 +4,7 @@
 # It returns location of x in given array arr  
 # if present, else returns -1 
 
-# Time Complexity : O(n)
+# Time Complexity : O(log(n))
 # Space Complexity : O(1)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
