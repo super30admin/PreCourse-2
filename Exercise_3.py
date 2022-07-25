839d7e700b268c580d67eadd3387c33717811a5d
Rohan Acharya <rohachar@cisco.com>
2022-07-24 21:38:12 -0500

    exercise3

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,41 @@
+# Time Complexity : push -> O(1), print_middle -> O(n)
+# Space Complexity : push -> O(n), print_middle -> O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head = Node(None)
         
   
-    def push(self, new_data): 
+    def push(self, new_data):
+        new_node=Node(new_data)
+        new_node.next=self.head
+        self.head=new_node 
         
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def print_middle(self):
+        if not self.head:
+            return self.head
+        slow=self.head
+        fast=self.head
+        while fast.next and fast.next.next:
+            slow=slow.next
+            fast=fast.next.next
+        return slow 
+
 
 # Driver code 
 list1 = LinkedList() 
@@ -22,5 +43,5 @@ list1.push(5)
 list1.push(4) 
 list1.push(2) 
 list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+list1.push(1)
+print(list1.print_middle().data)
