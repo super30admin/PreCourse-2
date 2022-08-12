9e5ca7202cf9466cbed7e46969b18c2cdf6c9ac6
srishti <kalrasrishti3@gmail.com>
2022-08-12 12:58:20 -0700

    Completed PreCourse2, Please Review

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,26 @@
-class BinarySearch { 
+//Time Complexity is O(log(N))
+//Space Complexity is O(1)
+class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        while(l<=r){
+
+            int mid = (l+r)/2;
+
+            if(arr[mid] > x ) {
+                r = mid - 1;
+            }
+            else if(arr[mid] == x){
+                return mid;
+            }
+            else
+                l=mid+1;
+
+        }
+        return -1;
+
     } 
   
     // Driver method to test above 
