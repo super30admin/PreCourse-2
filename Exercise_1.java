203ef2d073d41628a6cefa466cd9ee217df5bc50
RamyaMandapati <ramya.mandapati@sjsu.edu>
2022-08-12 14:48:03 +0530

    Completed PreCourse 2-Please review

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,27 @@
+// Time Complexity :  O(nlogn)
+// Space Complexity :   O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        if(l<r){
+            while(l<=r){
+                int mid=(l+r)/2;
+                if(arr[mid]==x){
+                    return mid;
+                }else if(arr[mid]<x){
+                    r=mid-1;
+                }else{
+                    l=mid+1;
+
+                }
+            }
+        }
+        return -1;
     } 
   
     // Driver method to test above 
