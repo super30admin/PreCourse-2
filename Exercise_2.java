2cddb8f0fc86884c15758f04a3a00d3936fc5e0a
Valmik Kalathia <valmikkalathia@outlook.com>
2022-08-14 19:03:52 -0400

    Completed sprint PreCourse2 -- Please Review

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,48 +1,63 @@
-class QuickSort 
-{ 
-    /* This function takes last element as pivot, 
-       places the pivot element at its correct 
-       position in sorted array, and places all 
-       smaller (smaller than pivot) to left of 
-       pivot and all greater elements to right 
-       of pivot */
+// Time Complexity :O(nlogn)
+// Space Complexity : O(n)
+class QuickSort
+{
     void swap(int arr[],int i,int j){
-        //Your code here   
+        //Your code here
+        int temp;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    
-    int partition(int arr[], int low, int high) 
-    { 
-   	//Write code here for Partition and Swap 
-    } 
-    /* The main function that implements QuickSort() 
-      arr[] --> Array to be sorted, 
-      low  --> Starting index, 
-      high  --> Ending index */
-    void sort(int arr[], int low, int high) 
-    {  
-            // Recursively sort elements before 
-            // partition and after partition 
-    } 
-  
-    /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
-        System.out.println(); 
-    } 
-  
-    // Driver program 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {10, 7, 8, 9, 1, 5}; 
-        int n = arr.length; 
-  
-        QuickSort ob = new QuickSort(); 
-        ob.sort(arr, 0, n-1); 
-  
-        System.out.println("sorted array"); 
-        printArray(arr); 
-    } 
+
+    int partition(int arr[], int low, int high)
+    {
+        int pivot = arr[low];
+        int i= low, j = high;
+        while(i<j){
+            while(arr[i]<=pivot){
+                i++;
+            }
+
+            while(arr[j]>pivot){
+                j--;
+            }
+
+            if(i<j)
+                swap(arr, i, j);
+        }
+        swap(arr, arr[low], j);
+        return j;
+
+
+    }
+
+    void sort(int arr[], int low, int high)
+    {
+        if(low<high){
+            int j = partition(arr,low, high);
+            sort(arr, low, j);
+            sort(arr, j+1, high);
+        }
+    }
+
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+    public static void main(String args[])
+    {
+        int arr[] = {10, 7, 8, 9, 1, 5};
+        int n = arr.length;
+
+        QuickSort ob = new QuickSort();
+        ob.sort(arr, 0, n-1);
+
+        System.out.println("sorted array");
+        printArray(arr);
+    }
 } 
