171c6c95a2190a50e8ae48ee6fc67a6730332114
ksper1 <111244308+ksper1@users.noreply.github.com>
2022-08-14 14:50:03 -0400

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,15 +2,25 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
     #write your code here
+    #picking the last element as pivot
+    for i in range(low,high):
+      if arr[i] <= arr[high]:
+        arr[i], arr[low] = arr[low], arr[i]
+        low += 1
+    arr[low], arr[high] = arr[high], arr[low]
+    return low
   
-
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
     #write your code here
+    #divide the arr into 2 parts and call the quicksort function recursively
+    if len(arr)>1:
+      mid = parition(arr,low,high)
+      quickSort(arr,low,mid-1)
+      quickSort(arr,mid+1,high)
+    return arr
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
