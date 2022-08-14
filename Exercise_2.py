0e7e3d406c89bc7f06730d27abaa104f15aa8c14
Gowtham Chandrasekaran <gowtham6309@gmail.com>
2022-08-14 01:58:47 -0700

    Completed Precourse2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,41 @@
+# Time Complexity : O(nlogn) as I am using randomized partition method
+# Space Complexity : O(1) as its in place sorting
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : Had to brush up the algorithm of quick sort
+
+
 # Python program for implementation of Quicksort Sort 
   
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
+# give you explanation for the approach: 
+# First I used the non randomised version of partitioning that will always take the end item of the array as the pivot. I read in a blog that choosing the pivot randomly is a good idea as it avoids attaining the worst case even if the array is already sorted i.e. avoiding the time complexity of O(n^2). Quick sort is not a stable sorting algo but merge sort is.
+
+import random
+
+def randomizedPartition(arr,start,end):
+    pivot_index = random.randint(start,end)
+    arr[pivot_index], arr[end] = arr[end], arr[pivot_index]
+    partition_index = partition(arr,start,end)
+    return(partition_index)
+
+def partition(arr,start,end):
     #write your code here
-  
+    pivot = arr[end]
+    partition_index = start
+    for i in range(start,end):
+        if arr[i]<=pivot:
+            arr[i], arr[partition_index] = arr[partition_index], arr[i]
+            partition_index += 1
+    arr[partition_index], arr[end] = arr[end], arr[partition_index]
+    return partition_index
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
+def quickSort(arr,start,end): 
     
     #write your code here
+    if start<end:
+        partition_index = randomizedPartition(arr,start,end)
+        quickSort(arr,start,partition_index-1)
+        quickSort(arr,partition_index+1,end)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
@@ -18,6 +43,6 @@ n = len(arr)
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
+    print ("%d" %arr[i]) 
   
  
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,65 @@
+# Time Complexity : O(n^2)
+# Space Complexity : O(n) as a stack is used
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : I did not know how to do this. Had to refer into existing codes on the internet to learn.
+
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
+def partition(array, low, high):
   #write your code here
+    i = ( low - 1 )
+    x = array[high]
+ 
+    for j in range(low , high):
+        if   array[j] <= x:
+ 
+            i = i+1
+            array[i],array[j] = array[j],array[i]
+ 
+    array[i+1],array[high] = array[high],array[i+1]
+    return (i+1)
 
 
-def quickSortIterative(arr, l, h):
+def quickSortIterative(array, low, high):
   #write your code here
+    size = len(array)
+    if size == 1:
+      return
+    stack = [0] * (size)
+    top = 0
+    stack[top] = low
+    top += 1
+    stack[top] = high
+ 
+    # Keep popping from stack while is not empty
+    while top >= 0:
+ 
+        # Pop high and low
+        high = stack[top]
+        top -= 1
+        low = stack[top]
+        top -= 1
+ 
+        # sorted array
+        p = partition( array, low, high )
 
+        # push left side to stack
+        if p-1 > low:
+            top += 1
+            stack[top] = low
+            top = top + 1
+            stack[top] = p - 1
+
+        #  push right side to stack
+        if p+1 < high:
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = high
+
+array = [10, 7, 8, 9, 1, 5]
+n = len(array)
+print("Original Array:", array)
+quickSortIterative(array, 0, n-1)
+print ("Sorted Array :", array)
