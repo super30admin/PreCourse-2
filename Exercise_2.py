2e22549a807573dc4c6c09257f1d40032a251e6f
armughan11 <52311490+armughan11@users.noreply.github.com>
2022-08-15 01:03:44 +0530

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -5,10 +5,26 @@ def partition(arr,low,high):
   
   
     #write your code here
-  
+    pivot = arr[high]
+    i = low-1
+    
+    for j in range(low,high):
+        if arr[j]<= pivot:
+            i = i+1
+            arr[i], arr[j] =arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    
+    return i+1
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
+    
+    if low< high:
+        
+        pivot = partition(arr,low,high)
+        quickSort(arr,low,pivot-1)
+        quickSort(arr,pivot+1,high)
+        
     
     #write your code here
   
