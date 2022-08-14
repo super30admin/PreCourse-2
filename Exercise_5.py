64eb01715b7f4ff9161105a4fbbc818c0aa79b17
armughan11 <52311490+armughan11@users.noreply.github.com>
2022-08-15 01:07:12 +0530

    Update Exercise_5.py

--- Exercise_5.py
+++ Exercise_5.py
@@ -1,6 +1,4 @@
-# Python program for implementation of Quicksort
 
-# This function is same in both iterative and recursive
 def partition(arr, low, high):
   #write your code here
     pivot = arr[high]
