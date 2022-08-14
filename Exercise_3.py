a9c49168b636f026c189f0b7b2a006051eed088d
armughan11 <52311490+armughan11@users.noreply.github.com>
2022-08-15 01:06:00 +0530

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,46 +1,43 @@
-# Python program for implementation of MergeSort 
-def mergeSort(arr):
-    if len(arr)>1:
-        mid = len(arr)//2
-        
-        left= arr[:mid]
-        right= arr[mid:]
-        
-        mergeSort(left)
-        mergeSort(right)
-        
-        i=j=k=0
-        while i < len(left) and j<len(right):
-            if left[i]<right[j]:
-                arr[k] = left[i]
-                i+=1
-            else:
-                arr[k]=right[j]
-                j+=1
-            k+=1
-        while i<len(left):
-            arr[k] = left[i]
-            i+=1
-            k+=1
-        while j<len(right):
-            arr[k] = right[j]
-            k+=1
-            j+=1
+# Node class  
+class Node:  
   
-  #write your code here
+    # Function to initialise the node object  
+    def __init__(self, data):  
+        self.data= data
+        self.next=None
+        
+class LinkedList: 
   
-# Code to print the list 
-def printList(arr): 
-    for i in arr:
-        print(i)
-    
-    #write your code here
+    def __init__(self): 
+        self.head= None
+        
   
-# driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+    def push(self, new_data): 
+        if self.head is None:
+            node = Node(new_data)
+            self.head =node
+            return
+        curr= self.head
+        while curr.next is not None:
+            curr = curr.next
+        node = Node(new_data)
+        curr.next = node
+  
+    # Function to get the middle of  
+    # the linked list 
+    def printMiddle(self): 
+        slow=self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        print(slow.data)
+
+# Driver code 
+list1 = LinkedList() 
+list1.push(5) 
+list1.push(4) 
+list1.push(2) 
+list1.push(3) 
+list1.push(1) 
+list1.printMiddle() 
