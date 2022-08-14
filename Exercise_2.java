e49e86d37a232f630aa6d9ebbc7ad7a201ffef96
Arjun Bhatia <arjunbhatia@Arjuns-MBP.hsd1.wa.comcast.net>
2022-08-14 14:45:39 -0700

    Completed exercise 3

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,27 +1,41 @@
 class QuickSort 
-{ 
+{
+    void swap(int arr[],int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;  
+    }
+    
     /* This function takes last element as pivot, 
        places the pivot element at its correct 
        position in sorted array, and places all 
        smaller (smaller than pivot) to left of 
        pivot and all greater elements to right 
        of pivot */
-    void swap(int arr[],int i,int j){
-        //Your code here   
-    }
-    
     int partition(int arr[], int low, int high) 
-    { 
-   	//Write code here for Partition and Swap 
+    {
+        int pivot = arr[high];
+        int partitionIdx = low;
+        for (int i = low; i < high; i++) {
+            if (arr[i] <= pivot) {
+                swap(arr, i, partitionIdx);
+                partitionIdx++;
+            }
+        }
+        swap(arr, partitionIdx, high);
+        return partitionIdx;
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
       low  --> Starting index, 
       high  --> Ending index */
     void sort(int arr[], int low, int high) 
-    {  
-            // Recursively sort elements before 
-            // partition and after partition 
+    {
+        if (low < high) {
+            int partitionIdx = partition(arr, low, high);
+            sort(arr, low, partitionIdx - 1);
+            sort(arr, partitionIdx + 1, high);
+        }
     } 
   
     /* A utility function to print array of size n */
