6a592d59e3a7fde378a0b65623b37cea4dd1e3f9
Arjun Bhatia <arjunbhatia@Arjuns-MBP.hsd1.wa.comcast.net>
2022-08-14 20:27:07 -0700

    Completed exercise 4

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,19 +1,63 @@
+// Time complexity
+// O(nlogn)
+
+// Space complexity
+// O(n)
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
+    {
+       int leftArr[] = new int[m-l+1];
+       int rightArr[] = new int[r-m];
+
+       for (int i = 0; i < leftArr.length; i++) {
+        leftArr[i] = arr[l+i];
+       }
+
+       for (int i = 0; i < rightArr.length; i++) {
+        rightArr[i] = arr[m+1+i];
+       }
+
+       int i = 0; int j = 0; int k = l;
+       while (i < leftArr.length && j < rightArr.length) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+        }
+        else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+       }
+
+       while (i < leftArr.length) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+       }
+
+       while (j < rightArr.length) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+       }
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
-    { 
-	//Write your code here
-        //Call mergeSort from here 
+    {
+        if (l < r) {
+            int midPoint = (l + r) / 2;
+            sort(arr, l, midPoint);
+            sort(arr, midPoint + 1, r);
+            merge(arr, l, midPoint, r);
+        }
     } 
   
     /* A utility function to print array of size n */
