42c729712d4b25ba900bb2bd1b70c231d2002274
manishwvn <48592499+manishwvn@users.noreply.github.com>
2022-08-14 23:39:25 -0700

    Completed Pre course 2

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,47 @@
-# Python program for implementation of MergeSort 
+# Python program for implementation of MergeSort
+def merge(left, right):
+  if not len(left) or not len(right):
+    return left or right
+  
+  result = []
+  i, j = 0, 0
+  while i < len(left) and j < len(right):
+    if left[i] < right[j]:
+      result.append(left[i])
+      i += 1
+      
+    else:
+      result.append(right[j])
+      j += 1
+      
+    while i < len(left):
+      result.append(left[i])
+      i += 1
+      
+    while j < len(right):
+      result.append(right[j])
+      j += 1
+
 def mergeSort(arr):
+  if len(arr) < 2:
+    return arr
   
-  #write your code here
+  mid = len(arr)//2
+  left = mergeSort(arr[:mid])
+  right = mergeSort(arr[mid:])
+  merge(left, right)
   
-# Code to print the list 
-def printList(arr): 
-    
-    #write your code here
-  
-# driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+
+# driver code to test the above code
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,53 @@
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+"""
+Description:
+This is the iterative version of QuickSort
+The idea is to use a stack to keep track of sub-arrays to be sorted.
+The first element of the stack is the index of the first element of the sub-array.
+The second element of the stack is the index of the last element of the sub-array.
+While the stack is not empty, we pop the top two elements from the stack and sort the sub-array.
+Partition the array around the pivot and push the sub-array indices to the stack.
+
+Time Complexity: O(n log n)
+Time Complexity for quicksort: O(n^2)(Worst Case when array is already sorted or reverse sorted)
+Space Complexity: O(log n)
+"""
+from collections import deque
+
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    while low <= high:
+        if arr[low] <= pivot:
+            i += 1
+            arr[i], arr[low] = arr[low], arr[i]
+        low += 1
+
+        arr[i+1], arr[high] = arr[high], arr[i+1]
+
+    return i+1
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
 
+    stack = deque()
+    stack.append([l, h])
+
+    while stack:
+        l, h = stack.pop()
+        p = partition(arr, l, h)
+        if p-1 > l:
+            stack.append([l, p-1])
+        if p+1 < h:
+            stack.append([p+1, h])
+
+
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSortIterative(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]),
