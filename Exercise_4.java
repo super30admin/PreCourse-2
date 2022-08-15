28d5dc0e388a3224ac0ca92b5155f1c59931013f
vagdevij52 <vagdevi.junnuri@sjsu.edu>
2022-08-14 20:43:51 -0700

    Completed Precourse-2

--- Exercise_4.java
+++ Exercise_4.java
@@ -5,7 +5,49 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+       //find the size of two sub arrays
+        int s1 = m-l+1;
+        int s2 = r-m;
+        int larr[] = new int[s1];
+        int rarr[] = new int[s2];
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < s1; ++i)
+            larr[i] = arr[l + i];
+        for (int j = 0; j < s2; ++j)
+            rarr[j] = arr[m + 1 + j];
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < s1 && j < s2) {
+            if (larr[i] <= rarr[j]) {
+                arr[k] = larr[i];
+                i++;
+            }
+            else {
+                arr[k] = rarr[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < s1) {
+            arr[k] = larr[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < s2) {
+            arr[k] = rarr[j];
+            j++;
+            k++;
+        }
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -13,7 +55,17 @@ class MergeSort
     void sort(int arr[], int l, int r) 
     { 
 	//Write your code here
-        //Call mergeSort from here 
+        if(l<r){
+            // find mid point
+            int m = l+(r-l)/2;
+            // Sort first half an second half
+            sort(arr, l,m);
+            sort(arr,m+1, r);
+            //Call mergeSort from here
+            merge(arr,l,m,r);
+        }
+
+
     } 
   
     /* A utility function to print array of size n */
