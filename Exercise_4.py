7782fe98689fba8d19544fdcf42bc62760970161
Meher Abhilash Komirishetty (Student) <mkomiris@sundevils.asu.edu>
2022-08-14 22:18:07 -0700

    completed Precourse 2

--- Exercise_4.py
+++ Exercise_4.py
@@ -2,10 +2,38 @@
 def mergeSort(arr):
   
   #write your code here
+    if(len(arr) > 1):
+        mid = len(arr) // 2
+        left = arr[0:mid]
+        right = arr[mid:]
+        mergeSort(left)
+        mergeSort(right)
+        i = 0
+        j = 0 
+        k = 0
+        while(i < len(left) and j < len(right)):
+          if(left[i] <= right[j]):
+            arr[k] = left[i]
+            i += 1
+          else:
+            arr[k] = right[j]
+            j += 1
+          k += 1
+        while(i < len(left)):
+          arr[k] = left[i]
+          k += 1
+          i += 1
+        while(j < len(right)):
+          arr[k] = right[j]
+          k += 1
+          j += 1
+
   
 # Code to print the list 
 def printList(arr): 
     
+    for item in arr:
+        print(item)
     #write your code here
   
 # driver code to test the above code 
