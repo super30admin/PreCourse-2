7a7df9ea387e293ad91a53cb6c8d6714c7f46ae1
Nidhi Agrawal <nidhiniles@gmail.com>
2022-08-17 13:31:14 -0700

    Completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,21 @@
+// Time Complexity : O(logN)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
+    {  
+    	while(l <= r)
+    {
+        int mid = l + (r-l)/2;
+        if(arr[mid]==x) return mid;
+        else if(arr[mid]>x) r = mid-1;
+        else l = mid+1;
+    }
+    return -1;
     } 
   
     // Driver method to test above 
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,9 @@
+// Time Complexity : O(NLOGN)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -5,15 +11,60 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
-    } 
+    	
+        // Find sizes of two subarrays to be merged
+        int left_length = m - l + 1;
+        int right_length = r - m;
+ 
+       
+        int[] leftarray = new int[left_length];
+        int[] rightarray = new int[right_length];
+ 
+   
+        for (int i = 0; i < left_length; ++i)
+        	leftarray[i] = arr[l + i];
+        for (int j = 0; j < right_length; ++j)
+        	rightarray[j] = arr[m + 1 + j];
+ 
+    
+ 
+        // Initial indexes of first and second subarrays
+        int left = 0, right = 0;
+ 
+        // Initial index of merged subarray array - start from l 
+        int curr = l;
+        while (left < left_length && right < right_length) {
+            if (leftarray[left] <= rightarray[right]) {
+                arr[curr++] = leftarray[left++]; 
+            }
+            else {
+                arr[curr++] = rightarray[right++];   
+            }
+        
+        }
+ 
+        while (left < left_length) {
+            arr[curr++] = leftarray[left++];
+        }
+ 
+        while (right < right_length) {
+            arr[curr++] = rightarray[right++];
+        }
+    }
   
     // Main function that sorts arr[l..r] using 
     // merge() 
+     
     void sort(int arr[], int l, int r) 
     { 
 	//Write your code here
-        //Call mergeSort from here 
+        //Call mergeSort from here
+    	if (l < r) {
+    		int mid = l+(r-l)/2;
+    		sort(arr, l , mid);
+    		sort(arr, mid+1, r);
+    		merge(arr, l , mid, r);
+    	}
     } 
   
     /* A utility function to print array of size n */
