76777d7176572335c951f78ea9bcad4858de8fd5
aishAgarwal04 <98933930+aishAgarwal04@users.noreply.github.com>
2022-09-05 02:41:07 -0700

    Completed Sprint Precourse2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,44 @@
 # Python program for implementation of Quicksort Sort 
-  
+# Any problem you faced while coding this : No
+# Time Complexity: O(n2) -> worst case
+# Time Complexity: O(nlogn) -> best and avg case
+# Space Complexity: O(logn) -> call stack
+
 # give you explanation for the approach
+# Quick sort is divide and conquer approach.
+# Last element is choose as the pivot
+# Store the elements leess than pivot in the left subarray, greater than the pivot in right subarray
+# Call quicksort recursively on left subarray, call quicksort recursively on right subarray, until array size is 1. 
+
 def partition(arr,low,high):
-  
-  
+   
     #write your code here
-  
+    i = low
+    j = high - 1
+    p = arr[high]
+
+    while i<j:
+    	while i<high and arr[i]<p:
+    		i+=1
+    	while j>low and arr[j]>=p:
+    		j-=1
+
+    	if i<j:
+    		arr[i], arr[j] = arr[j], arr[i]
+
+    if arr[i]>p:
+    	arr[i], arr[high] = arr[high], arr[i]
+
+    return i  
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
     #write your code here
+    if low<high:
+        partition_pos = partition(arr, low, high)
+        quickSort(arr, low, partition_pos-1)
+        quickSort(arr, partition_pos+1, high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,13 +1,51 @@
+# Any problem you faced while coding this : No
+# Time Complexity: O(nlogn)
+# Space Complexity: O(n)
+
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
   
   #write your code here
-  
+    if len(arr)>1:
+        r = len(arr)//2
+        left = arr[:r]
+        right = arr[r:]
+
+        #recursion
+        mergeSort(left)
+        mergeSort(right)
+
+        #merge
+        i = 0 #left subarray
+        j = 0 #right subarray
+        k = 0 #merged subarray
+    
+        while i<len(left) and j<len(right):
+    	    if left[i]<right[j]:
+    		    arr[k] = left[i]
+    		    i+=1
+    	    else:
+    		    arr[k] = right[j]
+    		    j+=1
+    	    k+=1
+
+        while i<len(left):
+    	    arr[k] = left[i]
+    	    i+=1
+    	    k+=1
+
+        while j<len(right):
+    	    arr[k] = right[j]
+    	    j+=1
+    	    k+=1
+
+
 # Code to print the list 
 def printList(arr): 
     
     #write your code here
-  
+    print(arr)
+
 # driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
