0642b85346632b97df1f951df25cf857edf528dd
Shubham Ayachit <shubham.s.ayachit@gmail.com>
2022-09-04 21:16:48 -0400

    PreCourse_2 Complete

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,27 @@
+//Complexity: 
+//Time : O(log n)
+//Space : O(n)
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        //approach: check if the value of r is less than or equal to l, if it is less, return -1
+        //get the mid index of l and r by rounding (l+r)/2
+        //if mid index value is equal to x then return mid else if value at mid is less than or greater than x then call binary search recursively
+        if(r<=l) {
+            return -1;
+        }
+        int mid = (int)Math.round((l+r)/2);
+        if(arr[mid] == x) {
+            return mid;
+        } else if(x > arr[mid]) {
+            return binarySearch(arr, mid, r, x);
+        } else {
+            return binarySearch(arr, l, mid, x);
+        }
+        
     } 
   
     // Driver method to test above 
