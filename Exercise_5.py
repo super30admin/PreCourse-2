6bc18a00707935b8220c5c7bdcab7fe0007c5084
HarishaKorapati <87511079+HarishaKorapati@users.noreply.github.com>
2022-09-05 12:32:28 -0700

    Update Exercise_5.py
    updated the quick sort iterative with sorting methodology

--- Exercise_5.py
+++ Exercise_5.py
@@ -15,10 +15,36 @@ def partition(arr, l, h):
 def quickSortIterative(arr, l, h):
 
   #write your code here
-  if l < h:
-    pi = partition(arr, l, h)
-    quickSortIterative(arr, l, pi-1)
-    quickSortIterative(arr, pi+1, h)  
+    size = h - l + 1
+    stack = [0] * (size)
+ 
+    top = -1
+ 
+    top = top + 1
+    stack[top] = l
+    top = top + 1
+    stack[top] = h
+
+    while top >= 0:
+ 
+        h = stack[top]
+        top = top - 1
+        l = stack[top]
+        top = top - 1
+
+        p = partition( arr, l, h )
+ 
+        if p-1 > l:
+            top = top + 1
+            stack[top] = l
+            top = top + 1
+            stack[top] = p - 1
+ 
+        if p+1 < h:
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = h 
      
     
     #write your code here
