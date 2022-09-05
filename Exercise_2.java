7675659179819aedabcdd55187f9576c40f2d83e
Nimay Patel <60466954+Nimay-1709@users.noreply.github.com>
2022-09-04 20:09:10 -0700

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,6 @@
+// Did this code successfully run on Leetcode : It runs successfully on Eclipse
+// Any problem you faced while coding this : Not much, other than coding for partition
+
 class QuickSort 
 { 
     /* This function takes last element as pivot, 
@@ -6,14 +9,43 @@ class QuickSort
        smaller (smaller than pivot) to left of 
        pivot and all greater elements to right 
        of pivot */
-    void swap(int arr[],int i,int j){
-        //Your code here   
-    }
+	
+	  void swap(int arr[],int i,int j){
+	        //Your code here  
+	    	
+	    }
+    
     
     int partition(int arr[], int low, int high) 
     { 
    	//Write code here for Partition and Swap 
+    	 int pivot = arr[high];
+         int i = (low-1); // index of smaller element
+         for (int j=low; j<high; j++)
+         {
+             // If current element is smaller than or
+             // equal to pivot
+             if (arr[j] <= pivot)
+             {
+                 i++;
+  
+                 // swap arr[i] and arr[j]
+                 int temp = arr[i];
+                 arr[i] = arr[j];
+                 arr[j] = temp;
+             }
+         }
+  
+         // swap arr[i+1] and arr[high] (or pivot)
+         int temp = arr[i+1];
+         arr[i+1] = arr[high];
+         arr[high] = temp;
+  
+         return i+1;
+    	
+    	
     } 
+   
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
       low  --> Starting index, 
@@ -22,6 +54,18 @@ class QuickSort
     {  
             // Recursively sort elements before 
             // partition and after partition 
+    	  if (low < high)
+          {
+              /* pi is partitioning index, arr[pi] is
+                now at right place */
+              int pi = partition(arr, low, high);
+   
+              // Recursively sort elements before
+              // partition and after partition
+              sort(arr, low, pi-1);
+              sort(arr, pi+1, high);
+          }
+    	
     } 
   
     /* A utility function to print array of size n */
