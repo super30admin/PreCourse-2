13a585cf78973437bbdbf91ed2a1d4b7410a423d
abhiaher22 <abhiaher22@gmail.com>
2022-09-25 14:45:43 -0700

    completed precourse-2 assignemnt-2

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,8 @@
-class QuickSort 
+
+// Time complexity = O(n^2)
+// Space complexity = O(n)
+
+class QuickSort
 { 
     /* This function takes last element as pivot, 
        places the pivot element at its correct 
@@ -7,12 +11,26 @@ class QuickSort
        pivot and all greater elements to right 
        of pivot */
     void swap(int arr[],int i,int j){
-        //Your code here   
+        //Your code here
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
     int partition(int arr[], int low, int high) 
     { 
-   	//Write code here for Partition and Swap 
+   	//Write code here for Partition and Swap
+        int i = low - 1;
+
+        for (int j = low; j <= high - 1; j++)  {
+            if (arr[j] < arr[high]) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+        return  i + 1;
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
@@ -21,7 +39,12 @@ class QuickSort
     void sort(int arr[], int low, int high) 
     {  
             // Recursively sort elements before 
-            // partition and after partition 
+            // partition and after partition
+        if (low < high) {     //
+            int pivot = partition(arr, 0, high);
+            sort(arr, 0, pivot - 1);
+            sort(arr, pivot + 1, high);
+        }
     } 
   
     /* A utility function to print array of size n */
