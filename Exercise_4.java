72bdab2853243f46ef212915aa2c1e69d9e60a08
Sayali277 <sayalik94@gmail.com>
2022-10-02 17:42:07 -0700

    Please review my PreCourse-2 assignments

--- Exercise_4.java
+++ Exercise_4.java
@@ -6,6 +6,46 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+       int n1 = m - l + 1;
+       int n2 = r -m ;
+
+       int L[] = new int[n1];
+       int R[] = new int[n2];
+
+       for(int i = 0 ;i<n1;i++){
+           L[i] = arr[l+i];
+       }
+       for(int j = 0 ; j <n2 ; j++)
+       {
+           R[j] = arr[m+1+j];
+       }
+       int i , j  = 0 ;
+       int k = l;
+       while ( i < n1 && j < n2)
+       {
+           if(L[i] <= R[j])
+           {
+               arr[k] = L[i];
+               i++;
+           }
+           else {
+               arr[k] = R[j];
+               j++;
+           }
+           k++
+       }
+       while (i<n1)
+       {
+           arr[k] = L[i];
+           i++;
+           k++;
+       }
+       while(j<n2)
+       {
+           arr[k] = R[j];
+           j++;
+           k++;
+       }
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +54,13 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+        if (l < r)
+        {
+            int m = l + (r-1)/2;
+            sort(arr,l,m);
+            sort(arr,m+1,r);
+        }
+
     } 
   
     /* A utility function to print array of size n */
