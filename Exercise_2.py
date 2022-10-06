c560d46423cf72168bc46b2afafd69826c3b3f8e
Rigved Patil <riggy@Visuri-2.local>
2022-10-06 16:14:37 -0700

    Complete Spring PreCourse2

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,22 +2,40 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
+    pivot=arr[high]
+
+    i=low-1
     #write your code here
+    for a in range(low,high):
+        if arr[a]<=pivot:
+
+            i=i+1
+            arr[i],arr[a]=arr[a],arr[i]
+    
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+
+    return i+1
+
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
     #write your code here
+    if low<high:
+        new=partition(arr,low,high)
+
+        quickSort(arr,low,new-1)
+
+        quickSort(arr,new+1,high)
+    
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
+print(arr)
+#for i in range(n): 
+ #   print ("%d" %arr[i]), 
   
  
--- Exercise_4.py
+++ Exercise_4.py
@@ -2,10 +2,45 @@
 def mergeSort(arr):
   
   #write your code here
+
+  #Checking if the size of the arr is not 1 or less than 1. Helps out avoid the situation when there is only 1 
+  # independant element and we don't need to divide it anymore
+  if len(arr)>1:
+
+    #Calculating mid in order to divide the array into parts
+    mid=len(arr)//2
+
+    left_side=arr[:mid]  #First part
+    right_side=arr[mid:] #Second Part
+
+    mergeSort(left_side)   # Sending the left_part to further divide
+    mergeSort(right_side)  # Sending the right_part to further divide
+
+    a,b,c=0,0,0
+
+    while a<len(left_side) and b<len(right_side):
+        if left_side[a]<=right_side[b]:
+            arr[c]=left_side[a]
+            a+=1
+        else:
+            arr[c]=right_side[b]
+            b+=1
+        c+=1
+
+    while a<len(left_side):
+        arr[c]=left_side[a]
+        a+=1
+        b+=1
+    
+    while b<len(right_side):
+        arr[c]=right_side[b]
+        b+=1
+        c+=1
+
   
 # Code to print the list 
 def printList(arr): 
-    
+    print(arr)
     #write your code here
   
 # driver code to test the above code 
