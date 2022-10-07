d95b169379b6a053f2d5e6f50556081055cdbd2a
Niyati Bavishi <niyatibavishi@Niyatis-MacBook-Pro.local>
2022-10-06 21:06:45 -0400

    Precourse2

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,12 +3,18 @@
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
+def binarySearch(arr, l, r, x):
+    #write your code here
+    mid = (l + r) // 2
+    if arr[mid] == x:
+        return mid
+    if l >= r:
+        return -1
+    if arr[mid] < x:
+        return binarySearch(arr, mid+1, r, x)
+    else:
+        return binarySearch(arr, l, mid-1, x)
+
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
@@ -17,6 +23,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print "Element is present at index :",result 
 else: 
     print "Element is not present in array"
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,33 @@
 # Python program for implementation of Quicksort
 
+def swap(index1, index2, arr):
+    arr[index1], arr[index2] = arr[index2], arr[index1]  
+
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+    #write your code here
+    index = l
+    pivot = arr[index]
+
+    while l < h:
+        while l < len(arr) and arr[l] <= pivot:
+            l += 1
+    
+        while arr[h] > pivot:
+            h -= 1
+    
+        if l < h:
+            swap(l, h, arr)
+
+    swap(index, h, arr)
+    
+    return h
 
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  if l < h:
+      index = partition(arr, l, h)
+      quickSortIterative(arr, l, index - 1)
+      quickSortIterative(arr, index + 1, h)
 
