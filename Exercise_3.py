70702efd7c966d3b7a312a6bec95aebcda9df245
P W <priyanka.wania@gmail.com>
2022-10-13 21:49:04 -0700

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,19 +2,33 @@
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
-    def __init__(self): 
+    def __init__(self):
+        self.head = None
         
-  
     def push(self, new_data): 
-        
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node    
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        # first pointer to jump to element leaving one immediately after it
+        # since second pointer reaches exactly the mid when first reaches the end return second pointers data
+        twostep_ptr = self.head 
+        second_ptr = self.head
+        
+        if self.head:
+            while(first_ptr and first_ptr.next):
+                first_ptr = first_ptr.next.next
+                second_ptr = second_ptr.next
+            return second_ptr.data
 
 # Driver code 
 list1 = LinkedList() 
