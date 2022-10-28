96ce20761ef9f67a68047d435fc11072a81ca21b
Rahul Raj <rahulraj@Rahuls-Air.lan>
2022-10-28 00:16:26 -0400

    Precourse 2 completed

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,3 +1,13 @@
+/*
+
+TC: O(lgn)
+SC: O(1)
+Logic: 
+1. we split the sorted array in the middle
+2. if target == current mid, then boom! we found it, so simply return
+3. if target > current mid, then clearly we'd find the element in the right side, so we recurse for the right end. Similarly for the left end.
+
+*/
 #include <stdio.h> 
   
 // A recursive binary search function. It returns 
@@ -5,7 +15,11 @@
 // otherwise -1 
 int binarySearch(int arr[], int l, int r, int x) 
 {   
-    //Your Code here 
+    if(l>r) return -1;
+    int mid = l+(r-l)/2;
+    if(arr[mid] == x)   return mid;
+    else if(x < arr[mid])   return binarySearch(arr, l, mid-1, x);
+    else    return binarySearch(arr, mid+1, r, x);
 } 
   
 int main(void) 
--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,10 +1,23 @@
+/*
+
+TC: O(n2) worst case, O(nlgn) avg case
+SC: O(1)
+Logic: 
+1. Partition the array around a pivot in O(n) using the partition method consisting of simple swaps
+2. let quicksort sort the elements around the partition element, ie. recurse for left of the partition and right.
+
+*/
+
+#include <iostream>
 #include <bits/stdc++.h> 
 using namespace std;  
   
 // A utility function to swap two elements  
 void swap(int* a, int* b)  
 {  
-    //Your Code here 
+    int temp=*a;
+    *a=*b;
+    *b=temp;
 }  
   
 /* This function takes last element as pivot, places  
@@ -14,7 +27,12 @@ to left of pivot and all greater elements to right
 of pivot */
 int partition (int arr[], int low, int high)  
 {  
-    //Your Code here 
+    int pivot = arr[high];
+    int i = low-1;
+    for(int j=low; j<high; j++)    
+        if(arr[j] < pivot)  i++, swap(&arr[i], &arr[j]);
+    swap(&arr[i+1], &arr[high]);
+    return i+1;
 }  
   
 /* The main function that implements QuickSort  
@@ -23,7 +41,10 @@ low --> Starting index,
 high --> Ending index */
 void quickSort(int arr[], int low, int high)  
 {  
-    //Your Code here 
+    if(low > high)  return;
+    int pi = partition(arr, low, high);
+    quickSort(arr, low, pi-1);
+    quickSort(arr, pi+1, high);
 }  
   
 /* Function to print an array */
