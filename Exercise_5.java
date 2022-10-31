22e05f271097df03d0b79998c4ac5ad468182e92
Virendra Singh Rathore <rathore26@northeastern.edu>
2022-10-30 20:20:06 -0400

    Completed PreCourse-2, Please review it

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,85 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
-    /* This function is same in both iterative and 
-       recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
-        //Compare elements and swap.
-    } 
-  
-    // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
-        //Try using Stack Data Structure to remove recursion.
-    } 
-  
-    // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
-    // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
-} 
+import java.util.Stack;
+
+class Pair {
+    private final int x;
+    private final int y;
+
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
+class IterativeQuickSort {
+    void swap(int arr[], int i, int j) {
+        // Try swapping without extra variable
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /*
+     * This function is same in both iterative and
+     * recursive
+     */
+    int partition(int arr[], int l, int h) {
+        // Compare elements and swap.
+        int i = l - 1;
+        for (int j = l; j <= h - 1; j++) {
+            if (arr[j] < arr[h]) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, h);
+        return i + 1;
+    }
+
+    // Sorts arr[l..h] using iterative QuickSort
+    void QuickSort(int arr[], int l, int h) {
+        // Try using Stack Data Structure to remove recursion.
+        Stack<Pair> stack = new Stack<>();
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        stack.push(new Pair(start, end));
+
+        // loop till stack is empty
+        while (!stack.empty()) {
+            start = stack.peek().getX();
+            end = stack.peek().getY();
+            stack.pop();
+            int pivot = partition(arr, start, end);
+            if (pivot - 1 > start) {
+                stack.push(new Pair(start, pivot - 1));
+            }
+            if (pivot + 1 < end) {
+                stack.push(new Pair(pivot + 1, end));
+            }
+        }
+    }
+
+    // A utility function to print contents of arr
+    void printArr(int arr[], int n) {
+        int i;
+        for (i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
+    // Driver code to test above
+    public static void main(String args[]) {
+        IterativeQuickSort ob = new IterativeQuickSort();
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 };
+        ob.QuickSort(arr, 0, arr.length - 1);
+        ob.printArr(arr, arr.length);
+    }
+}
