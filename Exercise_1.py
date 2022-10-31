2468f3e84c02844e007e53de9d275611cbe81410
Yash Gandhi <yashgandhi.2408@gmail.com>
2022-10-31 01:58:00 -0400

    PreCourse-2 Completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,34 @@
-# Python code to implement iterative Binary  
-# Search. 
-  
-# It returns location of x in given array arr  
-# if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
-# Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
+# Python code to implement iterative Binary Search.
+#Time Complexity: O(log n)
+#Space Complexity: O(1)
+# It returns location of x in given array arr
+# if present, else returns -1
+def binarySearch(arr, l, r, x):
+	#check base case
+	if r >= l:
+		# calculating mid
+		mid = (l + r) // 2
+		#if mid is equal to target return mid
+		if arr[mid] == x: 
+			return mid
+		#if mid is less than target, search on the left
+		elif arr[mid] > x:
+			return binarySearch(arr, l, mid - 1, x)
+		#if mid is greater than target, search right
+		elif arr[mid] < x:
+			return binarySearch(arr, mid + 1, r, x)
+	else:
+		# if the element is not present in arr
+		return -1
+
+# Test array
+arr = [2, 3, 4, 10, 40 ] 
 x = 10
-  
-# Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
-if result != -1: 
-    print "Element is present at index % d" % result 
-else: 
-    print "Element is not present in array"
+
+# Function call
+result = binarySearch(arr, 0, len(arr)-1, x)
+
+if result != -1:
+	print ("Element is present at index % d" % result)
+else:
+    print("Element is not present in array")
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,17 +1,34 @@
 # Python program for implementation of Quicksort Sort 
-  
+#Time Complexity Worst Case: O(n2), Average case : O(nlogn)
+#Space Complexity: O(1)
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+    #set pivot to the last element in arr
+    pivot = arr[high]
+
+    #set a pointer to track greater element
+    i = low - 1
+    
+    # traverse all element and compare each with pivot
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            # if element found is greater than the pivot swap 
+            (arr[i], arr[j]) = (arr[j], arr[i])
+            i = i + 1
+       # Swap the pivot element with the greater element specified by i
+    (arr[i + 1], arr[high]) = (arr[high], arr[i + 1])
+        # Return the position from where partition is done
+    return i + 1
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
-    #write your code here
-  
+    if low < high:
+        pi = partition(arr, low, high)
+    # Recursive call on the left of pivot
+        quickSort(arr, low, pi - 1)
+    # Recursive call on the right of pivot
+        quickSort(arr, pi + 1, high)
+
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
