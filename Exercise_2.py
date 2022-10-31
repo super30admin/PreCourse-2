51a506b678b8a30b8899b784bdbba4ec0d7eace2
Khushboo Gandhi <khushboo1028@gmail.com>
2022-10-30 21:09:04 -0700

    completed Pre-course-2 in python

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,15 +2,46 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+    #we set the pivot as the last element in the array
+    #our goal is to bring all elements less than pivot to left and all elements greater than pivot to the right
+    #in the end the pivot will be swapped to its correct position.
+    #we start of with the left pointer at one less than low
+    #we have a second pointer j that will loop through the entire array
+    #for every element j we will check if arr[j] is less than pivot. If it is, we will increment i and swap arr[i] and arr[j].
+    #This will bring the smaller numbers of the pivot to the left and numbers greater than the pivot to the right.
+    #Once we have looped through all elements, i will be present at the position of the last swap. We know our pivot is at the end.
+    #this means that i+1 is the position where the pivot must actually be there because the pivot is greater than arr[i]
+    #so we swap arr[i+1] with arr[high]
+    #in the end, now the pivot is in its correct position
+    #later, we return the position of i + 1 because that is how the array will now be divided.
+
+
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+        
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+        
+
+
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
+    if low < high:
+        p = partition(arr, low, high)
+        #p returns the position of the partion
+        #we perform quick sort on left side
+        quickSort(arr, low, p - 1)
+
+        #then we perform quick sort on right side
+        quickSort(arr, p+1, high)
     
-    #write your code here
+  
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,55 @@
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+def partition(arr, low, high):
+  pivot = arr[high]
+  i = low - 1
 
+  for j in range(low, high):
+      if arr[j] <= pivot:
+          i += 1
+          arr[i], arr[j] = arr[j], arr[i]
+      
+  arr[i + 1], arr[high] = arr[high], arr[i + 1]
+  return i + 1
+      
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  #for iterative quick sort, we use a stack approach.
+  #we can add the low and high values to the stack, partition it accordignly and get the new index of partition
+  #then we will add values to the left of stack, following with the values on the right. 
+  # We keep doing that until all the left and right values of pivot are added to the stack and then popped
+
+  stack = []
+  stack.append(l)
+  stack.append(h)
+
+  while(stack):
+    h = stack.pop()
+    l = stack.pop()
+
+    p = partition(arr, l, h)
+
+    if p-1 > l:
+      stack.append(l)
+      stack.append(p-1)
+
+    if p + 1 < h:
+      stack.append(p+1)
+      stack.append(h)
+
+
+
+# Driver code to test above 
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
+
+
+
+
 
