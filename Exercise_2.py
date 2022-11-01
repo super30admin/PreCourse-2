618ae304d83287a02b83a776a6aef81c5f0bd306
VijayMaddineni17 <vijay.maddineni17>
2022-10-31 23:01:54 -0700

    Completed Pre-course2

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,6 +2,15 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
+    pivot=arr[high]
+    i=low-1
+    for j in range(low, high):
+        if arr[j]<=pivot:
+            i=i+1
+            (arr[i], arr[j]) = (arr[j], arr[i])
+    (arr[i + 1], arr[high]) = (arr[high], arr[i + 1])   
+    return i+1
+
   
   
     #write your code here
@@ -11,6 +20,11 @@ def partition(arr,low,high):
 def quickSort(arr,low,high): 
     
     #write your code here
+    if low < high:
+        pi = partition(arr, low, high)
+        quickSort(arr,low, pi-1)
+        quickSort(arr,pi+1,high)
+ 
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
@@ -18,6 +32,6 @@ n = len(arr)
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
+    print ("%d" %arr[i])
   
  
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,41 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
+  if len(arr)>1:
+    mid=len(arr)//2#Mid will divide array into 2 halfs
+    L=arr[:mid] #Breaking Left part of arry
+    R=arr[mid:] #Breaking Right Part of Array
+    mergeSort(L)
+    mergeSort(R)
+    i=0
+    j=0
+    k=0
+    while i<len(L) and j<len(R):
+      if L[i]<=R[j]:
+        arr[k]=L[i]
+        i+=1
+      else:
+        arr[k]=R[j]
+        j+=1
+      k+=1
+      
+      while i<len(L):
+        arr[k]=L[i]
+        i+=1
+        k+=1
+        
+      while j<len(R):
+        arr[k]=R[j]
+        j+=1
+        k+=1
+        
+    
   
   #write your code here
   
 # Code to print the list 
 def printList(arr): 
-    
+    for i in range(len(arr)):
+      print(arr[i])
     #write your code here
   
 # driver code to test the above code 
@@ -13,6 +43,6 @@ if __name__ == '__main__':
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end="\n")  
     printList(arr) 
-    mergeSort(arr) 
+    mergeSort(arr)
     print("Sorted array is: ", end="\n") 
     printList(arr) 
