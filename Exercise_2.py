de1b8251fed4270573cf004e8fa17ff3c6e8fa94
Vutukuri Teja Venkata Nikhila <vnikhila@Vutukuris-MacBook-Pro.local>
2022-11-02 13:55:58 -0700

    Updated python files

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,31 @@
+#Time Complexity: O(nlogn)
+#Space; Happens in place so O(1)
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
+    # find a partition such that left of partition is has smaller elements. Right of partition has larger elements
+    #choosing last element as pivot
   
     #write your code here
-  
+    pivot = arr[high]
+    i = low-1
+    for j in range(low,high):
+        if arr[j]<=pivot:
+            i+=1
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    return i+1
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
     #write your code here
+    if low<high:
+        part = partition(arr,low,high)
+        #Recursively finding partitons on right part and left part
+        quickSort(arr,low,part-1)
+        quickSort(arr,part+1,high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,46 @@
+#Time Complexity: to just find position O(1) by maintaining size of list, to find element O(n)
 # Node class  
+
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
+        self.size = 0
   
     def push(self, new_data): 
+        newNode = Node(new_data)
+        n = self.head
+        if self.head == None:
+            self.head = newNode
+            self.size+=1
+        else:
+            while n.next is not  None:
+                n = n.next
+            n.next = newNode
+            self.size+=1
         
+
+
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        self.index = 0
+        n = self.head
+        if n is not None:
+            while self.index<self.size//2:
+                n = n.next
+                self.index+=1
+            print(n.data)
+        else:
+            print('No elements in the list')
 
 # Driver code 
 list1 = LinkedList() 
