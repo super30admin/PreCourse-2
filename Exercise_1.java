2065df4e5abfa247fbfe4226ea5d3d2f88c99e0e
Arbind Singh <getarbind@gmail.com>
2022-11-18 22:19:32 -0500

    commit2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,21 @@
+// Time Complexity : O(log n)
+// Space Complexity : 0(1)
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        while (l < r){
+            int mid = l + (r-l)/2;
+            if (arr[mid] == x){
+                return mid;
+            } else if (arr[mid] > x ){
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+        return -1;
     } 
   
     // Driver method to test above 
