957bac7d148982c2ef872d9d4e15ff2e212da553
Gagan Ujjini <gaganujjini@Gagans-MacBook-Air.local>
2022-11-20 23:39:41 -0600

    Finished Precourse_2: Please Review

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,34 @@
+# Time Complexity : Best case O(1) when the element at the mid is the looked up element. Worst case O(log n)
+# Space Complexity : O(1) since there is no additional Data structure used.
+# Did this code successfully run on Leetcode : Did not try running code on Leetcode as the question is slightly different there.
+# Any problem you faced while coding this : No isssues faced
+
 # Python code to implement iterative Binary  
 # Search. 
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
+def binarySearch(arr, l, r, x):
   #write your code here
-  
+  while l <= r:                       #using two pointers and this condition checks whether the pointers are within the boundary
+    m = (l + r)//2                    #we compute mid pointer m 
     
+    if arr[m] > x:                    #if element at the index m is greater than x then we need to look left
+      r = m - 1                       #hence we change the right pointer to m-1
+    elif arr[m] < x:                  #if element at m is less than x then we need to look right
+      l = m + 1                       #hence we increment left pointer to m+1
+    else:                             
+      return m                        #if the value we are looking for is exactly at index m we return m
+  return -1                           #if value we are looking of is not in the list we return -1
   
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
-  
+
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
-  
+
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print(f"Element is present at index {result}")
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,45 @@
+# Time Complexity : best and avg case time is O(n log n) when pivot is choosen as middle elem or when array elements are in disordered sequence
+#                   worst case time is O(n^2) when we choose the largest or smallest element as the pivot 
+# Space Complexity : O(1) space
+# Did this code successfully run on Leetcode : Did not try running code on leetcode as the question is bit different.
+# Any problem you faced while coding this : No issues faced
+
+
 # Python program for implementation of Quicksort Sort 
-  
+
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
     #write your code here
-  
+    pivot = arr[high]                                   #taking last element as the pivot
+    i, j = low, high - 1
+    while i < j:
+        while i < high and arr[i] < pivot:              # i looks for element bigger than pivot, if elem at i < pivot then i is incremented
+            i += 1                                      
+        while j > low and arr[j] >= pivot:              # j looks for element smaller than pivot, if elem at j >= pivot then j is decremented
+            j -= 1
+        
+        if i < j:                                       # if i < j then we swap the elements at i and j         
+            arr[i], arr[j] = arr[j], arr[i]
+
+    if arr[i] > pivot:                                  
+        arr[i], arr[high] = arr[high], arr[i]
+
+    return i                                            # returns the index of pivot where elements on left of i are less than pivot
+                                                        # and elements to right of i are greaterd than pivot
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
     #write your code here
-  
+    if low < high:
+        pivot = partition(arr, low, high)               # we call partition function that returns the index of pivot
+        quickSort(arr, low, pivot-1)                    # recursively call quicksort on left subarrray
+        quickSort(arr, pivot+1, high)                   # recursively call quicksort on right subarray
+
+
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+    print ("%d" %arr[i])
