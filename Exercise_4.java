6be57ae15d5237145f12a7d62282a5766cc19383
Jackson Raj A <jacksonraj1095@gmail.com>
2022-12-15 23:12:43 +0530

    Completed Sprint PreCourse-2

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,42 +1,72 @@
-class MergeSort 
-{ 
-    // Merges two subarrays of arr[]. 
-    // First subarray is arr[l..m] 
-    // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
-    } 
-  
-    // Main function that sorts arr[l..r] using 
-    // merge() 
-    void sort(int arr[], int l, int r) 
-    { 
-	//Write your code here
-        //Call mergeSort from here 
-    } 
-  
+// Time Complexity  : O(n log n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes (https://leetcode.com/problems/sort-an-array/)
+// Any problem you faced while coding this : no
+
+class MergeSort {
+
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r) {
+        int len1 = m - l + 1;
+        int len2 = r - m;
+
+        int[] arr1 = new int[len1];
+        int[] arr2 = new int[len2];
+
+        for (int i = 0; i < len1; i++) {
+            arr1[i] = arr[l + i];
+        }
+        for (int j = 0; j < len2; j++) {
+            arr2[j] = arr[m + 1 + j];
+        }
+        int i = 0, j = 0, k = l;
+        while (i < len1 && j < len2) {
+            if (arr1[i] <= arr2[j]) {
+                arr[k++] = arr1[i++];
+            } else {
+                arr[k++] = arr2[j++];
+            }
+        }
+        while (i < len1) {
+            arr[k++] = arr1[i++];
+        }
+        while (j < len2) {
+            arr[k++] = arr2[j++];
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sort(int arr[], int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
     /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
-  
-    // Driver method 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
-  
-        System.out.println("Given Array"); 
-        printArray(arr); 
-  
-        MergeSort ob = new MergeSort(); 
-        ob.sort(arr, 0, arr.length-1); 
-  
-        System.out.println("\nSorted array"); 
-        printArray(arr); 
-    } 
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String args[]) {
+        int arr[] = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Given Array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
 } 
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,70 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
+// Time Complexity : O(n log n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes (https://leetcode.com/problems/sort-an-array/)
+// Any problem you faced while coding this : no
+
+import java.util.Stack;
+
+class IterativeQuickSort {
+    void swap(int arr[], int i, int j) {
+        //Try swapping without extra variable
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     /* This function is same in both iterative and 
        recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
-        //Compare elements and swap.
-    } 
-  
+    int partition(int arr[], int l, int h) {
+        int pivotValue = arr[h];
+        int left = (l - 1);
+
+        for (int i = l; i <= h; i++) {
+            if (arr[i] < pivotValue) {
+                swap(arr, ++left, i);
+            }
+        }
+        left++;
+        swap(arr, left, h);
+        return left;
+    }
+
     // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
-        //Try using Stack Data Structure to remove recursion.
-    } 
-  
-    // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
-    // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
+    void QuickSort(int arr[], int l, int h) {
+        if (h - l + 1 <= 1) {
+            return;
+        }
+        Stack<Integer> st = new Stack<>();
+        st.push(l);
+        st.push(h);
+
+        while (!st.isEmpty()) {
+            h = st.pop();
+            l = st.pop();
+            int p = partition(arr, l, h);
+            if (p - 1 > l) {
+                st.push(l);
+                st.push(p - 1);
+            }
+            if (p + 1 < h) {
+                st.push(p + 1);
+                st.push(h);
+            }
+        }
+    }
+
+    // A utility function to print contents of arr
+    void printArr(int arr[], int n) {
+        int i;
+        for (i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
+    // Driver code to test above
+    public static void main(String args[]) {
+        IterativeQuickSort ob = new IterativeQuickSort();
+        int arr[] = {-1243, 4, 30, 5, 2, 1, 3, 2, 3, 100, 1243};
+        ob.QuickSort(arr, 0, arr.length - 1);
+        ob.printArr(arr, arr.length);
+    }
 } 
