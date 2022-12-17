1286bfd639da33c968be9496fa34b6438f21d34e
Rachna Ramkumar <rachna.ramkumar@nyu.edu>
2022-12-17 15:21:27 -0600

    Completed PreCourse-2

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,54 @@
+# Exercise_4 : Merge Sort.
+
+# Time Complexity: O(N log N), N = number of elements in array
+# Space Complexity: O(N), since we use an additional array to store the sorted elements
+# Successful Run on Leetcode: Yes (https://leetcode.com/problems/sort-an-array/)
+# Challenges: None
+
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
-  #write your code here
+  # if the array has more than one element, split the array into two halves
+  if len(arr) >= 2:
+    # find the mid of the array
+    mid = len(arr) // 2
+    # split the array into two halves
+    left = arr[:mid]
+    right = arr[mid:]
+    # recursively call mergeSort on the left and right halves of the array
+    mergeSort(left)
+    mergeSort(right)
+    # merge the two halves of the array
+    merge(arr, left, right)
+
+def merge(arr, left, right):
+  i = j = k = 0
+  # while the left and right pointers are less than the length of the left and right halves of the array
+  while i < len(left) and j < len(right):
+    # if the left element is less than the right element, add the left element to the array and increment the left pointer
+    if left[i] < right[j]:
+      arr[k] = left[i]
+      i += 1
+    # if the right element is less than the left element, add the right element to the array and increment the right pointer
+    else:
+      arr[k] = right[j]
+      j += 1
+    # increment the array pointer
+    k += 1
+  # if the left pointer is less than the length of the left half of the array, add the remaining elements to the array
+  while i < len(left):
+    arr[k] = left[i]
+    i += 1
+    k += 1
+  # if the right pointer is less than the length of the right half of the array, add the remaining elements to the array
+  while j < len(right):
+    arr[k] = right[j]
+    j += 1
+    k += 1
   
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
+    for i in range(len(arr)):         
+        print(arr[i],end=" ")
   
 # driver code to test the above code 
 if __name__ == '__main__': 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,62 @@
+# Exercise_5 : Iterative Quick Sort
+
+# Average-Case Time Complexity: O(N log N), N = number of elements in array
+# Worst-Case Time Complexity: O(N^2), N = number of elements in array
+# Space Complexity: O(N), N = number of elements in array
+# Successful Run on Leetcode: Yes (https://leetcode.com/problems/sort-an-array/)
+# Challenges: None
+
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+  # partition the array using the last element as the pivot
+  pivot = arr[h]
+  # set the index of the smaller element to the left of the pivot
+  i = l - 1
+  # iterate through the array
+  for j in range(l, h):
+    # if the current element is smaller than the pivot
+    if arr[j] < pivot:
+      # increment the index of the smaller element
+      i += 1
+      # swap the current element with the element at the index of the smaller element
+      arr[i], arr[j] = arr[j], arr[i]
+  # swap the element at the index of the smaller element with the pivot
+  arr[i + 1], arr[h] = arr[h], arr[i + 1]
+  # return the index of the pivot
+  return i + 1
+
+
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+  # quicksort using an iterative approach
+  # create a stack
+  stack = []
+  # push the initial values of l and h to the stack
+  stack.append(l)
+  stack.append(h)
+  # keep popping the stack until it is empty
+  while stack:
+    # pop the stack
+    h = stack.pop()
+    l = stack.pop()
+    # set the pivot
+    p = partition(arr, l, h)
+    # if the pivot is greater than 1, push the left and right subarrays to the stack
+    if p - 1 > l:
+      stack.append(l)
+      stack.append(p - 1)
+    if p + 1 < h:
+      stack.append(p + 1)
+      stack.append(h)
 
+
+# Driver code to test above 
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print("Sorted array is:") 
+for i in range(n): 
+    print(arr[i])
