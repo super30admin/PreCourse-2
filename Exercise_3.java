eb00369ba45323e1ea38b9c3a5076d454e2fa1b5
Jishva Shah <jishvamshah@gmail.com>
2022-12-18 08:52:47 -0800

    Completed Precourse-2.

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,53 +1,71 @@
-class LinkedList 
-{ 
-    Node head; // head of linked list 
-  
-    /* Linked list node */
-    class Node 
-    { 
-        int data; 
-        Node next; 
-        Node(int d) 
-        { 
-            data = d; 
-            next = null; 
-        } 
-    } 
-  
-    /* Function to print middle of linked list */
-   //Complete this function
-    void printMiddle() 
-    { 
-        //Write your code here
-	//Implement using Fast and slow pointers
-    } 
-  
-    public void push(int new_data) 
-    { 
-        Node new_node = new Node(new_data); 
-        new_node.next = head; 
-        head = new_node; 
-    } 
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :NA
+// Any problem you faced while coding this :Not knowing the traversal of linked list in the beginning can make it hard but once i knew it
+// finding middle element became easier.
 
-    public void printList() 
-    { 
-        Node tnode = head; 
-        while (tnode != null) 
-        { 
-            System.out.print(tnode.data+"->"); 
-            tnode = tnode.next; 
-        } 
-        System.out.println("NULL"); 
-    } 
-  
-    public static void main(String [] args) 
-    { 
-        LinkedList llist = new LinkedList(); 
-        for (int i=15; i>0; --i) 
-        { 
-            llist.push(i); 
-            llist.printList(); 
-            llist.printMiddle(); 
-        } 
-    } 
-} 
+class LinkedList
+{
+    Node head; // head of linked list
+
+    /* Linked list node */
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    /* Function to print middle of linked list */
+    //Complete this function
+    void printMiddle()
+    {
+        //Write your code here
+        //Implement using Fast and slow pointers
+        Node fast = head;
+        Node slow = head;
+
+        // Increment fast pointer by two nodes and slow pointer by one node
+        // on each iteration until fast pointer reaches the end of the list
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        // Print the data at the slow pointer
+        System.out.println("Middle element: " + slow.data);
+    }
+
+    public void push(int new_data)
+    {
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node;
+    }
+
+    public void printList()
+    {
+        Node tnode = head;
+        while (tnode != null)
+        {
+            System.out.print(tnode.data+"->");
+            tnode = tnode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String [] args)
+    {
+        LinkedList llist = new LinkedList();
+        for (int i=15; i>0; --i)
+        {
+            llist.push(i);
+            llist.printList();
+            llist.printMiddle();
+        }
+    }
+}
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,20 +1,76 @@
+// Time Complexity :O(n*log(n))
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :NA
+// Any problem you faced while coding this :I am not able to get the output without using an extra element wile swapping
+
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+        //Try swapping without extra variable 
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     } 
   
     /* This function is same in both iterative and 
        recursive*/
     int partition(int arr[], int l, int h) 
     { 
+        int low=l,high=h;
         //Compare elements and swap.
+        int pivot = arr[high];
+        int i = low - 1;
+ 
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+            i++;
+            swap(arr, i, j);
+            }
+        }
+ 
+        swap(arr, i + 1, high);
+        
+        return i + 1;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
+        int low=l,high=h;
         //Try using Stack Data Structure to remove recursion.
+        // Create an auxiliary stack
+        int[] stack = new int[high - low + 1];
+        
+        // initialize top of stack
+        int top = -1;
+        
+        // push initial values of low and high to stack
+        stack[++top] = low;
+        stack[++top] = high;
+        
+        // Keep popping from stack while is not empty
+        while (top >= 0) {
+            // Pop high and low
+            high = stack[top--];
+            low = stack[top--];
+        
+            // Set pivot element at its correct position in sorted array
+            int p = partition(arr, low, high);
+        
+            // If there are elements on left side of pivot, then push left
+            // side to stack
+            if (p - 1 > low) {
+            stack[++top] = low;
+            stack[++top] = p - 1;
+            }
+        
+            // If there are elements on right side of pivot, then push right
+            // side to stack
+            if (p + 1 < high) {
+            stack[++top] = p + 1;
+            stack[++top] = high;
+            }
+        }
     } 
   
     // A utility function to print contents of arr 
