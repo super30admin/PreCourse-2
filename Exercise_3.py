e30b8be270ecc6444e1d11df65e93dd400b44737
Rohit Bassi <rohitbassi@Rohits-MacBook-Pro.local>
2022-12-18 11:46:09 +0530

    Precourse 2 partial complete

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,34 @@
-# Node class  
+# Node class 
+#time:O(n/2) 
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+	    self.data = data
+	    self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head = None
         
   
     def push(self, new_data): 
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+    	slow = self.head
+    	fast = self.head
+    	if self.head is not None:
+            while (fast is not None and fast.next is not None):
+                fast = fast.next.next
+                slow = slow.next
+            print("The middle element is: ", slow.data)
 
 # Driver code 
 list1 = LinkedList() 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,62 @@
 # Python program for implementation of MergeSort 
-def mergeSort(arr):
-  
-  #write your code here
-  
+#time: O(nlog(n))
+def mergeSort(arr,l,r):
+
+    if l < r:
+
+	    # Same as (l+r)//2, but avoids overflow for
+	    # large l and h
+	    m = l+(r-l)//2
+
+	    # Sort first and second halves
+	    mergeSort(arr, l, m)
+	    mergeSort(arr, m+1, r)
+	    merge(arr, l, m, r)
+
+
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+    L = [0] * (n1)
+    R = [0] * (n2)
+ 
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+ 
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+
+    i = 0    
+    j = 0
+    k = l
+ 
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+ 
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
+	print(arr)
   
 # driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end="\n")  
     printList(arr) 
-    mergeSort(arr) 
+    mergeSort(arr,0,len(arr)-1) 
     print("Sorted array is: ", end="\n") 
     printList(arr) 
