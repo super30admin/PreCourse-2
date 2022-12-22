a173b19d4e05e5bc2cff032f32602e6c0f8fbc5a
Vivek Reddy Sripathi <viveksripathi5312@gmail.com>
2022-12-22 03:19:54 -0800

    third commit

--- Exercise_5.java
+++ Exercise_5.java
@@ -29,6 +29,39 @@ class IterativeQuickSort {
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+        int[] stack = new int[h - l + 1];
+ 
+        // initialize top of stack
+        int top = -1;
+ 
+        // push initial values of l and h to stack
+        stack[++top] = l;
+        stack[++top] = h;
+ 
+        // Keep popping from stack while is not empty
+        while (top >= 0) {
+            // Pop h and l
+            h = stack[top--];
+            l = stack[top--];
+ 
+            // Set pivot element at its correct position
+            // in sorted array
+            int p = partition(arr, l, h);
+ 
+            // If there are elements on left side of pivot,
+            // then push left side to stack
+            if (p - 1 > l) {
+                stack[++top] = l;
+                stack[++top] = p - 1;
+            }
+ 
+            // If there are elements on right side of pivot,
+            // then push right side to stack
+            if (p + 1 < h) {
+                stack[++top] = p + 1;
+                stack[++top] = h;
+            }
+        }
     } 
   
     // A utility function to print contents of arr 
