3f98c0ab00965a7a57a5d91d502ec7e798f81104
Akhil Reddy Duggasani <duggasan@usc.edu>
2023-01-12 18:11:23 +0530

    Completed all 5 exercises.

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,37 @@
+// Time Complexity : O(logN) where N is the number of elements in the list.
+// Space Complexity : O(1) iterative
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None
+
+
+// Your code here along with comments explaining your approach
+
+
+/**
+ * @author akhilreddy619
+ * Since the array is sorted, every time we check the middle element of the list.
+ * if that element matches with the search element, we return the index, else we check 
+ * 2 cases. 1. check if search element is less than mid element, if so, reduce search
+ * range to (l, mid - 1). 2. if search element is greater than mid element, then search
+ * in the range of (mid + 1, r). Thus every iteration, the search space becomes half.
+ *
+ */
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+    	while(l <= r) {
+    		int mid = (l + r) / 2;
+    		if(arr[mid] == x) {
+    			return mid;
+    		} else if(x < arr[mid]) {
+    			r = mid - 1;
+    		} else {
+    			l = mid + 1;
+    		}
+    	}
+    	return -1;
     } 
   
     // Driver method to test above 
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,48 +1,81 @@
-class QuickSort 
-{ 
-    /* This function takes last element as pivot, 
-       places the pivot element at its correct 
-       position in sorted array, and places all 
-       smaller (smaller than pivot) to left of 
-       pivot and all greater elements to right 
-       of pivot */
-    void swap(int arr[],int i,int j){
-        //Your code here   
-    }
-    
-    int partition(int arr[], int low, int high) 
-    { 
-   	//Write code here for Partition and Swap 
-    } 
-    /* The main function that implements QuickSort() 
-      arr[] --> Array to be sorted, 
-      low  --> Starting index, 
-      high  --> Ending index */
-    void sort(int arr[], int low, int high) 
-    {  
-            // Recursively sort elements before 
-            // partition and after partition 
-    } 
-  
-    /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
-        System.out.println(); 
-    } 
-  
-    // Driver program 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {10, 7, 8, 9, 1, 5}; 
-        int n = arr.length; 
-  
-        QuickSort ob = new QuickSort(); 
-        ob.sort(arr, 0, n-1); 
-  
-        System.out.println("sorted array"); 
-        printArray(arr); 
-    } 
-} 
+// Time Complexity : O(NlogN) N is the number of elements in the array.
+// Space Complexity : O(logN) for recursion
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None
+
+
+// Your code here along with comments explaining your approach
+
+
+/**
+ * @author akhilreddy619
+ * In quick sort, we have to partition the array into two parts every time by finding
+ * pivot index which separates smaller elements on one side and bigger elements (than pivot)
+ * on the other side. To find pivot index, we consider last element as pivot element. And we
+ * maintain a variable to track the index of the smaller elements that are less than pivot element with i.
+ * Then iterate over from low to high - 1 elements to check if any element is <= pivot.
+ * If found, we swap the current index with ith index. After all iterations we swap i+1th 
+ * index with pivot such that pivot separates smaller elements and bigger elements than the pivot.
+ * Repeat the process for the elements ranging from (low, pivot index - 1) and (pivot index + 1, high).
+ */
+class QuickSort {
+	/*
+	 * This function takes last element as pivot, places the pivot element at its
+	 * correct position in sorted array, and places all smaller (smaller than pivot)
+	 * to left of pivot and all greater elements to right of pivot
+	 */
+	void swap(int arr[], int i, int j) {
+		// Your code here
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	int partition(int arr[], int low, int high) {
+		// Write code here for Partition and Swap
+		int pivot = arr[high];
+		int i = low - 1;
+		for (int j = low; j <= high - 1; j++) {
+			if (arr[j] <= pivot) {
+				i++;
+				swap(arr, i, j);
+			}
+		}
+		swap(arr, i + 1, high);
+		return (i+1);
+	}
+
+	/*
+	 * The main function that implements QuickSort() arr[] --> Array to be sorted,
+	 * low --> Starting index, high --> Ending index
+	 */
+	void sort(int arr[], int low, int high) {
+		// Recursively sort elements before
+		// partition and after partition
+		if (low >= high)
+			return;
+		int pivot = partition(arr, low, high);
+		sort(arr, low, pivot - 1);
+		sort(arr, pivot + 1, high);
+	}
+
+	/* A utility function to print array of size n */
+	static void printArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	// Driver program
+	public static void main(String args[]) {
+		int arr[] = { 2, 10, 7, 8, 9, 1, 5 };
+		int n = arr.length;
+
+		QuickSort ob = new QuickSort();
+		ob.sort(arr, 0, n - 1);
+
+		System.out.println("sorted array");
+		printArray(arr);
+	}
+}
