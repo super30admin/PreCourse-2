ff5815bc9501c3dd55b468bce7777f50cb91be6c
Hemangi Ahire <hemangiahire@Hemangis-MacBook-Air.local>
2023-01-19 20:46:22 -0800

    Completed sprint precourse2

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,7 +1,15 @@
+//Time Complexity-O(logn)
+//Space Complexity-O(1)
+// Problem : issues in understanding complexity analysis
+
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
 	//Try swapping without extra variable 
+    	
+    	int temp = arr[i];
+    	arr[i] = arr[j];
+    	arr[j] = temp ;
     } 
   
     /* This function is same in both iterative and 
@@ -9,12 +17,43 @@ class IterativeQuickSort {
     int partition(int arr[], int l, int h) 
     { 
         //Compare elements and swap.
+    	int pivot=h;
+        int s=l;
+        for(int i=l;i<pivot;i++)
+        {
+            if(arr[i]<arr[pivot])
+            {
+                if(s!=i)
+                swap(arr,s,i);
+                s++;
+            }
+
+        }
+        swap(arr,s,pivot);
+        return s;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+        int stack[] = new int[h - l + 1];
+        int top = -1;
+        stack[++top] = l;
+        stack[++top] = h;
+        while (top >= 0) {
+            h = stack[top--];
+            l = stack[top--];
+            int p = partition(arr, l, h);
+            if (p - 1 > l) {
+                stack[++top] = l;
+                stack[++top] = p - 1;
+            }
+            if (p + 1 < h) {
+                stack[++top] = p + 1;
+                stack[++top] = h;
+            }
+        }
     } 
   
     // A utility function to print contents of arr 
