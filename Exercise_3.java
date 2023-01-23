31c1df8c63495acba819ab1280e7801be55a29ee
Shubhangi-Srivastava <srivastava.sh@northeastern.edu>
2023-01-23 12:53:01 -0500

    added 2 ex

--- Exercise_3.java
+++ Exercise_3.java
@@ -17,10 +17,23 @@ class LinkedList
     /* Function to print middle of linked list */
    //Complete this function
     void printMiddle() 
-    { 
+    {  
         //Write your code here
-	//Implement using Fast and slow pointers
-    } 
+        //initializing slow and fast to head
+        Node slow = head;
+        Node fast = head;
+        
+        //checking fast isn't null, so is it's nect node
+        while(fast != null && fast.next != null){
+            slow = slow.next; //moving slow by 1 step
+            fast = fast.next.next; //movong fast by 2 steps
+        }
+
+        //when fast reaches to the end of the ll, slow will reach the middle that time
+        //hence we print the data at slow
+        System.out.println("Mid element:" + slow.data);
+      
+    }
   
     public void push(int new_data) 
     { 
--- Exercise_4.java
+++ Exercise_4.java
@@ -3,17 +3,57 @@ class MergeSort
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
     // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
+    void merge(int arr[], int start, int mid, int end) 
     {  
        //Your code here  
-    } 
+       int[] mix = new int[end - start];
+       int i = start;
+       int j = mid;
+       int k = 0;
+
+       while(i < mid && j < end){
+        if(arr[i] < arr[j]){
+            mix[k] = arr[i];
+            i++;
+        } else {
+            mix[k] = arr[j];
+            j++;
+        }
+        k++;
+        }
+        //it may be possible that one of the arrays isn't complete
+        //copy the remaining elements
+        while(i < mid){
+            mix[k] = arr[i];
+            i++;
+            k++;     
+          }
+       while(j < end){
+        mix[k] = arr[j];
+            j++;
+            k++; 
+       }  
+       for(int l = 0; l < mix.length; l++){
+        arr[start + l] = mix[l];
+       }
+       } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
-    void sort(int arr[], int l, int r) 
+    void sort(int arr[], int start, int end) 
     { 
 	//Write your code here
+    if(end - start == 1){
+        return;
+    }
+
+    int mid = start + (end - start)/2;
+
         //Call mergeSort from here 
+        sort(arr, start, mid);
+        sort(arr, mid, end);
+
+        merge(arr, start, mid, end);
     } 
   
     /* A utility function to print array of size n */
@@ -34,7 +74,7 @@ class MergeSort
         printArray(arr); 
   
         MergeSort ob = new MergeSort(); 
-        ob.sort(arr, 0, arr.length-1); 
+        ob.sort(arr, 0, arr.length); 
   
         System.out.println("\nSorted array"); 
         printArray(arr); 
