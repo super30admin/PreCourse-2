2d63a2691390392efde1eecc181ebc985277d6f6
SiddeshIyer-Gmoney <iyersiddesh10@gmail.com>
2023-01-23 13:39:46 -0800

    Completed Sprint Precourse2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,40 @@
-# Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+# Python program for implementation of Quicksort Sort
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+# give you explanation for the approach
+def partition(arr, low, high):
+    # write your code here
+    pivot = arr[high]
+
+    # pointer for higher element
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i+1
+            #  swap element i with element j
+            (arr[i], arr[j]) = (arr[j], arr[i])
+    # swap the pivot element with the greater element indicated by i
+    (arr[i+1], arr[high]) = (arr[high], arr[i + 1])
+
+    return i + 1
+
+# Function to do Quick sort
+
+
+def quickSort(arr, low, high):
+
+    # write your code here
+    pi = partition(arr, low, high)
+
+    quickSort(arr, low, pi-1)
+
+    quickSort(arr, pi+1, high)
+
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]),
