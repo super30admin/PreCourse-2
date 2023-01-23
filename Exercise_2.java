2660037e9f0660854279c697e31ec540358e00e8
Abhiruchi Shinde <92135919+ruh123@users.noreply.github.com>
2023-01-22 18:54:49 -0800

    completed Quicksort in java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,8 @@
-class QuickSort 
+//Time complexity : O(n2)
+// Space Complexity :O(1)
+// Code executed successfully :yes 
+
+public class QuickSort 
 { 
     /* This function takes last element as pivot, 
        places the pivot element at its correct 
@@ -7,12 +11,37 @@ class QuickSort
        pivot and all greater elements to right 
        of pivot */
     void swap(int arr[],int i,int j){
-        //Your code here   
+        //Your code here  
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;		
     }
     
     int partition(int arr[], int low, int high) 
     { 
    	//Write code here for Partition and Swap 
+	int pivot = arr[high];
+ 
+        // Index of smaller element and
+        // indicates the right position
+        // of pivot found so far
+        int i = (low - 1);
+ 
+        for (int j = low; j <= high - 1; j++) {
+ 
+            // If current element is smaller
+            // than the pivot
+            if (arr[j] < pivot) {
+ 
+                // Increment index of
+                // smaller element
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+	
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
@@ -22,6 +51,18 @@ class QuickSort
     {  
             // Recursively sort elements before 
             // partition and after partition 
+			
+			 if (low < high) {
+ 
+            // pi is partitioning index, arr[p]
+            // is now at right place
+            int pi = partition(arr, low, high);
+ 
+            // Separately sort elements before
+            // partition and after partition
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
     } 
   
     /* A utility function to print array of size n */
