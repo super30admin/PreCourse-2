bc6ee437b098938f78eac7c4c861a6ec1873a55a
Pallavi9049 <60310109+Pallavi9049@users.noreply.github.com>
2023-01-23 19:18:08 -0500

    Precourse 2 for review

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,40 @@
-# Python code to implement iterative Binary  
-# Search. 
-  
+# Python code to implement iterative Binary Search. 
+# // Time Complexity : O(log N)
+# // Space Complexity : O(N)
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : Needed to go through algorithm
+
+
+# // Your code here along with comments explaining your approach
+
 # It returns location of x in given array arr  
 # if present, else returns -1 
+
+#Used recursion and kept iterating on small sub arrays formed with the help of mid
+#Sorted Array in ascending order
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
+	result = -1
+    if r >= l:
+        mid = (l+r)//2
+        if (arr[mid]==x):
+            return mid
+        elif arr[mid] > x:
+            return binarySearch(arr, l, mid - 1, x)
+        else:
+            return binarySearch(arr, mid + 1, r, x)
+    else:
+        return result
+
+
+
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
-  
+
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
-  
+
 if result != -1: 
-    print "Element is present at index % d" % result 
+print "Element is present at index % d" % result 
 else: 
-    print "Element is not present in array"
+print "Element is not present in array"
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,39 @@
-# Node class  
+# Node class 
+# // Time Complexity : O(N)
+# // Space Complexity : O(N)
+# // Did this code successfully run on Leetcode :Yes
+''' Any problem you faced while coding this : 
+Initiated two pointers slowNode and FastNode.
+slowNode incremented with 1 node and fastNode incremented with 2 nodes resp.'''
+
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
   
     def push(self, new_data): 
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
         
-  
-    # Function to get the middle of  
-    # the linked list 
+  '''when the fastNode reaches the end node
+  slow node is at the middle of the singly linked list 
+  and we have returned that value'''
+    # Function to get the middle of the linked list 
     def printMiddle(self): 
+        slowNode = self.head
+        fastNode = self.head
+        while fastNode and fastNode.next:
+            slowNode = slowNode.next
+            fastNode = fastNode.next.next
+        print(slowNode.data)
 
 # Driver code 
 list1 = LinkedList() 
