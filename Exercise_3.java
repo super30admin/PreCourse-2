3c19516977842e171069c2d4b286bf14746d3a59
rutujamandlikFT <123407038+rutujamandlikFT@users.noreply.github.com>
2023-01-25 20:09:45 -0500

    Update Exercise_3.java

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,3 +1,6 @@
+//TC: O(n)
+//SC: O(1)
+
 class LinkedList 
 { 
     Node head; // head of linked list 
@@ -20,6 +23,15 @@ class LinkedList
     { 
         //Write your code here
 	//Implement using Fast and slow pointers
+    Node fast = head;
+    Node slow = head;
+
+    while(fast != null && fast.next != null){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    System.out.println("The middle node is: " + slow.data);
+
     } 
   
     public void push(int new_data) 
