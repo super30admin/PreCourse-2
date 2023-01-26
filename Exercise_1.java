81a8483cafb264dc75ac4c32c90badc1a08585df
rutujamandlikFT <123407038+rutujamandlikFT@users.noreply.github.com>
2023-01-25 20:08:53 -0500

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,31 @@
+// Time Complexity : O(Log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : N/A
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        if (r >= l) {
+            int mid = l + (r - l) /2;
+
+            if(arr[mid] == x){
+                return mid;
+            }
+            if(arr[mid] > x){
+                return binarySearch(arr, l , mid - 1, x);
+
+            }
+            return binarySearch(arr, mid + 1, r, x);
+        }
+        return -1;
     } 
   
     // Driver method to test above 
