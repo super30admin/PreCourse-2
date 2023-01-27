d712ad326c49a61559c79be00fea5fd30c3aaa2d
Radhika Kulkarni <radhikankulkarni@gmail.com>
2023-01-27 13:57:03 -0700

    Completed Precourse-2 for practice

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,6 +3,17 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        while(l <= r) {
+            int m = l + (r - l) / 2;
+            if(arr[m] == x) {
+                return m;
+            } else if(arr[m] < x) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return -1;
     } 
   
     // Driver method to test above 
--- Exercise_3.java
+++ Exercise_3.java
@@ -20,6 +20,14 @@ class LinkedList
     { 
         //Write your code here
 	//Implement using Fast and slow pointers
+        Node slow, fast;
+        slow = head;
+        fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.print.ln(slow.data);
     } 
   
     public void push(int new_data) 
