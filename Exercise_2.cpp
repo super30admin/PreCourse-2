f7521387176ed879240fcbee29543a02fac49777
tanujars <tanujars2100@gmail.com>
2023-01-29 03:15:27 -0700

    Review pre-course2

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,47 +1,68 @@
-#include <bits/stdc++.h> 
-using namespace std;  
-  
-// A utility function to swap two elements  
-void swap(int* a, int* b)  
-{  
-    //Your Code here 
-}  
-  
-/* This function takes last element as pivot, places  
-the pivot element at its correct position in sorted  
-array, and places all smaller (smaller than pivot)  
-to left of pivot and all greater elements to right  
+// #include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+// Time Complexity : O(nlog n)
+// Space Complexity : O(n) // if in place then O(log n)
+// An utility function to swap two elements
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
 of pivot */
-int partition (int arr[], int low, int high)  
-{  
-    //Your Code here 
-}  
-  
-/* The main function that implements QuickSort  
-arr[] --> Array to be sorted,  
-low --> Starting index,  
+int partition (int arr[], int low, int high)
+{
+    //Your Code here
+    int pivot = arr[high];
+    int partition = low;
+    for(int i = low; i < high; i++) {
+        if(arr[i] <= pivot) {
+            swap(arr[i], arr[partition]);
+            partition++;
+        }
+    }
+    // putting the pivot in the right place
+    swap(arr[partition], arr[high]);
+    return partition;
+}
+
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
 high --> Ending index */
-void quickSort(int arr[], int low, int high)  
-{  
-    //Your Code here 
-}  
-  
+void quickSort(int arr[], int low, int high)
+{
+    if(low <= high) {
+        int i = partition(arr, low, high);
+        quickSort(arr, low, i - 1);
+        quickSort(arr, i + 1, high);
+    }
+}
+
 /* Function to print an array */
-void printArray(int arr[], int size)  
-{  
-    int i;  
-    for (i = 0; i < size; i++)  
-        cout << arr[i] << " ";  
-    cout << endl;  
-}  
-  
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
 // Driver Code 
-int main()  
-{  
-    int arr[] = {10, 7, 8, 9, 1, 5};  
-    int n = sizeof(arr) / sizeof(arr[0]);  
-    quickSort(arr, 0, n - 1);  
-    cout << "Sorted array: \n";  
-    printArray(arr, n);  
-    return 0;  
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
 }  
--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,50 +1,64 @@
-#include<bits/stdc++.h>  
-using namespace std;  
-  
-// Struct  
-struct Node  
-{  
-    int data;  
-    struct Node* next;  
-};  
-  
+// #include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+// Struct
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
 /* Function to get the middle of the linked list*/
-void printMiddle(struct Node *head)  
-{  
-  //YourCode here
-  //Use fast and slow pointer technique
-}  
-  
+void printMiddle(struct Node *head)
+{
+    //YourCode here
+    //Use fast and slow pointer technique
+    struct Node *slow = head;
+    struct Node *fast = head;
+    if(head != NULL) {
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        cout << slow->data;
+    }
+}
+
 // Function to add a new node  
-void push(struct Node** head_ref, int new_data)  
-{  
-    struct Node* new_node = new Node;  
-    new_node->data = new_data;  
-    new_node->next = (*head_ref);  
-    (*head_ref) = new_node;  
-}  
-  
+void push(struct Node** head_ref, int new_data)
+{
+    struct Node* new_node = new Node;
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
 // A utility function to print a given linked list  
-void printList(struct Node *ptr)  
-{  
-    while (ptr != NULL)  
-    {  
-        printf("%d->", ptr->data);  
-        ptr = ptr->next;  
-    }  
-    printf("NULL\n");  
-}  
-  
+void printList(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d->", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+}
+
 // Driver Code 
-int main()  
-{    
-    struct Node* head = NULL;    
-    for (int i=15; i>0; i--)  
-    {  
-        push(&head, i);  
-        printList(head);  
-        printMiddle(head);  
-    }  
-  
-    return 0;  
+int main()
+{
+    struct Node* head = NULL;
+    for (int i=15; i>0; i--)
+    {
+        push(&head, i);
+        printList(head);
+        printMiddle(head);
+    }
+
+    return 0;
 }  
