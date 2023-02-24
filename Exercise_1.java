b859ca9d43299dc9880ddaecc2507c99f8a74fd2
sivakami-projects <126191544+sivakami-projects@users.noreply.github.com>
2023-02-24 12:11:53 -0800

    Update Exercise_1.java
    Binary search implementation.

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,21 +1,31 @@
 class BinarySearch { 
-    // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
-    } 
-  
-    // Driver method to test above 
-    public static void main(String args[]) 
-    { 
-        BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
-        int n = arr.length; 
-        int x = 10; 
+   
+    public static void main(String[] args) {
+		
+		BinarySearch ob = new BinarySearch(); 
+        int arr[] = { 2, 3, 4, 10, 40, 60 }; 
+        int n = arr.length, x = 4; 
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
         else
             System.out.println("Element found at index " + result); 
+	}
+	
+	// Returns index of x if it is present in arr[l.. r], else return -1 
+    int binarySearch(int arr[], int l, int r, int x) 
+    { 
+        //Write your code here
+        if(l > r || l < 0 || r >= arr.length)
+            return -1;
+            
+        int mid = (r - l) / 2 + l;
+        
+        if(arr[mid] == x)
+            return mid;
+        if(x > arr[mid])
+            return binarySearch(arr, mid + 1, r, x);
+        else
+            return binarySearch(arr, l, mid, x);
     } 
 } 
