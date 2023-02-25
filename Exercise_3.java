00da24bd0103f18d2027dc8398a834c0f5dc93b2
Ashmitha <ashdale30@gmail.com>
2023-02-24 16:19:30 -0800

    Commit Precourse 2: 24/02/2023

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,4 +1,6 @@
-class LinkedList 
+//Space Complexity - O(1)
+//Time Complexity - O(n)  
+class Exercise_3 
 { 
     Node head; // head of linked list 
   
@@ -20,7 +22,20 @@ class LinkedList
     { 
         //Write your code here
 	//Implement using Fast and slow pointers
+    if (head != null) 
+    { 
+        Node fast_ptr= head;
+        Node slow_ptr= head;
+        while (fast_ptr != null && fast_ptr.next != null) 
+        { 
+            fast_ptr = fast_ptr.next.next; 
+            slow_ptr = slow_ptr.next; 
+          
+        } 
+        System.out.println("The middle element is [" + 
+                            slow_ptr.data + "] \n"); 
     } 
+}
   
     public void push(int new_data) 
     { 
@@ -42,8 +57,8 @@ class LinkedList
   
     public static void main(String [] args) 
     { 
-        LinkedList llist = new LinkedList(); 
-        for (int i=15; i>0; --i) 
+        Exercise_3 llist = new Exercise_3(); 
+        for (int i=12; i>0; --i) 
         { 
             llist.push(i); 
             llist.printList(); 
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,4 +1,6 @@
-class MergeSort 
+//Time Complexity - O(n log n) 
+//Space Complexity - O(n)
+class Exercise_4 
 { 
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
@@ -6,7 +8,60 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
-    } 
+        // Find sizes of two subarrays to be merged 
+        int n1 = m - l + 1; 
+        int n2 = r - m; 
+ 
+        /* Create temp arrays */
+        int L[] = new int [n1]; 
+        int R[] = new int [n2]; 
+ 
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; i++) 
+            L[i] = arr[l + i]; 
+        for (int j=0; j<n2; j++) 
+            R[j] = arr[m + 1+ j]; 
+ 
+ 
+        /* Merge the temp arrays */
+ 
+        // Initial indexes of first and second subarrays 
+        int i = 0, j = 0; 
+ 
+        // Initial index of merged subarry array 
+        int k = l; 
+        while (i < n1 && j < n2) 
+        { 
+            if (L[i] <= R[j]) 
+            { 
+                arr[k] = L[i]; 
+                i++; 
+            } 
+            else
+            { 
+                arr[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        } 
+ 
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+ 
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) 
+        { 
+            arr[k] = R[j]; 
+            j++; 
+            k++; 
+        } 
+     }  
+       
   
     // Main function that sorts arr[l..r] using 
     // merge() 
@@ -14,13 +69,25 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+        if(l < r )
+        { 
+            // Find the middle point 
+            int m = (l+r)/2; 
+
+            // Sort first and second halves 
+            sort(arr, l, m); 
+            sort(arr , m+1, r); 
+
+            // Merge the sorted halves 
+            merge(arr, l, m, r); 
+        }
     } 
   
     /* A utility function to print array of size n */
     static void printArray(int arr[]) 
     { 
         int n = arr.length; 
-        for (int i=0; i<n; ++i) 
+        for (int i=0; i<n; i++) 
             System.out.print(arr[i] + " "); 
         System.out.println(); 
     } 
@@ -28,12 +95,12 @@ class MergeSort
     // Driver method 
     public static void main(String args[]) 
     { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
+        int arr[] = {12, 11, 13, 5, 6, 71}; 
   
         System.out.println("Given Array"); 
         printArray(arr); 
   
-        MergeSort ob = new MergeSort(); 
+        Exercise_4 ob = new Exercise_4(); 
         ob.sort(arr, 0, arr.length-1); 
   
         System.out.println("\nSorted array"); 
