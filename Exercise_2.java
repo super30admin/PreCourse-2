c9fa801cb010e070fac3906f452a492b8a3d90fa
sivakami-projects <126191544+sivakami-projects@users.noreply.github.com>
2023-02-25 19:01:29 -0800

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -8,12 +8,36 @@ class QuickSort
        of pivot */
     void swap(int arr[],int i,int j){
         //Your code here   
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
     int partition(int arr[], int low, int high) 
     { 
-   	//Write code here for Partition and Swap 
+   	        //Write code here for Partition and Swap 
+            int leftPointer = low;
+            int rightPointer = high - 1;
+            int pivot = arr[high];
+        
+            while (leftPointer < rightPointer) {             
+              while (arr[leftPointer] <= pivot && leftPointer < rightPointer)
+                leftPointer++;
+
+              while (arr[rightPointer] >= pivot && leftPointer < rightPointer)
+                rightPointer--;
+
+              swap(arr, leftPointer, rightPointer);
+            }
+
+            if(arr[leftPointer] > arr[high])
+              swap(arr, leftPointer, high);
+            else
+              leftPointer = highIndex;
+
+            return leftPointer;
     } 
+    
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
       low  --> Starting index, 
@@ -22,6 +46,12 @@ class QuickSort
     {  
             // Recursively sort elements before 
             // partition and after partition 
+        
+            if (low >= high)
+              return;
+            int leftPointer = partition(array, lowIndex, highIndex);
+            sort(array, lowIndex, leftPointer - 1);
+            sort(array, leftPointer + 1, highIndex);
     } 
   
     /* A utility function to print array of size n */
@@ -45,4 +75,8 @@ class QuickSort
         System.out.println("sorted array"); 
         printArray(arr); 
     } 
+    
+    /*Time Complexity - Worst case - O(n^2) where n is the size of the input array "arr"
+                        Best case - O(n logn)
+    */
 } 
