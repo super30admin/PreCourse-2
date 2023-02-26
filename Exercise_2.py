43bf5a60418d1adb4773e4c10de1990bb05c4cf9
Alekhya1311 <alekhyabairaboina1311@gmail.com>
2023-02-26 01:45:09 -0800

    Completed Precourse-2

--- Exercise_2.py
+++ Exercise_2.py
@@ -5,12 +5,32 @@ def partition(arr,low,high):
   
   
     #write your code here
+    # selecting last element as pivot
+    pivot = arr[high]
+    i = low-1 
+    for j in range(low,high): 
+         # If the current element is smaller than or equal to the pivot,
+        # swapping with the element at partition index and incrementing the index
+        if arr[j] <= pivot: 
+             i +=1 
+             arr[i],arr[j] = arr[j],arr[i]
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+
+    return i+1 
+           
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
     #write your code here
+    if low < high: 
+
+        pivot = partition(arr,low,high)
+        quickSort(arr,low,pivot-1)
+        quickSort(arr,pivot+1,high)
+   
+
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
--- Exercise_5.py
+++ Exercise_5.py
@@ -3,8 +3,45 @@
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
   #write your code here
+    i = l - 1
+    pivot = arr[h]
+    for j in range(l, h):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[h] = arr[h], arr[i+1]
+
+    return i+1
 
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  stack = []
 
+  stack.append(l)
+  stack.append(h)
+
+  while stack : 
+    h = stack.pop()
+    l = stack.pop()
+    p = partition(arr, l, h)
+
+    if l < p-1 : 
+      stack.append(l)
+      stack.append(p-1) 
+
+    if h > p+1 : 
+      stack.append(p+1)
+      stack.append(h)
+
+
+
+
+# Driver code to test above 
+arr = [4, 3, 5, 2, 1, 3, 2, 3] 
+n = len(arr) - 1
+quickSortIterative(arr, 0, n) 
+print ("Sorted array is:") 
+for i in range(n+1): 
+    print ("%d" %arr[i])
