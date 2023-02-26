5ea424cf10e32aa9e85271282578510dd8a9d56b
sivakami-projects <126191544+sivakami-projects@users.noreply.github.com>
2023-02-25 21:05:52 -0800

    Update Exercise_5.java

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,20 +1,37 @@
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+	//Try swapping without extra variable
+	arr[i] = arr[i] + arr[j];
+	arr[j] = arr[i] - arr[j];
+	arr[i] = arr[i] - arr[j];
     } 
   
     /* This function is same in both iterative and 
        recursive*/
     int partition(int arr[], int l, int h) 
     { 
-        //Compare elements and swap.
+	int pivot = arr[h];
+        int i = (l - 1); 
+        for (int j = low; j <= high - 1; j++) {
+            // If current element is smaller than or equal to pivot
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+	return i + 1;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
-        //Try using Stack Data Structure to remove recursion.
+	if (low < high) {
+            int pivot = partition(arr, low, high);       
+            qSort(arr, low, pivot - 1);
+            qSort(arr, pivot + 1, high);
+        }
     } 
   
     // A utility function to print contents of arr 
