cdd10c6c777769b63dc7b6b03176b6d899b15086
Ravali Kolli <ravalikolli@Ravalis-MacBook-Air.local>
2023-02-26 20:40:48 -0800

    Completed Precourse2- Please review

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,43 @@
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes on VSCODE
+# Any problem you faced while coding this : No
+
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data 
+        self.next = None
         
 class LinkedList: 
   
-    def __init__(self): 
+    def __init__(self):
+        self.head = None
         
-  
     def push(self, new_data): 
+        newNode = Node(new_data)
+        if self.head == None:
+            self.head = newNode
+            return
+        else:
+            curr = self.head
+            while (curr.next) != None:
+                curr = curr.next
+            curr.next = newNode
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        slow = self.head
+        fast = self.head
+        while(fast!=None and fast.next!=None):
+            slow = slow.next
+            fast = fast.next.next
+        return slow.data
+
 
 # Driver code 
 list1 = LinkedList() 
@@ -23,4 +46,4 @@ list1.push(4)
 list1.push(2) 
 list1.push(3) 
 list1.push(1) 
-list1.printMiddle() 
+print(list1.printMiddle())
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,10 +1,37 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
-  #write your code here
+    if (len(arr) > 1):
+        mid = len(arr)//2
+        leftArray  = arr[:mid]
+        rightArray = arr[mid:]
+
+        mergeSort(leftArray)
+        mergeSort(rightArray)
+
+        i = j = k = 0
+
+        while (i < len(leftArray) and j < len(rightArray)):
+            if (leftArray[i] < rightArray[j]):
+              arr[k] = leftArray[i]
+              i += 1
+            else:
+              arr[k] = rightArray[j]
+              j += 1
+            k += 1
+
+        while (i <len(leftArray)):
+           arr[k] = leftArray[i]
+           i += 1
+           k += 1
+        while (j <len(rightArray)):
+           arr[k] = rightArray[j]
+           j += 1
+           k += 1
   
 # Code to print the list 
 def printList(arr): 
+   for i in range(len(arr)):
+      print(arr[i], end = " ")
     
     #write your code here
   
