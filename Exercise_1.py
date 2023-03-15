3a0fbb69edb135e2b261accf65a99d7dc9b3d64f
AnirudhS30 <127216139+AnirudhS30@users.noreply.github.com>
2023-03-14 20:14:24 -0400

    Add files via upload

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,10 +4,19 @@
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
+   if l<=r :
+    #write your code here
+    mid= (l+r)//2
+    if x== arr[mid]:
+        return mid
+    elif x >arr[mid]:
+        #l= mid+1
+        return binarySearch(arr, mid+1, r, x)
+    else:
+        #r= mid-1
+        return binarySearch(arr, l, mid-1, x)
+   else: 
+      return -1
   
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
@@ -17,6 +26,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print(f"Element is present at index: {result}")
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,42 @@
-# Node class  
-class Node:  
-  
+# Node class
+class Node:
+
     # Function to initialise the node object  
-    def __init__(self, data):  
-        
+    def __init__(self, data, next):  
+        self.data= data
+        self.next= next
+
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head= None
   
     def push(self, new_data): 
-        
+        if self.head is None:
+            self.head= Node(new_data, None)
+
+        self.new_node= Node(data= new_data, next= None)
+        node_loc = self.head
+        while node_loc.next != None:
+            node_loc = node_loc.next
+        node_loc.next = self.new_node
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        i = 1
+        node_loc = self.head 
+        while  node_loc.next is not None: 
+            node_loc = node_loc.next 
+            i += 1 
+
+        size = i + 1 
+        mid = (size + 1)//2
+        node_loc = self.head
+        while mid > 1: 
+            node_loc = node_loc.next
+            mid -= 1 
+        print(node_loc.data)
 
 # Driver code 
 list1 = LinkedList() 
