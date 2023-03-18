e027045e97728cf6ac295a2a4e0072a41fdc4ed3
UjasShah <89996494+UjasShah@users.noreply.github.com>
2023-03-17 19:13:07 -0600

    Precourse-2 completed, please review

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,39 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        mergeSort(L) # sort the left half
+        mergeSort(R) # sort the right half
+
+        # Merge the two sorted halves
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
   
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
-    #write your code here
+
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
   
 # driver code to test the above code 
 if __name__ == '__main__': 
@@ -15,4 +42,4 @@ if __name__ == '__main__':
     printList(arr) 
     mergeSort(arr) 
     print("Sorted array is: ", end="\n") 
-    printList(arr) 
+    printList(arr)
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,53 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+  pivot = arr[h]
+  i = l - 1
+  for j in range(l, h):
+      if arr[j] <= pivot:
+          i += 1
+          arr[i], arr[j] = arr[j], arr[i]
+  arr[i + 1], arr[h] = arr[h], arr[i + 1]
+  return i + 1
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    stack = [0] * (h - l + 1)
+    top = -1
 
+    top += 1
+    stack[top] = l
+    top += 1
+    stack[top] = h
+
+    while top >= 0:
+
+        h = stack[top]
+        top -= 1
+        l = stack[top]
+        top -= 1
+
+        p = partition(arr, l, h)
+
+        # If there are elements on left side of pivot,
+        # then push left side to stack
+        if p - 1 > l:
+            top += 1
+            stack[top] = l
+            top += 1
+            stack[top] = p - 1
+
+        # If there are elements on right side of pivot,
+        # then push right side to stack
+        if p + 1 < h:
+            top += 1
+            stack[top] = p + 1
+            top += 1
+            stack[top] = h
+
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSortIterative(arr, 0, n - 1)
+print("Sorted array is:", arr)
