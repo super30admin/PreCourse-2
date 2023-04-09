1093817a25a53ad9f6c754ef27e8e8b1b2c6408e
NickilVishwaaMS <nickilvishwaams@gmail.com>
2023-04-10 01:09:56 +0530

    Completed Sprint Precourse2

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,39 @@
-# Python program for implementation of MergeSort 
+# Time Complexity O(nlog(n))
+# Space Complexity O(nlog(n))
 def mergeSort(arr):
+            left=[]
+            right=[]
+            if len(arr) > 1:
+              mid = len(arr)//2
+              left = arr[:mid]
+              right = arr[mid:]
+            #breaking down the array into 2 half and then further breaking down the element to  2 half till we reach just 1 element in each half
+              mergeSort(left)
+              mergeSort(right)
+
+            i = j = k = 0
+            #recombining the the smaller half into bigger half while sorting out the element
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    arr[k] = left[i]
+                    i += 1
+                else:
+                    arr[k] = right[j]
+                    j += 1
+                k += 1
+            while i < len(left):
+                arr[k] = left[i]
+                i += 1
+                k += 1
+            while j < len(right):
+                arr[k] = right[j]
+                j += 1
+                k += 1
   
-  #write your code here
   
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
+    print(arr)
   
 # driver code to test the above code 
 if __name__ == '__main__': 
