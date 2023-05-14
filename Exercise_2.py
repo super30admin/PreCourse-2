b10b022fe27129568adc655181608b9526f0860d
Saloni Modi <salonimodi@Salonis-MBP.lan>
2023-05-13 23:38:31 -0700

    Completed Pre-Course-2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,30 @@
 # Python program for implementation of Quicksort Sort 
-  
 # give you explanation for the approach
+# time complexity: O(logn) 
+# Space complexity: O(logn) 
 def partition(arr,low,high):
-  
-  
-    #write your code here
+    i = low
+    j = high - 1
+    pivot = arr[high]
+    while i < j:
+        while i < high and arr[i] < pivot:
+            i += 1
+        while j > low and arr[j] > pivot:
+            j -= 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+    if arr[i] > pivot:
+        arr[i], arr[high] = arr[high], arr[i]
+    return i
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
-    #write your code here
+    if high > low:
+        partition_pos = partition(arr, low, high)
+        quickSort(arr, low, partition_pos-1)
+        quickSort(arr, partition_pos+1, high)      
+
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,13 +1,41 @@
 # Python program for implementation of MergeSort 
+# time complexity: O(nlogn) 
+# Space complexity: O(n) 
 def mergeSort(arr):
-  
-  #write your code here
-  
+    if len(arr) > 1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
+
+        mergeSort(left_arr)
+        mergeSort(right_arr)
+
+        i = j = k = 0
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+        
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+        
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+
 # Code to print the list 
 def printList(arr): 
+    for i in arr:
+        print(i)
     
-    #write your code here
-  
+
 # driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
