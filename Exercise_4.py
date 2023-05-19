aaf8c55acaff015d0c4eb7e8610c43fa008fd091
SuvidAnand41292 <suvid41292@gmail.com>
2023-05-18 20:48:50 -0700

    Completed Precourse2 - Please Review

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,51 @@
+def merge(left_arr, right_arr):
+  merged = []
+  i = 0
+  j = 0
+
+  while i < len(left_arr) and j < len(right_arr):
+    if left_arr[i] < right_arr[j]:
+      merged.append(left_arr[i])
+      i += 1
+    else:
+      merged.append(right_arr[j])
+      j += 1
+
+  while i < len(left_arr):
+    merged.append(left_arr[i])
+    i += 1
+
+  while j < len(right_arr):
+    merged.append(right_arr[j])
+    j += 1
+
+  return merged
+
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
+  if len(arr) <= 1:
+    return arr
   
   #write your code here
-  
+  l = 0
+  r = len(arr)-1
+  mid = (l+r)//2
+  left_arr = mergeSort(arr[:mid])
+  right_arr = mergeSort(arr[mid:])
+
+  return merge(left_arr, right_arr)
+
 # Code to print the list 
 def printList(arr): 
-    
+
     #write your code here
+    print(arr)
   
 # driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end="\n")  
     printList(arr) 
-    mergeSort(arr) 
+    arr = mergeSort(arr) 
     print("Sorted array is: ", end="\n") 
     printList(arr) 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,46 @@
 # Python program for implementation of Quicksort
+# Time Complexity: O(nlogn)
+# Space Complexity: O(n)
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
   #write your code here
+  pivot = arr[l]
+  i = l + 1
+  j = h
+  while True:
+      while i <= j and arr[i] <= pivot:
+          i += 1
+      while i <= j and arr[j] > pivot:
+          j -= 1
+      if i <= j:
+          arr[i], arr[j] = arr[j], arr[i]
+      else:
+          break
+  arr[l], arr[j] = arr[j], arr[l]
+  return j
 
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  stack = []
+  stack.append((0, len(arr) - 1))  # Initial range to sort
+
+  while stack:
+      l, h = stack.pop()
+      pivot_idx = partition(arr, l, h)
+
+      if pivot_idx - 1 > l:
+          stack.append((l, pivot_idx - 1))
+      if pivot_idx + 1 < h:
+          stack.append((pivot_idx + 1, h))
+
+
+# Driver code to test above 
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
 
