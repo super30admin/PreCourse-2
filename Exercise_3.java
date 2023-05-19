980a06f11b99ca5f922f62499214cf764af5903e
Rishabh <rishabhpandey@Rishabhs-Macbook.local>
2023-05-18 23:23:10 -0700

    Completed sprint for PreCourse2

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,53 +1,79 @@
-class LinkedList 
-{ 
-    Node head; // head of linked list 
-  
-    /* Linked list node */
-    class Node 
-    { 
-        int data; 
-        Node next; 
-        Node(int d) 
-        { 
-            data = d; 
-            next = null; 
-        } 
-    } 
-  
-    /* Function to print middle of linked list */
-   //Complete this function
-    void printMiddle() 
-    { 
-        //Write your code here
-	//Implement using Fast and slow pointers
-    } 
-  
-    public void push(int new_data) 
-    { 
-        Node new_node = new Node(new_data); 
-        new_node.next = head; 
-        head = new_node; 
-    } 
+//Time Complexity for Push operations is O(1). While print operations have time Complexity of O(n)
+// Space Complexity is O(n)
 
-    public void printList() 
-    { 
-        Node tnode = head; 
-        while (tnode != null) 
-        { 
-            System.out.print(tnode.data+"->"); 
-            tnode = tnode.next; 
-        } 
-        System.out.println("NULL"); 
-    } 
-  
-    public static void main(String [] args) 
-    { 
-        LinkedList llist = new LinkedList(); 
-        for (int i=15; i>0; --i) 
-        { 
-            llist.push(i); 
-            llist.printList(); 
-            llist.printMiddle(); 
-        } 
-    } 
-} 
+class LinkedList {
+    Node head; // head of linked list
+
+    /* Linked list node */
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    /* Function to print middle of linked list */
+    // Complete this function
+    void printMiddle() {
+        // Write your code here
+        // Implement using Fast and slow pointers
+        // Assuming the second node will be the middle node if the input is of even
+        // length(as given on leet code)
+        Node slowPtr = head;
+        Node fastPtr = head;
+
+        if (head != null) {
+            while (fastPtr != null && fastPtr.next != null) {
+                fastPtr = fastPtr.next.next;
+                slowPtr = slowPtr.next;
+            }
+            System.out.println("Middle element: " + slowPtr.data);
+        }
+
+        // // Assuming that first element is the middle one for even length input
+        // Node slowPtr = head;
+        // Node fastPtr = head;
+        // Node prevSlowPtr = null;
+
+        // if (head != null) {
+        // while (fastPtr != null && fastPtr.next != null) {
+        // fastPtr = fastPtr.next.next;
+        // prevSlowPtr = slowPtr;
+        // slowPtr = slowPtr.next;
+        // }
+        // if (fastPtr == null) {
+        // System.out.println("Middle element: " + prevSlowPtr.data);
+        // } else {
+        // System.out.println("Middle element: " + slowPtr.data);
+        // }
+        // }
+
+    }
+
+    public void push(int new_data) {
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node;
+    }
+
+    public void printList() {
+        Node tnode = head;
+        while (tnode != null) {
+            System.out.print(tnode.data + "->");
+            tnode = tnode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        LinkedList llist = new LinkedList();
+        for (int i = 15; i > 0; --i) {
+            llist.push(i);
+            llist.printList();
+            llist.printMiddle();
+        }
+    }
+}
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,75 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
-    /* This function is same in both iterative and 
-       recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
-        //Compare elements and swap.
-    } 
-  
-    // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
-        //Try using Stack Data Structure to remove recursion.
-    } 
-  
-    // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
-    // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
-} 
+
+//Time Complexity - Worst Case: O(n^2), Best Case and Average Case: O(n log n) 
+//Space Complexity - In the best and average cases, where the partitioning is balanced, the stack size is O(log n). However in best case it would be O(n)
+import java.util.Stack;
+
+class IterativeQuickSort {
+    void swap(int arr[], int i, int j) {
+        // Try swapping without extra variable
+        if (i != j) {
+            arr[i] = arr[i] + arr[j];
+            arr[j] = arr[i] - arr[j];
+            arr[i] = arr[i] - arr[j];
+        }
+    }
+
+    /*
+     * This function is same in both iterative and
+     * recursive
+     */
+    int partition(int arr[], int l, int h) {
+        // Compare elements and swap.
+        int pivot = arr[h];
+        int i = (l - 1);
+
+        for (int j = l; j <= h - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, h);
+        return (i + 1);
+    }
+
+    // Sorts arr[l..h] using iterative QuickSort
+    void QuickSort(int arr[], int l, int h) {
+        // Try using Stack Data Structure to remove recursion.
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(l);
+        stack.push(h);
+
+        while (!stack.isEmpty()) {
+            h = stack.pop();
+            l = stack.pop();
+
+            int p = partition(arr, l, h);
+
+            if (p - 1 > l) {
+                stack.push(l);
+                stack.push(p - 1);
+            }
+
+            if (p + 1 < h) {
+                stack.push(p + 1);
+                stack.push(h);
+            }
+        }
+    }
+
+    // A utility function to print contents of arr
+    void printArr(int arr[], int n) {
+        int i;
+        for (i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
+    // Driver code to test above
+    public static void main(String args[]) {
+        IterativeQuickSort ob = new IterativeQuickSort();
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 };
+        ob.QuickSort(arr, 0, arr.length - 1);
+        ob.printArr(arr, arr.length);
+    }
+}
