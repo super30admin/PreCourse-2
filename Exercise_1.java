3206a494717bb68784949078e4b037f6070b4da6
apte-ame <apte.ame@northeastern.edu>
2023-05-19 21:37:15 -0400

    Completed Sprint Precourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,31 @@
+// Time Complexity : O(logN)  as N/2^k = 1
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : ran it successfully on local machine
+// Any problem you faced while coding this : Had to read about the steps of the algo and also about recursion
+
+/*Divide the array around middle element and check whether the element to be searched is the middle element or is in left
+or is in the right array and then repeat this recursively till the array cannot be divided further*/ 
+// Your code here along with comments explaining your approach
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
+        if(l>r){
+            return -1;
+        }
         //Write your code here
+        int mid = (l + r)/2;
+        if(x==arr[mid]){
+            return mid;
+        }else if(x<arr[mid]){
+            return binarySearch(arr, l, mid-1, x);
+        }else if(x>arr[mid]){
+            return binarySearch(arr, mid+1, r, x);
+        }else{
+            return -1;
+        }
+
     } 
   
     // Driver method to test above 
@@ -11,7 +34,7 @@ class BinarySearch {
         BinarySearch ob = new BinarySearch(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
-        int x = 10; 
+        int x = 4; 
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,7 +1,29 @@
+// Time Complexity : O(N^2)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : ran it successfully on local machine
+/* Any problem you faced while coding this : Had to read about the steps of the algo and also about recursion and 
+found it bit uncomfortable to calculate the time and space complexity of any algo*/
+
+/*
+-- Choose the last element of the array as the pivot. Now start traversing the array from the start.
+-- As soon as we encounter element which is greater than the pivot assign it as the leftpointer.
+-- Then continue traversing the array till we get an element smaller than the pivot. Now swap this smaller element with the leftpointer.
+-- Now increment the leftpointer by one and again continue traversing the array and repeat this swapping process whenever we encounter an element smaller than the pivot element.
+-- Once we reach the second last element swap the pivot element with the leftpointer.
+-- Repeat all the above steps for elements on the left and right of the leftpointer iteratively using stack and repeat this till we further cannot partition the array.
+-- To swap without making use of extra variable used exor operator
+*/ 
+
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
 	//Try swapping without extra variable 
+        if(i!=j){  // as when we perform exor on same values we get 0 which hampers our output
+            arr[i] = arr[i] ^ arr[j];
+            arr[j] = arr[i] ^ arr[j];
+            arr[i] = arr[i] ^ arr[j];
+        }
+
     } 
   
     /* This function is same in both iterative and 
@@ -9,12 +31,43 @@ class IterativeQuickSort {
     int partition(int arr[], int l, int h) 
     { 
         //Compare elements and swap.
+        int leftPointer = l;
+        for(int i=l;i<h;i++){
+            if(arr[i]<=arr[h]){
+                swap(arr,i,leftPointer);
+                leftPointer++;
+            }    
+        }
+        swap(arr, leftPointer, h);
+        return leftPointer;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+        int size = h-l+1;
+        int[] myStack = new int[size];
+        int top = -1;
+        myStack[++top] = l;
+        myStack[++top] = h;
+        while(top>=0){
+            h = myStack[top--];
+            l = myStack[top--];
+
+            int partIndex = partition(arr, l, h);
+
+            if(partIndex-1 > l){
+                myStack[++top] = l;
+                myStack[++top] = partIndex - 1;
+            }
+
+            if(partIndex+1 < h){
+                myStack[++top] = partIndex + 1;
+                myStack[++top] = h;
+            }
+        }
+
     } 
   
     // A utility function to print contents of arr 
