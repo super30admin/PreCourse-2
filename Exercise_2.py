7bde024c2e1afb62da9808349a1cfe5c918ec69d
puneet khanna <puneetkhanna@puneetmacbook.attlocal.net>
2023-05-20 14:02:35 -0700

    Completed PreCourse-2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,32 @@
+# Time Complexity: O(n logn)
+# Space Complexity: O(logn)
+# Did this code successfully run on Leetcode: Yes
+# Any problem you faced while coding this: No
+
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
+def quickSort(arr,low,high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quickSort(arr, low, pi - 1)
+        quickSort(arr, pi + 1, high)
+        
 # Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr,0,n-1)
+print ("Sorted array is:")
+for i in range(n):
+    print ("%d" %arr[i])
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,27 @@
+# Time Complexity: O(n logn)
+# Space Complexity: O(logn)
+# Did this code successfully run on Leetcode: Yes
+# Any problem you faced while coding this: No
+
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
-
+    pivot = arr[h]
+    i = l - 1
+    for j in range(l, h):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    return i + 1
 
 def quickSortIterative(arr, l, h):
-  #write your code here
-
+    stack = [(l, h)]
+    while stack:
+        l, h = stack.pop()
+        pivot_idx = partition(arr, l, h)
+        if pivot_idx - 1 > l:
+            stack.append((l, pivot_idx - 1))
+        if pivot_idx + 1 < h:
+            stack.append((pivot_idx + 1, h))
