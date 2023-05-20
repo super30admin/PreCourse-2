c70f89cafcb50f5209838b4afebc1e5113cee99a
skapoor2024 <s.kapoor@ufl.edu>
2023-05-20 03:09:55 -0400

    Completed Precourse-2

--- Exercise_4.cpp
+++ Exercise_4.cpp
@@ -1,21 +1,18 @@
-#include<stdlib.h> 
-#include<stdio.h> 
-  
-// Merges two subarrays of arr[]. 
-// First subarray is arr[l..m] 
-// Second subarray is arr[m+1..r] 
-void merge(int arr[], int l, int m, int r) 
-{ 
-    //Your code here
-} 
-  
-/* l is for left index and r is right index of the 
-   sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r) 
-{ 
-    //Your code here
-} 
-  
+
+/*
+
+Time Complexity : O(NlogN)
+log N for splitting and O(N) for merging sub arrays
+Space Complexity : O(N)
+Creating new array to temporary store the array values 
+and replace existing values in the main array with sorted elements.
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
 /* UTILITY FUNCTIONS */
 /* Function to print an array */
 void printArray(int A[], int size) 
@@ -25,6 +22,80 @@ void printArray(int A[], int size)
         printf("%d ", A[i]); 
     printf("\n"); 
 } 
+
+// Merges two subarrays of arr[]. 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
+void merge(int arr[], int l, int m, int r) 
+{ 
+    //Your code here
+
+    const int first_sub_len {m-l+1};
+    const int sec_sub_len{r-m};
+
+    int* first_sub = new int[first_sub_len];
+    int* sec_sub = new int[sec_sub_len];
+
+    for(int i{0};i<first_sub_len;++i)
+        first_sub[i] = arr[l+i];
+    
+    // printArray(first_sub,first_sub_len);
+    // cout<<endl;
+
+    
+    for(int i{0};i<sec_sub_len;++i)
+        sec_sub[i] = arr[m+1+i];
+
+    // printArray(sec_sub,sec_sub_len);
+    // cout<<endl;
+    
+    int first_sub_id{0},sec_sub_id{0},main_sub_id{l};
+    while(first_sub_id<first_sub_len && sec_sub_id<sec_sub_len)
+    {
+        if(first_sub[first_sub_id]>=sec_sub[sec_sub_id])
+        {
+            arr[main_sub_id++] = sec_sub[sec_sub_id++];
+        }
+        else if(first_sub[first_sub_id]<sec_sub[sec_sub_id])
+        {
+            arr[main_sub_id++] = first_sub[first_sub_id++];
+        }
+        else
+        {
+            arr[main_sub_id++] = first_sub[first_sub_id++];
+            arr[main_sub_id++] = sec_sub[sec_sub_id++];
+
+        }
+    }
+
+    while(first_sub_id<first_sub_len)
+        arr[main_sub_id++] = first_sub[first_sub_id++];
+
+    while(sec_sub_id<sec_sub_len)
+        arr[main_sub_id++] = sec_sub[sec_sub_id++];
+
+    // for(int i{l};i<=r;++i)
+    //     cout<<arr[i]<<" ";
+    // cout<<endl;
+
+    delete[] first_sub;
+    delete[] sec_sub;
+} 
+  
+/* l is for left index and r is right index of the 
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r) 
+{ 
+    //Your code here
+    if (l>=r)
+        return;
+    int m{(l+r)/2};
+    mergeSort(arr,l,m);
+    mergeSort(arr,m+1,r);
+    merge(arr,l,m,r);
+}
+  
+
   
 /* Driver program to test above functions */
 int main() 
