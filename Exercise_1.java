61cbf808a479b1395f8770d80b11a44d1807cd00
rohitkannaujiya7 <rohitkanojia070501@gmail.com>
2023-05-20 19:44:56 -0700

    Completed Sprint Precourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,21 @@
+// TC: O(log n)
+// SC: O(1)
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        int mid;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            if (arr[mid] == x)
+                return mid;
+            if (arr[mid] < x)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return -1;
     } 
   
     // Driver method to test above 
