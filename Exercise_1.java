a6f26b38f3c8f4ea294bce7bf027d960244cb048
kulkarnisneha <sneha.kulkarni@verizonconnect.com>
2023-05-22 16:55:56 -0400

    completed Precourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,18 +1,52 @@
-class BinarySearch { 
+// Time Complexity :
+    //best case: O(1), if the element is found at the middle of the list
+    //worst case and avg case: O(log(n)), since we would be dividing the list in half in each iteration
+// Space Complexity : O(n) where n is number of elements and we will need an array of length n
+// Did this code successfully run on Leetcode :N/A
+// Any problem you faced while coding this :
+//Assumptions: assumed that the input array is always sorted
+class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
+    {
+        int index = -1;
+        int mid = -1;
+
+        //input validations
+        if(arr.length == 0 || l < 0 || r <0)
+        {
+            return index;
+        }
+
+        while(r >= l)
+        {
+            mid = (l+r)/2; //this should return the floor of the avg in case there are even no. of elements
+            if(arr[mid] == x)
+            {
+                return mid;
+            }
+            else if(x > arr[mid])
+            {
+                //search the right/higher half of the list
+                l = mid +1;
+            }
+            else
+            {
+                //search the left/lower half of the list
+                r = mid -1;
+            }
+        }
+        return index;
     } 
   
     // Driver method to test above 
     public static void main(String args[]) 
     { 
-        BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
         int n = arr.length; 
-        int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
+        int x = 10;
+        int result = ob.binarySearch(arr, 0, n - 1, x);
         if (result == -1) 
             System.out.println("Element not present"); 
         else
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,19 +1,73 @@
-class MergeSort 
+
+// Time Complexity :
+    //O(nlog(n)): for a given list of n elements, the list always gets split into halves until we have one element in each sub-list; thus we need to split log(n) no. of times
+//merging happens in O(n) time.
+// Space Complexity ://O(n): as we need extra arrays to store the sub-lists
+// Did this code successfully run on Leetcode : N/A
+// Any problem you faced while coding this :
+class MergeSort
 { 
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+
+        int len1 = m-l+1;
+        int len2 = r-m;
+
+        int[] leftArray = new int[len1];
+        int[] rightArray = new int[len2];
+
+        //populate the two temp arrays
+        for(int i=0; i<len1; i++)
+        {
+            leftArray[i] = arr[l+i];
+        }
+
+        for(int j=0; j<len2; j++)
+        {
+            rightArray[j] = arr[m+1+j];
+        }
+        //merge the two temp arrays
+        int k =l, i=0, j=0;
+        while(i<len1 && j<len2)
+        {
+            if(leftArray[i] <= rightArray[j])
+            {
+                arr[k++] = leftArray[i++];
+            }
+            else
+            {
+               arr[k++] = rightArray[j++];
+            }
+        }
+
+        //if the subArrays are not of the equal length there will be elements left over in one of the lists that need
+        //to be appended to our sortedArray
+        for(; i<len1; i++)
+        {
+            arr[k++] = leftArray[i];
+        }
+
+        for (;j<len2; j++)
+        {
+            arr[k++] = rightArray[j];
+        }
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
-    { 
-	//Write your code here
-        //Call mergeSort from here 
+    {
+        int m;
+        if(l<r)
+        {
+            m = (l+r)/2;
+            sort(arr, l, m);
+            sort(arr, m+1, r);
+            merge(arr, l, m, r);
+        }
     } 
   
     /* A utility function to print array of size n */
@@ -28,7 +82,7 @@ class MergeSort
     // Driver method 
     public static void main(String args[]) 
     { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
+        int arr[] = {12, 11, 13, 5, 6, 7};
   
         System.out.println("Given Array"); 
         printArray(arr); 
