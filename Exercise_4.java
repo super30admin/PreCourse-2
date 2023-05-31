6bc0922b4cbfbe4747811a7c6a9f0b1f18897a4c
indrajeetkumar <officio.indrajeet@gmail.com>
2023-05-31 16:54:52 +0530

    Completed Sprint Precourse2

--- Exercise_4.java
+++ Exercise_4.java
@@ -6,6 +6,45 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+       int n1 = (m-l+1);
+       int n2 = (r-m);
+       
+       // setting up auxiliary arrays    
+       int[] left = new int[n1];
+       int[] right = new int[n2];
+
+       for(int i=0;i<n1;i++) {
+           left[i] = arr[l+i];
+       }
+       for(int j=0;j<n2;j++) {
+           right[j] = arr[m+1+j];
+       }
+
+       int i=0 , j=0 , k=l;
+
+       while(i < n1 && j < n2) {
+            if(left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+                k++;
+            }
+            else {
+                arr[k] = right[j];
+                j++;
+                k++;
+            }
+       }
+       while(i < n1) {
+           arr[k] = left[i];
+           i++;
+           k++;
+       }
+       while(j < n2) {
+           arr[k] = right[j];
+           j++;
+           k++;
+       }
+
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +53,12 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+        if(l < r) {
+            int mid = l + (r-l)/2;
+            sort(arr, l, mid);
+            sort(arr, mid+1, r);
+            merge(arr, l, mid, r);
+        }
     } 
   
     /* A utility function to print array of size n */
