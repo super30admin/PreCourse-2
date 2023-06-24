f97ae2c6d549af5373c5e7164052984ed788d1d7
N Priyanka Gowda <priyanka1999gowda@gmail.com>
2023-06-24 07:30:05 -0700

    completed exercise 4

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,39 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        mergeSort(left_half)
+        mergeSort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
   #write your code here
   
 # Code to print the list 
 def printList(arr): 
-    
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
     #write your code here
   
 # driver code to test the above code 
