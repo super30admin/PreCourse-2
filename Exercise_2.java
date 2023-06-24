7d22d928e3c8c0452d39ad54d82f460a1399e51d
Bency2712 <bencythomas6645@gmail.com>
2023-06-24 16:13:47 -0400

    Completed Sprint PreCourse2

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,9 @@
+// Time Complexity : O(NlogN) 
+// Space Complexity : O(1) 
+// Did this code successfully run on Leetcode : Yes 
+// Any problem you faced while coding this:  No
+// Your code here along with comments explaining your approach: We consider the first element of the array as pivot, the first index as low, and the last as high. Then we increment low until an element greater than pivot is found and we decrement high until an element lesser than pivot is found. Once that is done, we swap those elements. We keep doing this until the low becomes greater than the high. We keep all the elements lesser than the pivot, to its left side, and all the elements greater than it to its right. Hence we have two function calls for sort. The index where the pivot is placed becomes the partition.
+
 class QuickSort 
 { 
     /* This function takes last element as pivot, 
@@ -8,20 +14,52 @@ class QuickSort
        of pivot */
     void swap(int arr[],int i,int j){
         //Your code here   
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
     
     int partition(int arr[], int low, int high) 
     { 
    	//Write code here for Partition and Swap 
+        int i=low, j=high, pivot=arr[low];
+
+        while(i<j)
+        {
+            while(arr[i]<=pivot && i<=high-1)
+            {
+                i++;
+            }
+            while(arr[j]>pivot && j>=low+1)
+            {
+                j--;
+            }
+
+            if(i<j)
+            {
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,low,j);
+            return j;
+
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
       low  --> Starting index, 
       high  --> Ending index */
+
+
     void sort(int arr[], int low, int high) 
     {  
             // Recursively sort elements before 
             // partition and after partition 
+            if (low<high)
+            {
+                int pindex=partition(arr, low, high);
+                sort(arr,low,pindex-1);
+                sort(arr,pindex+1,high);
+            }
     } 
   
     /* A utility function to print array of size n */
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,7 +1,20 @@
+// Time Complexity : O(N^2) 
+// Space Complexity : O(logN) 
+// Did this code successfully run on Leetcode : Yes 
+// Any problem you faced while coding this:  No
+// Your code here along with comments explaining your approach: In this method we use stack for implementing the recursive approach. We push the low and high from array into the stack and the pop the indices. Then, we perform the partition and push new subarray indices onto the stack until it's empty.
+
+import java.util.Stack;
+
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
 	//Try swapping without extra variable 
+    if (i != j) {
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+    }
     } 
   
     /* This function is same in both iterative and 
@@ -9,12 +22,49 @@ class IterativeQuickSort {
     int partition(int arr[], int l, int h) 
     { 
         //Compare elements and swap.
+        int pivot=arr[l];
+        int i=l;
+        for(int j=l+1;j<=h;j++)
+        {
+            if(arr[j]<pivot)
+            {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, l, i);
+        return i;
+
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+        Stack<Integer> stack=new Stack<>();
+        stack.push(l);
+        stack.push(h);
+
+        while(!stack.isEmpty())
+        {
+             h=stack.pop();
+             l=stack.pop();
+             
+        int pindex=partition(arr, l, h);
+
+        if (pindex-1>l)
+        {
+        stack.push(l);
+        stack.push(pindex-1);
+        }
+        
+        if (pindex-1>l)
+        {
+        stack.push(h);
+        stack.push(pindex+1);
+        }
+    }
+        
     } 
   
     // A utility function to print contents of arr 
