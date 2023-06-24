9ec603daa17ba496c39d29ee7f134d86201c4315
Sarat Kumar Kaniti <sarat@Sarats-Laptop.attlocal.net>
2023-06-24 16:06:09 -0700

    Completed Precourse-2. Please review

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,20 +1,61 @@
+// Time Complexity : nlog(n) 
+// Space Complexity : O(log(n)) - stack space is used to mimic the recursion. It is the height of the recursion stack.
+// Did this code successfully run on Leetcode : no, this algorithm fails for large inputs.
+// Any problem you faced while coding this : no
+
+import java.util.Stack;
+
 class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
+    void swap(int arr[], int i, int j) { 
+	    int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     } 
   
     /* This function is same in both iterative and 
        recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
-        //Compare elements and swap.
+    int partition(int arr[], int l, int h) { 
+        int i = l, j = h, pivot = arr[l];
+        while(i<j){
+            while(arr[i] <= pivot && i<h){
+                i++;
+            }
+
+            while(arr[j] > pivot && j>l){
+                j--;
+            }
+
+            if(i<j){
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, l, j);
+        return j;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
-        //Try using Stack Data Structure to remove recursion.
+    void QuickSort(int arr[], int lo, int hi) { 
+        Stack<Integer> stack = new Stack<>();
+        stack.push(lo);
+        stack.push(hi);
+
+        while(!stack.isEmpty()){
+             int h = stack.pop();
+             int l = stack.pop();
+
+            int pIndex = partition(arr, l, h);
+
+            if(pIndex - 1 > l){
+                stack.push(l);
+                stack.push(pIndex-1);
+            }
+
+            if(pIndex+1 < h){
+                stack.push(pIndex+1);
+                stack.push(h);
+            }
+        }
     } 
   
     // A utility function to print contents of arr 
