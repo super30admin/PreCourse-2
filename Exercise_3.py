56cc23ddcc3437c0ad1c4957913f9f099100925b
NisargaVenkatesh <venkatesh.ni@northeastern.edu>
2023-06-25 11:17:17 -0400

    Exercise for review

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,40 @@
-# Node class  
+# Node class 
+#time complexity O(N)
+#space complexity O(N) 
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head = None
         
   
     def push(self, new_data): 
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        if self.head is None:
+            return
+
+        slow_ptr = self.head
+        fast_ptr = self.head
+
+        while fast_ptr is not None and fast_ptr.next is not None:
+            fast_ptr = fast_ptr.next.next
+            slow_ptr = slow_ptr.next
+
+        print("The middle element is:", slow_ptr.data)
+
 
 # Driver code 
 list1 = LinkedList() 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,59 @@
 # Python program for implementation of Quicksort
+#time complexity O(N log N)
+#space complexity O(log N) 
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+  pivot = arr[h]
+  i = l - 1
+
+  for j in range(l, h):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+  arr[i + 1], arr[h] = arr[h], arr[i + 1]
+  return i + 1
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+ size = h - l + 1
+ stack = [0] * size
+ top = -1
+
+ top += 1
+ stack[top] = l
+ top += 1
+ stack[top] = h
+
+ while top >= 0:
+        h = stack[top]
+        top -= 1
+        l = stack[top]
+        top -= 1
+
+        pivot_index = partition(arr, l, h)
+
+        if pivot_index - 1 > l:
+            top += 1
+            stack[top] = l
+            top += 1
+            stack[top] = pivot_index - 1
+
+        if pivot_index + 1 < h:
+            top += 1
+            stack[top] = pivot_index + 1
+            top += 1
+            stack[top] = h
+
+
+# Driver code
+if __name__ == '__main__':
+    arr = [10, 7, 8, 9, 1, 5]
+    n = len(arr)
+    quickSortIterative(arr, 0, n - 1)
+    print("Sorted array is:")
+    for i in range(n - 1):
+        print(arr[i], end=" ")
+    print(arr[n - 1])
 
