5354a96d12b8b993e502babb313e2b2a692b78dd
NisargaVenkatesh <venkatesh.ni@northeastern.edu>
2023-06-25 09:48:12 -0400

    Execrcise 1 and 2 for review

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,34 @@
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
+
+#time complexity O(n log n)
+#space complexity O(log n) 
 def partition(arr,low,high):
   
-  
-    #write your code here
-  
+  i = low - 1  # Index of smaller element
+  pivot = arr[high]  # Pivot element
+
+  for j in range(low, high):
+        #current element is smaller than or equal to the pivot
+        if arr[j] <= pivot:
+            i = i + 1  
+            arr[i], arr[j] = arr[j], arr[i]  
+
+  arr[i + 1], arr[high] = arr[high], arr[i + 1]  
+  return i + 1 
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
-    #write your code here
+    if low < high:
+       
+        partition_index = partition(arr, low, high)
+
+        # Recursive call on the subarrays
+        quickSort(arr, low, partition_index - 1)
+        quickSort(arr, partition_index + 1, high)
+
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
