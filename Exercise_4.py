b2f2f92b22398c4c67926982620459b8674abb55
Vikas Shenoy <p.vikas.shenoy@gmail.com>
2023-06-25 13:22:54 -0700

    Completed Precourse2

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,43 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
   
-  #write your code here
+  if len(arr)>1:
+
+    mid=len(arr)//2
+    right=arr[mid:]
+    left=arr[:mid]
+
+    mergeSort(right)
+    mergeSort(left)
+
+    i=j=k=0 #using i,j,k for right,left,merge array respectively
+
+    while i<len(left) and j<len(right):
+      if left[i]<right[j]:
+        arr[k]=left[i]
+        i+=1
+        k+=1
+      else:
+        arr[k]=right[j]
+        j+=1
+        k+=1
+    
+    #any left over elements in right or left arrays we append them to the main array
+    while i<len(left):
+      arr[k]=left[i]
+      i+=1
+      k+=1
+
+    while j<len(right):
+      arr[k]=right[j]
+      j+=1
+      k+=1
   
 # Code to print the list 
 def printList(arr): 
     
-    #write your code here
+    for i in range(len(arr)):
+      print(arr[i])
   
 # driver code to test the above code 
 if __name__ == '__main__': 
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,62 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+  pivot=arr[l]
+  i=l+1
+  j=h
+
+  while True:
+      while i<=j and arr[i]<=pivot:
+          i+=1
+      while i<=j and arr[j]>=pivot:
+          j-=1
+      if i<=j:
+          arr[i],arr[j]=arr[j],arr[i]
+      else:
+          break
+      
+  arr[l],arr[j]=arr[j],arr[l]
+
+  return j
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    size = h - l + 1
+    stack = [0] * (size)
+    top = -1
+ 
+    top = top + 1
+    stack[top] = l
+    top = top + 1
+    stack[top] = h
 
+    while top >= 0:
+ 
+        # Pop both high and low out
+        h = stack[top]
+        top = top - 1
+        l = stack[top]
+        top = top - 1
+ 
+        p = partition( arr, l, h )
+
+        # left side onto stack
+        if p-1 > l:
+            top = top + 1
+            stack[top] = l
+            top = top + 1
+            stack[top] = p - 1
+
+        # right side onto stack
+        if p+1 < h:
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = h
+
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
