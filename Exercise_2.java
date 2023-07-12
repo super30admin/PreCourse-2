24eafb179f709cfdd271540609fc468c64a916a7
dkaswala <dkaswala@uw.edu>
2023-07-11 22:22:46 -0700

    Completed sprint PreCourse2.

--- Exercise_2.java
+++ Exercise_2.java
@@ -6,13 +6,28 @@ class QuickSort
        smaller (smaller than pivot) to left of 
        pivot and all greater elements to right 
        of pivot */
-    void swap(int arr[],int i,int j){
-        //Your code here   
+    void swap(int arr[],int i,int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
     int partition(int arr[], int low, int high) 
     { 
-   	//Write code here for Partition and Swap 
+        int pivot = arr[high];
+        int i = low - 1;
+        for(int j=low; j<high; j++)
+        {
+            if(arr[j] <= pivot)
+            {
+                i++;
+                swap(arr, i, j);
+            }
+        } 
+        swap(arr, i+1, high);
+ 
+        return i+1;
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
@@ -20,8 +35,15 @@ class QuickSort
       high  --> Ending index */
     void sort(int arr[], int low, int high) 
     {  
+        if(low<high)
+        {
+            int pi = partition(arr, low, high);
             // Recursively sort elements before 
             // partition and after partition 
+            sort(arr, low, pi-1);
+            sort(arr, pi+1, high);
+        }
+        
     } 
   
     /* A utility function to print array of size n */
