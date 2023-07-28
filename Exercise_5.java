3597e98b081ed06544a4d13aba937587c56ffa68
KrishnaChaithvika <krishna.chaithvika@gmail.com>
2023-07-28 16:21:59 -0700

    Pre Course - 2

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,7 +1,19 @@
+// Time Complexity : O(NLogN)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : Yes
+import java.util.Stack;
+
 class IterativeQuickSort { 
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+        //Try swapping without extra variable 
+        int temp  = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        // arr[i] = (arr[i] + arr[j]);
+        // arr[j] = (arr[i] - arr[j]);
+        // arr[i] = (arr[i] - arr[j]);
     } 
   
     /* This function is same in both iterative and 
@@ -9,12 +21,38 @@ class IterativeQuickSort {
     int partition(int arr[], int l, int h) 
     { 
         //Compare elements and swap.
+        int pivotElement = arr[h];
+        int highIndex = l-1;
+        for(int i = l; i < h; i++) {
+            if(arr[i] <= pivotElement) {
+                highIndex++;
+                this.swap(arr, highIndex, i);
+            }
+        }
+        this.swap(arr, highIndex+1, h);
+        return highIndex+1;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+        Stack<Integer> stack = new Stack<>();
+        stack.push(l);
+        stack.push(h);
+        while(!stack.isEmpty()) {
+            h = stack.pop();
+            l = stack.pop();
+            int p = this.partition(arr, l ,h);
+            if(p-1 > l) {
+                stack.push(l);
+                stack.push(p-1);
+            }
+            if(p+1 < h) {
+                stack.push(p+1);
+                stack.push(h);
+            }
+        }
     } 
   
     // A utility function to print contents of arr 
@@ -30,7 +68,10 @@ class IterativeQuickSort {
     { 
         IterativeQuickSort ob = new IterativeQuickSort(); 
         int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
+        System.out.println("Before Recursion: ");
+        ob.printArr(arr, arr.length); 
         ob.QuickSort(arr, 0, arr.length - 1); 
+        System.out.println("\nAfter Recursion: ");
         ob.printArr(arr, arr.length); 
     } 
 } 
