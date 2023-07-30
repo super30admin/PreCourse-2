876d1d1021d482d0fed9da67c47d9c77165d8b46
Shreya Verma <shreyaverma@Shreyas-Air.lan>
2023-07-30 16:22:56 -0400

    Precourse-2 completed

--- Exercise_4.java
+++ Exercise_4.java
@@ -6,6 +6,36 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+       int num1 = m-1+1
+       int num2 = r-m;
+
+       // creating the subarrays
+       int L[] = new int[num1];
+       int R[] = new int[num2];
+
+       //copying the data
+       for (int i=0; i<num1; i++){
+        L[i] = arr[l+i];
+       }
+       for(int j=0; j<num2; j++){
+        R[j] = arr[j+m+1];
+       }
+
+       // initial index 
+       int k = l;
+
+       //comparing the values of both the sub arrays
+       while(i<num1 && j <num2){
+        if(L[i]<R[j]){
+            arr[k] = L[i];
+            i++;
+        }
+        else{
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+       }
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +44,17 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+        if(l<r){
+            // mid point
+            int m = l + (r - l) / 2;
+
+            // sorting
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            //merging 
+            merge(arr, l, m, r);
+        }
     } 
   
     /* A utility function to print array of size n */
