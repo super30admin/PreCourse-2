39b8197fbf456be07df7ff6ba343bb13978335d1
diveshgadhvi5 <divesh.gadhvi@rutgers.edu>
2023-07-31 19:17:52 -0500

    Completed Precourse2 - Divesh Gadhvi

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,29 @@
+# Time Complexity : O(nlogn) for average/best case. O(n*n) for worst case
+# Space Complexity : O(logn) for average/best case. O(n) for worst case
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
+    pivot = arr[high] #Selecting the last element of the array as the pivot element
+    i = low-1 
+    for j in range(low,high): #Traversing through all the elements
+        if arr[j] <= pivot: #Comparing the elements with the pivot
+            i += 1 #If the element is less than or equal to pivot, swap it with the element at i+1 index
+            (arr[i], arr[j]) = (arr[j], arr[i])
+    
+    (arr[i+1], arr[high]) = (arr[high], arr[i+1])  #Swap the pivot element to its correct sorted position
+    return i+1 #return the pivot index
+
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
-    #write your code here
+    if low<high:
+        pivot_index = partition(arr, low, high) #Partition the array and get the pivot index
+
+        #Recursively sort the sub-arrays on the left and right of the pivot
+        quickSort(arr, low, pivot_index-1) 
+        quickSort(arr, pivot_index+1, high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
@@ -19,5 +32,3 @@ quickSort(arr,0,n-1)
 print ("Sorted array is:") 
 for i in range(n): 
     print ("%d" %arr[i]), 
-  
- 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,27 @@
+# Time Complexity : average and worst-case time complexity is O(n log n).
+# Space Complexity : O(log n) on average and O(n) in the worst case due to the stack space used for simulating the iterative process. (In place sorting take O(1) space complexity)
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+  pivot = arr[h] #Selecting the last element of the array as the pivot element
+  i = l-1 
+  for j in range(l,h): #Traversing through all the elements
+      if arr[j] <= pivot: #Comparing the elements with the pivot
+          i += 1 #If the element is less than or equal to pivot, swap it with the element at i+1 index
+          (arr[i], arr[j]) = (arr[j], arr[i])
+    
+  (arr[i+1], arr[h]) = (arr[h], arr[i+1])  #Swap the pivot element to its correct sorted position
+  return i+1 #return the pivot index
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+  stack = [(l,h)] #Create an auxiliary stack
 
+  while stack:
+    l,h = stack.pop()
+    if l<h:
+      pivot_index = partition(arr, l, h)
+      #Push the sub-arrays to the stack for further sorting
+      stack.append((l, pivot_index - 1))
+      stack.append((pivot_index + 1, h))
