0636314d3fd22ef71b1d4125535bb06806972eaf
UmangThakur15 <umangthakuremail@gmail.com>
2023-08-27 16:14:38 -0400

    Exercise 3 done

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,19 +2,36 @@
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
-        
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+
+   #Singly Linked List
 class LinkedList: 
   
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
+    def __init__(self):
+        self.head=None
+    def push(self, new_data): #to create linked list
+        node=Node(new_data)
+        node.next=self.head
+        self.head=node
         
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):  #Time complexity is O(n)
+        count =0
+        mid_node=self.head
+
+        while self.head is not None:
+            if count%2 !=0:
+                mid_node=mid_node.next
+            self.head=self.head.next
+            count+=1
+
+        if mid_node is not None:
+            print("The middle element is:", mid_node.data)
+
 
 # Driver code 
 list1 = LinkedList() 
@@ -22,5 +39,7 @@ list1.push(5)
 list1.push(4) 
 list1.push(2) 
 list1.push(3) 
-list1.push(1) 
+list1.push(1)
+list1.push(11)
+list1.push(21)
 list1.printMiddle() 
