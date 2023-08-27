df45977bc765fb085f4e8337b5b10ca045f75395
UmangThakur15 <umangthakuremail@gmail.com>
2023-08-27 17:45:28 -0400

    Exercise 2 done

--- Exercise_2.py
+++ Exercise_2.py
@@ -27,8 +27,6 @@ def quickSort(arr,low,high):
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
 quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i])
+print ("Sorted array is:",arr)
   
  
