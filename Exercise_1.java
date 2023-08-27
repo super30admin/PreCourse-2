23baa66e9e9d20f8642995416e7081f22c3cf4da
jaissica <jaissica.hora@gmail.com>
2023-08-27 16:27:52 -0400

    Exercise 1 Without complexity

--- Exercise_1.java
+++ Exercise_1.java
@@ -2,7 +2,22 @@ class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+        
+        while(l<=r)
+        {
+           int mid = (l+r)/2;
+           if (arr[mid]>x)
+           {
+             r = mid -1;   
+           }
+           else if (arr[mid]<x){
+             l = mid +1;
+           }
+           else {
+            return mid;
+           }
+        }
+        return -1;
     } 
   
     // Driver method to test above 
@@ -12,7 +27,7 @@ class BinarySearch {
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
         int x = 10; 
-        int result = ob.binarySearch(arr, 0, n - 1, x); 
+        int result = ob.binarySearch(arr, 0, n - 1, 40); 
         if (result == -1) 
             System.out.println("Element not present"); 
         else
