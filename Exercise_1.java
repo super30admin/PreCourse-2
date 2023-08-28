8c0f68cd5a0fff80124db6de92c6d5cddd7fe035
vinith bandoju <vinith007.bandoju.com>
2023-08-27 22:02:46 -0700

    Completed precourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -20,9 +20,9 @@ class BinarySearch {
     public static void main(String args[]) 
     { 
         BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 ,50, 70};
+        int arr[] = { 2, 3, 4, 10, 40};
         int n = arr.length; 
-        int x = 3;
+        int x = 10;
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
