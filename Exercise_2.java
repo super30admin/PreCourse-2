06f55bfc56e5b3c3835e789f4e4bb4a508894966
jaissica <jaissica.hora@gmail.com>
2023-08-27 22:57:54 -0400

    Exercise 5 done

--- Exercise_2.java
+++ Exercise_2.java
@@ -16,15 +16,9 @@ class QuickSort
     int partition(int arr[], int low, int high) 
     { 
         int pivot = arr[high];
-       
         int i = (low - 1);
-
         for (int j = low; j <= high - 1; j++) {
-
-          
             if (arr[j] < pivot) {
-
-               
                 i++;
                 swap(arr, i, j);
             }
