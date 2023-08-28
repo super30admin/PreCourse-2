56522cef2173e417fc104bca0774e412f84eda2a
nrajaram99 <99rniharika10@gmail.com>
2023-08-27 20:56:42 -0500

    Completed Precourse-2.

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,34 @@
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None  
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None 
   
     def push(self, new_data): 
-        
+        node_new = Node(new_data)
+        node_new.next = self.head
+        self.head = node_new
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        pointer1 = self.head
+        pointer2 = self.head
+        if self.head is not None:
+            while(pointer2 is not None and pointer2.next is not None):
+                pointer2 = pointer2.next.next
+                pointer1 = pointer1.next
+            print("The middle element is ", pointer1.data) 
 
 # Driver code 
 list1 = LinkedList() 
--- Exercise_4.py
+++ Exercise_4.py
@@ -2,11 +2,41 @@
 def mergeSort(arr):
   
   #write your code here
+  if len(arr) > 1:
+      mid = len(arr) // 2  # Find mid Element
+      left = arr[:mid]
+      right = arr[mid:]
+
+      mergeSort(left)  #Sort the left side
+      mergeSort(right) # Sort the right Side
+
+      i = j = k = 0
+
+      while i < len(left) and j < len(right):
+          if left[i] < right[j]:
+              arr[k] = left[i]
+              i += 1
+          else:
+              arr[k] = right[j]
+              j += 1
+          k += 1
+
+      while i < len(left):
+          arr[k] = left[i]
+          i += 1
+          k += 1
+
+      while j < len(right):
+          arr[k] = right[j]
+          j += 1
+          k += 1
   
 # Code to print the list 
 def printList(arr): 
     
     #write your code here
+    for i in range(len(arr)):
+        print(arr[i])
   
 # driver code to test the above code 
 if __name__ == '__main__': 
