228c24090bed9a60f28ea553b8113cd384d2ca28
anaghayn <anagha2605@gmail.com>
2023-09-02 22:53:12 -0700

    Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -5,12 +5,26 @@ def partition(arr,low,high):
   
   
     #write your code here
+  item = arr[high]
+  i = low - 1 
+
+  for j in range(low, high):
+      if arr[j] < item:
+          i += 1
+          arr[i], arr[j] = arr[j], arr[i] 
+  arr[i + 1], arr[high] = arr[high], arr[i + 1] 
+  return i + 1 
+  
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
     #write your code here
+  if low < high:
+      index = partition(arr, low, high)
+      quickSort(arr, low, index - 1)
+      quickSort(arr, index + 1, high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
