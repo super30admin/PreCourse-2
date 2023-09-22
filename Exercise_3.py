6ab594ef1235f8d2332748d752dc184340f70a9b
nidhisharma0160 <140275441+nidhisharma0160@users.noreply.github.com>
2023-09-22 16:32:50 +0530

    Completed Exercise_3.py please review

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,43 @@
+#time complexity: O(n)
+#space complexity : O(1)
 # Node class  
 class Node:  
-  
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data): 
+      self.value = data
+      self.next = None
         
 class LinkedList: 
-  
-    def __init__(self): 
+    def __init__(self, data):    #Linkedlist constructor
+      new_node = Node(data)
+      self.head = new_node
+      self.tail = new_node
+      self.length = 1
+      
+    def push(self, new_data): #function to push items in linked list
+      new_node = Node(data)
+      if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+      else:
+            self.tail.next = new_node
+            self.tail = new_node
+      self.length += 1
+      return True
         
-  
-    def push(self, new_data): 
-        
-  
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):      
+      mid = int(self.length/2)      #taking index of middle node
+      temp = self.head
+      if self.length%2 == 0:        #if Linkedlist has even nodes then prints two middle numbers
+            for i in range(mid-1):
+                temp = temp.next
+            print(f"Middle is: {temp.value} & {temp.next.value}")
+      else:                         #if Linkedlist has odd nodes then prints one middle number
+            for i in range(mid):
+                temp=temp.next
+            print(f"Middle is: {temp.value}")
 
 # Driver code 
 list1 = LinkedList() 
