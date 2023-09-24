96eb3edb6dd3d28f3c66a0547706e4f407c3db0c
omkar morogiri <omkarmorogiri@omkars-MacBook-Air.local>
2023-09-24 19:39:04 -0400

    Done Precourse 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,47 @@
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
+#Time Complexity:  O(n log n)
+#Space Complexity: O(1)
+
+def partition(arr, low, high):
+    # Choose the rightmost element as the pivot
+    pivot = arr[high]
     
-    #write your code here
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
+    # Initialize the index of the smaller element
+    i = low - 1
+
+    for j in range(low, high):
+        # If the current element is smaller than or equal to the pivot
+        if arr[j] <= pivot:
+            i = i + 1
+            # Swap arr[i] and arr[j]
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    # Swap arr[i+1] and arr[high] (the pivot)
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+
+    # Return the index of the pivot
+    return i + 1
+
+def quickSort(arr, low, high):
+    if low < high:
+        # Find the partition index
+        partition_index = partition(arr, low, high)
+
+        # Recursively sort elements before and after the partition
+        quickSort(arr, low, partition_index - 1)
+        quickSort(arr, partition_index + 1, high)
+
+# Driver code to test the quicksort implementation
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n - 1)
+
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i])
+
   
  
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,40 @@
 # Python program for implementation of Quicksort
-
-# This function is same in both iterative and recursive
+# Time complexity: O(n log n)
+# space complexity: O(1)
 def partition(arr, l, h):
-  #write your code here
+    pivot = arr[h]
+    i = l - 1  # Index of the smaller element
 
+    for j in range(l, h):
+        if arr[j] <= pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    return i + 1
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    stack = [l, h]
+
+    while stack:
+        h = stack.pop()
+        l = stack.pop()
+
+        pivot_index = partition(arr, l, h)
+
+        if pivot_index - 1 > l:
+            stack.append(l)
+            stack.append(pivot_index - 1)
+
+        if pivot_index + 1 < h:
+            stack.append(pivot_index + 1)
+            stack.append(h)
+
+# Driver code to test the quicksort implementation
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    n = len(arr)
+    print("Given array is:", arr)
+    quickSortIterative(arr, 0, n - 1)
+    print("Sorted array is:", arr)
 
