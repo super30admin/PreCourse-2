cf85c8189f0d6e344e5e86899809541da1c19090
apekshakhilari <apeksha.khilari@tamr.com>
2023-09-24 19:42:17 -0700

    Completed PreCourse-2: Please review

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,65 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# Exercise_3 : Find Mid Point of a Singly Linked List.
+# Time Complexity : O(n) as we are appending new node to the end of the list
+# Space Complexity : O(n) as n is length of the list
+# Did this code successfully run on Leetcode : yes, problem 876
+# Any problem you faced while coding this : NA
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+# Node class
+class Node:
+
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        # if head is none, make the incoming data to be node 1
+        if self.head is None:
+            self.head = Node(new_data)
+            return
+        curr = self.head
+        # if there is at least one node, set curr pointer at the start and move
+        # it forward until curr.next is null.
+        # If yes, create new node using incoming data & append it next to wherever curr is pointing
+        while curr.next is not None:
+            curr = curr.next
+        curr.next = Node(new_data)
+        return
+
+    # Function to get the middle of
+    # the linked list
+    def printMiddle(self):
+        # slow & fast ptr start at head (first node)
+        slow = self.head
+        fast = self.head
+
+        # if linked list is empty, return none
+        if self.head is None:
+            return None
+        # slow moves one hop and fast moves two hops at a time
+        while fast and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        print(slow.data)
+        return slow
+
+
+# Driver code
+list1 = LinkedList()
+list1.push(1)
+list1.push(2)
+list1.push(3)
+list1.push(4)
+list1.push(100)
+list1.push(6)
+list1.push(5)
+list1.push(5)
+list1.push(100)
+list1.push(100)
+list1.printMiddle()
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,68 @@
-# Python program for implementation of MergeSort 
+# Python program for implementation of MergeSort
+
+# Time Complexity : O(n log n)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : no
+# Any problem you faced while coding this : Facing problem in understanding how to store the result in the given
+# array instead of creating a new list
+
+
 def mergeSort(arr):
-  
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
-    #write your code here
-  
-# driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+    # if only one element in the array, return the array since its already sorted
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    # split arr in two halves and keep doing it until you get an array of single element (i.e. meaning its sorted)
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
+
+    # pass sorted arrays to merge function to merge them
+    return (merge(left, right))
+
+
+def merge(arr_1, arr_2):
+    sorted_list = []
+    i = j = 0
+
+    # iterate through each element in both the arrays until either of them runs out
+    while i < len(arr_1) and j < len(arr_2):
+        if arr_1[i] <= arr_2[j]:
+            sorted_list.append(arr_1[i])
+            i += 1
+        else:
+            sorted_list.append(arr_2[j])
+            j += 1
+
+    # if array 2 ran out, append whatever outstanding in array 1 as it is to the sorted list
+    while i < len(arr_1):
+        sorted_list.append(arr_1[i])
+        i += 1
+
+    # if array 1 ran out, append whatever outstanding in array 2 as it is to the sorted list
+    while j < len(arr_2):
+        sorted_list.append(arr_2[j])
+        j += 1
+
+    return sorted_list
+
+
+# write your code here
+
+# # Code to print the list
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+
+
+# write your code here
+
+# driver code to test the above code
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7, 3, 100]
+    print("Given array is", end="\n")
+    printList(arr)
+    out = mergeSort(arr)
+    print("\nSorted array is: ", end="\n")
+    # print (out)
+    printList(out)
