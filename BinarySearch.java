7d11524db7cd4e1d78036d5c4723fb83d3331641
Pratik Dhumal <dhumalpratik151@gmail.com>
2023-09-25 17:10:46 -0700

    ex3 pending

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,15 +3,30 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        int mid;
+        while(l<=r){
+            mid = (l+r)/2;
+            if(arr[mid] == x){
+                return mid;
+            }else if(x<arr[mid]){
+                r = mid -1;
+                binarySearch(arr,l,r,x);
+            }else{
+                l = mid + 1;
+                binarySearch(arr,l,r,x);
+            }
+        }
+
+        return -1;
     } 
   
     // Driver method to test above 
     public static void main(String args[]) 
     { 
         BinarySearch ob = new BinarySearch(); 
-        int arr[] = { 2, 3, 4, 10, 40 }; 
+        int arr[] = { 2, 3, 4, 10, 40 };
         int n = arr.length; 
-        int x = 10; 
+        int x = 10;
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
--- Exercise_4.java
+++ Exercise_4.java
@@ -5,7 +5,42 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+       //Your code here
+
+        int tempArr1[] = new int[m+1-l];
+        int tempArr2[] = new int[r-m];
+
+        for (int i = 0; i < m+1-l; i++)
+            tempArr1[i] = arr[l + i];
+        for (int i = 0; i < r-m; i++)
+            tempArr2[i] = arr[m + 1 + i];
+
+        int i=0 ,j = 0;
+        int mainArrPtr = l;
+        while (i < m+1-l && j < r-m) {
+            if (tempArr1[i] <= tempArr2[j]) {
+                arr[mainArrPtr] = tempArr1[i];
+                i++;
+            }
+            else {
+                arr[mainArrPtr] = tempArr2[j];
+                j++;
+            }
+            mainArrPtr++;
+        }
+
+        while (i < m+1-l) {
+            arr[mainArrPtr] = tempArr1[i];
+            i++;
+            mainArrPtr++;
+        }
+
+        while (j < r-m) {
+            arr[mainArrPtr] = tempArr2[j];
+            j++;
+            mainArrPtr++;
+        }
+
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -13,7 +48,15 @@ class MergeSort
     void sort(int arr[], int l, int r) 
     { 
 	//Write your code here
-        //Call mergeSort from here 
+        //Call mergeSort from here
+        if(l<r){
+            int mid = (l+r)/2;
+
+            sort(arr, l, mid);
+            sort(arr, mid+1, r);
+            merge(arr,l,mid,r);
+        }
+
     } 
   
     /* A utility function to print array of size n */
@@ -28,7 +71,7 @@ class MergeSort
     // Driver method 
     public static void main(String args[]) 
     { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
+        int arr[] = {12, 11, 13, 5, 6, 7,22,34,54,0,12,5};
   
         System.out.println("Given Array"); 
         printArray(arr); 
