0d0d77693a8aa08662207a59204074893a687208
Pramit Gupta <gupta.pramit@gmail.com>
2023-09-25 20:41:02 -0400

    e4

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,10 +1,36 @@
 # Python program for implementation of MergeSort 
+#Time: O(nlogn) Space: O(n)
 def mergeSort(arr):
-  
-  #write your code here
-  
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        mergeSort(left)
+        mergeSort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+
 # Code to print the list 
-def printList(arr): 
+def printList(arr):
+   
+   for i in arr:
+      print(i) 
     
     #write your code here
   
