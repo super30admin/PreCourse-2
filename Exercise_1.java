51695f77dfd0b9cf2fec9f715fe79c0ef31a9fc7
sharanvodapally <sharan.vodapally@gmail.com>
2023-09-28 17:46:35 -0500

    Precourse 2

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,9 +1,22 @@
-class BinarySearch { 
+// Time - o(logn)
+// space - 1
+
+class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
-    } 
+    int binarySearch(int arr[], int l, int r, int x) {
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] < x) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
   
     // Driver method to test above 
     public static void main(String args[]) 
