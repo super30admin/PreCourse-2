e4116ea4b75780ae55311360e6053bd0894c4ca7
hansrajpabbati <hansrajhpabbati@gmail.com>
2023-11-01 13:09:05 -0700

    class IterativeQuickSort {

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,74 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
-    /* This function is same in both iterative and 
+class IterativeQuickSort {
+    void swap(int arr[], int i, int j)
+    {
+        //Try swapping without extra variable
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    /* This function is same in both iterative and
        recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
+    int partition(int arr[], int l, int h)
+    {
         //Compare elements and swap.
-    } 
-  
-    // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
+        int pivot = arr[h];
+        int idx = l;
+
+        for(int i = l; i < h; i++){
+            if(arr[i] <= pivot){
+                swap(arr, i, idx);
+                idx++;
+            }
+        }
+        swap(arr, idx, h);
+        return idx;
+    }
+
+    // Sorts arr[l..h] using iterative QuickSort
+    void QuickSort(int arr[], int l, int h)
+    {
         //Try using Stack Data Structure to remove recursion.
-    } 
-  
+        int[] stack = new int[h - l + 1];
+
+        int top = -1;
+
+        stack[++top] = l;
+        stack[++top] = h;
+
+        while (top >= 0) {
+            h = stack[top--];
+            l = stack[top--];
+
+            int pivot = partition(arr, l, h);
+
+            if (pivot - 1 > l) {
+                stack[++top] = l;
+                stack[++top] = pivot - 1;
+            }
+
+            if (pivot + 1 < h) {
+                stack[++top] = pivot + 1;
+                stack[++top] = h;
+            }
+        }
+
+    }
+
     // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
+    void printArr(int arr[], int n)
+    {
+        int i;
+        for (i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
     // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
+    public static void main(String args[])
+    {
+        IterativeQuickSort ob = new IterativeQuickSort();
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 };
+        ob.QuickSort(arr, 0, arr.length - 1);
+        ob.printArr(arr, arr.length);
+    }
 } 
