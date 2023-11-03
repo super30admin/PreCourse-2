da4bbecf0d4d1bbdc5e1ecbf65e55153e061ec86
Rushikesh Dalvi <dalvi.ru@northeastern.edu>
2023-11-03 14:41:07 -0400

    Precourse-2 Completed

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,40 @@
+#Space complexity  and time  complexity  is O(n) 
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
-        
+        self.data = data
+        self.next = None    
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
   
     def push(self, new_data): 
+        new_node = Node(new_data) 
+        new_node.next = self.head 
+        self.head = new_node
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        slow_pointer = self.head
+        fast_pointer = self.head
+        if self.head is not None:
+            while fast_pointer is not None and fast_pointer.next is not None:
+                slow_pointer = slow_pointer.next
+                fast_pointer = fast_pointer.next.next
+            print("The middle element is: ", slow_pointer.data)
+        else:
+            print("The list is empty")
 
 # Driver code 
 list1 = LinkedList() 
 list1.push(5) 
 list1.push(4) 
-list1.push(2) 
+list1.push(88) 
 list1.push(3) 
 list1.push(1) 
 list1.printMiddle() 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,43 @@
 # Python program for implementation of MergeSort 
+# time complexity of O(n log n) in all cases
+# Space complexity is O(n)
+
 def mergeSort(arr):
-  
-  #write your code here
-  
+      if len(arr) > 1:
+        mid = len(arr) // 2   # Finding the mid of the array
+        left_half = arr[:mid]  # Divide the array into two halves
+        right_half = arr[mid:]
+
+        mergeSort(left_half)  # Recursively sort the first half
+        mergeSort(right_half)  # Recursively sort the second half
+
+        i = j = k = 0
+
+        # Copy data to temporary lists left_half[] and right_half[]
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Check if any element was left
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
   
 # driver code to test the above code 
 if __name__ == '__main__': 
