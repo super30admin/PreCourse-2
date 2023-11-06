0a01557e22f0e5f561bcfcda0ecd7cc593f4d365
Sanchit Pathak <sanchitpathak17@gmail.com>
2023-11-05 22:23:29 -0600

    PreCourse-2 completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,28 @@
-# Python code to implement iterative Binary  
-# Search. 
-  
-# It returns location of x in given array arr  
-# if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
-# Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
+# Python code to implement iterative Binary Search.
+# It returns location of x in given array arr, if present, else returns -1
+# Time Complexity is log O(log n). Space complexity is O(1).
+
+def binarySearch(arr, l, r, x):
+    while l <= r:
+        mid = (l + r) // 2              # Find middle of the array
+
+        if x > arr[mid]:                # Check if wanted value is greater than middle value
+            l = mid + 1                 # Set l to be greater than middle value to continue search to the right of it
+        elif x < arr[mid]:              # Check if wanted value is less than middle value
+            r = mid - 1                 # Set r to be less than middle value to continue search to the left of it
+        else:
+            return mid                  # Check if wanted value is equal to middle value
+    return -1
+
+
+# Test array
+arr = [2, 3, 4, 10, 40]
 x = 10
-  
-# Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
-if result != -1: 
-    print "Element is present at index % d" % result 
-else: 
-    print "Element is not present in array"
+
+# Function call
+result = binarySearch(arr, 0, len(arr) - 1, x)
+
+if result != -1:
+    print("Element is present at index %d" % result)
+else:
+    print("Element is not present in array")
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,55 @@
-# Python program for implementation of Quicksort
+# Python program for implementation of QuickSort
+
+# Time complexity for quick sort is O(n log n) and space complexity is O(n)
+
+# In partition function, we set pivot to right most value in array i.e. 3 in this case from [6, 2, 4, 1, 3]. Initially, left is 0 and right is 4 in this case. Then we loop over the array and if jth value is less than pivot value,
+# we swap the jth value with the ith value (previous value) in case of 2 < 3, arr[1] and arr[0] got swapped to get [2, 6, 4, 1, 3] and i gets incremented to 1. [1] at j[3] also gets later in the loop with with i[1] to get
+# [2, 1, 4, 6, 3]. Then we move the pivot to i[2] to get [2, 1, 3, 6, 4]. Then, we recursively sort right side array first to make it [2, 1, 3, 4, 6] and then left side to get the output.
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+def partition(arr, left, right):
+    pivot = arr[right]
+    i = left
+
+    for j in range(left, right):
+        if arr[j] < pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+
+    temp = arr[i]
+    arr[i] = arr[right]
+    arr[right] = temp
+    return i
 
 
-def quickSortIterative(arr, l, h):
-  #write your code here
+def quickSortIterative(arr, left, right):
+    if not arr:
+        return
 
+    while left < right:
+        pivot = partition(arr, left, right)
+        if pivot - left < right - pivot:
+            quickSortIterative(arr, left, pivot - 1)
+            left = pivot + 1
+        else:
+            quickSortIterative(arr, pivot + 1, right)
+            right = pivot - 1
+    return arr
+
+
+# Code to print the list
+def printList(arr):
+    if len(arr) > 1:
+        for i in range(len(arr)):
+            print(arr[i], end=" ")
+        print()
+
+
+# driver code to test the above code
+if __name__ == '__main__':
+    arr = [6, 2, 4, 1, 3]
+    print("Given array is", end="\n")
+    printList(arr)
+    quickSortIterative(arr, 0, len(arr) - 1)
+    print("Quick Sorted array is: ", end="\n")
+    printList(arr)
