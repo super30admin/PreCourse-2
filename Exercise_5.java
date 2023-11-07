df8b983d1ee64f50751d25cf0501b352e9e38144
manalisanjayraut <raut.ma@northeastern.edu>
2023-11-06 19:08:25 -0500

    adding precourse-2 changes

--- Exercise_5.java
+++ Exercise_5.java
@@ -2,6 +2,13 @@ class IterativeQuickSort {
     void swap(int arr[], int i, int j) 
     { 
 	//Try swapping without extra variable 
+     if (i == j) {
+        return; // No need to swap if i and j are the same
+    }
+       arr[i] = arr[i] + arr[j];
+       arr[j]= arr[i] - arr[j];
+       arr[i]=arr[i]-arr[j];
+
     } 
   
     /* This function is same in both iterative and 
@@ -9,12 +16,34 @@ class IterativeQuickSort {
     int partition(int arr[], int l, int h) 
     { 
         //Compare elements and swap.
+
+        int pivot = arr[h];
+        int i = l-1;
+
+        for(int j = l; j <= h-1; j++)
+        {
+            if(arr[j] <= pivot)
+            {
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr, i+1, h);
+        return i+1;
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+
+        if(l < h){
+
+            int p = partition(arr, l, h);
+
+            QuickSort(arr,l,p-1);
+            QuickSort(arr,p+1,h);
+        }
     } 
   
     // A utility function to print contents of arr 
