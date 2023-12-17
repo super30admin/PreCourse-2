a7158b16f34027c04b2ac7f0e64267fddb8b47ce
Monisha Sydam <112054217+Monisha3696@users.noreply.github.com>
2023-12-17 17:07:37 -0600

    Update Exercise_3.js

--- Exercise_3.js
+++ Exercise_3.js
@@ -2,7 +2,6 @@ class LinkedList {
     constructor() {
         this.head = null; // head of linked list
     }
-​
     /* Linked list node */
     static Node = class {
        constructor(d) {
@@ -11,21 +10,34 @@ class LinkedList {
        this.next = null;
        }
     }
-​
     /* Function to print middle of linked list */
     //Complete this function
-    function printMiddle() {
+     printMiddle() {
         //Write your code here
        	//Implement using Fast and slow pointers
+        let length = 0;
+        let temp = this.head;
+
+        // Calculate the length of the linked list
+        while (temp !== null) {
+            length++;
+            temp = temp.next;
+        }
+        // Traverse to the middle element
+        temp = this.head;
+        for (let i = 0; i < Math.floor(length / 2); i++) {
+            temp = temp.next;
+        }
+
+        console.log("Middle element: " + temp.data);
+
     }
-​
-    function push(new_data) {
-        let new_node = new this.Node(new_data);
+     push(new_data) {
+        let new_node = new LinkedList.Node(new_data);
         new_node.next = this.head;
         this.head = new_node;
     }
-​
-    function printList() {
+     printList() {
         let tnode = this.head;
         while (tnode != null) {
             console.log(tnode.data + "->");
@@ -34,10 +46,11 @@ class LinkedList {
         console.log("NULL");
     }
 }
-​
 let llist = new LinkedList();
 for (let i = 15; i > 0; --i) {
     llist.push(i);
-    llist.printList();
+   llist.printList();
     llist.printMiddle();
 }
+
+ 
