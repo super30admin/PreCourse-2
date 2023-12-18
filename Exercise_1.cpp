0d15aaf664829948da659173da365ade15dc02d2
Manjunath <manjunathshastryr@gmail.com>
2023-12-19 00:09:43 +0530

    Exercise completed

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -4,15 +4,32 @@
 // location of x in given array arr[l..r] is present, 
 // otherwise -1 
 int binarySearch(int arr[], int l, int r, int x) 
-{   
-    //Your Code here 
-} 
-  
-int main(void) 
-{ 
-    int arr[] = { 2, 3, 4, 10, 40 }; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
-    int x = 10; 
+{
+    while (l<=r)
+    {
+        int mid = l+ (r-l)/2;
+        if (arr[mid]==x)
+        {
+            return mid+1;
+        }
+        else if (arr[mid]<x)
+        {
+            return binarySearch(arr, mid+1, r, x);
+        }
+        else if (arr[mid]>x)
+        {
+            return binarySearch(arr, l, mid-1, x);
+        }
+    }
+
+    return -1;
+}
+
+int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 10;
     int result = binarySearch(arr, 0, n - 1, x); 
     (result == -1) ? printf("Element is not present in array") 
                    : printf("Element is present at index %d", 
