d1295d287bb2b61c87577ef3a3494d006bc6a3f3
SathwikReddyGK <sathwikreddy.gk@gmail.com>
2023-12-18 18:41:37 -0500

    Precourse2, Exercise1,2,3,4,5

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,50 @@
+# // Time Complexity : O(n/2) since I am keeping the length and we need to travel half the size of linked list
+# // Space Complexity : O(n) since we are creating nodes for each element
+# // Did this code successfully run on Leetcode : NA
+# // Any problem you faced while coding this : None
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
-    def __init__(self): 
-        
+    def __init__(self):
+        self.head = None
+        self.prev = None
+        self.length = 0
   
-    def push(self, new_data): 
-        
-  
+    def push(self, new_data):
+        newNode = Node(new_data)
+        if self.head == None:
+            self.head = newNode
+            self.prev = self.head
+            self.length += 1
+        else:
+            self.prev.next = newNode
+            self.prev = newNode
+            self.length += 1
+            
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        mid = (self.length-1)//2
+
+        if mid <= 0:
+            print(self.head.data)
+            return
+        
+        tempHead = self.head
+
+        while mid>0:
+            tempHead = tempHead.next
+            mid -= 1
+        
+        print(tempHead.data)
+
 
 # Driver code 
 list1 = LinkedList() 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,43 @@
+# // Time Complexity : O(nlogn)
+# // Space Complexity : O(2n) since we are creating arrays for each half
+# // Did this code successfully run on Leetcode : NA
+# // Any problem you faced while coding this : None
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
-  #write your code here
-  
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2                    # Divides the array into 2-halves until the length of array is less than 1
+    left_half = mergeSort(arr[:mid])
+    right_half = mergeSort(arr[mid:])
+
+    return merge(left_half, right_half)        # Then the Merge function is called to merge both arrays
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):                # Compares the lowest among both arrays and appends it
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+    return result
+
+
 # Code to print the list 
-def printList(arr): 
-    
-    #write your code here
-  
+def printList(arr):
+    print(arr)
+
 # driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end="\n")  
     printList(arr) 
-    mergeSort(arr) 
+    arr = mergeSort(arr) 
     print("Sorted array is: ", end="\n") 
     printList(arr) 
