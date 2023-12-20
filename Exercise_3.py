816e5fe71fac94b7ab39601f4eb144fbb779dad4
Vd1299 <vishvesh.dumbre@gmail.com>
2023-12-19 16:33:03 -0800

    Precourse 2 Completed

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,34 @@
 # Node class  
+#TC = O(n)
+#SC = O(1)
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
-        
+    def __init__(self, value):  
+        self.value = value
+        self.next = None
+
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
   
-    def push(self, new_data): 
-        
+    def push(self, new_value): 
+        newNode = Node(new_value)
+        newNode.next = self.head
+        self.head = newNode
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        slow, fast = self.head, self.head
+        if self.head is not None:
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            print("Middle Element:", slow.value)
+        else:
+            print("Empty List")
 
 # Driver code 
 list1 = LinkedList() 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,57 @@
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
+#TC = O(nlogn)
+#SC = O(logn)
+
+def partition(arr, low, high):
   #write your code here
+    i = low - 1
+    pivot = arr[high]
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    arr[i+1], arr[high] = arr[high], arr[i+1]
 
+    return i + 1
 
-def quickSortIterative(arr, l, h):
+def quickSortIterative(arr, low, high):
   #write your code here
+    stack = [0] * (high - low + 1)
+    top = -1
 
+    top += 1
+    stack[top] = low
+    top += 1
+    stack[top] = high
+
+    while top >= 0:
+        high = stack[top]
+        top -= 1
+        low = stack[top]
+        top -= 1
+
+        p = partition(arr, low, high)
+
+        if p - 1 > low:
+            top += 1
+            stack[top] = low
+            top += 1
+            stack[top] = p - 1
+
+        if p + 1 < high:
+            top += 1
+            stack[top] = p + 1
+            top += 1
+            stack[top] = high
+
+
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
+  
