f5eb2fc2ead7b921eff2eafe5be19e44031271db
God7wyn <jamesgodwynj@gmail.com>
2023-12-23 21:37:26 -0500

    Completed PreCourse 2

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,47 +1,94 @@
-#include <bits/stdc++.h> 
-using namespace std;  
-  
-// A utility function to swap two elements  
-void swap(int* a, int* b)  
-{  
-    //Your Code here 
-}  
-  
-/* This function takes last element as pivot, places  
-the pivot element at its correct position in sorted  
-array, and places all smaller (smaller than pivot)  
-to left of pivot and all greater elements to right  
+/*
+-------------------------------------
+* Recursive Quick Sort Implememtation
+-------------------------------------
+
+* Time Complexity:
+*        - Average Case: O(nlog(n))
+*        - Worst Case: O(n^2)
+
+* Space Complexity: O(n)
+
+Did this code successfully run on Leetcode : NA
+Any problem you faced while coding this : NO
+
+*/
+
+#include <iostream>
+
+// A utility function to swap two elements
+void swap(int* a, int* b)
+{
+    //Your Code here
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
 of pivot */
-int partition (int arr[], int low, int high)  
-{  
-    //Your Code here 
-}  
-  
-/* The main function that implements QuickSort  
-arr[] --> Array to be sorted,  
-low --> Starting index,  
+int partition (int arr[], int low, int high)
+{
+    // Always picking the first element as the pivot
+    int pivot_element = arr[low];
+    int start = low, end = high;
+
+    while (start < end) {
+        // Find left most element greater than or equal to the pivot element
+        while (start < end && arr[start] <= pivot_element)
+            start++;
+        // Find right most element less than or equal to the pivot element
+        while (arr[end] > pivot_element)
+            end--;
+
+        if (start < end)
+            swap(&arr[start], &arr[end]);
+    }
+    // Swap pivot element with the last element
+    swap(&arr[low], &arr[end]);
+    return end;
+}
+
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
 high --> Ending index */
-void quickSort(int arr[], int low, int high)  
-{  
-    //Your Code here 
-}  
-  
+void quickSort(int arr[], int low, int high)
+{
+    // Base case
+    if (low >= high)
+        return;
+
+    // Partition the array
+    int p = partition(arr, low, high);
+
+    // Sort the left side
+    quickSort(arr, low, p - 1);
+
+    // Sort the right side
+    quickSort(arr, p + 1, high);
+
+}
+
 /* Function to print an array */
-void printArray(int arr[], int size)  
-{  
-    int i;  
-    for (i = 0; i < size; i++)  
-        cout << arr[i] << " ";  
-    cout << endl;  
-}  
-  
-// Driver Code 
-int main()  
-{  
-    int arr[] = {10, 7, 8, 9, 1, 5};  
-    int n = sizeof(arr) / sizeof(arr[0]);  
-    quickSort(arr, 0, n - 1);  
-    cout << "Sorted array: \n";  
-    printArray(arr, n);  
-    return 0;  
-}  
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
+
+// Driver Code
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
+    std::cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,50 +1,73 @@
-#include<bits/stdc++.h>  
-using namespace std;  
-  
-// Struct  
-struct Node  
-{  
-    int data;  
-    struct Node* next;  
-};  
-  
+/*
+----------------------------------
+* Find mid of a singly linked list
+----------------------------------
+
+* Time Complexity: O(n)
+
+* Space Complexity: O(1)
+
+Did this code successfully run on Leetcode : NA
+Any problem you faced while coding this : NO
+
+*/
+
+#include <iostream>
+
+// Struct
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
 /* Function to get the middle of the linked list*/
-void printMiddle(struct Node *head)  
-{  
-  //YourCode here
-  //Use fast and slow pointer technique
-}  
-  
-// Function to add a new node  
-void push(struct Node** head_ref, int new_data)  
-{  
-    struct Node* new_node = new Node;  
-    new_node->data = new_data;  
-    new_node->next = (*head_ref);  
-    (*head_ref) = new_node;  
-}  
-  
-// A utility function to print a given linked list  
-void printList(struct Node *ptr)  
-{  
-    while (ptr != NULL)  
-    {  
-        printf("%d->", ptr->data);  
-        ptr = ptr->next;  
-    }  
-    printf("NULL\n");  
-}  
-  
-// Driver Code 
-int main()  
-{    
-    struct Node* head = NULL;    
-    for (int i=15; i>0; i--)  
-    {  
-        push(&head, i);  
-        printList(head);  
-        printMiddle(head);  
-    }  
-  
-    return 0;  
-}  
+void printMiddle(struct Node *head)
+{
+  if (head == nullptr) {
+    std::cout << "The list is empty." << std::endl;
+    return;
+  }
+  Node* fast = head;
+  Node* slow = head;
+
+  while (fast != nullptr && fast->next != nullptr) {
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+  std::cout << "Mid Point: " << slow->data << std::endl;
+}
+
+// Function to add a new node
+void push(struct Node** head_ref, int new_data)
+{
+    struct Node* new_node = new Node;
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+// A utility function to print a given linked list
+void printList(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d->", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+}
+
+// Driver Code
+int main()
+{
+    struct Node* head = NULL;
+    for (int i=15; i>0; i--)
+    {
+        push(&head, i);
+        printList(head);
+        printMiddle(head);
+    }
+
+    return 0;
+}
