e3e53dafcb21a27bf728d5bb0c6d838335619b1b
iarshtejay <arshtejay@gmail.com>
2024-01-22 10:12:27 -0400

    feat: Excercise 1 solution

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,29 @@
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+// The conditions change when you change the value of the right position.
+// It can be either r=nums.length or r=nums.length-1. The other if conditions 
+// in binary search have to be modified based on this initial condition.
+
+
+// Your code here along with comments explaining your approach
+
+
+
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+        int mid;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if(x==arr[mid]) return mid;
+            if(x<arr[mid])  r=mid-1;
+            else l=mid+1;
+        }
+        return -1;
     } 
   
     // Driver method to test above 
