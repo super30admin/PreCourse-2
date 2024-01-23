3f85fdf2aba2df169b445d507af90b2726ef4beb
Manideepa3 <saginatm1995@gmail.com>
2024-01-22 18:12:46 -0600

    completed pre-course-2

--- Exercise_3.java
+++ Exercise_3.java
@@ -20,6 +20,15 @@ class LinkedList
     { 
         //Write your code here
 	//Implement using Fast and slow pointers
+        Node slow = head;
+        Node fast = head;
+        if (head != null) {
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            System.out.println("Middle of the linked list: " + slow.data);
+        }
     } 
   
     public void push(int new_data) 
