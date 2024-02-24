5a321cb6330fd7044921a0bea8a00f423a9f698d
nishant170892 <160679621+nishant170892@users.noreply.github.com>
2024-02-23 23:54:25 -0500

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -2,7 +2,21 @@ class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+        l = 0;
+        r=arr.length-1;
+        while(l<r){
+            int mid = l+(r-l)/2;
+            if(arr[mid]==x){
+                return mid;
+            }
+            else if(x<arr[mid]){
+                r= mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return -1;
     } 
   
     // Driver method to test above 
