ac424e17c9aa1b8d0062373f7aa70f01468843a0
Sneha Sorde <snehassorde@gmail.com>
2024-02-25 12:18:51 -0600

    Done PreCourse2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,37 @@
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : Taking time for implementation
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data
+        self.next = None  
         
 class LinkedList: 
   
     def __init__(self): 
-        
-  
+        self.head = None
+
+
     def push(self, new_data): 
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        slow = fast = self.head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        print("The middle element : ", slow.data)
 
 # Driver code 
 list1 = LinkedList() 
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,55 @@
+# Time Complexity : O(nlogn)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : 
+# Any problem you faced while coding this : No
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
   
   #write your code here
+    if len(arr) > 1:
+        #mid point
+        mid = len(arr)//2
+
+        #divide the left array elements
+        l = arr[:mid]
+
+        #divide the right array elements
+        r= arr[mid:]
+
+        #sort first and second half
+        mergeSort(l)
+        mergeSort(r)
+
+        i=j=k=0
+        #merge two halves back together
+        while i < len(l) and j < len(r):
+            if l[i] <= r[j]:
+                arr[k] = l[i]
+                i+=1
+            else:
+                arr[k] = r[j]
+                j+=1
+            k+=1
+
+        #copying the remaining elements from the left half
+        while i < len(l):
+            arr[k] = l[i]
+            i+=1
+            k+=1
+        
+        #copying remaining elements from right half
+        while j < len(r):
+            arr[k] = r[j]
+            j+=1
+            k+=1
+            
   
 # Code to print the list 
 def printList(arr): 
-    
+     
     #write your code here
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
   
 # driver code to test the above code 
 if __name__ == '__main__': 
@@ -14,5 +57,6 @@ if __name__ == '__main__':
     print ("Given array is", end="\n")  
     printList(arr) 
     mergeSort(arr) 
+    print("\n")
     print("Sorted array is: ", end="\n") 
     printList(arr) 
