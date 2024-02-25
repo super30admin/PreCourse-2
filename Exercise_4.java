efa79acaccff0410f679864fef5df48b79409fcd
nishant170892 <160679621+nishant170892@users.noreply.github.com>
2024-02-25 00:59:53 -0500

    Update Exercise_4.java

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,9 @@
+// Time Complexity : O(NlogN)
+// Space Complexity : O(N)+O(logN)
+// Did this code successfully run on Leetcode : N/A
+// Any problem you faced while coding this : No
+
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -6,6 +12,34 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+
+	int[] sortedArray = new int[r-l+1];
+	int currIndex = 0;
+	int i=l;
+	int j = m+1;
+
+	    while(i<=m && j<=r){
+		    if(arr[i]<arr[j]){
+			   sortedArray[currIndex]=arr[i];
+			    i++
+		    }
+		    else{
+			    sortedArray[currIndex] = arr[j];
+			    j++;
+		    }
+	    }
+
+	      while(i<=m){  
+		      sortedArray[currInd++]=arr[i++];
+	      }
+        while(j<=r){
+		sortedArray[currInd++]=arr[j++];
+	}
+
+	     for(currIndex=0;currIndex<sortedArr.length;currIndex++) {
+		     arr[l+currIndex]=sortedArray[currIndex];
+	     }
+	    
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +48,15 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+
+	    if(l==r) return;
+
+	    int mid = l+(r-l)/2;
+	    sort(arr,l,mid);
+	    sort(arr,mid+1,right);
+
+	    merge(arr,l,mid,r);
+	    
     } 
   
     /* A utility function to print array of size n */
