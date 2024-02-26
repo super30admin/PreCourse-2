ac6c57a72fc1cad0b90515f154da598852d681a8
Raghavendra Sagar <srilekhapulaparthi@Srilekhas-MBP.attlocal.net>
2024-02-25 20:32:16 -0600

    Completed PreCourse 2

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,17 +1,66 @@
-class MergeSort 
+// Time Complexity : O(nlogn) since we're dividing the array by half for each operation
+// Space Complexity : O(n)
+// Any problem you faced while coding this : No
+class MergeSort
 { 
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
-    {  
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int n1_arr[] = new int[n1];
+        int n2_arr[] = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            n1_arr[i] = arr[l + i];
+        }
+
+        for (int i = 0; i < n2; i++) {
+            n2_arr[i] = arr[m+1+i];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = l;
+
+        while (i < n1 && j < n2) {
+            if (n1_arr[i] <= n2_arr[j]) {
+                arr[k] = n1_arr[i];
+                i++;
+            } else {
+                arr[k] = n2_arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = n1_arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = n2_arr[j];
+            j++;
+            k++;
+        }
+
        //Your code here  
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
-    { 
+    {
+        if (l < r) {
+            int m = l + (r-l)/2;
+            sort(arr, l, m);
+            sort(arr, m+1, r);
+            merge(arr, l, m, r);
+        }
 	//Write your code here
         //Call mergeSort from here 
     } 
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,73 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
+// Time Complexity : O(nlogn) since we're dividing the array by half for each operation
+// Space Complexity : O(logn)
+// Any problem you faced while coding this : No
+
+import java.util.Stack;
+
+class IterativeQuickSort {
+    void swap(int arr[], int i, int j) {
+        if (i != j) {
+            arr[i] = arr[i] + arr[j];
+            arr[j] = arr[i] - arr[j];
+            arr[i] = arr[i] - arr[j];
+        }
+        //Try swapping without extra variable
+    }
+
     /* This function is same in both iterative and 
        recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
-        //Compare elements and swap.
-    } 
-  
+    int partition(int arr[], int l, int h) {
+        int pivot = arr[h];
+        int i = l - 1;
+
+        for (int j = l; j < h; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, h);
+        return i + 1;
+    }
+
     // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
+    void QuickSort(int arr[], int l, int h) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(l);
+        stack.push(h);
+
+        while (!stack.isEmpty()) {
+            h = stack.pop();
+            l = stack.pop();
+            int pivot = partition(arr, l, h);
+            if (pivot - 1 > l) {
+                stack.push(l);
+                stack.push(pivot - 1);
+            }
+
+            if (pivot + 1 < h) {
+                stack.push(pivot + 1);
+                stack.push(h);
+            }
+        }
+
+
         //Try using Stack Data Structure to remove recursion.
-    } 
-  
+    }
+
     // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
+    void printArr(int arr[], int n) {
+        int i;
+        for (i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
     // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
+    public static void main(String args[]) {
+        IterativeQuickSort ob = new IterativeQuickSort();
+        int arr[] = {4, 3, 5, 2, 1, 3, 2, 3};
+        ob.QuickSort(arr, 0, arr.length - 1);
+        ob.printArr(arr, arr.length);
+    }
 } 
