b1390e497730a7a98a30618b7074e2269d259084
vaishnavimore23 <vaishnavimore23@outlook.com>
2024-02-26 02:13:29 -0800

    Precourse 2 Done

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,51 @@
-# Node class  
-class Node:  
-  
-    # Function to initialise the node object  
-    def __init__(self, data):  
-        
-class LinkedList: 
-  
-    def __init__(self): 
-        
-  
-    def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
-    def printMiddle(self): 
+# Node class
 
-# Driver code 
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+
+# Time Complexity = O(n) Space Complexity = O(1)
+class Node:
+
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = Node(None)
+        self.length = 0
+
+    def push(self, new_data):
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        new_node = Node(new_data)
+        temp.next = new_node
+        self.length += 1
+
+    # Function to get the middle of
+    # the linked list
+    def printMiddle(self):
+        mid = self.length // 2
+        print("Mid index of LinkedList is ", mid)
+        temp = self.head
+        for i in range(mid + 1):
+            temp = temp.next
+        print("Mid of LinkedList is ", temp.data)
+
+
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2)
+list1.push(3)
+list1.push(1)
+
+temp = list1.head
+while temp:
+    print(temp.data)
+    temp = temp.next
+print(list1.length)
+list1.printMiddle()
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,63 @@
-# Python program for implementation of MergeSort 
-def mergeSort(arr):
-  
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
-    #write your code here
-  
-# driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+# Time Complexity = O(nLogn)  Space Complexity = O(n)
+
+
+# Python program for implementation of MergeSort
+def mergeSort(arr, l, r):
+    if l < r:
+        mid = (l + r) // 2  # find the mid of array
+        mergeSort(arr, l, mid)  # pass the left part of array to mergesort
+        mergeSort(arr, mid + 1, r)  # pass the right part of array to mergesort
+        merge(arr, l, mid, r)  # Merge all divided array
+
+
+def merge(arr, l, mid, r):
+    n1 = mid - l + 1  # Initialize the size of Left array
+    n2 = r - mid  # Initialize the size of Right array
+    lArr = []
+    rArr = []
+    for i in range(0, n1):  # Construct left array which is the left hand side of mid
+        lArr.append(arr[l + i])
+    for i in range(0, n2):
+        rArr.append(
+            arr[mid + 1 + i]
+        )  # Construct right array which is the right hand side of mid
+
+    # Initialize pointers to traverse from left , right, and main array correspondingly
+    # here k starts from l location as we have to store element in the main array from start location which is  l for each case
+
+    i, j, k = (0, 0, l)
+    # compare element from left array and right array and store it in the main array accordingly
+    while (i < n1) and (j < n2):
+        if lArr[i] <= rArr[j]:
+            arr[k] = lArr[i]
+            i += 1
+            k += 1
+        else:
+            arr[k] = rArr[j]
+            j += 1
+            k += 1
+
+    # Store remaining elements after comparison in the main array
+    while i < n1:
+        arr[k] = lArr[i]
+        i += 1
+        k += 1
+    while j < n2:
+        arr[k] = rArr[j]
+        j += 1
+        k += 1
+
+
+# Code to print the list
+def printList(arr):
+    print(arr)
+
+
+# driver code to test the above code
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    printList(arr)
+    mergeSort(arr, 0, len(arr) - 1)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
