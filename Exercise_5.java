76d049f74899a991f3f4eba396d9ac00d6f22bfc
Mohan <mohanmanjunatha26@gmail.com>
2024-02-27 18:35:22 -0500

    almost

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,36 +1,72 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
-    { 
-	//Try swapping without extra variable 
-    } 
-  
-    /* This function is same in both iterative and 
-       recursive*/
-    int partition(int arr[], int l, int h) 
-    { 
-        //Compare elements and swap.
-    } 
-  
-    // Sorts arr[l..h] using iterative QuickSort 
-    void QuickSort(int arr[], int l, int h) 
-    { 
-        //Try using Stack Data Structure to remove recursion.
-    } 
-  
-    // A utility function to print contents of arr 
-    void printArr(int arr[], int n) 
-    { 
-        int i; 
-        for (i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
-    // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        IterativeQuickSort ob = new IterativeQuickSort(); 
-        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-        ob.QuickSort(arr, 0, arr.length - 1); 
-        ob.printArr(arr, arr.length); 
-    } 
-} 
+import java.util.Stack;
+class Excercise_5{
+    // Function to swap two elements in the array without using an extra variable
+    void swap(int arr[], int i, int j) {
+        if (i != j) {
+            arr[i] = arr[i] + arr[j];
+            arr[j] = arr[i] - arr[j];
+            arr[i] = arr[i] - arr[j];
+        }
+    }
+
+    // Function to partition the array and return the pivot index
+    int partition(int arr[], int l, int h) {
+        int pivot = arr[h];
+        int i = (l - 1);
+
+        for (int j = l; j < h; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, h);
+        return i + 1;
+    }
+
+    // Function to sort the array using QuickSort iteratively
+    void QuickSort(int arr[], int l, int h) {
+        // Create a stack to store subarray boundaries
+        Stack<Integer> stack = new Stack<>();
+        // Push initial values of l and h to the stack
+        stack.push(l);
+        stack.push(h);
+
+        // Keep popping from stack while it's not empty
+        while (!stack.isEmpty()) {
+            // Pop h and l from stack
+            h = stack.pop();
+            l = stack.pop();
+
+            // Set pivot element at its correct position
+            int p = partition(arr, l, h);
+
+            // If there are elements on the left side of the pivot, push them to the stack
+            if (p - 1 > l) {
+                stack.push(l);
+                stack.push(p - 1);
+            }
+
+            // If there are elements on the right side of the pivot, push them to the stack
+            if (p + 1 < h) {
+                stack.push(p + 1);
+                stack.push(h);
+            }
+        }
+    }
+
+    // Function to print the array
+    void printArr(int arr[], int n) {
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
+    // Driver code to test the sorting
+    public static void main(String args[]) {
+        Excercise_5 ob = new Excercise_5();
+        int arr[] = {4, 3, 5, 2, 1, 3, 2, 3};
+        int n = arr.length;
+        ob.QuickSort(arr, 0, n - 1);
+        ob.printArr(arr, n);
+    }
+}
