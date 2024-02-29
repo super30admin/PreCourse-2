97242f729d27d9380e9fc715f10d255286064d5c
sapna-khatri <sapnafbkhatri@gmail.com>
2024-02-29 14:07:39 -0500

    Done precourse2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,35 @@
 # Node class  
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Could not find on leetcode
+# Any problem you faced while coding this : No problem
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data # Assign data
+        self.next = None # Initialize next to None
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head = None
         
-  
     def push(self, new_data): 
-        
+        newNode = Node(new_data)
+        newNode.next = self.head
+        self.head = newNode
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        slow = self.head
+        fast = self.head
+
+        while(fast is not None and fast.next is not None):
+            fast = fast.next.next
+            slow = slow.next
+        print("Middle element:", slow.data)
 
 # Driver code 
 list1 = LinkedList() 
@@ -23,4 +38,4 @@ list1.push(4)
 list1.push(2) 
 list1.push(3) 
 list1.push(1) 
-list1.printMiddle() 
+list1.printMiddle()
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,12 +1,46 @@
 # Python program for implementation of MergeSort 
+# Time Complexity : O(n logn)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Could not find on leetcode
+# Any problem you faced while coding this : No problem
 def mergeSort(arr):
   
-  #write your code here
+  if len(arr) > 1:
+        mid = len(arr) // 2  # Finding the mid of the array
+        L = arr[:mid]  # Dividing the array elements into 2 halves
+        R = arr[mid:]
+
+        mergeSort(L)  # Sorting the first half
+        mergeSort(R)  # Sorting the second half
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
   
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
   
 # driver code to test the above code 
 if __name__ == '__main__': 
